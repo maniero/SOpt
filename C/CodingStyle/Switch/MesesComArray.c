8d5aee7b2ab1c83759faa890c62f068218d2f1bd
Antonio Maniero <maniero@klip.net>
2017-01-12 07:45:39 -0300

    http://pt.stackoverflow.com/a/176676/101

---
+++ C/CodingStyle/Switch/MesesComArray.c
@@ -0,0 +1,6 @@
+char *mes(int i) {
+  char *meses[12] = { "janeiro", "fevereiro", "marco", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
+  return meses[i];
+}
+
+//http://pt.stackoverflow.com/a/176676/101
