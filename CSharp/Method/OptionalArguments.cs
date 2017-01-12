b07f47b770079a1219f3979800dba9799693e2c3
Antonio Maniero <maniero@klip.net>
2017-01-12 07:29:59 -0300

    http://pt.stackoverflow.com/a/176386/101

---
+++ CSharp/Method/OptionalArguments.cs
@@ -0,0 +1,8 @@
+using static System.Console;
+					
+public class Program {
+	public static void Main() => WriteLine(teste(null));
+	public static string teste(string x = "xxx") => x;
+}
+
+//http://pt.stackoverflow.com/a/176386/101
