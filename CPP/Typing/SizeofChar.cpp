dba0d2ce34c2f6fcd78c64b47761c67bf62a55b3
Antonio Maniero <maniero@klip.net>
2017-01-19 07:18:23 -0300

    http://pt.stackoverflow.com/q/178443/101

---
+++ CPP/Typing/SizeofChar.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a = 'a';
+    cout << sizeof(char) << "\n";
+    cout << sizeof(a) << "\n";
+    cout << sizeof('a')  << "\n";
+}
+
+//http://pt.stackoverflow.com/q/178443/101
