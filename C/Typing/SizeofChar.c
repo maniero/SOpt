315435c088b4956d898be374099c9734be81d8b9
Antonio Maniero <maniero@klip.net>
2017-01-19 07:18:17 -0300

    http://pt.stackoverflow.com/q/178443/101

---
+++ C/Typing/SizeofChar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char a = 'a';
+    printf("%d\n", sizeof(char));
+    printf("%d\n", sizeof(a));
+    printf("%d\n", sizeof('a'));
+}
+
+//http://pt.stackoverflow.com/q/178443/101
