5bc197b8b41e1cdd6b8adb14f0e4c01f881ba701
Antonio Maniero <maniero@klip.net>
2017-01-19 12:11:29 -0300

    http://pt.stackoverflow.com/q/178522/101

---
+++ C/Array/ArrayLength.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char *palavra[] = {"casa", "carro"};
+    printf("%d", );
+    for(int i = 0; i < sizeof(palavra) / sizeof(palavra[0]); i++) {
+        printf(palavra[i]);
+    }
+}
+
+//http://pt.stackoverflow.com/q/178522/101
