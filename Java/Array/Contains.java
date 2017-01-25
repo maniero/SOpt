3465a147ff17e571d747341c7e4787e17dd64e35
Antonio Maniero <maniero@klip.net>
2017-01-25 10:29:27 -0300

    http://pt.stackoverflow.com/q/179804/101

---
+++ Java/Array/Contains.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Ideone {
+	public static void main (String[] args) {
+		ArrayList<String> strListas = new ArrayList<>();
+		strListas.add("Paulo");
+		strListas.add("Adriano");
+		strListas.add("Paula");
+		System.out.println(strListas.contains("Pa"));
+	}
+}
+
+//http://pt.stackoverflow.com/q/179804/101
