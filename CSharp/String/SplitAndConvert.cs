4aff50ec887b11d5c0ddcf979f665f4c7efde332
Antonio Maniero <maniero@klip.net>
2017-01-30 21:11:05 -0300

    http://pt.stackoverflow.com/q/180975/101

--- CSharp/String/SplitAndConvert.cs
+++ CSharp/String/SplitAndConvert.cs
@@ -4,9 +4,14 @@ using System.Linq;
 					
 public class Program {
 	public static void Main() {
-		var lista = new List<string> { "12/10", "01/02", "123/12", "4/5" };
-		var listaClassificada = lista.OrderBy(x => Convert.ToInt32((x.Split('/', '-'))[0]));
+		var lista = new List<string> { "12/10", "01/02", "123/12", "A/1", "4/5" };
+		var listaClassificada = lista.OrderBy(x => ConversaoParcial(x));
 		listaClassificada.ToList().ForEach(Console.WriteLine);
+	}
+	public static string ConversaoParcial(string texto) {
+		int valor;
+		string textoParcial = texto.Split('/', '-')[0];
+		return int.TryParse(textoParcial, out valor) ? textoParcial.PadLeft(4, '0') : textoParcial.PadLeft(4, 'A');
 	}
 }
 
