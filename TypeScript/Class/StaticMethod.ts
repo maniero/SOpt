b28f4fd55545a497b194e99931eae33205a9c040
Antonio Maniero <maniero@klip.net>
2017-02-08 16:38:11 -0300

    http://pt.stackoverflow.com/q/182951/101

---
+++ TypeScript/Class/StaticMethod.ts
@@ -0,0 +1,17 @@
+export class Utils {
+    constructor(public toastCtrl: ToastController) {}  
+
+    static showToast(obejct, message, pos) {
+        let toast = object.toastCtrl.create({
+            message: message,
+            duration: 3000,
+            position: pos
+        });
+      toast.onDidDismiss(() => {
+          console.log('Dismissed toast');
+      });
+      toast.present();
+    }
+}
+
+//http://pt.stackoverflow.com/q/182951/101
