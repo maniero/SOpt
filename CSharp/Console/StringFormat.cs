f796faf1e8e689db17c790c89f5c39fb0c6e5749
Antonio Maniero <maniero@klip.net>
2017-02-10 07:00:38 -0300

    http://pt.stackoverflow.com/q/183232/101

---
+++ CSharp/Console/StringFormat.cs
@@ -0,0 +1,19 @@
+using static System.Console;
+					
+public class Program {
+	public static void Main() {
+        var A1 = ReadLine();
+        var A2 = ReadLine();
+        var A3 = ReadLine();
+        var A4 = ReadLine();
+        var A5 = ReadLine();
+        var A6 = ReadLine();
+        var A7 = ReadLine();
+        var A8 = ReadLine();
+        Write($"Segunda: {A1}, {A2}, {A3}, {A4}", A1, A2, A3, A4);
+        ReadLine();
+        Write("Terça: {A5}, {A8}, {A6}, {A2}, {A7}", A5, A8, A6, A2, A7);
+	}
+}
+
+//http://pt.stackoverflow.com/q/183232/101
