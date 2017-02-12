1fb0dabbc1603c940128e73e485f55a26877b054
Antonio Maniero <maniero@klip.net>
2017-02-12 10:40:12 -0300

    http://pt.stackoverflow.com/q/183640/101

---
+++ C/Array/MultiDimensional.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main () {
+	int DIM_X = 1000;
+	int DIM_Y = 1000;
+	int vectorMD[DIM_X][DIM_Y];
+	for (int x = 0; x < DIM_X; x++) {
+	    for (int y = 0; y < DIM_Y; y++) {
+	        vectorMD[x][y] = x;
+	    }
+	}
+}
+
+//http://pt.stackoverflow.com/q/183640/101
