9986397c4c07309ec53915775e21bf96c44f9c9b
Antonio Maniero <maniero@klip.net>
2017-02-13 13:35:55 -0300

    http://pt.stackoverflow.com/q/183808/101

---
+++ Java/Collections/Shuffle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Collections;
+
+class HelloWorld {
+	public static void main (String[] args) {
+		 ArrayList<Integer> lista = new ArrayList<>();
+		 for (int i = 0; i < 50; i++) {
+		 	lista.add(i);
+		 }
+		 seleciona(lista);
+		 for (int i = 0; i < 50; i++) {
+		 	System.out.println(lista.get(i));
+		 }
+	}
+	public static void seleciona(ArrayList<Integer> arraylist) {
+        Collections.shuffle(arraylist);
+    }
+}
+
+//http://pt.stackoverflow.com/q/183808/101
