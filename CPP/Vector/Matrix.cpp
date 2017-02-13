b2d44b1b2f250d530a4a89ebebd041fb389e97fd
Antonio Maniero <maniero@klip.net>
2017-02-13 09:42:16 -0300

    http://pt.stackoverflow.com/q/183755/101

---
+++ CPP/Vector/Matrix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int n, m;
+    cin >> n >> m;
+    n = max(n, m);
+    vector<vector<int>> cedula(n, std::vector<int>(n));
+}
+
+//http://pt.stackoverflow.com/q/183755/101
