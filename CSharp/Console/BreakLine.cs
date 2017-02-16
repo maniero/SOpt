b59271a045d25f65ca3744b63313436223944654
Antonio Maniero <maniero@klip.net>
2017-02-16 08:49:46 -0300

    http://pt.stackoverflow.com/q/184489/101

---
+++ CSharp/Console/BreakLine.cs
@@ -0,0 +1,28 @@
+using static System.Console;
+
+public class Program {
+    public static void Main() {
+        var A1 = ReadLine();
+        var A2 = ReadLine();
+        var A3 = ReadLine();
+        var A4 = ReadLine();
+        var A5 = ReadLine();
+        var A6 = ReadLine();
+        var A7 = ReadLine();
+        var A8 = ReadLine();
+        WriteLine($"Segunda: {A1}, {A8}, {A4}, {3},");
+        WriteLine($"Terça: {A2}, {A7}, {A3}, {A6},");
+
+        Write($"Segunda: {A1}, {A8}, {A4}, {3},");
+        WriteLine();
+        Write($"Terça: {A2}, {A7}, {A3}, {A6},");
+
+        Write($"Segunda: {A1}, {A8}, {A4}, {3},\nTerça: {A2}, {A7}, {A3}, {A6}\n");
+
+        Write($@"Segunda: {A1}, {A8}, {A4}, {3},
+    Terça: {A2}, {A7}, {A3}, {A6}\n");
+
+    }
+}
+
+//http://pt.stackoverflow.com/q/184489/101
