305fa06e5a31715a05199b1a9b0f9fe69c733ed8
Antonio Maniero <maniero@klip.net>
2017-02-16 16:44:15 -0300

    http://pt.stackoverflow.com/q/184690/101

---
+++ CSharp/Math/AtanDegreeToRadin.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+using static System.Math;
+					
+public class Program {
+	public static void Main() {
+		WriteLine(Atan(5) * 180 / PI);
+	}
+}
+
+//http://pt.stackoverflow.com/q/184690/101
