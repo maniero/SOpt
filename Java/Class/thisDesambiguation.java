ef78dc6ee043f356a3d9efd9fe7ce0efd0ef6c95
Antonio Maniero <maniero@klip.net>
2017-02-20 21:55:41 -0300

    http://pt.stackoverflow.com/q/185542/101

---
+++ Java/Class/thisDesambiguation.java
@@ -0,0 +1,15 @@
+class B {
+    private int b;
+    public int getB() { return b; }
+    public void setB(int b) { this.b = b; }
+}
+
+class A {
+    public static void main (String[] args) {
+        B b = new B();
+        b.setB(5);
+        System.out.println(b.getB());
+    }
+}
+
+/http://pt.stackoverflow.com/q/185542/101
