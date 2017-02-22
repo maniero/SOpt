648580ba7558745bfb0d270f54b14c14a442bc7c
Antonio Maniero <maniero@klip.net>
2017-02-22 19:18:13 -0300

    http://pt.stackoverflow.com/q/186098/101

---
+++ Python/ConvertStringToFloat.py
@@ -0,0 +1,12 @@
+nota1 = float(input("Digite a nota 1: "))
+nota12 = float(input("Digite a nota 1: "))
+
+nota2 = float(input("Digite a nota 2: "))
+nota22 = float(input("Digite a nota 2: "))
+media1 = ((nota1 + nota12) / 2)
+media2 = ((nota2 + nota22) / 2)
+
+print(media1)
+print(media2)
+
+#http://pt.stackoverflow.com/q/186098/101
