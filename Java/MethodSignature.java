2805ecfb4e6b921ca601148c6da9251515681959
Antonio Maniero <maniero@klip.net>
2017-02-22 23:14:55 -0300

    http://pt.stackoverflow.com/q/186132/101

---
+++ Java/MethodSignature.java
@@ -0,0 +1,20 @@
+public class Nome {
+    private String nome;
+    //e todo os resto aqui, construtor, métodos acessores, operações, validações, etc.
+}
+public class Endereco {
+    private String endereco;
+    //e todo os resto aqui, construtor, métodos acessores, operações, validações, etc.
+}
+public class Telefone {
+    private String telefone;
+    //e todo os resto aqui, construtor, métodos acessores, operações, validações, etc.
+}
+
+public Pessoa(Nome nome, Endereco endereco, Telefone telefone) {
+    this.nome = nome;
+    this.endereco = endereco;
+    this.telefone = telefone;
+}
+
+//http://pt.stackoverflow.com/q/186132/101
