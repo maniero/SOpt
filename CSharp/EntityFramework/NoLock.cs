546ec3f3106246df449f37ceec59578131cc25fb
Antonio Maniero <maniero@klip.net>
2017-02-24 08:56:51 -0300

    http://pt.stackoverflow.com/q/185994/101

---
+++ CSharp/EntityFramework/NoLock.cs
@@ -0,0 +1,10 @@
+ProductsNewViewData viewData = new ProductsNewViewData();
+using (var t = new TransactionScope(TransactionScopeOption.Required,
+    new TransactionOptions { 
+        IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted 
+    })) {
+   viewData.Suppliers = northwind.Suppliers.ToList();
+   viewData.Categories = northwind.Categories.ToList();
+ }
+ 
+ //http://pt.stackoverflow.com/q/185994/101
