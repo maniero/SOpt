4922443b4301685e5a5888dbe3ddcefef46463aa
Antonio Maniero <maniero@klip.net>
2017-02-24 08:27:24 -0300

    http://pt.stackoverflow.com/q/186421/101

---
+++ C/Algorithm/VariableWrong.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int nombre, carre ;
+    printf("Introduza un numero");
+    scanf("%d", &nombre);
+    carre = nombre * nombre;
+    printf("A sua raiz quadrada é: %d", carre);
+}
+
+//http://pt.stackoverflow.com/q/186421/101
