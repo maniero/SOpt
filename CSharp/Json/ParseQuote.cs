c44bf3275722ad41cd9e73a4595478b50804953b
Antonio Maniero <maniero@klip.net>
2017-03-01 12:56:09 -0300

    http://pt.stackoverflow.com/q/187181/101

---
+++ CSharp/Json/ParseQuote.cs
@@ -0,0 +1,13 @@
+using System;
+using Newtonsoft.Json.Linq;
+					
+public class Program {
+	public static void Main() {
+		var text = "{\"SomeResponse\":{\"FIrstAttribute\":8,\"SecondAttribute\":\"On\",\"ThirdAttribute\":{\"Id\":2,\"FirstName\":\"Okkie\",\"Name\":\"Bokkie\",\"Street\":\"\",\"StreetNumber\":null,\"PostCode\":\"\",\"City\":\"\",\"Country\":\"}}}";
+		var token = JToken.Parse(text);
+        var json = JObject.Parse((string) token);
+        Console.WriteLine(json);
+	}
+}
+
+//http://pt.stackoverflow.com/q/187181/101
