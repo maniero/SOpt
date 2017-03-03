82ce1753935f00e7b01fd2277866a59ed79ef86c
Antonio Maniero <maniero@klip.net>
2017-03-03 00:11:55 -0300

    http://pt.stackoverflow.com/q/187587/101

---
+++ Python/Loop.py
@@ -0,0 +1,8 @@
+while True:
+    cpf = input('Digite os nove primeiros dígitos do CPF: ')
+    print(len(cpf))
+    if len(cpf) == 9:
+        break
+    #mais código aqui
+
+#http://pt.stackoverflow.com/q/187587/101
