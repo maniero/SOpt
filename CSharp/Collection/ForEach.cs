007c72ff96ff1e8c2e5d9954758881db929bf142
Antonio Maniero <maniero@klip.net>
2017-03-07 07:24:45 -0300

    http://pt.stackoverflow.com/q/188067/101

---
+++ CSharp/Collection/ForEach.cs
@@ -0,0 +1,16 @@
+using static System.Console;
+using System.Collections.Generic;
+public class C {
+    public static void Main() {
+        int[] array = new int[]{1, 2, 3, 4, 5, 6};
+        foreach (int item in array) {
+            WriteLine(item);
+        }
+        var lista = new List<int>{1, 2, 3, 4, 5, 6};
+        foreach (int item in lista) {
+            WriteLine(item);
+        }
+    }
+}
+
+//http://pt.stackoverflow.com/q/188067/101
