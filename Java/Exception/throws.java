0c02a854f8c7d6905d972ba122984897a687d065
Antonio Maniero <maniero@klip.net>
2017-03-10 07:43:26 -0300

    http://pt.stackoverflow.com/q/17025/101

--- Java/Exception/throws.java
+++ Java/Exception/throws.java
@@ -1,14 +1,13 @@
 import java.util.*;
     
 public class Estudos{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String palavra = "Java";    
         Scanner in = new Scanner(System.in);
         System.out.print("Informe um inteiro: ");
         int indice = in.nextInt();
         try {
-            System.out.println("O caractere no índice " + 
-                "informado é " + palavra.charAt(indice));
+            System.out.println("O caractere no índice informado é " + palavra.charAt(indice));
         }
         catch(StringIndexOutOfBoundsException e) {
             System.out.println("Erro:" + e.getMessage());
