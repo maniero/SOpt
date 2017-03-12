e90eaf3490c8b11f1990f8470d224ce562daca99
Antonio Maniero <maniero@klip.net>
2017-03-12 14:05:22 -0300

    http://pt.stackoverflow.com/q/189478/101

---
+++ Java/Math/DoubleDivision.java
@@ -0,0 +1,8 @@
+class Ideone {
+	public static void main (String[] args) {
+		double teste = 673.0 / 5455 * 100;
+        System.out.println(teste);  
+	}
+}
+
+//http://pt.stackoverflow.com/q/189478/101
