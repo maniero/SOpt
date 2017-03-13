d011a827f659542e4b8fe90078d4c40334210d58
Antonio Maniero <maniero@klip.net>
2017-03-13 10:36:02 -0300

    http://pt.stackoverflow.com/q/189633/101

---
+++ C/Function/IgnoringReturn.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    char *texto = malloc(10UL);
+    if (texto == NULL) {
+        printf("Houve um erro de alocação de memória");
+        exit(EXIT_FAILURE);
+    }
+    int x;
+    if (scanf("%d", x) != 1) {
+        printf("Houve um erro de leitura do dado");
+        free(texto);
+        exit(EXIT_FAILURE);
+    }
+    printf("%d", texto);
+    free(texto);
+    return EXIT_SUCCESS;
+}
+
+//http://pt.stackoverflow.com/q/189633/101
