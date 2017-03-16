803ae5bc573a5bd8bdbf3d558414dbe1acb46d2d
Antonio Maniero <maniero@klip.net>
2017-03-16 14:05:05 -0300

    http://pt.stackoverflow.com/q/190575/101

---
+++ CPP/Typing/SizedInt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int getBit8(int *pInt32, int index) {
+    return (*pInt32 >> (index * 8)) & (0xff >> sizeof(int));
+}
+
+int getBit8x(int32_t *pInt32, int index) {
+    return (*pInt32 >> (index * 8)) & (0xff >> sizeof(int));
+}
+
+int main() {
+	int a[] = { 0, 1, 2, 3 };
+	getBit8(a, 2);
+	getBit8x(a, 1);
+	printf("%d %d %d %d", a[0], a[1], a[2], a[3]);
+}
+
+//http://pt.stackoverflow.com/q/190575/101
