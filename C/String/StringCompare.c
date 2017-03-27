82cb7cd00268b91bb557c74b15429c47041714b4
Antonio Maniero <maniero@klip.net>
2017-03-27 19:34:18 -0300

    http://pt.stackoverflow.com/q/193124/101

---
+++ C/String/StringCompare.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char lugar[20]; //cria o array de caracteres para armazenar o texto
+    printf("Digite o nome de um lugar: ");
+    scanf("%20s", lugar); //não precisa da referência porque o array já é uma, precisa %s
+    if (strcmp(lugar, "Escola") == 0) { //use a função para comparar todos os caracteres
+        printf("\nEstudar");
+    }
+}
+
+//http://pt.stackoverflow.com/q/193124/101
