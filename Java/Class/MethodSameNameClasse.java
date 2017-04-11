8b621b0001a668af0184eef87276843869308575
Antonio Maniero <maniero@klip.net>
2017-04-11 18:41:20 -0300

    http://pt.stackoverflow.com/q/197666/101

---
+++ Java/Class/MethodSameNameClasse.java
@@ -0,0 +1,19 @@
+class HelloWorld {
+ 	public static void main(String[] args) { 
+ 		Teste1 teste = new Teste1();
+ 		System.out.println("Criou a classe");
+ 		teste.Teste1();
+ 	}
+}
+
+class Teste1 {
+    public void Teste1() {
+        System.out.println("Olá");
+    }
+
+    public void Teste2() {
+        System.out.println("Oi");
+    }
+}
+
+//http://pt.stackoverflow.com/q/197666/101
