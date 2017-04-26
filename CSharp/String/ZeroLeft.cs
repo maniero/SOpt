ef7bb1580747afae55874f850cc68e47484d55c3
Antonio Maniero <maniero@klip.net>
2017-04-26 18:44:10 -0300

    https://pt.stackoverflow.com/q/200867/101

---
+++ CSharp/String/ZeroLeft.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		var teste = "1";
+		WriteLine(teste.PadLeft(6, '0'));
+	}
+}
+
+//https://pt.stackoverflow.com/q/200867/101
