2a942f6de008e7c02e738d400b477d8ebaa9452e
Antonio Maniero <maniero@klip.net>
2017-04-27 10:59:48 -0300

    https://pt.stackoverflow.com/q/200975/101

---
+++ C/Array/CountValidElements.c
@@ -0,0 +1,13 @@
+void ReposicionaObjetos(){
+    int contaElementosValidos = 0;
+    memset(marcadoresVisiveis, 0, sizeof marcadoresVisiveis);
+    for int (i = 0; i < config->marker_num; i++) {
+        if( config->marker[i].visible >= 0 ){
+            marcadoresVisiveis[i] = config->marker[i];
+            contaElementosValidos++;
+        }
+    }
+    //faz o que precisa com contaElementosValidos
+}
+
+//https://pt.stackoverflow.com/q/200975/101
