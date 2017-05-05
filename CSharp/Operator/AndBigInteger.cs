6c9291dd8a4f707278dcd4c07fc8d0ab0dca440a
Antonio Maniero <maniero@klip.net>
2017-05-05 08:30:29 -0300

    https://pt.stackoverflow.com/q/202352/101

--- CSharp/Operator/AndBigInteger.cs
+++ CSharp/Operator/AndBigInteger.cs
@@ -5,13 +5,11 @@ public class Program {
 	public static void Main() {
 		WriteLine(VerificaPermissao((BigInteger)1, (BigInteger)0));
 		WriteLine(VerificaPermissao((BigInteger)1, (BigInteger)1));
+		WriteLine(VerificaPermissao((BigInteger)2, (BigInteger)1));
 	}
 	public static bool VerificaPermissao(BigInteger perm1, BigInteger perm) {
-        if (perm1 == null || perm == null || (perm1.IsZero) || (perm.IsZero ))
-            return false;
         return (perm & perm1) != 0;
     }
-
 }
 
 //https://pt.stackoverflow.com/q/202352/101
