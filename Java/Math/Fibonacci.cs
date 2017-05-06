4cede1e188998e3e34275500f1582e9935faaac7
Antonio Maniero <maniero@klip.net>
2017-05-06 16:57:40 -0300

    https://pt.stackoverflow.com/q/202847/101

--- Java/Math/Fibonacci.cs
+++ Java/Math/Fibonacci.cs
@@ -1,10 +1,10 @@
 class HelloWorld {
     public static void main(String[] args) {
-        long termo1 = 1;
-        long termo2 = 2;
-        long soma = 0;
-        for (int i = 0; i < 10000; i++) {
-            long novoTermo = termo1 + termo2;
+        int termo1 = 1;
+        int termo2 = 2;
+        int soma = 0;
+        while (termo2 < 100000) {
+            int novoTermo = termo1 + termo2;
             if (novoTermo % 2 == 0) {
                 soma += novoTermo;
             }
