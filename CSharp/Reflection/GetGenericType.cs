24bee7f5a32b0a92e700bae86d197f69a0237e86
Antonio Maniero <maniero@klip.net>
2017-05-11 13:04:44 -0300

    https://pt.stackoverflow.com/q/203917/101

---
+++ CSharp/Reflection/GetGenericType.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Reflection;
+
+public class Program {
+    public static void Main(string[] args) {
+		var pessoa = new Pessoa { id = 1, Nome = "Nicola Bogar Uccio", DataNascimento = new DateTime(1988, 08, 24) };
+		PropertyInfo[] properties = Entities<Pessoa>.GetPropertiesComoEUQuero();
+    }
+}
+
+public class Pessoa {
+    public int id { get; set; }
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+}
+
+public static class Entities<TEntidade> where TEntidade : class {
+    public static PropertyInfo[] GetPropertiesComoEUQuero() {         
+        var entity = Activator.CreateInstance(typeof(TEntidade)) as TEntidade;
+        PropertyInfo[] properties = entity.GetType().GetProperties();
+        return properties;
+    }
+}
+
+//https://pt.stackoverflow.com/q/203917/101
