83b1d19cf30dfdda827cf408442aa2d2fef1d1a6
Antonio Maniero <maniero@klip.net>
2017-05-16 12:20:06 -0300

    https://pt.stackoverflow.com/q/205022/101

--- CSharp/Algorithm/SerialIf.cs
+++ CSharp/Algorithm/SerialIf.cs
@@ -1,12 +1,11 @@
 var WebReq = HttpWebRequest.Create(GET_Data);
 using (var WebRes = WebReq.GetResponse)
 using (var Reader = new StreamReader(WebRes.GetResponseStream)) {
-        var Str = Reader.ReadLine;
-        if (Str.Contains(Answer1)) return "True";
-        if (Str.Contains(Answer2)) return "Banned";
-        if (Str.Contains(Answer3)) return "Invalid";
-        return "Invalid";
-    }
+var Str = Reader.ReadLine;
+if (Str.Contains(Answer1)) return "True";
+if (Str.Contains(Answer2)) return "Banned";
+if (Str.Contains(Answer3)) return "Invalid";
+return "Invalid";
 }
 
 //https://pt.stackoverflow.com/q/205022/101
