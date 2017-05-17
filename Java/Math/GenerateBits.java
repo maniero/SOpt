f0233adae0a765e052e385ad88fa1ce98acdbcdb
Antonio Maniero <maniero@klip.net>
2017-05-17 11:13:44 -0300

    https://pt.stackoverflow.com/q/205295/101

---
+++ Java/Math/GenerateBits.java
@@ -0,0 +1,9 @@
+class HelloWorld  {
+	public static void main (String[] args) {
+		for (int i = 0; i < 32; i++) {
+			System.out.println("" + i / 16 % 2 + i / 8 % 2 + i / 4 % 2 + i / 2 % 2 + i % 2);
+		}
+	}
+}
+
+//https://pt.stackoverflow.com/q/205295/101
