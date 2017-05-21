6f57b4a5461f21116a60cd42e1d4ff0101524594
Antonio Maniero <maniero@klip.net>
2017-05-21 19:05:51 -0300

    https://pt.stackoverflow.com/q/206373/101

---
+++ CPP/Syntax/Namespace.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+  int main(){
+  long long xm;
+  long long ym;
+  long long xp;
+  long long yp;
+  cin >> xm;
+  cin >> ym;
+  cin >> xp;
+  cin >> yp;
+  long long t = (xp + yp) - (xm + ym);
+  cout << t << endl;
+}
+
+//https://pt.stackoverflow.com/q/206373/101
