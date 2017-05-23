2c645b80bff0dd21538caa82427c6894fb439dcd
Antonio Maniero <maniero@klip.net>
2017-05-23 15:03:51 -0300

    https://pt.stackoverflow.com/q/206902/101

---
+++ CSharp/Operator/Precedence.cs
@@ -0,0 +1,22 @@
+using System;
+public class C {
+    public int Main() {
+        var conta = new Conta();
+        var txtValor = new Form();
+        bool retorno;
+ 		if ((retorno = conta.Saca(Convert.ToDouble(txtValor.Text)))) {
+            return 1;
+        }
+        return 0;
+     }
+}
+
+public class Conta {
+    public bool Saca(double x) => true;
+}
+
+public class Form {
+    public String Text;
+}
+
+//https://pt.stackoverflow.com/q/206902/101
