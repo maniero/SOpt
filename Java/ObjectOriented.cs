90c8f3a74733331f165690be8c2f4e7407965855
Antonio Maniero <maniero@klip.net>
2017-05-25 08:34:25 -0300

    https://pt.stackoverflow.com/q/206847/101

---
+++ Java/ObjectOriented.cs
@@ -0,0 +1,10 @@
+public void insere(Carro carro) {
+    int contador = 0;
+    for (carro : vetVeiculo) {
+        if(caro == null) break;
+        contador++;
+    }
+    vetVeiculo[contador] = carro;        
+}
+
+//https://pt.stackoverflow.com/q/206847/101
