244a77ba308d4006ad3e076ed9d5b898223fdb98
Antonio Maniero <maniero@klip.net>
2017-05-26 13:18:11 -0300

    https://pt.stackoverflow.com/q/207800/101

---
+++ CSharp/Tuple/ReturnTwoValues.cs
@@ -0,0 +1,10 @@
+public (bool, TipoDoRetornoDoMetodoDadosDermaClube) CarregaProdutosDermaClube(string consulta) {
+    var buscaProdutosDermaClube = new BuscaProdutosDermaClubeDAL();
+    retorno = this.buscaProdutosDermaClube.DadosDermaClube("");
+    if (string.IsNullOrEmpty(consulta)) {
+        return (false, null);
+    }
+    return (retorno[0].idproduto.Contains(consulta), retorno);
+}
+
+//https://pt.stackoverflow.com/q/207800/101
