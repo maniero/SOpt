04f41343efda9dfc9b619d24b02d4c6a5f199d79
Antonio Maniero <maniero@klip.net>
2017-05-27 14:26:45 -0300

    https://pt.stackoverflow.com/q/208028/101

--- Go/NumberLength.go
+++ Go/NumberLength.go
@@ -3,8 +3,9 @@ import ("fmt"
         "strconv")
 
 func main() {
-	t := strconv.Itoa(123)
-    fmt.Println(len(t))
+	t := strconv.Itoa(57890)
+    fmt.Println("Tamanho:", len(t))
+    fmt.Printf("Terceiro caracteres: %c", t[2])
 }
 
 //https://pt.stackoverflow.com/q/208028/101
