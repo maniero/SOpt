172a3382453c820b3d33d380f59ab5275b9377f5
Antonio Maniero <maniero@klip.net>
2017-05-27 14:37:20 -0300

    https://pt.stackoverflow.com/q/208028/101

--- Go/NumberLength.go
+++ Go/NumberLength.go
@@ -1,11 +1,13 @@
 package main
 import ("fmt"
-        "strconv")
+        "strconv"
+        "math")
 
 func main() {
 	t := strconv.Itoa(57890)
     fmt.Println("Tamanho:", len(t))
     fmt.Printf("Terceiro caracteres: %c", t[2])
+    fmt.Println("Tamanho matematicamente calculado:", math.Floor(math.Log10(math.Abs((57890)))) + 1)
 }
 
 //https://pt.stackoverflow.com/q/208028/101
