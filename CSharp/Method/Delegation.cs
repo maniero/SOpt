7d6ba11258dcab1e4646ea114d8c67e43a13b8ae
Antonio Maniero <maniero@klip.net>
2017-05-31 15:18:34 -0300

    https://pt.stackoverflow.com/q/209082/101

---
+++ CSharp/Method/Delegation.cs
@@ -0,0 +1,19 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		ContaPoupanca poupanca = new ContaPoupanca();
+	    poupanca.CalculaInvestimento();
+    	Conta conta = new ContaPoupanca();
+    	conta.CalculaInvestimento();
+	}
+}
+
+public class Conta {
+	public virtual void CalculaInvestimento() {}
+}
+public class ContaPoupanca : Conta {
+	public override void CalculaInvestimento() { WriteLine("ok"); }
+}
+
+//https://pt.stackoverflow.com/q/209082/101
