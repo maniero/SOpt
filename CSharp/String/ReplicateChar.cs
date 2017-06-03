c3e45332a026e137c28dfef149c2d33dac3da3bc
Antonio Maniero <maniero@klip.net>
2017-06-03 16:44:26 -0300

    https://pt.stackoverflow.com/q/210004/101

---
+++ CSharp/String/ReplicateChar.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program {
+	public static void Main() {
+		var texto = "carambola";
+		var adivinha = new String('_', texto.Length);
+		Console.WriteLine(adivinha);
+	}
+}
+
+//https://pt.stackoverflow.com/q/210004/101
