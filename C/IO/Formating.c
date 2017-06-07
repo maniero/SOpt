763865e237e05beeb31fb7df449992e708c75c61
Antonio Maniero <maniero@klip.net>
2017-06-07 07:55:42 -0300

    https://pt.stackoverflow.com/q/210531/101

--- C/IO/Formating.c
+++ C/IO/Formating.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #define max 10
 
-int main(void ) {
+int main(void) {
 	int a[max];
-	for (int i = 1; i <= max; i++) {
+	for (int i = 0; i < max; i++) {
 		printf("\nDigite o %i elemento do vetor A: ", i);
 		scanf("%d", &a[i]);
 	}
 	printf("\n");
-	for (int i = 1; i <= max; i++) {
+	for (int i = 0; i < max; i++) {
 		printf("%d ", a[i]);
 	}
 	int par = 0;
-	for (int i = 1; i <= max; i++) {
-		if (a[i] % 2 == 0){
+	for (int i = 0; i < max; i++) {
+		if (a[i] % 2 == 0) {
 			printf("\nPar: %d", a[i]);
 			par++;	
 		}
