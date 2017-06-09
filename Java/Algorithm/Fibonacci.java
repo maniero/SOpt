9a387e0eec5fd0fa7275c569724293c5831241f5
Antonio Maniero <maniero@klip.net>
2017-06-09 08:44:24 -0300

    https://pt.stackoverflow.com/q/211562/101

---
+++ Java/Algorithm/Fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class HelloWorld {
+	public static void main(String[] args) {
+	    Scanner x = new Scanner(System.in);
+	    System.out.println("Digite a quantidade de termos");
+	    int n1 = 0, n2 = 1;
+	    for (int qtd = x.nextInt(); qtd > 0; qtd--) {
+	    	System.out.print(n1 + " ");
+	   		int n3 = n1 + n2;
+	        n1 = n2;
+	        n2 = n3;
+	    }
+	    System.out.println("Fim");
+	}
+}
+
+//https://pt.stackoverflow.com/q/211562/101
