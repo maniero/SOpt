7bf91d961cc7dc55e71ffb5581b308e312e2dff1
Antonio Maniero <maniero@klip.net>
2017-06-21 10:56:52 -0300

    https://pt.stackoverflow.com/q/213908/101

--- C/String/EncodeString.c
+++ C/String/EncodeString.c
@@ -5,8 +5,7 @@ int main() {
     printf("String: ");
     scanf("%200s", frase);
     for (char *i = frase; *i != '\0'; i++) {
-        *i += *i < 'A' || *i > 'Z' ? 0 :
-                    *i > 'W' ? -23 : 3;
+        *i = *i < 'A' || *i > 'Z' ? *i : (((*i - 65) + 3) % 26)  + 65;
     }
     printf("String: %s", frase);
 }
