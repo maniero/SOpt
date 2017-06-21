e2971b882ebe26f26e9544904ab48e2e42685b7d
Antonio Maniero <maniero@klip.net>
2017-06-21 10:12:20 -0300

    https://pt.stackoverflow.com/q/213870/101

--- VB.NET/IO/String/SplitLimit.vb
+++ VB.NET/IO/String/SplitLimit.vb
@@ -5,14 +5,12 @@ Public Module Module1
 		Dim Morada As String = "Avenida luis de camoes, travessa de santa rita numero 588"
 		Dim limite As Integer = 50
 		Dim posicao As Integer = Morada.LastIndexOf(" ", limite)
-		If Morada = Nothing Or Morada.Length < limite OrElse posicao = -1
+		If Morada = Nothing OrElse Morada.Length < limite OrElse posicao = -1
 			Console.WriteLine(Morada)
 		Else
-			If posicao <> -1
-				Console.WriteLine(Morada.Substring(0, Morada.LastIndexOf(" ", limite)))
-				Console.WriteLine(Morada.Substring(Morada.LastIndexOf(" ", limite) + 1))
+			Console.WriteLine(Morada.Substring(0, Morada.LastIndexOf(" ", limite)))
+			Console.WriteLine(Morada.Substring(Morada.LastIndexOf(" ", limite) + 1))
 			End If
-		End If
 	End Sub
 End Module
 
