a27e222f3a2468e7ff8f98e1fae5dd023b9ddeb4
Antonio Maniero <maniero@klip.net>
2017-06-22 11:55:27 -0300

    https://pt.stackoverflow.com/q/213870/101

--- VB.NET/IO/String/SplitLimit.vb
+++ VB.NET/IO/String/SplitLimit.vb
@@ -2,16 +2,29 @@ Imports System
 				
 Public Module Module1
 	Public Sub Main()
-		Dim Morada As String = "Avenida luis de camoes, travessa de santa rita numero 588"
-		Dim limite As Integer = 50
-		Dim posicao As Integer = Morada.LastIndexOf(" ", limite)
-		If Morada = Nothing OrElse Morada.Length < limite OrElse posicao = -1
-			Console.WriteLine(Morada)
-		Else
-			Console.WriteLine(Morada.Substring(0, Morada.LastIndexOf(" ", limite)))
-			Console.WriteLine(Morada.Substring(Morada.LastIndexOf(" ", limite) + 1))
-			End If
+		For Each texto in CropText("Avenida luis de camoes, travessa de santa rita numero 588", 50)
+			Console.WriteLine(texto)
+		Next
+		For Each texto in CropText("Avenida luis de camoes, travessa de santa rita", 50)
+			Console.WriteLine(texto)
+		Next
+		For Each texto in CropText("Avenidaluisdecamoes,travessadesantaritanumero588teste", 50)
+			Console.WriteLine(texto)
+		Next
 	End Sub
+
+	Public Function CropText(text As String, limit As Integer) As String()
+		If text.Length < limit
+			Return New String() {text}
+		Else
+			Dim position As Integer = text.LastIndexOf(" ", limit)
+			If position = -1
+				Return New String() {text}
+			Else
+				return New String() {text.Substring(0, position), text.Substring(position + 1)}
+			End If
+		End If
+	End Function
 End Module
 
 'https://pt.stackoverflow.com/q/213870/101
