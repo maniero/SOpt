de2b1ce9672d21f74b18a58a55ef8105c63777fa
Antonio Maniero <maniero@klip.net>
2017-06-26 20:04:26 -0300

    https://pt.stackoverflow.com/q/215776/101

---
+++ CPP/Algorithm/Power.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std; 
+
+int calc(int base) {
+    return pow(base, 2);
+}
+
+int main() {   
+    int vet[200];
+    for (int x = 0; x < 200; x++) {
+        vet[x] = x;
+        cout << endl << "Posição " << x + 1 << " : " << vet[x];
+    }
+    for (int x = 0; x < 200; x++) {
+        cout << endl << vet[x] << "² =  " << calc(vet[x]);
+    }
+}
+
+//https://pt.stackoverflow.com/q/215776/101
