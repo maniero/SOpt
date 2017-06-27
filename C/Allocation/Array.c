3750aaa19f9df8d515e41b6148ab01c768c90bd7
Antonio Maniero <maniero@klip.net>
2017-06-27 01:34:33 -0300

    https://pt.stackoverflow.com/q/215819/101

---
+++ C/Allocation/Array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 30
+
+struct CARRO{
+	int id;
+	char fabricante[MAX];
+	char modelo[MAX];
+	int ano;
+	char cor[MAX];
+	int preco;
+};
+
+int main() {
+	int qtd;
+	scanf("%d", &qtd);
+	struct CARRO *carros = malloc(qtd * sizeof(struct CARRO));
+	free(carros); //só para usar a variável, neste caso não precisaa
+}
+
+//https://pt.stackoverflow.com/q/215819/101
