ba49a46b281f07f30b67e907021edf430fe89c38
Antonio Maniero <maniero@klip.net>
2017-06-27 10:15:34 -0300

    https://pt.stackoverflow.com/q/215873/101

---
+++ CSharp/Class/ListMembers.cs
@@ -0,0 +1,36 @@
+using static System.Console;
+using System.Collections.Generic;
+
+namespace arrayteste {
+    public class Cidade{
+        public string Nome { get; set; }
+        public List<Casa> Casas { get; set; } = new List<Casa>();
+        public Cidade(string nome, Casa casa) {
+            Nome = nome;
+            Casas.Add(casa);
+        }
+		public void NovaCasa(Casa casa) {
+			Casas.Add(casa);
+		}
+    }
+    public class Casa {
+        public string Dono { get; set; }
+        public string Cor { get; set; }
+        public Casa(string dono, string cor) {
+            Dono = dono;
+            Cor = cor;
+        }
+
+    }
+    public class Program {
+        public static void Main(string[] args) {
+            var cidade1 = new Cidade("Lisboa", new Casa("João", "verde"));
+            var cidade2 = new Cidade("Porto", new Casa("José", "vermelha"));
+			cidade1.NovaCasa(new Casa("Joaquim", "azul"));
+            WriteLine(cidade1.Casas[0].Dono);
+            WriteLine(cidade1.Casas[1].Dono);
+        }
+    }
+}
+
+//https://pt.stackoverflow.com/q/215873/101
