ca47ffa3eaa8c4fa4b7772fca01e041c151be1de
Antonio Maniero <maniero@klip.net>
2017-07-10 10:57:20 -0300

    https://pt.stackoverflow.com/q/219227/101

---
+++ CSharp/Math/DivideIntegerResultDouble.cs
@@ -0,0 +1,12 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		int soma = 7;
+		int cont = 2;
+		double media = (double)soma / (double)cont;
+		WriteLine(media);
+	}
+}
+
+//https://pt.stackoverflow.com/q/219227/101
