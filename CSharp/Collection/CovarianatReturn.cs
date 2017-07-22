c19f58041b78f82ac5f7b71e778ad4ca6d10dbcf
Antonio Maniero <maniero@klip.net>
2017-07-22 10:21:59 -0300

    https://pt.stackoverflow.com/q/223364/101

---
+++ CSharp/Collection/CovarianatReturn.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+					
+public interface IObjetosBase {
+     IList<IObjetosBase> Get();
+}
+
+public class ObjetosBase : IObjetosBase {
+    public virtual IList<IObjetosBase> Get() { return new List<IObjetosBase>(); }
+}
+
+public class Aluno : ObjetosBase {
+	public override IList<IObjetosBase> Get() { return new List<IObjetosBase>() { new ObjetosBase() }; }
+}
+
+//https://pt.stackoverflow.com/q/223364/101
