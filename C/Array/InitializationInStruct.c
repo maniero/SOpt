cbe12a0e14e9b16b69551efbab7a2264180b024e
Antonio Maniero <maniero@klip.net>
2017-07-23 14:14:03 -0300

    https://pt.stackoverflow.com/q/223519/101

---
+++ C/Array/InitializationInStruct.c
@@ -0,0 +1,16 @@
+int main(void) {
+	struct horario {
+	    char M[8][40];
+	};
+	struct horario hora = { .M = {
+		{"X 8 9 10 11 12 13 14 15 16 17 18 19 20"},
+        {"S"},
+        {"T"},
+        {"Q"},
+        {"Q"},
+        {"S"},
+        {"S"},
+        {"D"}}};
+}
+
+//https://pt.stackoverflow.com/q/223519/101
