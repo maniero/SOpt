e01ea230379cb4193543cca6584d39f7ef388aa1
Antonio Maniero <maniero@klip.net>
2017-07-24 11:41:06 -0300

    https://pt.stackoverflow.com/q/221957/101

--- CSharp/Exception/AvoidNull.cs
+++ CSharp/Exception/AvoidNull.cs
@@ -1,8 +1,8 @@
 for (int i = 0; i < numberOfProcessAvailable; i++) {
     var IptcDirectory = directories.OfType<IptcDirectoryBase>().FirstOrDefault();
-    imageFileInfo.Keywords = IptcDirectory.GetProperty('TagKeywords') == null ? "padrao keywords" : IptcDirectory.GetDescription(IptcDirectory.TagKeywords);
-    imageFileInfo.LensModel = IptcDirectory.GetProperty('TagLensModel') == null ? "padrao lensmodel" : IptcDirectory.GetDescription(IptcDirectory.TagLensModel);
-    imageFileInfo.TagMake = IptcDirectory.GetProperty('TagMake') == null ? "padrao make" : IptcDirectory.GetDescription(IptcDirectory.TagMake);
+    imageFileInfo.Keywords = IptcDirectory.TagKeywords == null ? "padrao keywords" : IptcDirectory.GetDescription(IptcDirectory.TagKeywords);
+    imageFileInfo.LensModel = IptcDirectory.TagLensModel == null ? "padrao lensmodel" : IptcDirectory.GetDescription(IptcDirectory.TagLensModel);
+    imageFileInfo.TagMake = IIptcDirectory.TagMake == null ? "padrao make" : IptcDirectory.GetDescription(IptcDirectory.TagMake);
 }
 
 //https://pt.stackoverflow.com/q/221957/101
