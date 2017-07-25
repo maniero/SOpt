606c333eef747c06e4b744d2067112353d1c0940
Antonio Maniero <maniero@klip.net>
2017-07-25 16:45:23 -0300

    https://pt.stackoverflow.com/q/223997/101

---
+++ C/String/ParseDate.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int day, year;
+    char weekday[20], month[20], dtm[100];
+    printf(
+        "\n\tDigite Dia Mes a ano em string"
+        "\n\tex.(Saturday March 25 1989): "
+    );
+    sscanf(dtm, "%s %s %d  %d", weekday, month, &day, &year);
+    printf("%s %d, %d = %s\n", month, day, year, weekday);
+}
+
+//https://pt.stackoverflow.com/q/223997/101
