0f3197944240439ba49b7c5d6ba2d82931a7aefa
Antonio Maniero <maniero@klip.net>
2017-07-26 09:25:41 -0300

    Update ShortYear.cs

--- CSharp/Struct/ShortYear.cs
+++ CSharp/Struct/ShortYear.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Diagnostics.Contracts;
 
-public class Program {
+public struct Program {
 	public static void Main() {
 		short numero = 2017;
 		var m = new ShortYear(numero);
 		Console.WriteLine(m);
 	}
 }
-
-public class ShortYear { //provavelmente teria implemnetações de IFormattable, IConvertible, IComparable<ShortYear>, IEquatable<ShortYear> e operadores
+//provavelmente teria implemnetações de operadores, outros métodos e das interfaces
+//IFormattable, IConvertible, IComparable<ShortYear>, IEquatable<ShortYear> e outras
+public class ShortYear {
 	public byte Year;
 	public const short Century = 1900;
 	public ShortYear(short value) {
@@ -23,9 +24,7 @@ public class ShortYear { //provavelmente teria implemnetações de IFormattable,
     public static implicit operator short(ShortYear value) => (short)(value.Year + Century);
     public static implicit operator int(ShortYear value) => value.Year + Century;
 	public override int GetHashCode() => Year.GetHashCode();
-	public override String ToString() {
-		return (Year + Century).ToString();
-	}
+	public override String ToString() => (Year + Century).ToString();
 }
 
 //https://pt.stackoverflow.com/q/221079/101
