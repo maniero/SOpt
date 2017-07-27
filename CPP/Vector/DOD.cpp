35c3ab0927162f66105845406ae4a6c4e48963ac
Antonio Maniero <maniero@klip.net>
2017-07-27 09:48:20 -0300

    https://pt.stackoverflow.com/q/224138/101

---
+++ CPP/Vector/DOD.cpp
@@ -0,0 +1,19 @@
+class Ball {
+    Point  position;
+    Color  color;
+    double radius;
+    void draw();
+};
+
+vector<Ball> balls;
+
+//DOD:
+
+class Balls {
+    vector<Point>  position;
+    vector<Color>  color;
+    vector<double> radius;
+    void draw();
+};
+
+//https://pt.stackoverflow.com/q/224138/101
