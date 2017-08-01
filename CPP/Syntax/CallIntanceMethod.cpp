e97bfd0ad5b54348e6bc577a095342a822c30e72
Antonio Maniero <maniero@klip.net>
2017-08-01 09:54:45 -0300

    https://pt.stackoverflow.com/q/225056/101

---
+++ CPP/Syntax/CallIntanceMethod.cpp
@@ -0,0 +1,7 @@
+byte *CryrazCore::ComputeByteArray(byte inputData[], bool decryptMode) {
+    int cs = this->PushChecksum();
+    //deveria usar os parâmetros
+    return alguma coisa que seja um ponteiro para byte;
+}
+
+//https://pt.stackoverflow.com/q/225056/101
