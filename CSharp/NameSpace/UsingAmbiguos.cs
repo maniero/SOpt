2e5527a23efcedc37f476efa0b46614331218407
Antonio Maniero <maniero@klip.net>
2017-08-04 11:08:17 -0300

    https://pt.stackoverflow.com/q/226559/101

---
+++ CSharp/NameSpace/UsingAmbiguos.cs
@@ -0,0 +1,18 @@
+using Console;
+
+namespace BusinessDAL {
+    public class ArquivoDAL {   
+        string valor =  Console.Business.Caminho;
+        static void Main()=> System.Console.WriteLine("Hello, World!"); //só p/ compilar
+    }
+}
+
+//Outro arquivo:
+
+namespace Console {
+    public class Business {
+        public static string Caminho = "";
+    }
+}
+
+//https://pt.stackoverflow.com/q/226559/101
