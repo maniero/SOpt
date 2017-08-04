5ec75c3d29dfedced59bc0fcdba7df9bb425268e
Antonio Maniero <maniero@klip.net>
2017-08-04 15:52:07 -0300

    https://pt.stackoverflow.com/q/226603/101

--- Python/StripString.py
+++ Python/StripString.py
@@ -1,4 +1,8 @@
-preco_casa = float(input("...").replace('.', ''))
-print(preco_casa)
+preco_casa_raw = input("Qual o preço do imóvel?")
+preco_casa = float(preco_casa_raw.replace('.', ''))
+seu_salario = float(input("Qual o seu salário mensal?:"))
+anos_a_pagar = float(input("Quantidade de anos a pagar pelo imóvel:"))
+valor_prestacao = preco_casa / (anos_a_pagar * 12)
+print (" O valor da prestação será de R$%.2f" % valor_prestacao)
 
 #https://pt.stackoverflow.com/q/226603/101
