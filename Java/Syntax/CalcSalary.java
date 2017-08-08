96ceb40f89d90101e74fe0902d92ed3d14be6cc4
Antonio Maniero <maniero@klip.net>
2017-08-08 18:29:42 -0300

    https://pt.stackoverflow.com/q/227561/101

---
+++ Java/Syntax/CalcSalary.java
@@ -0,0 +1,23 @@
+class Funcionario {
+    String nome, dep, banco, RG;
+    double salario;
+    
+    void recebeAumento(double percentual) {
+        salario += salario / 100 * percentual;
+    }
+    public double calculaGanhoAnual() {
+         return salario * 12;
+    }
+    public static void main(String[] args) {
+        Funcionario f1 = new Funcionario();
+        f1.nome = "João";
+        f1.dep = "Defesa";        
+        f1.banco = "Banco do Brasil S.A";
+        f1.RG = "798956-5";
+        f1.salario = 27000.0;
+        f1.recebeAumento(2000);
+        System.out.println("Nome: " + f1.nome + "\nDepartamento: " + f1.dep + "\nBanco: " + f1.banco + "\nRG: " + f1.RG + "\nSalário Anual: " + f1.calculaGanhoAnual());
+    }
+}
+
+//https://pt.stackoverflow.com/q/227561/101
