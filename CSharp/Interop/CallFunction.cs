eddd64093645648ae2b7d18e3a6c0c0dc8447712
Antonio Maniero <maniero@klip.net>
2017-08-08 09:01:53 -0300

    https://pt.stackoverflow.com/q/227301/101

---
+++ CSharp/Interop/CallFunction.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+
+public class Program {
+    [DllImport("teste.DLL", EntryPoint="print")]
+    static extern void print(string message);
+
+    public static void Main(string[] args) {
+        print("Chamando C");
+    }
+}
+
+//Em C:
+
+#include <stdio.h>
+void print(const char *message) {
+    printf("%s", message);
+}
+
+//https://pt.stackoverflow.com/q/227301/101
