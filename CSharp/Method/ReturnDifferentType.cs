30ef14e5c339eb90779b41f68b216f6a5f492aab
Antonio Maniero <maniero@klip.net>
2017-08-16 13:50:08 -0300

    https://pt.stackoverflow.com/q/228963/101

---
+++ CSharp/Method/ReturnDifferentType.cs
@@ -0,0 +1,9 @@
+public PessoaJuridica RetornarPessoaJuridica(string id) {
+    return new PessoaJuridica();
+}
+
+public PessoaFisica RetornarPessoaFisica(string id) {
+    return new PessoaFisica();
+}
+
+//https://pt.stackoverflow.com/q/228963/101
