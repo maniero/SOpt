41d2c123367cf3a06a03336cf06e6c2f0c1398e2
Antonio Maniero <maniero@klip.net>
2017-08-30 00:42:56 -0300

    https://pt.stackoverflow.com/q/233378/101

---
+++ Java/Array/FindString.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Ideone {
+	public static void main (String[] args) {
+		    ArrayList<String> posicoes = new ArrayList<String>();
+		    posicoes.add("Ricardo;01051509912;gmail");
+		    posicoes.add("Renato;123456789123;hotmail");
+		    posicoes.add("Rodrigo;09873923121;yahoo");
+		    System.out.println(BuscaString(posicoes, "01051509912"));
+		    System.out.println(BuscaString(posicoes, "123456789123"));
+	}
+	public static int BuscaString(ArrayList<String> lista, String busca) {
+	    for (int i = 0; i < lista.size(); i++) {
+	    	if (lista.get(i).contains(busca)) {
+	    		return i;
+	    	}
+	    }
+		return -1;
+	}
+}
+
+//https://pt.stackoverflow.com/q/233378/101
