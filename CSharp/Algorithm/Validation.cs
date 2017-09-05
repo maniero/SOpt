63c0a2d127234da37cf252f266083eee412fd6d6
Antonio Maniero <maniero@klip.net>
2017-09-05 15:46:52 -0300

    https://pt.stackoverflow.com/q/235052/101

---
+++ CSharp/Algorithm/Validation.cs
@@ -0,0 +1,23 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		var idArtista = 0;
+		while (true) {
+			WriteLine("Digite o ID do artista:");
+			if (int.TryParse(ReadLine(), out idArtista)) break;
+		}
+		WriteLine("Digite o nome do artista:");
+		var nomeArtista = ReadLine();
+		WriteLine("Digite o sobrenome do artista:");
+		var  sobrenomeArtista = ReadLine();
+		var  idReparador = 0;
+		while (true) {
+			WriteLine("Digite o ID do reparador:");
+			int.Parse(ReadLine());
+			if (int.TryParse(ReadLine(), out idReparador) && idArtista != idReparador) break;
+		}
+	}
+}
+
+//https://pt.stackoverflow.com/q/235052/101
