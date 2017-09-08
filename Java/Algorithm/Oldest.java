c4ba16325cca3092a91b48c0191e779f96a6b729
Antonio Maniero <maniero@klip.net>
2017-09-08 16:28:13 -0300

    https://pt.stackoverflow.com/q/235770/101

--- Java/Algorithm/Oldest.java
+++ Java/Algorithm/Oldest.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
 class Pessoa {
-    private static String nome;
-    private static int idade;
+    public String nome;
+    public int idade;
+}
+class Programa {
     public static String comparaIdade(Pessoa[] pessoa){
         int maior = pessoa[0].idade;
         int indice = 0;
         for (int i = 1; i < 3; i++) {
+        	System.out.println(pessoa[i].idade);
             if (pessoa[i].idade > maior) {
                 maior = pessoa[i].idade;
                 indice = i;
@@ -18,7 +21,8 @@ class Pessoa {
     public static void main(String[] args){
         Pessoa[] pessoa = new Pessoa[3];
         Scanner input = new Scanner(System.in);
-        for (int i = 0; i<3; i++) {
+        for (int i = 0; i < 3; i++) {
+        	pessoa[i] = new Pessoa();
             System.out.println("Digite o nome da pessoa: ");
             pessoa[i].nome = input.nextLine();
             System.out.println("Digite a idade da pessoa: ");
