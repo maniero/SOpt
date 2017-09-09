28c3da299464ac17814b9fe0d8d61fc3f4d420f1
Antonio Maniero <maniero@klip.net>
2017-09-09 19:17:03 -0300

    https://pt.stackoverflow.com/q/236001/101

---
+++ Ruby/SyntaxFunction.rb
@@ -0,0 +1,8 @@
+larguraDaLinha = 40
+str = '--> text <--'
+puts str.ljust(larguraDaLinha)
+puts str.center(larguraDaLinha)
+puts str.rjust(larguraDaLinha)
+puts str.ljust(larguraDaLinha/2) + str.rjust(larguraDaLinha/2)
+    
+#https://pt.stackoverflow.com/q/236001/101
