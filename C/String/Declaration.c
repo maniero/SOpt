2fd8afc7df6cd5d3be865e58b9e3e0a01c0b48f9
Antonio Maniero <maniero@klip.net>
2017-09-11 00:10:59 -0300

    https://pt.stackoverflow.com/q/236175/101

---
+++ C/String/Declaration.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char str[] = "teste";
+	char *strx = "teste";
+	printf("%s", str);
+	printf("%s", strx);
+}
+
+//https://pt.stackoverflow.com/q/236175/101
