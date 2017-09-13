f2a1d1771b66af448dd585244cc1dab0937fb631
Antonio Maniero <maniero@klip.net>
2017-09-13 15:57:21 -0300

    https://pt.stackoverflow.com/q/237073/101

---
+++ Java/Array/LimitRandom.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Sorteio {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o numero de pessoas no sorteio: ");
+        int n = sc.nextInt();
+        String pessoa [] = new String [n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Informe a " + (i + 1) + "a pessoa: ");
+            pessoa[i] = sc.nextLine();
+            if (i == 0) sc.nextLine();
+        }
+        Random rd = new Random(3);
+        System.out.println("O vencedor do sorteio e: " + pessoa[rd.nextInt(n)]);
+    }
+}
+
+//https://pt.stackoverflow.com/q/237073/101
