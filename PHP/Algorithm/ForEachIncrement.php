c8446587b06e2b5600dd6282a80c5a70ecd467c4
Antonio Maniero <maniero@klip.net>
2017-09-18 21:06:44 -0300

    https://pt.stackoverflow.com/q/238483/101

--- PHP/Algorithm/ForEachIncrement.php
+++ PHP/Algorithm/ForEachIncrement.php
@@ -2,12 +2,13 @@
 $adultos = array( "a", "b", "c" );
 $i = 1;
 foreach ($adultos as $adulto):
-	echo "<strong>Quarto " . $i++ . "</strong><br>";
-	echo "Adultos: " . $adulto . "<br>";
+    echo "<strong>Quarto " . $i++ . "</strong><br>";
+    echo "Adultos: " . $adulto . "<br>";
 endforeach;
+
 for ($i = 0; $i < count($adultos); $i++) {
-	echo "<strong>Quarto ". $i++ ."</strong><br>";
-	echo "Adultos: " . $adultos[$i] . "<br>";
+    echo "<strong>Quarto ". $i ."</strong><br>";
+    echo "Adultos: " . $adultos[$i++] . "<br>";
 }
 
 //https://pt.stackoverflow.com/q/238483/101
