313663de2f481a68935273141482246e027023e0
Antonio Maniero <maniero@klip.net>
2017-09-25 08:07:09 -0300

    https://pt.stackoverflow.com/q/239574/101

--- C/Algorithm/SequenceRepetition.c
+++ C/Algorithm/SequenceRepetition.c
@@ -2,13 +2,13 @@
 
 void tarefa(int r) {
     for (int e = 1; e <= r; e++) printf(" %d  ", r);
+    printf("\n");
 }
-
 
 int main(int argc, char *argv[]) {
     int x;
     scanf("%d", &x);
-    for (int j = 0; j <= x; j++) tarefa(j);  
+    for (int j = 1; j <= x; j++) tarefa(j);  
 }
 
 //https://pt.stackoverflow.com/q/239574/101
