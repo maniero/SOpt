3740f9fd243031d2dead16f955c02e7ca6d3b243
Antonio Maniero <maniero@klip.net>
2017-09-29 11:20:07 -0300

    https://pt.stackoverflow.com/q/241794/101

---
+++ CPP/Array/Inverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i;
+    cin >> i;
+    int vet[i];
+    for (int t = 0; t < i; t++) {
+        cin >> vet[t];
+    }
+    for (int f = i - 1; f >= 0; f--) {
+        cout << vet[f] << " ";
+    }
+}
+
+//https://pt.stackoverflow.com/q/241794/101
