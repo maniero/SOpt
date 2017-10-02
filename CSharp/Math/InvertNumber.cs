50208e78a4745a6145d5cb8dc78642be9bbbb931
Antonio Maniero <maniero@klip.net>
2017-10-02 08:34:49 -0300

    https://pt.stackoverflow.com/q/242302/101

--- CSharp/Math/InvertNumber.cs
+++ CSharp/Math/InvertNumber.cs
@@ -3,7 +3,7 @@ using static System.Console;
 public class Program {
 	public static void Main() {
 		Write("Informe um número inteiro para ser invertido: ");
-		if (!int.TryParse(ReadLine(), out var numero)) return;
+		if (!int.TryParse(ReadLine(), out var numero) && numero >= 0) return;
 		var invertido = 0;
 		while (numero > 0) {
 		   invertido = invertido * 10 + numero % 10;
