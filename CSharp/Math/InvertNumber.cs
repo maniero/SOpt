f318379d73f9dcab21bee206953cd1daccd75e0a
Antonio Maniero <maniero@klip.net>
2017-10-02 07:59:21 -0300

    https://pt.stackoverflow.com/q/242302/101

---
+++ CSharp/Math/InvertNumber.cs
@@ -0,0 +1,16 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		Write("Informe um número inteiro para ser invertido: ");
+		if (!int.TryParse(ReadLine(), out var numero)) return;
+		var invertido = 0;
+		while (numero > 0) {
+		   invertido = invertido * 10 + numero % 10;
+		   numero /= 10;
+		}
+		WriteLine(invertido);;
+	}
+}
+
+//https://pt.stackoverflow.com/q/242302/101
