c092ce665670da950c402d5f119563f5a743fa07
Antonio Maniero <maniero@klip.net>
2017-10-09 11:07:50 -0300

    https://pt.stackoverflow.com/q/244617/101

---
+++ CSharp/Collection/AddNewItem.cs
@@ -0,0 +1,25 @@
+using static System.Console;
+using System.Collections.Generic;
+
+public class Program {
+    public static void Main(string[] args) {
+		var list = new List<Item>() {
+			new Item() { Nome = "GPU" },
+            new Item() { Nome = "Livro" }
+		};
+        list.Add(new Item() { Nome = "Cadeira" });
+		var item = new Item() { Nome = "Abóbora" };
+		list.Add(item);
+		item = new Item() { Nome = "Outro objeto" };
+		list.Add(item);
+        for (int i = 0; i < list.Count; i++) {
+			WriteLine($"Item: {list[i].Nome}");
+        }
+    }
+}
+
+public class Item {
+	public string Nome;
+}
+
+//https://pt.stackoverflow.com/q/244617/101
