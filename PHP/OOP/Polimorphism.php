6eac07ec2989eb946f159974e79e0492f2ffcc08
Antonio Maniero <maniero@klip.net>
2017-10-19 05:36:54 -0200

    https://pt.stackoverflow.com/q/247363/101

---
+++ PHP/OOP/Polimorphism.php
@@ -0,0 +1,26 @@
+<?php
+class Pessoa {     
+	private $nome;
+	private $idade;
+	private $sexo;  
+	public function __construct($stringNome, $stringIdade, $stringSexo) {
+		$this->nome  = $stringNome;
+		$this->idade = $stringIdade;
+		$this->sexo  = $stringSexo; 
+	}  
+} 
+
+class Funcionario extends Pessoa { 
+	private $empresa;
+	private $salario;
+	public function __construct($stringNome, $stringIdade, $stringSexo, $stringEmpresa, $stringSalario) {
+		parent::__construct($this->nome,$this->idade,$this->sexo); // CHAMAR METODO DAS CLASSE PAI 
+		$this->empresa = $stringEmpresa;
+		$this->salario = $stringSalario;     
+	}
+}
+
+$vendedor = new Funcionario("Yuri", "19", "Masculino", "Tam", "3000");
+?>
+
+//https://pt.stackoverflow.com/q/247363/101
