e402f066394ec369a149272068b55fa7fd48cb11
Antonio Maniero <maniero@klip.net>
2017-10-19 12:07:11 -0200

    https://pt.stackoverflow.com/q/35149/101

---
+++ C/Array/SizeInRuntime.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int tam;
+    printf("\ndigite tam: ");
+    scanf("%d", &tam);
+    int vet[tam];
+    printf("%d", vet[0]);
+}
+
+//https://pt.stackoverflow.com/q/35149/101
