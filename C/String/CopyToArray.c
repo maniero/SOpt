d0f90dd3e00158c2b9ab7f9ac6f575b6036800ec
Antonio Maniero <maniero@klip.net>
2017-10-19 20:56:46 -0200

    https://pt.stackoverflow.com/q/247686/101

---
+++ C/String/CopyToArray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Portuguese");
+	int escolha;
+	char sexo[11];
+	printf("Escolha uma das opções abaixo: ");
+	printf("\n1- Sou mulher ");
+	printf("\n2- Sou homem ");
+	scanf("%d", &escolha);
+	if (escolha == 1) {
+		strcpy(sexo, "Feminino");
+	} else {
+		strcpy(sexo, "Masculino");
+	}
+	printf("Você é do sexo: %s", sexo);
+}
+
+//https://pt.stackoverflow.com/q/247686/101
