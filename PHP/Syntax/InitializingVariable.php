7db0113962bb40b68f629f30b2bb30223c64d5f7
Antonio Maniero <maniero@klip.net>
2017-10-20 14:07:45 -0200

    https://pt.stackoverflow.com/q/247899/101

---
+++ PHP/Syntax/InitializingVariable.php
@@ -0,0 +1,10 @@
+<?php
+echo gettype($var) . ' - ';
+$var;
+echo gettype($var) . ' - ';
+$var = '';
+echo gettype($var) . ' - ';
+$var = 'teste';
+echo gettype($var);
+
+//https://pt.stackoverflow.com/q/247899/101
