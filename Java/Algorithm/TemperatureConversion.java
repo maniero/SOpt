c703482ad79c7fd6ad2553fe27506f3348973348
Antonio Maniero <maniero@klip.net>
2017-10-28 09:49:04 -0200

    https://pt.stackoverflow.com/q/250434/101

---
+++ Java/Algorithm/TemperatureConversion.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+class Ideone {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); 
+	    System.out.println("Por favor indique um valor minimo:");
+	    float min = scanner.nextFloat();
+	    System.out.println("Agora indique um valor máximo:");
+	    float max = scanner.nextFloat();
+	    System.out.printf("Fahrenheit Celsius\n");
+	    for (float i = min; i <= max; i += .5) {
+	        System.out.printf("%6.2f     %6.2f%n", i, ((i - 32.0) * ( 5.0 / 9.0)));
+	    }
+	}
+}
+
+//https://pt.stackoverflow.com/q/250434/101
