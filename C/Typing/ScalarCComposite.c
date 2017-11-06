cf9ecf7d92b957e15ffd8ae019a9a00374404bbd
Antonio Maniero <maniero@klip.net>
2017-11-06 10:57:26 -0200

    https://pt.stackoverflow.com/q/252004/101

---
+++ C/Typing/ScalarCComposite.c
@@ -0,0 +1,11 @@
+typedef struct {
+    int matricula;
+    char nome[80];
+    int datanasc;
+    float not1;
+    float not2;
+    float media;
+    Data aniv;
+} Alunos;
+
+//https://pt.stackoverflow.com/q/252004/101
