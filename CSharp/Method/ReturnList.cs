ee357c475fbe645442fc6cbc46654f0f6ffabb2c
Antonio Maniero <maniero@klip.net>
2017-11-07 12:15:39 -0200

    https://pt.stackoverflow.com/q/252328/101

---
+++ CSharp/Method/ReturnList.cs
@@ -0,0 +1,26 @@
+using static System.Console;
+using System.Collections.Generic;
+
+public class Program {
+	public static void Main() {
+		var lista = ListMethod.GenerateList(5);
+	}
+}
+
+public static class ListMethod {
+	public static List<int> GenerateList(int length) {
+        var numbers = new List<int>(length);
+        for (int c = 0; c < length; c++) {
+            WriteLine($"Qual é o {c + 1}º número da lista?");
+			if (!int.TryParse(ReadLine(), out var number)) {
+				WriteLine("Valor digitado errado, digite novamente");
+				c--;
+				continue;
+			}
+            numbers.Add(number);
+        }
+        return numbers;
+    }
+}
+
+//https://pt.stackoverflow.com/q/252328/101
