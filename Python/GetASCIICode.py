4a5a2598c8cbf3356d5aaba0cb84b808c2df133b
Antonio Maniero <maniero@klip.net>
2017-11-09 09:12:38 -0200

    https://pt.stackoverflow.com/q/253730/101

--- Python/GetASCIICode.py
+++ Python/GetASCIICode.py
@@ -1,3 +1,10 @@
-print(chr(ord('A') + 3))
+def DeslocaASCII(texto):
+    novoTexto = ''
+    for letra in texto:
+        numero = ord(letra)
+        novoTexto += chr(numero + (-23 if numero > 87 else 3))
+    return novoTexto
+    
+print(DeslocaASCII('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
 
 #https://pt.stackoverflow.com/q/253730/101
