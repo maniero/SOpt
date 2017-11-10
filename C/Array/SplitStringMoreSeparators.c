940274809e203cbad9ef12ee92f5e530f0a8defc
Antonio Maniero <maniero@klip.net>
2017-11-10 18:12:47 -0200

    https://pt.stackoverflow.com/q/254401/101

--- C/Array/SplitStringMoreSeparators.c
+++ C/Array/SplitStringMoreSeparators.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    char str[] = "Utilizador 1 --> Bom dia! Tudo bem contigo?\nUtilizador 2 --> Comigo tudo excelente. Que tens feito?\nUtilizador 1 --> De momento estou a trabalhar num projeto... E tu?\nUtilizador 2 --> Eu tenho estudado uma nova linguagem, Java. Bastante interessante. Devias experimentar.\nUtilizador 2 --> Talvez experimente quando tiver algum tempo livre!\n";
+    char str[] = "\nUtilizador 1 --> Bom dia! Tudo bem contigo?\n\nUtilizador 2 --> Comigo tudo excelente. Que tens feito?\nUtilizador 1 --> De momento estou a trabalhar num projeto... E tu?\nUtilizador 2 --> Eu tenho estudado uma nova linguagem, Java. Bastante interessante. Devias experimentar.\nUtilizador 2 --> Talvez experimente quando tiver algum tempo livre!\n";
     char **matriz = malloc(sizeof(char *) * 255);
     int linha = 0;
     matriz[linha] = malloc(255);
@@ -12,7 +12,10 @@ int main() {
             if (str[caractere] == '.' && str[caractere + 1] == '.' && str[caractere + 2] == '.') {
             	matriz[linha][++coluna] = str[++caractere];
             	matriz[linha][++coluna] = str[++caractere];
-            	matriz[linha][++coluna] = '\0';
+            }
+            if (str[caractere] == '\n' && coluna == 0) {
+            	coluna = -1;
+            	continue;
             }
             if (str[caractere] == '\n' || str[caractere] == '\0') {
             	matriz[linha][coluna] = '\0';
