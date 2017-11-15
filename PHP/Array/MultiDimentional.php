53213ee7c3628c462ea599d097822af738f5e02d
Antonio Maniero <maniero@klip.net>
2017-11-15 12:06:29 -0200

    https://pt.stackoverflow.com/q/255573/101

---
+++ PHP/Array/MultiDimentional.php
@@ -0,0 +1,12 @@
+$array_exemplo = array(
+	0 => array(
+        "primeiro" => "1",
+        "segundo" => "2",
+        "terceiro" => "3"
+    )
+);
+var_dump($array_exemplo);
+var_dump($array_exemplo[0]);
+var_dump($array_exemplo[0]["segundo"]);
+
+//https://pt.stackoverflow.com/q/255573/101
