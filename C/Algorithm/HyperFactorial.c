2349239c3836e532b6699efd598e00bff607700e
Antonio Maniero <maniero@klip.net>
2017-11-16 16:47:06 -0200

    https://pt.stackoverflow.com/q/255963/101

---
+++ C/Algorithm/HyperFactorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int hiperfatorial(int n) {
+    double ret = 1.0;
+    do ret *= pow(n, 2); while (n-- > 1);
+    return (int)ret;
+}
+
+int main(void) {
+	int n;
+	printf("\nDigite um numero: ");
+	scanf("%d", &n);
+	printf("\nO hiperfatorial desse numero eh: %d", hiperfatorial(n));
+}
+
+//https://pt.stackoverflow.com/q/255963/101
