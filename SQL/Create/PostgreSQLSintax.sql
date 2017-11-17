a85bbbaa88e27596dc92cead8ae40ac6ad25f8ca
Antonio Maniero <maniero@klip.net>
2017-11-17 08:31:31 -0200

    https://pt.stackoverflow.com/q/256066/101

---
+++ SQL/Create/PostgreSQLSintax.sql
@@ -0,0 +1,12 @@
+CREATE TABLE categoria (
+    codigo SERIAL,
+    nome VARCHAR(50) NOT NULL
+);
+
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
+
+--https://pt.stackoverflow.com/q/256066/101
