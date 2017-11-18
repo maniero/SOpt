834bd2e076137ee25df39dbdb32f3c886d443d79
Antonio Maniero <maniero@klip.net>
2017-11-18 11:40:03 -0200

    https://pt.stackoverflow.com/q/256361/101

---
+++ Java/Method/WordsToNumber.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Ideone {
+    public static long converte(String entrada) {
+	    switch (entrada) {
+	    case "um":
+	        return 1;
+	    case "dois":
+	        return 2;
+	    case "três":
+	        return 3;
+	    case "quatro":
+	        return 4;
+	    case "cinco":
+	        return 5;
+	    case "seis":
+	        return 6;
+	    case "sete":
+	        return 7;
+	    case "oito":
+	        return 8;
+	    case "nove":
+	        return 9;
+	    case "dez":
+	        return 10;
+	    default:
+	        return -1;
+	    }
+	}
+	public static void main(String[] args) {
+	    Scanner key = new Scanner(System.in);
+	    System.out.println("Inicialização...\nInstruções:\n1-Digite o nome de um número entre um~dez.\n2-O programa encerra ao digitar 'fim'.");
+	    while (true) {
+	        String entrada = key.nextLine();
+	        if (entrada.equals("fim")) {
+	            break;
+	        }
+	        long convertido = converte(entrada);
+	        if (convertido == -1) {
+	            System.out.println("Palavra inválida");
+	            continue;
+	        }
+            System.out.println(convertido);
+	    }
+	    System.out.println("Fim!");
+	    key.close();
+	}
+}
+
+//https://pt.stackoverflow.com/q/256361/101
