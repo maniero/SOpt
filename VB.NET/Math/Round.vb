d8436cba08877e4a65378663b4845bbdb2be095d
Antonio Maniero <maniero@klip.net>
2017-11-24 11:55:39 -0200

    https://pt.stackoverflow.com/q/255701/101

---
+++ VB.NET/Math/Round.vb
@@ -0,0 +1,10 @@
+Imports System.Console
+Imports System.Math
+
+Public Class Program
+	Public Shared Sub Main()
+		WriteLine(Round(2500D / 220, 2))
+	End Sub
+End Class
+
+//https://pt.stackoverflow.com/q/255701/101
