3496cb0bff868cb045359067b6cf3bcd0c16e23e
Antonio Maniero <maniero@klip.net>
2017-12-04 17:58:52 -0200

    https://pt.stackoverflow.com/q/260390/101

---
+++ CSharp/String/Escape.cs
@@ -0,0 +1,11 @@
+using static System.Console;
+
+public class Program {
+	public static void Main()
+	{
+		var texto = @"É necessário Cadastrar o(s) seguinte(s) Parâmetro(s):SENHA_FTP\r\n";
+		WriteLine(texto.Replace(@"\", @"\\"));
+	}
+}
+
+//https://pt.stackoverflow.com/q/260390/101
