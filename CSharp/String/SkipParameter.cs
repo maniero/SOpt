5c597bc7856abcab4849f12eec57ed1c6518ea25
Antonio Maniero <maniero@klip.net>
2017-12-07 09:17:19 -0200

    https://pt.stackoverflow.com/q/260892/101

---
+++ CSharp/String/SkipParameter.cs
@@ -0,0 +1,9 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		WriteLine("{0}, {2}, {3}", 1, null, "teste", 23.5);
+	}
+}
+
+//https://pt.stackoverflow.com/q/260892/101
