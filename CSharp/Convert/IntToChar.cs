1fb04b3c5257cbda32ee834619c0f08023efba91
Antonio Maniero <maniero@klip.net>
2017-12-11 10:47:04 -0200

    https://pt.stackoverflow.com/q/261784/101

---
+++ CSharp/Convert/IntToChar.cs
@@ -0,0 +1,11 @@
+using static System.Console;
+using static System.Convert;
+
+public class Program {
+	public static void Main() {
+		var x = 2;
+		WriteLine(ToChar(x + 48));
+	}
+}
+
+//https://pt.stackoverflow.com/q/261784/101
