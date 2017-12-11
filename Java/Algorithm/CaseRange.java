64ad7f5bfd64ecb14756a4fd6c0db59578082a9a
Antonio Maniero <maniero@klip.net>
2017-12-11 14:26:39 -0200

    https://pt.stackoverflow.com/q/258024/101

---
+++ Java/Algorithm/CaseRange.java
@@ -0,0 +1,21 @@
+class Ideone {
+	public static void main (String[] args)	{
+		int x = 534;
+		switch (x / 200) {
+			case 0:
+				System.out.println("entre 0 e 199");
+				break;
+			case 1:
+				System.out.println("entre 200 e 399");
+				break;
+			case 2:
+				System.out.println("entre 400 e 599");
+				break;
+			default:
+				System.out.println("600 ou mais");
+				break;
+		}
+	}
+}
+
+//https://pt.stackoverflow.com/q/258024/101
