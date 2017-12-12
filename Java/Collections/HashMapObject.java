b6088ef8a9165c7a375491d5bba083b6489f79eb
Antonio Maniero <maniero@klip.net>
2017-12-12 14:13:40 -0200

    https://pt.stackoverflow.com/q/257120/101

---
+++ Java/Collections/HashMapObject.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Program {
+	public static void main (String[] args) {
+		Map<String, Alimento> mapalimentos = new HashMap<>();
+	    mapalimentos.put("Maçã", new Alimento(50, 10));
+	    mapalimentos.put("Melancia", new Alimento(30, 20));
+	}
+}
+
+class Alimento {
+    public Alimento(int calorias, int proteinas) {
+        Calorias = calorias;
+        Proteinas = proteinas;
+    }
+    int Calorias;
+    int Proteinas;
+}
+
+//https://pt.stackoverflow.com/q/257120/101
