f2360268e3a4d6e44f972c29cdaaa45534d98331
Antonio Maniero <maniero@klip.net>
2017-12-12 13:39:20 -0200

    https://pt.stackoverflow.com/q/257173/101

---
+++ Python/ValidationSex.py
@@ -0,0 +1,9 @@
+def getSex():
+    while True:
+        sex = input('Digite seu sexo:')
+        if sex == 'M' or sex == 'F':
+            return sex
+
+print(getSex())
+
+#https://pt.stackoverflow.com/q/257173/101
