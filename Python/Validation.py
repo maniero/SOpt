d8bbda2ccc7978f54a31e9d04ed7fb7a3e7bf9f9
Antonio Maniero <maniero@klip.net>
2017-12-12 13:30:56 -0200

    https://pt.stackoverflow.com/q/257201/101

---
+++ Python/Validation.py
@@ -0,0 +1,14 @@
+print('''Choice your number
+[1] Banana
+[2] Apple ''')
+while True:
+    option = int(input('Your option: '))
+    if option == 1:
+        print('Banana choiced')
+        break
+    elif option == 2:
+        print('Apple choiced')
+        break
+    print('Try again')
+
+#https://pt.stackoverflow.com/q/257201/101
