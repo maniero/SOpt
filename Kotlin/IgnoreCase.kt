bdeaa8622fda8496a22f585fd4145a1f2ed997b4
Antonio Maniero <maniero@klip.net>
2017-12-13 12:52:32 -0200

    https://pt.stackoverflow.com/q/256697/101

---
+++ Kotlin/IgnoreCase.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    println("Texto Em Caixa".equals("texto em caixa", ignoreCase = true));
+    println("Texto Em Caixa".equals("texto em caixa", ignoreCase = false));
+}
+
+//https://pt.stackoverflow.com/q/256697/101
