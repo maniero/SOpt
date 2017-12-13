5fd529b2695aceb190922e7a953c87c2846ea2ed
Antonio Maniero <maniero@klip.net>
2017-12-13 13:45:15 -0200

    https://pt.stackoverflow.com/q/257817/101

---
+++ CSharp/Syntax/VariableEscopeTypes.cs
@@ -0,0 +1,17 @@
+class Exemplo {
+    private int valor;
+    public string nome;
+}
+
+class Exemplo {
+    public static int total;
+}
+
+class Exemplo {
+    public static int total;
+    private int valor;
+    public string nome;
+    public string Metodo(int parametro) => (total + valor + parametro).ToString() + nome;
+}
+
+//https://pt.stackoverflow.com/q/257817/101
