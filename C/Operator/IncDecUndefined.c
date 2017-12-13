86f47f397c17f88231fe8533db341f0f48ecac7f
Antonio Maniero <maniero@klip.net>
2017-12-13 11:01:10 -0200

    https://pt.stackoverflow.com/q/262396/101

---
+++ C/Operator/IncDecUndefined.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    int i = 5;
+    printf("%d", i++);
+    printf("%d", i--);
+    printf("%d", ++i);
+    printf("%d", --i);
+    printf("%d", i);
+}
+
+//https://pt.stackoverflow.com/q/262396/101
