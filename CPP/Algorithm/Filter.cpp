ec354283a7b4dc18a5ace29b566667bb46d48632
Antonio Maniero <maniero@klip.net>
2017-12-28 12:29:08 -0200

    https://pt.stackoverflow.com/q/37498/101

---
+++ CPP/Algorithm/Filter.cpp
@@ -0,0 +1,9 @@
+for (i=0; i<num; i++) {
+    if (v[i].nota >= 6) {
+        cout << "Mat.: " << v[i].matricula << endl;
+        cout << "Nome: " << v[i].nome << endl;
+        cout << "Nota: " << v[i].nota << endl;
+    } 
+}
+
+//https://pt.stackoverflow.com/q/37498/101
