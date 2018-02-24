1ad7c6b897b2381cd20bdcf9733bb8db2d034c4c
Antonio Maniero <maniero@klip.net>
2018-02-24 12:58:44 -0300

    https://pt.stackoverflow.com/q/278619/101

--- CSharp/String/SplitCamelCase.cs
+++ CSharp/String/SplitCamelCase.cs
@@ -4,6 +4,9 @@ using System.Text;
 public static class Program {
 	public static void Main() {
 		WriteLine("CamelCase".SplitCamelCase());
+		WriteLine("Camel-Case".SplitCamelCase());
+		WriteLine("Camel---Case".SplitCamelCase());
+		WriteLine("Camel.Case".SplitCamelCase());
 		WriteLine("Ca".SplitCamelCase());
 		WriteLine("aC".SplitCamelCase());
 		WriteLine("CC".SplitCamelCase());
@@ -11,12 +14,23 @@ public static class Program {
 		WriteLine("CamelCaseC".SplitCamelCase());
 		WriteLine("".SplitCamelCase());
 		WriteLine("pascalCase".SplitCamelCase());
+		WriteLine("CamelCase".SplitCamelCase('-', ""));
+		WriteLine("Camel-Case".SplitCamelCase('-', ""));
+		WriteLine("Camel---Case".SplitCamelCase('-', ""));
+		WriteLine("Camel.Case".SplitCamelCase('-', ""));
+		WriteLine("Ca".SplitCamelCase('-', ""));
+		WriteLine("aC".SplitCamelCase('-', ""));
+		WriteLine("CC".SplitCamelCase('-', ""));
+		WriteLine("C".SplitCamelCase('-', ""));
+		WriteLine("CamelCaseC".SplitCamelCase('-', ""));
+		WriteLine("".SplitCamelCase('-', ""));
+		WriteLine("pascalCase".SplitCamelCase('-', ""));
 	}
-	public static string SplitCamelCase(this string text, char separator = '-') {
+	public static string SplitCamelCase(this string text, char separator = '-', string separators = "-=_+!@#$%&*()'^~[]{}/?;:.,<>|\\\"") {
 		if (string.IsNullOrEmpty(text) || text.Length < 2) return text;
 		var sb = new StringBuilder(text.Length + text.Length / 3);
 		for (var i = 0; i < text.Length; i++) {
-			if (char.IsUpper(text[i]) && i > 0) sb.Append(separator);
+			if (char.IsUpper(text[i]) && i > 0 && !separators.Contains(text[i - 1].ToString())) sb.Append(separator);
 			sb.Append(text[i]);
 			
 		}
