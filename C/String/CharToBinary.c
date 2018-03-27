248e675de5ee28096436fad18889a763a672bf4f
Antonio Maniero <maniero@klip.net>
2018-03-26 22:54:55 -0300

    https://pt.stackoverflow.com/q/286742/101

--- C/String/CharToBinary.c
+++ C/String/CharToBinary.c
@@ -3,13 +3,17 @@
 
 void BinFormat(char character, char *text) {
 	text[8] = '\0';
-    for (int i = 7; i >= 0; i--) text[i] = ((character >> (i - 1)) & 1) + '0';
+    for (int i = 7; i >= 0; i--) text[i] = ((character >> i) & 1) + '0';
 }
 
 int main(void) {
 	char *text = malloc(9);
-	BinFormat('A', text);
-	printf("%s", text);
+    BinFormat('A', text);
+    printf("%s\n", text);
+    BinFormat('B', text);
+    printf("%s\n", text);
+    BinFormat('C', text);
+    printf("%s\n", text);
 }
 
 //https://pt.stackoverflow.com/q/286742/101
