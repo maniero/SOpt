34c22f3907a9724fb2a88e18c63d7973e2acb134
Antonio Maniero <maniero@klip.net>
2018-04-19 09:24:26 -0300

    https://pt.stackoverflow.com/q/291742/101

--- CSharp/String/RemoveDuplicateinSequence.cs
+++ CSharp/String/RemoveDuplicateinSequence.cs
@@ -1,12 +1,12 @@
 using static System.Console;
 
 public class Program {
-    public static void Main() {
-        var texto = "Abcc111de12234";
-        var lista = new char[texto.Length];
-        for (int i = 0, j = 0; i < texto.Length; i++) if (!char.IsDigit(texto[i]) || (i != texto.Length - 1 && texto[i] != texto[i + 1])) lista[j++] = texto[i];
-        WriteLine(new string(lista));
-    }
+	public static void Main() {
+		var texto = "Abcc111de12234";
+		var lista = new char[texto.Length];
+		for (int i = 0, j = 0; i < texto.Length; i++) if (!char.IsDigit(texto[i]) || (i == 0 || texto[i] != texto[i - 1])) lista[j++] = texto[i];
+		WriteLine(new string(lista));
+	}
 }
 
 //https://pt.stackoverflow.com/q/291742/101
