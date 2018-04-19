30476cd61b2e365a87e8b41e1e09d8ebd80194cd
Antonio Maniero <maniero@klip.net>
2018-04-19 17:11:29 -0300

    https://pt.stackoverflow.com/q/292702/101

---
+++ Kotlin/ArrayInit.kt
@@ -0,0 +1,10 @@
+import java.util.*
+ 
+fun main(args: Array<String>) {
+    val numeros: IntArray = intArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+	val numeros2 = Array(10, { i -> i })
+    println(Arrays.toString(numeros))
+    println(Arrays.toString(numeros2))
+}
+
+//https://pt.stackoverflow.com/q/292702/101
