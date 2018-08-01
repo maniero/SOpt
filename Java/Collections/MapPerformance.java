6dd39e9ec4fe04c5227c3da7a2d18d4b3f63aeac
Antonio Maniero <maniero@klip.net>
2018-08-01 12:29:49 -0300

    https://pt.stackoverflow.com/q/319076/101

---
+++ Java/Collections/MapPerformance.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Ideone {
+	public static HashMap<String, String> lista = new HashMap<>();
+	
+	private static void Put(String teste, String outra) {
+	    for (int i = 0; i < 100000; i++) if (lista.containsKey("antonio")) lista.put(teste, outra);
+	}
+	
+	private static void Replace(String teste, String outra) {
+	    for (int i = 0; i < 100000; i++) lista.replace(teste, outra);
+	}
+	
+	public static void main(String[] args) {
+	    long inicio = System.currentTimeMillis();
+	    Put("antonio", "antonio");
+	    System.out.println("Put: " + (System.currentTimeMillis() - inicio)  + " ms");
+	    inicio = System.currentTimeMillis();
+	    Replace("antonio", "antonio");
+	    System.out.println("Replace: " + (System.currentTimeMillis() - inicio) + " ms");
+	}
+}
+
+//https://pt.stackoverflow.com/q/319076/101
