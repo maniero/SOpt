849c4a57d9a4c7f49be4a271883430c629571c97
Antonio Maniero <maniero@klip.net>
2018-08-01 14:30:42 -0300

    https://pt.stackoverflow.com/q/318767/101

---
+++ CPP/Console/Upeercase.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n;  
+    cin >> n; 
+    cout << uppercase << hex << n << endl; 
+}
+
+//https://pt.stackoverflow.com/q/318767/101
