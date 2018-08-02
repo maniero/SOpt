c002e84779abc301725bcd7592c6ae770c475fff
Antonio Maniero <maniero@klip.net>
2018-08-02 08:25:52 -0300

    https://pt.stackoverflow.com/q/319066/101

--- CSharp/Linq/CountBooleansFileds.cs
+++ CSharp/Linq/CountBooleansFileds.cs
@@ -4,12 +4,11 @@ using System.Linq;
 
 public class Program {
 	public static void Main(string[] args) {
-		var lista = new List<Dados>() { new Dados { Campo1 = true, Campo2 = false, Campo3 = false, Campo4 = true },
+		var lista = new List<Dados>() { new Dados { Campo1 = true, Campo2 = false, Campo3 = true, Campo4 = true },
 				new Dados {	Campo1 = false,	Campo2 = false,	Campo3 = true, Campo4 = false }	};
-		WriteLine(lista.Select(x => (x.Campo1 ? 1 : 0) + (x.Campo2 ? 1 : 0) + (x.Campo3 ? 1 : 0) + (x.Campo4 ? 1 : 0) + (x.Campo4 ? 1 : 0)).ToList()[0]);
-		WriteLine(lista.Sum(x => (x.Campo1 ? 1 : 0) + (x.Campo2 ? 1 : 0) + (x.Campo3 ? 1 : 0) + (x.Campo4 ? 1 : 0) + (x.Campo4 ? 1 : 0)));
+		WriteLine(lista.Select(x => (x.Campo1 ? 1 : 0) + (x.Campo2 ? 1 : 0) + (x.Campo3 ? 1 : 0) + (x.Campo4 ? 1 : 0)).ToList()[0]);
+		WriteLine(lista.Sum(x => (x.Campo1 ? 1 : 0) + (x.Campo2 ? 1 : 0) + (x.Campo3 ? 1 : 0) + (x.Campo4 ? 1 : 0)));
 	}
-
 }
 
 public class Dados {
