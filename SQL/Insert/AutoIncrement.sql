64073d845b48166f6cddd649b3a136f41666460e
Antonio Maniero <maniero@klip.net>
2018-08-02 09:19:46 -0300

    https://pt.stackoverflow.com/q/318632/101

---
+++ SQL/Insert/AutoIncrement.sql
@@ -0,0 +1,9 @@
+create table teste(
+id int auto_increment,
+nome varchar(100) not null,
+valor decimal(5,2),
+constraint id_pk primary key (id));
+
+insert into teste values (null, 'Danilo',333.33)
+
+//https://pt.stackoverflow.com/q/318632/101
