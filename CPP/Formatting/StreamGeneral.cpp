8ac9a3a26b3c3783d1ec1cf9dfc4ab35bf33f078
Antonio Maniero <maniero@klip.net>
2018-08-02 13:39:38 -0300

    https://pt.stackoverflow.com/q/319570/101

---
+++ CPP/Formatting/StreamGeneral.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	auto x = 65;
+    cout << "i make " << x << " program\n";
+    cout << "i make " << hex << x << " program " << endl;
+    cout << "i make " << (char)x << " program " << endl;
+}
+
+//https://pt.stackoverflow.com/q/319570/101
