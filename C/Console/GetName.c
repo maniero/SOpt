701af184d00758b1961aeaa145f03a19e9709336
Antonio Maniero <maniero@klip.net>
2018-08-05 21:07:48 -0300

    https://pt.stackoverflow.com/q/320255/101

---
+++ C/Console/GetName.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int idade = 0;
+	printf("Hello! How old are you? ");
+	scanf("%d\n", &idade);
+	while (idade == 0) {
+	    printf("\nAge cannot be 0 nor be decimal. Re-enter your age: ");
+	    scanf("%d", &idade);
+	}
+	char nome[21];
+	fgets(nome, 20, stdin);
+	nome[strcspn(nome, "\n")] = 0;
+	printf("\nHello %s, aged %d!\n", nome, idade);
+}
+
+//https://pt.stackoverflow.com/q/320255/101
