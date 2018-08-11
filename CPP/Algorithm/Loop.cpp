c31d922fa92ed7292be10d0a0f36cc7ca21b0e07
Antonio Maniero <maniero@klip.net>
2018-08-11 17:30:38 -0300

    https://pt.stackoverflow.com/q/321702/101

---
+++ CPP/Algorithm/Loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        string nome;
+        cin >> nome;
+        cout << nome << endl;
+    }
+}
+
+//https://pt.stackoverflow.com/q/321702/101
