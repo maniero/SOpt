2612ea0755cb3757fd6f5f1ddc0a7020d4914e07
Antonio Maniero <maniero@klip.net>
2018-08-17 18:17:27 -0300

    https://pt.stackoverflow.com/q/323222/101

---
+++ Java/String/SequenceSubstring.java
@@ -0,0 +1,10 @@
+class Substring {
+    public static void todasSubstrings(String str) {
+        for (int j = str.length(); j >= 0; j--) for (int i = 0; i < j; i++) System.out.println(str.substring(i, j));
+    }
+    public static void main(String[] args) {
+        sb.todasSubstrings("UTFPR");
+    }
+}
+
+//https://pt.stackoverflow.com/q/323222/101
