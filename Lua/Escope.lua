6b16a5319a0332da0be77d2ec5082440d6db97a6
Antonio Maniero <maniero@klip.net>
2018-08-28 10:48:37 -0300

    https://pt.stackoverflow.com/q/325490/101

---
+++ Lua/Escope.lua
@@ -0,0 +1,14 @@
+local math = require("math")
+local a = 2
+local b = 3
+local c = 4
+local x1
+do
+   local a2 = 2 * a
+   local d = math.sqrt(b ^ 2 - 4 * a * c)
+   x1 = (-b + d) / a2
+   x2 = (-b - d) / a2
+end
+print(x1, x2)
+
+--https://pt.stackoverflow.com/q/325490/101
