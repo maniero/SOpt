3f28aa895ece7b42ae3b90dd17401609ad6655af
Antonio Maniero <maniero@klip.net>
2018-09-06 11:48:40 -0300

    https://pt.stackoverflow.com/q/325788/101

---
+++ Java/Algorithm/Looping.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Ideone {
+	public static void main(String[] args) {
+	    Scanner x = new Scanner(System.in);
+	    System.out.println("inisira a quatidade de bimestres");
+	    int bim = x.nextInt();
+	    double soma= 0;
+	    for (int i = 0; i < bim; i++) {
+	        System.out.println("Insira a nota do " + (i + 1) + "º Bimestre");
+	        soma += x.nextDouble();
+	    }
+	    System.out.println(soma / bim);
+	}
+}
+
+//https://pt.stackoverflow.com/q/325788/101
