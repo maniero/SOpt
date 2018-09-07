24069986a55aed1daca83e6bce55e5b8e70425bb
Antonio Maniero <maniero@klip.net>
2018-09-07 16:28:03 -0300

    https://pt.stackoverflow.com/q/328113/101

---
+++ C/Typing/ScanfFormat2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float x;
+    float y;
+    printf ("Entre o numerador 'x' :\n");
+    scanf ("%f", &x);
+    printf ("Entre o denominador 'y' :\n");
+    scanf ("%f", &y);
+    printf("O resultado da divisao entre x e y, em formato ponto flutuante e : %f\n\n", x / y);
+}
+
+//https://pt.stackoverflow.com/q/328113/101
