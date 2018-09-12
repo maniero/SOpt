aa90af188a23c9eac4c2e5897cad257c204ffda6
Antonio Maniero <maniero@klip.net>
2018-09-12 10:08:05 -0300

    https://pt.stackoverflow.com/q/318525/101

---
+++ CSharp/Struct/RefExample.cs
@@ -0,0 +1,15 @@
+
+public readonly ref struct RefRoPoint4D {
+    public readonly double X;
+    public readonly double Y;
+    public readonly double Z;
+    public readonly double W;
+    public RefRoPoint4D(double x, double y, double z, double w) {
+        X = x;
+        Y = y;
+        Z = z;
+        W = w;
+    }
+}
+
+//https://pt.stackoverflow.com/q/318525/101
