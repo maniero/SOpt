594c48ab7a4ea57ac0b10203b282a95d5ca9f61a
Antonio Maniero <maniero@klip.net>
2018-09-13 11:50:16 -0300

    https://pt.stackoverflow.com/q/329320/101

---
+++ Python/Semicolon.py
@@ -0,0 +1,12 @@
+x = 5; print(x);
+if x > 0: print("maior que zero"); print(x);
+
+#Mesmo que
+
+x = 5
+print(x)
+if x > 0:
+	print("maior que zero")
+	print(x)
+
+#https://pt.stackoverflow.com/q/329320/101
