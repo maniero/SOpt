6bdccc3d4e99c544a29f0ef83e4c733328b6430e
Antonio Maniero <maniero@klip.net>
2018-09-14 11:29:23 -0300

    https://pt.stackoverflow.com/q/39202/101

---
+++ PHP/Switch2If.php
@@ -0,0 +1,9 @@
+<?php
+$num = 0;
+if ($num > 100) echo 'valor muito alto';
+else if ($num < 80 && $num > 51) echo 'valor medio';
+else if ($num == 50) echo 'valor perfeito';
+else if ($num <= 10) echo 'valor muito baixo';
+else if ($num == 0) echo'sem valor';
+
+//https://pt.stackoverflow.com/q/39202/101
