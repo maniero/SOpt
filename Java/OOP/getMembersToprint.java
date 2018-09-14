66bc21f74ee538852970e599ec716be0c173d6dc
Antonio Maniero <maniero@klip.net>
2018-09-14 12:46:09 -0300

    https://pt.stackoverflow.com/q/329560/101

---
+++ Java/OOP/getMembersToprint.java
@@ -0,0 +1,9 @@
+for (Produto produto : ListaDeProdutos ) {
+    if (produto.nome.contains("Banda")) {
+        System.out.println(produto.nome);
+        System.out.println(produto.preco);
+        System.out.println(produto.genero);
+    }
+}
+
+//https://pt.stackoverflow.com/q/329560/101
