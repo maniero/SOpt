417287c1d4439b6850da98a30335db0f9bb2707c
Antonio Maniero <maniero@klip.net>
2018-09-19 09:37:30 -0300

    https://pt.stackoverflow.com/q/40271/101

---
+++ CSharp/Class/Template.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$
+namespace $rootnamespace$
+{
+    public class $safeitemrootname$
+    {
+    }
+}
+
+//https://pt.stackoverflow.com/q/40271/101
