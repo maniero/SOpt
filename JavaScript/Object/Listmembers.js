ddbabfc962ceaed5b594425dcda0b12b5862c104
Antonio Maniero <maniero@klip.net>
2018-09-20 12:16:21 -0300

    https://pt.stackoverflow.com/q/40682/101

---
+++ JavaScript/Object/Listmembers.js
@@ -0,0 +1,5 @@
+var meuObjeto = { a: 1, "b": 2 };
+for (var chave in meuObjeto) console.log("key " + chave + "; value " + meuObjeto[chave]);
+console.log("tipo: " + typeof(meuObjeto));
+
+//https://pt.stackoverflow.com/q/40682/101
