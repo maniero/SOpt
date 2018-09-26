b9527b5d77a1c497dc3fcb8f9e7bdf9fde4f832b
Antonio Maniero <maniero@klip.net>
2018-09-26 10:58:13 -0300

    https://pt.stackoverflow.com/q/41499/101

---
+++ JavaScript/Syntax/Ifs.js
@@ -0,0 +1,15 @@
+var bola = "redonda";
+if (bola == "comestivel") return comer();
+if (bola == "liquida") return beber();
+if (bola == "redonda") chutar();
+
+//e
+
+var bola = "redonda";
+if (bola == "comestivel") return comer();
+else if (bola == "liquida") return beber();
+else if (bola == "redonda") chutar();
+
+//são equivalentes.
+
+//https://pt.stackoverflow.com/q/41499/101
