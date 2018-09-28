f106c5660030a2753c02c390538d8bb6834c97da
Antonio Maniero <maniero@klip.net>
2018-09-27 22:06:06 -0300

    https://pt.stackoverflow.com/q/332795/101

---
+++ Python/LoopDataEntry.py
@@ -0,0 +1,8 @@
+while(True):
+    if int(input("Digite um numero: ")) <= 10:
+        print("Ok, numero registrado")
+        break
+    else:
+       print("Numero invalido, digite outro numero menor que 10")
+ 
+ #https://pt.stackoverflow.com/q/332795/101
