d00e31516db7d8b934387ddc13cfb6b30bcd3f51
Antonio Maniero <maniero@klip.net>
2018-10-05 10:05:23 -0300

    https://pt.stackoverflow.com/q/41832/101

---
+++ C/Allocation/MemberStruct.c
@@ -0,0 +1,6 @@
+A[i].loc = malloc(sizeof(Local));
+scanf("%[^\n]",A[i].loc->ender); //agora o loc aponta para algum lugar preparado p/ receber o ender
+printf("Digite a sala: ");
+scanf("%d",&A[i].loc->sala);
+
+//https://pt.stackoverflow.com/q/41832/101
