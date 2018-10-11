10c927a39149e48010e9ff8b2b3d929452e1aab9
Antonio Maniero <maniero@klip.net>
2018-10-11 08:49:21 -0300

    https://pt.stackoverflow.com/q/42711/101

---
+++ PHP/String/Plural.php
@@ -0,0 +1,18 @@
+<?php
+
+function pluralization( $numItems = 0, $texts) {
+	if ($texts == NULL) {
+        $texts = array("(nenhum)", "(1 item)", "($numItems itens)");
+	}
+    return $texts[min($numItems, 2)];
+}
+
+echo pluralization(0);
+echo pluralization(0);
+echo pluralization(1);
+echo pluralization(2);
+echo pluralization(3);
+$elementos = 4;
+echo pluralization($elementos, ["nada", "um elemento", "$elementos elementos"]);
+
+//https://pt.stackoverflow.com/q/42711/101
