d42349e109492ec899b0dd17a6bcc26eac639865
Antonio Maniero <maniero@klip.net>
2018-10-11 07:45:59 -0300

    https://pt.stackoverflow.com/q/41944/101

---
+++ CSharp/Method/ExtensionExample.cs
@@ -0,0 +1,18 @@
+using System;
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		WriteLine("HelloWorld".ToCamelCase());
+	}
+}
+
+namespace System {
+    public static class StringExt {
+        public static string ToCamelCase(this string text) {
+            return char.ToLower(text[0]) + text.Substring(1);
+        }
+    }
+}
+
+//https://pt.stackoverflow.com/q/41944/101
