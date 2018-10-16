5ca9be7926a3150a18c0c924347de527ad7a610c
Antonio Maniero <maniero@klip.net>
2018-10-16 06:56:01 -0300

    https://pt.stackoverflow.com/q/336030/101

---
+++ Python/PrintSyntax.py
@@ -0,0 +1,11 @@
+matricula = int (input("Digite a matrícula do Aluno: "))
+nota1  = float (input("Digite a primeira nota do aluno: "))
+nota2  = float (input("Digite a primeira nota do aluno: "))
+nota3  = float (input("Digite a primeira nota do aluno: "))
+media = ( nota1 +  (nota2 * 2) + (nota3 * 3)) / 6
+if media > 6:
+    print(" O Aluno  de matricula ", matricula, " Foi APROVADO com a média: ", media)
+else:
+    print(" O Aluno  de matricula ", matricula, " Foi REPROVADO com a média: ", media)
+    
+#https://pt.stackoverflow.com/q/336030/101
