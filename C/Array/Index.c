bf4e3e19e9fd3c3928c1d043d4ed7bf7e043bf06
Antonio Maniero <maniero@klip.net>
2018-10-16 09:00:25 -0300

    https://pt.stackoverflow.com/q/336064/101

---
+++ C/Array/Index.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+    int numero[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    printf("\nA distribuicao desses numeros em ordem inversa : ");
+    for (int i = 7; i >= 0; i--) printf("\n--------\n%d", numero[i]);
+    }
+}
+
+//https://pt.stackoverflow.com/q/336064/101
