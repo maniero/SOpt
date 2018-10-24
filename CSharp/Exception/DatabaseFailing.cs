856e722394f44be402f4d574c53d28f3d32585d8
Antonio Maniero <maniero@klip.net>
2018-10-24 10:27:31 -0300

    https://pt.stackoverflow.com/q/337830/101

--- CSharp/Exception/DatabaseFailing.cs
+++ CSharp/Exception/DatabaseFailing.cs
@@ -1,11 +1,8 @@
 public String IdentificarAcessoDAL(FuncionarioDTO acesso) {
-    try {
-        conexao.cmd.Connection = conexao.conexao;
-        conexao.cmd.CommandText = "SELECT CARGO.NOME_CARGO FROM LOGIN INNER JOIN FUNCIONARIO ON FUNCIONARIO.CPF_FUNCIONARIO = LOGIN.CPF_FUNCIONARIO INNER JOIN CARGO ON CARGO.ID_CARGO = FUNCIONARIO.ID_CARGO WHERE LOGIN.USUARIO_LOGIN = '" + acesso.User + "' AND LOGIN.SENHA_LOGIN = '" + acesso.Password + "'";
-        conexao.conexao.Open();
-        return funcionarioDTO.Funcao = conexao.cmd.ExecuteScalar().ToString();
-    } finally {
-        conexao.conexao.Close();
+    using (var connection = new SqlConnection(connectionString)) {
+        var command = new SqlCommand("SELECT CARGO.NOME_CARGO FROM LOGIN INNER JOIN FUNCIONARIO ON FUNCIONARIO.CPF_FUNCIONARIO = LOGIN.CPF_FUNCIONARIO INNER JOIN CARGO ON CARGO.ID_CARGO = FUNCIONARIO.ID_CARGO WHERE LOGIN.USUARIO_LOGIN = '" + acesso.User + "' AND LOGIN.SENHA_LOGIN = '" + acesso.Password + "'", connection);
+        command.Connection.Open();
+        command.ExecuteNonQuery();
     }
 }
 
