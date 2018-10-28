add813bd4c8f873bfc513c0a1cb6c1b5e81c0d41
Antonio Maniero <maniero@klip.net>
2018-10-28 07:58:18 -0300

    https://pt.stackoverflow.com/q/339502/101

--- Python/Introspection.py
+++ Python/Introspection.py
@@ -7,6 +7,10 @@ print(C.__qualname__)
 print(C.f.__qualname__)
 print(C.D.__qualname__)
 print(C.D.g.__qualname__)
+print(C.__name__)
+print(C.f.__name__)
+print(C.D.__name__)
+print(C.D.g.__name__)
 
 def f():
    def g(): pass
@@ -14,5 +18,7 @@ def f():
 
 print(f.__qualname__)
 print(f().__qualname__)
+print(f.__name__)
+print(f().__name__)
 
 #https://pt.stackoverflow.com/q/339502/101
