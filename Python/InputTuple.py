89c0f3f00a1d3a7d6667aa50916d8396b7632a75
Antonio Maniero <maniero@klip.net>
2018-10-28 08:12:37 -0300

    https://pt.stackoverflow.com/q/339514/101

---
+++ Python/InputTuple.py
@@ -0,0 +1,7 @@
+def funcao():
+    return int(input('insira um número:'))
+tupla = (funcao(), funcao(), funcao(), funcao())
+print(tupla.count(9))
+print(tupla)
+
+#https://pt.stackoverflow.com/q/339514/101
