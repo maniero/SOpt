c29b885e5f745f4f69052b35b9266004810044ad
Antonio Maniero <maniero@klip.net>
2018-11-07 09:49:34 -0200

    https://pt.stackoverflow.com/q/341156/101

---
+++ Python/DictKeyValue.py
@@ -0,0 +1,5 @@
+lanchonete = {"Salgado" : 4.5, "Lanche" : 16.5, "Suco" : 3, "Refrigerante" : 3.5, "Doce" : 1}
+for item in lanchonete:
+    print("{0:20} {1:6.2f}".format(item, lanchonete[item]))
+    
+#https://pt.stackoverflow.com/q/341156/101
