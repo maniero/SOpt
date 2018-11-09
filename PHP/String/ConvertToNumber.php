a703dbc9be692e51421954287086efa71fda845c
Antonio Maniero <maniero@klip.net>
2018-11-09 15:03:47 -0200

    https://pt.stackoverflow.com/q/342357/101

--- PHP/String/ConvertToNumber.php
+++ PHP/String/ConvertToNumber.php
@@ -2,8 +2,14 @@
 $numero = "5";
 $decimal = "30";
 echo (int)$numero + ((int)$decimal / 100.0);
+echo "\n";
+if (is_numeric($numero) && is_numeric($decimal)) echo (int)$numero + ((int)$decimal / 100.0);
+else echo "melhor não calcular, tem erro aí";
 $decimal = "a30";
 echo "\n";
 echo (int)$numero + ((int)$decimal / 100.0);
+echo "\n";
+if (is_numeric($numero) && is_numeric($decimal)) (int)$numero + ((int)$decimal / 100.0);
+else echo "melhor não calcular, tem erro aí";
 
 //https://pt.stackoverflow.com/q/342357/101
