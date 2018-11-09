b6316c7b1becdb670002e131d6e3de9c30892c5a
Antonio Maniero <maniero@klip.net>
2018-11-09 13:01:15 -0200

    https://pt.stackoverflow.com/q/341971/101

---
+++ CSharp/Typing/CastDouble.cs
@@ -0,0 +1,12 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		int valorUni = 8;
+		int valorTotal = 116;
+		double result = ((double)valorUni / valorTotal) * 100;
+		WriteLine(result);
+	}
+}
+
+//https://pt.stackoverflow.com/q/341971/101
