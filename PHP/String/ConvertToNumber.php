ef17f605f3866fc49f885bd6e67f896be30f9113
Antonio Maniero <maniero@klip.net>
2018-11-09 14:47:33 -0200

    https://pt.stackoverflow.com/q/342357/101

---
+++ PHP/String/ConvertToNumber.php
@@ -0,0 +1,9 @@
+<?php
+$numero = "5";
+$decimal = "30";
+echo (int)$numero + ((int)$decimal / 100.0);
+$decimal = "a30";
+echo "\n";
+echo (int)$numero + ((int)$decimal / 100.0);
+
+//https://pt.stackoverflow.com/q/342357/101
