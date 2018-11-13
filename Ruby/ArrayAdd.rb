5b787320e2355963b77759cf6952750540d440a3
Antonio Maniero <maniero@klip.net>
2018-11-13 08:19:55 -0200

    https://pt.stackoverflow.com/q/342688/101

---
+++ Ruby/ArrayAdd.rb
@@ -0,0 +1,6 @@
+puts "Alunos\n\n"
+alunos = [["Daniel: ", "Nota: 10\n\n"],["Abima: ", "Nota: 10\n\n"], ["Wilame: ", "Nota: 10\n\n"],["Felipe: ","Nota: 10\n\n"]]
+alunos.push(["João: ", "Nota: 8\n\n"])
+puts alunos
+
+#https://pt.stackoverflow.com/q/342688/101
