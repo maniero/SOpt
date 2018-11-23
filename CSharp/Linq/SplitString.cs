a7f9289cd026d71de1091e2ebe604f04b2013149
Antonio Maniero <maniero@klip.net>
2018-11-23 19:18:08 -0200

    https://pt.stackoverflow.com/q/345563/101

--- CSharp/Linq/SplitString.cs
+++ CSharp/Linq/SplitString.cs
@@ -7,9 +7,11 @@ public class Program {
 		var lista = new List<string> { "1,Joao",
 			"2,Maria",
 			"3,José1" };
-		var item = lista.Select(x => x.Split(',')).SingleOrDefault(a => a[1].Contains("1"));
-		WriteLine(item[1]);
+		var nome = lista.Select(x => x.Split(',')).SingleOrDefault(a => a[1].Contains("1"));
+		var id = lista.Select(x => x.Split(',')).SingleOrDefault(a => a[0].Contains("1"));
+		WriteLine(nome[1]);
+		WriteLine(id[1]);
 	}
 }
 
-//
+//https://pt.stackoverflow.com/q/345563/101
