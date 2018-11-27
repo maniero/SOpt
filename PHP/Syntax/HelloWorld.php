b8ca106c34d0cd87ba6b88d3d014ee5f71443aca
Antonio Maniero <maniero@klip.net>
2018-11-27 08:48:51 -0200

    https://pt.stackoverflow.com/q/345436/101

---
+++ PHP/Syntax/HelloWorld.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <p><?php echo "1 + 1 é " . (1 + 1); ?></p>
+    </body>
+</html>
+
+//https://pt.stackoverflow.com/q/345436/101
