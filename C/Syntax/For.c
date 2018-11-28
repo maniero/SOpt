8ce0f26017251877e2dbf118766009d5c01d4992
Antonio Maniero <maniero@klip.net>
2018-11-28 11:26:00 -0200

    https://pt.stackoverflow.com/q/43142/101

---
+++ C/Syntax/For.c
@@ -0,0 +1,5 @@
+void funcao(char cidadeXY[][150], int coordenadasX[], int coordenadasY[]) {
+    for (int i = 0; i < 30; i++) printf("%s\n", cidadeXY[i]);
+}
+
+//https://pt.stackoverflow.com/q/43142/101
