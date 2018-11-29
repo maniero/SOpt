c0b63554bed7eff493a728e7757651e83683ea5c
Antonio Maniero <maniero@klip.net>
2018-11-29 19:34:30 -0200

    https://pt.stackoverflow.com/q/346896/101

--- Python/MaxMinListString.py
+++ Python/MaxMinListString.py
@@ -1,9 +1,12 @@
 a = 'joao 1 0 10 2 3'
 b = a.split()
-b = list(map(int, b))
-c = max(b)
-print(c)
-c = min(b)
-print(c)
+c = []
+for i in b:
+    try:
+        c.append(int(i))
+    except ValueError:
+        pass
+print(max(c))
+print(min(c))
 
 #https://pt.stackoverflow.com/q/346896/101
