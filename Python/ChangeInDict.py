c403769a89d1b0e25d5185736f93e73e19efe79c
Antonio Maniero <maniero@klip.net>
2018-11-30 08:41:50 -0200

    https://pt.stackoverflow.com/q/346650/101

---
+++ Python/ChangeInDict.py
@@ -0,0 +1,11 @@
+dic = {}
+n = int(input()) 
+for i in range(n):
+    palavra, adjetivo = input().split()
+    dic[palavra] = adjetivo
+frase = input().split()
+for palavra in frase:
+    if palavra in dic:
+        print(dic[i])
+
+#https://pt.stackoverflow.com/q/346650/101
