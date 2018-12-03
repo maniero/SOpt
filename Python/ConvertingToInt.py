7890bbe59f5eda6e3761847ec861f3dc52a4e842
Antonio Maniero <maniero@klip.net>
2018-12-03 09:28:47 -0200

    https://pt.stackoverflow.com/q/347387/101

---
+++ Python/ConvertingToInt.py
@@ -0,0 +1,9 @@
+print(int('12\n'))
+print(int('\n123'))
+print(int('1234 '))
+print(int(' 1235'))
+print(int('1236c'))
+print(int('a1237'))
+print(int('123 8'))
+
+#https://pt.stackoverflow.com/q/347387/101
