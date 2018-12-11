54c630d39e598aec6a8f485cb2b7b8097fa5a854
Antonio Maniero <maniero@klip.net>
2018-12-11 12:53:05 -0200

    https://pt.stackoverflow.com/q/349349/101

---
+++ PHP/Engine/Echo.php
@@ -0,0 +1,13 @@
+<?php 
+$a = 10;
+$b = 6;
+echo ++$a; // pre incremento
+echo "<br>";
+echo $b++;
+echo "<br>";
+echo$b; // pós incremento
+echo"<br>";
+echo--$a;// pré decremento
+
+
+//https://pt.stackoverflow.com/q/349349/101
