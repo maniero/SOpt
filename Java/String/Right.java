bbace6db6f707e2ee30ca18c629bbba372c5e777
Antonio Maniero <maniero@klip.net>
2018-12-17 12:52:08 -0200

    https://pt.stackoverflow.com/q/349464/101

---
+++ Java/String/Right.java
@@ -0,0 +1,12 @@
+class Program {
+	public static void main (String[] args) {
+		System.out.println(Right("OlaMundo", 3));
+	}
+
+	public static String Right(String text, int length) {
+		if (text.length() < length) return null;
+		return text.substring(text.length() - length);
+	}
+}
+
+//https://pt.stackoverflow.com/q/349464/101
