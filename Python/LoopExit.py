8e81b7b6c0cad29a10c6fab41958817b50e707e2
Antonio Maniero <maniero@klip.net>
2018-12-18 09:54:10 -0200

    https://pt.stackoverflow.com/q/350241/101

---
+++ Python/LoopExit.py
@@ -0,0 +1,11 @@
+cont = 0
+n = 0
+total = 0
+while True:
+    n = int(input("Digite 999 para parar"))
+    if n == 999: break
+    cont += 1
+    total += n
+print(total)
+
+#https://pt.stackoverflow.com/q/350241/101
