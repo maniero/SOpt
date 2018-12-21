a727247e1e172a7d94f40994a24ae2a74a3442a5
Antonio Maniero <maniero@klip.net>
2018-12-21 14:05:33 -0200

    https://pt.stackoverflow.com/q/44389/101

---
+++ CPP/Typing/StructBasic.cpp
@@ -0,0 +1,11 @@
+//declara a composição da estrutura para ser usada depois
+struct estrutura {
+    int x;
+    int y;
+}
+//cria a variável exemplo do tipo estrutura e guarda os valores nos seus elementos
+estrutura exemplo = { 1, 2 };
+//acessar
+cout << exemplo.x; //imprime 1
+
+//https://pt.stackoverflow.com/q/44389/101
