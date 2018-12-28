f1858d6326faa87fb648edccea4c00ec863cc6c3
Antonio Maniero <maniero@klip.net>
2018-12-28 10:17:12 -0200

    https://pt.stackoverflow.com/q/353393/101

---
+++ Python/Age.py
@@ -0,0 +1,7 @@
+idade = int(input("Qual a sua idade? "))
+if idade >= 45:
+    print("Tá velho cara, já era!")
+else:
+    print("A vida está só começando rapaz!")
+    
+#https://pt.stackoverflow.com/q/353393/101
