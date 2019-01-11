2695447049943b16e4384ddf014dcb21e834d822
Antonio Maniero <maniero@klip.net>
2019-01-11 09:05:22 -0200

    https://pt.stackoverflow.com/q/44715/101

---
+++ Python/Round.py
@@ -0,0 +1,10 @@
+a = 13.946
+print(a)
+print("%.2f" % a)
+print(round(a, 2))
+print("%.2f" % round(a,2))
+print("{0:.2f}".format(a))
+print("{0:.2f}".format(round(a,2)))
+print("{0:.15f}".format(round(a,2)))
+
+#https://pt.stackoverflow.com/q/44715/101
