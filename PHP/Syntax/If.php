492067b5ba6f8902b1065bd9c9349a64c35788b0
Antonio Maniero <maniero@klip.net>
2019-01-14 07:54:07 -0200

    https://pt.stackoverflow.com/q/355514/101

---
+++ PHP/Syntax/If.php
@@ -0,0 +1,8 @@
+<?php
+    $valor01 = 10;
+    $valor02 = 8;
+    if ($valor01 > $valor02) echo "valor01 é maior que valor02<br>";
+    else echo "valor02 é maior que valor01";
+?>
+
+//https://pt.stackoverflow.com/q/355514/101
