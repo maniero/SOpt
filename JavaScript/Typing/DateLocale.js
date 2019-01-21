1e5cc0295dfe17c09cd7cac451c54c015522c461
Antonio Maniero <maniero@klip.net>
2019-01-21 12:31:40 -0200

    https://pt.stackoverflow.com/q/45234/101

---
+++ JavaScript/Typing/DateLocale.js
@@ -0,0 +1,4 @@
+var date = new Date(1389135600*1000); // converte para data
+console.log(date.toLocaleDateString("pt-BR")); //formata de acordo com o requisito
+
+//https://pt.stackoverflow.com/q/45234/101
