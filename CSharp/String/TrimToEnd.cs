69be6ba421ed210529e50845addb53ba9c72b3a0
Antonio Maniero <maniero@klip.net>
2019-01-30 08:50:45 -0200

    https://pt.stackoverflow.com/q/359110/101

---
+++ CSharp/String/TrimToEnd.cs
@@ -0,0 +1,27 @@
+using System;
+using static System.Console;
+
+public class Program {
+    static void Main() {
+        var url = "http://abc.com";
+        var posicao = url.IndexOf(':');
+        if (posicao < 0) return; //aqui você trata como quiser se não achar a string
+        WriteLine(url.Substring(posicao));
+        WriteLine(url[posicao..]); //só para C# 8
+    }
+}
+
+//using System;
+using static System.Console;
+
+public class Program {
+    static void Main() {
+        var url = "http://abc.com";
+        var posicao = url.IndexOf(':');
+        if (posicao < 0) return; //aqui você trata como quiser se não achar a string
+        WriteLine(url.Substring(posicao));
+        WriteLine(url[posicao..]); //só para C# 8
+    }
+}
+
+//https://pt.stackoverflow.com/q/359110/101
