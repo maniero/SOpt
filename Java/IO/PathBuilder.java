1ba3c07d9e93df4e81b953b350a136f4b9ed4448
Antonio Maniero <maniero@klip.net>
2019-02-01 09:15:50 -0200

    https://pt.stackoverflow.com/q/45336/101

---
+++ Java/IO/PathBuilder.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class Program {
+	public static void main (String[] args) {
+    	File path = new File("src", "models");
+		System.out.println(path.getPath());
+		System.out.println(path.getAbsolutePath());
+		File novoPath = new File(".", path.getPath());
+		System.out.println(novoPath.getPath());
+		System.out.println(novoPath.getAbsolutePath());
+	}
+}
+
+//https://pt.stackoverflow.com/q/45336/101
