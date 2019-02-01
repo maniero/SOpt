1a3a03045a22e0f659e93596c64eaf44488b1d28
Antonio Maniero <maniero@klip.net>
2019-02-01 10:13:31 -0200

    https://pt.stackoverflow.com/q/45347/101

---
+++ Java/String/HandleCharFromString.java
@@ -0,0 +1,16 @@
+class Program {
+	public static void main (String[] args)	{
+		solve("Teste");
+	}
+	public static void solve(String text) {
+	    int len = text.length();
+	    StringBuffer buffer = new StringBuffer(len);
+	    for(int i = 0; i < len; i++){
+	        char letter = text.charAt(i);
+	        buffer.append(i % 2 == 0 ? Character.toUpperCase(letter) : Character.toLowerCase(letter));
+	    }
+	    System.out.println(buffer);
+	}
+}
+
+//https://pt.stackoverflow.com/q/45347/101
