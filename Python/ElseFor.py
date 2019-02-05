5d4139805638cadf7ca07f8181ed6788f10e0bb6
Antonio Maniero <maniero@klip.net>
2019-02-05 09:58:35 -0200

    https://pt.stackoverflow.com/q/280773/101

---
+++ Python/ElseFor.py
@@ -0,0 +1,16 @@
+i = 1
+for j in range(0, i):
+    if j % 2 == 1:
+        break
+    print(j)
+else:
+    print("ok")
+i = 3
+for j in range(0, i):
+    if j % 2 == 1:
+        break
+    print(j)
+else:
+    print("ok")
+    
+#https://pt.stackoverflow.com/q/280773/101
