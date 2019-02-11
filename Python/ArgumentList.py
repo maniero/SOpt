dac66e5079c7e0a6cd45fd83be8cdd86ad073ac3
Antonio Maniero <maniero@klip.net>
2019-02-11 08:51:22 -0200

    https://pt.stackoverflow.com/q/360929/101

---
+++ Python/ArgumentList.py
@@ -0,0 +1,7 @@
+from itertools import product 
+
+for i in product(*[[1,2,3,4,5,6],[12,15,16,13,-4,2]]): 
+    print(i)
+for i in product(*[[1,2,3,4,5,6],[12,15,16,13,-4,2],[123,154,165,136,-41,22]]): 
+    print(i)
+#https://pt.stackoverflow.com/q/360929/101
