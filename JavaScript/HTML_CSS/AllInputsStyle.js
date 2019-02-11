746207fb9f2dca351746ac2459880d29e168343d
Antonio Maniero <maniero@klip.net>
2019-02-11 09:00:23 -0200

    https://pt.stackoverflow.com/q/361108/101

---
+++ JavaScript/HTML_CSS/AllInputsStyle.js
@@ -0,0 +1,11 @@
+input {
+    background: #C0C0C0;
+    height: 20px;
+}
+
+Login:<br/>
+<input type="text"><br/>
+Senha:<br/>
+<input type="password">
+
+//https://pt.stackoverflow.com/q/361108/101
