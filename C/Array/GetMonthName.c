8b7f2e9009fd3101797d804245733f8904af2480
Antonio Maniero <maniero@klip.net>
2019-02-11 12:20:06 -0200

    https://pt.stackoverflow.com/q/361599/101

---
+++ C/Array/GetMonthName.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int mes;
+    printf("Numero do mes: ");
+    scanf("%d", &mes);
+    if (mes < 1 || mes > 12) {
+    	printf("Erro\n");
+    	return 0;
+    }
+    printf("%s", (char *[12]){ "Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" }[mes - 1]);
+}
+
+//https://pt.stackoverflow.com/q/361599/101
