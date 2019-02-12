b7cef9c5a28d245f416b671fefb2defab74a5049
Antonio Maniero <maniero@klip.net>
2019-02-11 23:09:20 -0200

    https://pt.stackoverflow.com/q/361612/101

--- CPP/Syntax/Closure.cpp
+++ CPP/Syntax/Closure.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main() {
     int i = 1;
-    auto loop = [=]() { cout << i; };
+    auto loop = [=]() { cout << i; }; //não pode fazer o incremento aqui
     loop();
+    auto loop2 = [&]() { cout << i++; };
+    loop2();
+    cout << i;
 }
 
 //https://pt.stackoverflow.com/q/361612/101
