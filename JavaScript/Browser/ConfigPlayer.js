c3b72ce51205f10bce5b45313283ea045559e144
Antonio Maniero <maniero@klip.net>
2019-02-19 09:37:41 -0300

    https://pt.stackoverflow.com/q/45543/101

---
+++ JavaScript/Browser/ConfigPlayer.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <audio id="player" src="http://upload.wikimedia.org/wikipedia/en/9/9f/Sample_of_%22Another_Day_in_Paradise%22.ogg" autoplay></audio>
+<div>
+    <button onclick="document.getElementById('player').muted = !document.getElementById('player').muted">Mute / Unmute</button>
+</div>
+</body>
+</html>
+
+//https://pt.stackoverflow.com/q/45543/101
