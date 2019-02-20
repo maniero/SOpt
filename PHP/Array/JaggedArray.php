eb73b4024c549865a8f4a38365b812e441df3672
Antonio Maniero <maniero@klip.net>
2019-02-20 13:24:22 -0300

    https://pt.stackoverflow.com/q/46090/101

---
+++ PHP/Array/JaggedArray.php
@@ -0,0 +1,8 @@
+$newarray = array();
+for($y = 0; $y <= 1; $y++){
+    $newarray[$y]['date'] = array();
+    $newarray[$y]['time'] = array();
+    $newarray[$y]['valeur'] = array();
+}
+
+//https://pt.stackoverflow.com/q/46090/101
