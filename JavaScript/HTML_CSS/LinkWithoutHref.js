9bbc398511a6f6bd259c7287c0596265ad47baa5
Antonio Maniero <maniero@klip.net>
2019-02-20 09:24:14 -0300

    https://pt.stackoverflow.com/q/45628/101

---
+++ JavaScript/HTML_CSS/LinkWithoutHref.js
@@ -0,0 +1,17 @@
+#divlink {
+    background-color: #D0D0D0;
+    width: 100px;
+    height: 100px;
+}
+a.link {
+    display: block;
+    height: 100%;
+    width: 100%;
+    text-decoration: none;
+}
+
+<div id="divlink">
+    <a href="http://example.com" class="link">Novo acesso</a>
+</div>
+
+//https://pt.stackoverflow.com/q/45628/101
