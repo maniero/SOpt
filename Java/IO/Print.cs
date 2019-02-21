23d785b8c5124f18d5cd6b3b60fe13e5814be269
Antonio Maniero <maniero@klip.net>
2019-02-21 09:09:16 -0300

    https://pt.stackoverflow.com/q/47102/101

---
+++ Java/IO/Print.cs
@@ -0,0 +1,10 @@
+class Ideone {
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		System.out.print("x: " + x + " y: " + y);
+		System.out.printf("x: %d y: %d", x, y);
+	}
+}
+
+//https://pt.stackoverflow.com/q/47102/101
