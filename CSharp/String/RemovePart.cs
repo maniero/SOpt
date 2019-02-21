20560e1ea7ae7bbede2387405ee1ac48a7f19994
Antonio Maniero <maniero@klip.net>
2019-02-21 10:16:21 -0300

    https://pt.stackoverflow.com/q/363960/101

---
+++ CSharp/String/RemovePart.cs
@@ -0,0 +1,23 @@
+using static System.Console;
+public class Program {
+	public static void Main() {
+        var texto = "0001>0002>0003>0004>0005";
+		var posicao = -1;
+        for (var i = 0; i < 3; i++) {
+			posicao = texto.IndexOf(">", posicao + 1);
+			if (posicao == -1) break;
+		}
+        if (posicao > -1) texto = texto.Substring(posicao + 1);
+        WriteLine(texto);
+        texto = "0001>0002";
+		posicao = -1;
+        for (var i = 0; i < 3; i++) {
+			posicao = texto.IndexOf(">", posicao + 1);
+			if (posicao == -1) break;
+		}
+        if (posicao > -1) texto = texto.Substring(posicao + 1);
+        WriteLine(texto);
+    }
+}
+
+//https://pt.stackoverflow.com/q/363960/101
