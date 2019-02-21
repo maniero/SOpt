a7bd003355998fc10a0193506fcef12c18ea4700
Antonio Maniero <maniero@klip.net>
2019-02-21 09:02:08 -0300

    https://pt.stackoverflow.com/q/47044/101

---
+++ JavaScript/HTML_CSS/SpanDivOnImg.js
@@ -0,0 +1,20 @@
+#blocovertical1{
+  display:block;
+}
+
+#textoEspecial1{
+  display:inline;
+  color:red;
+}
+
+#textoEspecial2{
+  display:block;
+  color:blue;
+}
+
+<div id="blocovertical1">
+   <p>Lorem ipsum dolor sit amet, <span id="textoEspecial1">consectetuer adipiscing</span> elit. Duis congue vehicula purus.</p>
+   <p>Nam <span id="textoEspecial2">eget magna nec</span> sapien fringilla euismod. Donec hendrerit.</p> 
+</div>
+
+//https://pt.stackoverflow.com/q/47044/101
