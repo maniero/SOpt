2a396d4dae954ad29f005710426591358ab2f64a
Antonio Maniero <maniero@klip.net>
2019-02-25 16:24:28 -0300

    https://pt.stackoverflow.com/q/364821/101

---
+++ Java/Math/Increment.java
@@ -0,0 +1,7 @@
+public class NumerosQueComeçamComAlgarimo5 {
+    public static void main(String [] args) {
+        for (int i = 5; i <= 200; i+= 10) System.out.println(i);
+    }
+}
+
+//https://pt.stackoverflow.com/q/364821/101
