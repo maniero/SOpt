76062be5717506cfae9744f5e300b192178d874e
Antonio Maniero <maniero@klip.net>
2019-03-08 08:59:47 -0300

    https://pt.stackoverflow.com/q/366245/101

---
+++ Java/Typing/PrimitiveNull.java
@@ -0,0 +1,10 @@
+public class NuloOuNao {
+    public static boolean isZero(Float num) {
+        return num == null;
+    }
+    public static void main(String[] args) {
+        isZero(10);
+    }
+}
+
+//https://pt.stackoverflow.com/q/366245/101
