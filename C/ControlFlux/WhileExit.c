c0854e6f19c8ffe9c89d67c65d20bbeff2995f75
Antonio Maniero <maniero@klip.net>
2019-05-24 09:48:19 -0300

    https://pt.stackoverflow.com/q/386314/101

---
+++ C/ControlFlux/WhileExit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    while (1) {
+    	float vi, vf;
+        printf("\n#############################");
+        printf("\nValor Inicial: "); scanf("%f", &vi);
+        printf("\nValor Final: "); scanf("%f", &vf);
+        int o = (vi <= vf) ? 1 : -1;
+        for (int i = vi; i != vf + o; i += o) printf("\n %d", i);
+        char continuar;
+        printf("\nDigite F para encerrar: "); scanf("%s", &continuar);
+		if (continuar == 'F') break;
+    }
+}
+
+//https://pt.stackoverflow.com/q/386314/101
