1561ed7a8444631d39ab72efa2659f591b7efc32
Antonio Maniero <maniero@klip.net>
2019-05-30 09:51:23 -0300

    https://pt.stackoverflow.com/q/386314/101

--- C/ControlFlux/WhileExit.c
+++ C/ControlFlux/WhileExit.c
@@ -9,7 +9,7 @@ int main() {
         int o = (vi <= vf) ? 1 : -1;
         for (int i = vi; i != vf + o; i += o) printf("\n %d", i);
         char continuar;
-        printf("\nDigite F para encerrar: "); scanf("%s", &continuar);
+        printf("\nDigite F para encerrar: "); scanf("%c", &continuar);
 		if (continuar == 'F') break;
     }
 }
