d7209467a25f887d4d7f5c72f4f087d082d5d342
Antonio Maniero <maniero@klip.net>
2019-06-03 14:29:25 -0300

    https://pt.stackoverflow.com/q/384158/101

---
+++ Dart/Function/ParameterFunction.dart
@@ -0,0 +1,7 @@
+void metodoComFuncao(List<int> lista, Function func) {
+    for (var i = 0; i < lista.lenght; i++) if (lista[i] % 2 == 0) func();  
+}
+
+metodoComFuncao(lista, funcao)
+
+//https://pt.stackoverflow.com/q/384158/101
