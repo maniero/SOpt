df327a451bd1081bc8a0e03a1a0a030e96762ee1
Antonio Maniero <maniero@klip.net>
2019-06-15 20:49:47 -0300

    https://pt.stackoverflow.com/q/391441/101

---
+++ C/Array/RotateMatrix90.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main (void) {
+    int matriz[3][3], rot[3][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Digite o valor da matriz[%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        printf("\n");
+        for (int j = 0; j < 3; j++) printf(" %d ", matriz[i][j]);
+    }
+    for (int i = 0; i < 3; i++) {
+    	for (int j = 2; j >= 0; j--) {
+    		rot[i][j] = matriz[j][2 - i];
+    	}
+    }
+    printf("\nMATRIZ 90º");
+    for (int i = 0; i < 3; i++) {
+        printf("\n");
+        for (int j = 0; j < 3; j++) printf(" %d ", rot[i][j]);
+    }
+}
+
+//https://pt.stackoverflow.com/q/391441/101
