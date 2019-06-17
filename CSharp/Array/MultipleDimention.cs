0a8d9cccb4bb91536eccc16f7fac812e87ee262e
Antonio Maniero <maniero@klip.net>
2019-06-17 12:09:37 -0300

    https://pt.stackoverflow.com/q/391662/101

---
+++ CSharp/Array/MultipleDimention.cs
@@ -0,0 +1,9 @@
+public class Program {
+	public static void Main() {
+		var players = new string[,,,]{ { { { "Armando", "P" }, { "Dave", "S" } }, { { "Richard", "R" }, { "Michael", "S" } } }, { { { "Allen", "S" }, { "Omer", "P" } }, { { "David E.", "R" }, { "Richard X.", "P" } } } };
+		var players3 = new string[,,] { { { "Armando", "P" }, { "Dave", "S" } }, { { "Richard", "R" }, { "Michael", "S" } }, { { "Allen", "S" }, { "Omer", "P" } }, { { "David E.", "R" }, { "Richard X.", "P" } } };
+		var players2 = new string[,] { { "Armando", "P" }, { "Dave", "S" }, { "Richard", "R" }, { "Michael", "S" }, { "Allen", "S" }, { "Omer", "P" }, { "David E.", "R" }, { "Richard X.", "P" } };
+	}
+}
+
+//https://pt.stackoverflow.com/q/391662/101
