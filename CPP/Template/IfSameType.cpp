5119fc76701e0690c075914272788a6bf0d479d3
Antonio Maniero <maniero@klip.net>
2019-06-17 12:33:35 -0300

    //https://pt.stackoverflow.com/q/391548/101

---
+++ CPP/Template/IfSameType.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string.h>
+#include <type_traits>
+
+using namespace std;
+
+template<typename T>
+int len(T args) {
+    if constexpr (is_same_v<T, const char*>) return strlen(args);
+    else return args.size(); 
+}
+
+int main() {
+    string a = "Ola Mundo!";
+    const char *b = "Ola Mundo!";
+    vector<string> c = {"Ola", "Mundo", "!"};
+    cout << len(a) << "\n";
+    cout << len(b) << "\n";
+    cout << len(c) << "\n";
+}
+
+////https://pt.stackoverflow.com/q/391548/101
