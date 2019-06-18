5058a85f275a666121bba3d62c9554af1ed8941c
Antonio Maniero <maniero@klip.net>
2019-06-18 12:15:11 -0300

    https://pt.stackoverflow.com/q/47707/101

---
+++ JavaScript/HTML_CSS/TableBorderless2.js
@@ -0,0 +1,39 @@
+#tabela1 {
+    border: 0px;
+    margin: 0px auto;
+    width: 500px;
+    height: 600px;
+}
+#tabela2 {
+    border: 1px solid;
+    margin: 0px auto;
+    width: 500px;
+    height: 600px;
+}
+
+<table id="tabela1">
+    <tr>
+        <td>
+            texto1
+        </td>
+    </tr>
+    <tr>
+        <td>
+            texto2
+        </td>
+    </tr>
+</table>
+<table id="tabela2">
+    <tr>
+        <td>
+            texto1
+        </td>
+    </tr>
+    <tr>
+        <td>
+            texto2
+        </td>
+    </tr>
+</table>
+
+<!-- https://pt.stackoverflow.com/q/47707/101
