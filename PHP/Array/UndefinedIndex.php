5bbcacda9598b548abd76e4c24b7c1ccf23974da
Antonio Maniero <maniero@klip.net>
2019-06-21 10:21:44 -0300

    https://pt.stackoverflow.com/q/48483/101

---
+++ PHP/Array/UndefinedIndex.php
@@ -0,0 +1,5 @@
+echo "<br><a href=\"testonho.php?newsid={$myrow['ID']}\">Read More...</a>
+            || <a href=\"edit_news.php?newsid={$myrow['ID']}\">Edit</a>
+             || <a href=\"delete_news.php?newsid={$myrow['ID']}\">Delete</a><br><hr>";
+             
+//https://pt.stackoverflow.com/q/48483/101
