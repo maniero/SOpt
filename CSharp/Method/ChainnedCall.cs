2733427af6148b3ff25f336495152ce6d80befee
Antonio Maniero <maniero@klip.net>
2019-06-24 15:19:58 -0300

    https://pt.stackoverflow.com/q/393087/101

---
+++ CSharp/Method/ChainnedCall.cs
@@ -0,0 +1,11 @@
+public class Program {
+	public static void Main() {
+		new ClasseA().Metodo();
+	}
+}
+
+public class ClasseA {
+	public void Metodo() {}
+}
+
+//https://pt.stackoverflow.com/q/393087/101
