7eb1a68967749161891d962129d6fee1cb40bf01
Antonio Maniero <maniero@klip.net>
2019-07-03 14:04:44 -0300

    https://pt.stackoverflow.com/q/49601/101

---
+++ Java/IO/ScreenResolutionAndroid.java
@@ -0,0 +1,7 @@
+Display display = getWindowManager().getDefaultDisplay();
+Point size = new Point();
+display.getSize(size);
+int width = size.x;
+int height = size.y;
+
+//https://pt.stackoverflow.com/q/49601/101
