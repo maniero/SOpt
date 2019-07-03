c2869b2ad55834f069f0cc7f8db29a7e223fa2df
Antonio Maniero <maniero@klip.net>
2019-07-03 09:41:54 -0300

    https://pt.stackoverflow.com/q/49227/101

---
+++ Java/IO/GetMiliSeconds.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main {
+	public static void main (String[] args) {
+		System.out.println(System.currentTimeMillis());
+	}
+}
+
+//https://pt.stackoverflow.com/q/49227/101
