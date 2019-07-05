84030b2dd86e24825cc4424116508a1edecbf0d9
Antonio Maniero <maniero@klip.net>
2019-07-05 09:42:42 -0300

    https://pt.stackoverflow.com/q/394953/101

--- JavaScript/Syntax/Leap.js
+++ JavaScript/Syntax/Leap.js
@@ -1,8 +1,9 @@
-function calcularBissexto(){
-    var bi = parseInt(document.getElementById('bissexto').value);
-    var msg = document.getElementById('msg');
-    if (bi % 4 == 0 && bi % 100 != 0 || bi % 400 == 0) msg.innerHTML += 'E bissexto'
-    else msg.innerHTML+= 'Nao e bissexto';
+function calcularBissexto(ano) {
+    return ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0 ? 'E bissexto' : 'Nao e bissexto';
 }
+console.log(calcularBissexto(1980));
+console.log(calcularBissexto(1981));
+console.log(calcularBissexto(1900));
+console.log(calcularBissexto(2000));
 
 //https://pt.stackoverflow.com/q/394953/101
