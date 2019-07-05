c600319cc754b02978f43ce1c303d5b76189da2f
Antonio Maniero <maniero@klip.net>
2019-07-05 10:20:11 -0300

    https://pt.stackoverflow.com/q/135423/101

---
+++ CSharp/Syntax/UsingStatic.cs
@@ -0,0 +1,13 @@
+using static System.Console;
+using static System.Math;
+using static System.Convert;
+using static System.DateTime;
+					
+public class Program {
+	public static void Main() {
+		WriteLine(Round(ToDouble("123.45")));
+		WriteLine(Now);
+	}
+}
+
+//https://pt.stackoverflow.com/q/135423/101
