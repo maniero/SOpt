7599c01635ded730078657e163a404eae6e58e84
Antonio Maniero <maniero@klip.net>
2019-07-05 16:17:39 -0300

    https://pt.stackoverflow.com/q/395616/101

--- C/Algorithm/GetAlgarism.c
+++ C/Algorithm/GetAlgarism.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+#include<string.h>
 
 int main() {
     const char *RA = "19480685";
     int tamanho = strlen(RA);    
-    int vetor[tamnho];
-    for (int i = 0; i < tamanho; i++) vetor[i] = RA[i] + '0';
-    for (int i = 0; i < strlen(RA); i++) printf ("%d\n", vetor[i]);
+    int vetor[tamanho];
+    for (int i = 0; i < tamanho; i++) vetor[i] = RA[i];
+    for (int i = 0; i < strlen(RA); i++) printf ("%c\n", vetor[i]);
 }
 
 //https://pt.stackoverflow.com/q/395616/101
