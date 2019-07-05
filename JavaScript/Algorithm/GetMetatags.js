30f0d73982b258862b99fcd096f2295287c0f4a3
Antonio Maniero <maniero@klip.net>
2019-07-05 13:38:52 -0300

    https://pt.stackoverflow.com/q/49859/101

---
+++ JavaScript/Algorithm/GetMetatags.js
@@ -0,0 +1,55 @@
+<script>
+function getParentUrl() {
+    var isInIframe = (parent !== window),
+        parentUrl = null;
+
+    if (isInIframe) {
+        parentUrl = document.referrer;
+    }
+
+    return parentUrl;
+}
+var referencia = getParentUrl();
+
+(function() {
+  var httpRequest;
+  document.getElementById("ajaxButton").onclick = function() {
+          makeRequest('suapagina.php?referencia=' + referencia); };
+
+  function makeRequest(url) {
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+      httpRequest = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE
+      try {
+        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+      } 
+      catch (e) {
+        try {
+          httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        } 
+        catch (e) {}
+      }
+    }
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+  }
+
+  function alertContents() {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        alert(httpRequest.responseText);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+})();
+</script>
+
+//https://pt.stackoverflow.com/q/49859/101
