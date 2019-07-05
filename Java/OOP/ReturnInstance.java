08d80722ff9cd79bd47ac86569c961c9da768d24
Antonio Maniero <maniero@klip.net>
2019-07-05 10:17:53 -0300

    https://pt.stackoverflow.com/q/49802/101

---
+++ Java/OOP/ReturnInstance.java
@@ -0,0 +1,8 @@
+public static void main (String[] args) {
+    Server instancia = metodo(); //a variável instancia terá uma instância de Server
+}
+public static Server metodo() {
+    return new Server(); //só faz sentido se fizer outras coisas no método
+}
+
+//https://pt.stackoverflow.com/q/49802/101
