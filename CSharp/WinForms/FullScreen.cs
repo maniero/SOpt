cfa8c958e7957b48622564bdca255d5430926926
Antonio Maniero <maniero@klip.net>
2019-07-09 11:57:46 -0300

    https://pt.stackoverflow.com/q/50116/101

---
+++ CSharp/WinForms/FullScreen.cs
@@ -0,0 +1,7 @@
+private void Form1_Load(object sender, EventArgs e) {
+    this.TopMost = true;
+    this.FormBorderStyle = FormBorderStyle.None;
+    this.WindowState = FormWindowState.Maximized;
+}
+
+//https://pt.stackoverflow.com/q/50116/101
