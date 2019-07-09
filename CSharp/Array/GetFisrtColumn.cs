ef4b5e15aec6b08171a91b31e6938593c01fd4ad
Antonio Maniero <maniero@klip.net>
2019-07-09 10:05:48 -0300

    https://pt.stackoverflow.com/q/396160/101

---
+++ CSharp/Array/GetFisrtColumn.cs
@@ -0,0 +1,15 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		string[,] operacoes = { 
+		   { "SOMA", "x", "" },
+		   { "SUBTRAÇÃO", "x", "" },
+		   { "MULTIPLICAÇÃO", "x", "" },
+		   { "DIVISÃO", "x", "" }
+		};
+		for (var i = 0; i <= operacoes.GetLength(1); i++) WriteLine(operacoes[i, 0]);
+	}
+}
+
+//https://pt.stackoverflow.com/q/396160/101
