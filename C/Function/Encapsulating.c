d4c472e5260b747f6d739a1b04804370feb10268
Antonio Maniero <maniero@klip.net>
2019-07-10 13:01:30 -0300

    https://pt.stackoverflow.com/q/50363/101

---
+++ C/Function/Encapsulating.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+enum direction { DOWN, RIGHT, UP, LEFT };
+
+void funcaoExemplo(enum direction var) {
+    if(var == direction.DOWN) printf("%d", RIGHT); //só para testar
+}
+ 
+int main(void){
+    funcaoExemplo(DOWN);
+    return 0;
+}
+
+//https://pt.stackoverflow.com/q/50363/101
