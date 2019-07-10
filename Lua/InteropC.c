0e1ce5b2480101d113ccd73e5a099d129d451d4d
Antonio Maniero <maniero@klip.net>
2019-07-10 13:08:30 -0300

    https://pt.stackoverflow.com/q/50413/101

---
+++ Lua/InteropC.c
@@ -0,0 +1,14 @@
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+int main() {
+    lua_State *L = lua_open();
+    luaL_openlibs(L);
+    luaL_loadstring(L, "print('Ola mundo')");
+    lua_close(L);
+}
+
+//https://pt.stackoverflow.com/q/50413/101
