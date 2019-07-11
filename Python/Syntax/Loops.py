29cede47c08022710fe24578b6d54791cfc0643b
Antonio Maniero <maniero@klip.net>
2019-07-11 12:47:45 -0300

    https://pt.stackoverflow.com/q/50649/101

---
+++ Python/Syntax/Loops.py
@@ -0,0 +1,17 @@
+i = 1 #inicializador
+while i < 10: #verifica a condição
+    print(i) #o corpo, a ação
+    i += 2 #o passo de "incremento"
+
+i = 10 #inicializador
+while i > 0: #verifica a condição
+    print(i) #o corpo, a ação
+    i -= 1 #o passo de "incremento"
+
+for i in range(1, 10, 2):
+    print(i)
+
+for i in range(10, 1, -1):
+    print(i)
+    
+#https://pt.stackoverflow.com/q/50649/101
