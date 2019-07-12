dd885f7363a1601019ae07aacc4640e1e7d39481
Antonio Maniero <maniero@klip.net>
2019-07-12 10:10:53 -0300

    https://pt.stackoverflow.com/q/50891/101

---
+++ PHP/String/Concatenation.php
@@ -0,0 +1,4 @@
+$msg = $this->usuario . " " . $this->senha . " " . $oneclick['card']['cardNumber'] .
+    " " . $oneclick['card']['holderName'] . " " . $oneclick['card']['expirationDate'];
+    
+//https://pt.stackoverflow.com/q/50891/101
