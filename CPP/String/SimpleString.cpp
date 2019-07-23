047ac5dfa37c8466d913e3dce9beb6c421cde123
Antonio Maniero <maniero@klip.net>
2019-07-23 10:46:25 -0300

    https://pt.stackoverflow.com/q/397712/101

---
+++ CPP/String/SimpleString.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nome;
+    cin >> nome;
+    cout << nome << endl;
+}
+
+//https://pt.stackoverflow.com/q/397712/101
