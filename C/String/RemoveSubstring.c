449d390a255eae57e5151d373d020beb92d18deb
Antonio Maniero <maniero@klip.net>
2019-07-23 09:22:06 -0300

    https://pt.stackoverflow.com/q/398277/101

--- C/String/RemoveSubstring.c
+++ C/String/RemoveSubstring.c
@@ -2,13 +2,16 @@
 
 void StrlDel(char *s1, char *s2) {
     int i = 0;
+    int notFound = 1;
     while (s1[i]) {
     	int j;
         for (j = 0; s2[j] && s2[j] == s1[i + j]; j++);
-        if (!s2[j]) {
+       	printf("%c", s1[i]);
+        if (!s2[j] && notFound) {
         	int k;
             for (k = i; s1[k + j]; k++) s1[k] = s1[k + j];
             s1[k] = 0;
+            notFound = 0;
         } else i++;
     }
 }
@@ -17,6 +20,6 @@ int main(void) {
     char palavras[] = "O rato roeu a rolha da garrafa";
     StrlDel(palavras, "ra");
     printf("\n%s\n", palavras);
- }
+}
  
- //https://pt.stackoverflow.com/q/398277/101
+//https://pt.stackoverflow.com/q/398277/101
