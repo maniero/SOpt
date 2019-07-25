8750c1d1cb7f547f9ce53a49b0c5187ffa6faead
Antonio Maniero <maniero@klip.net>
2019-07-25 12:48:06 -0300

    https://pt.stackoverflow.com/q/50942/101

---
+++ JavaScript/HTML_CSS/Scrollbar.js
@@ -0,0 +1,20 @@
+body { //aqui pode ser outro elemento específico que quiser
+    overflow-x: hidden; //se realmente quer impedir que tenha uma barra vertical.
+    overflow-y: scroll; //pode-se usar auto para deixar o browser decidir quando usar.
+    white-space: nowrap; //n]ao deixa quebrar a linha
+}
+
+body { 
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+} 
+
+<img src="http://lorempixel.com/150/150/people/0/a/">
+<img src="http://lorempixel.com/150/150/people/0/b/">
+<img src="http://lorempixel.com/150/150/people/0/c/">
+<img src="http://lorempixel.com/150/150/people/0/d/">
+<img src="http://lorempixel.com/150/150/people/0/e/">
+<img src="http://lorempixel.com/150/150/people/0/f/">
+
+//https://pt.stackoverflow.com/q/50942/101
