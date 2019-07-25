1ee15b384a1d413002df26c718c5ac5afd177231
Antonio Maniero <maniero@klip.net>
2019-07-25 16:27:40 -0300

    https://pt.stackoverflow.com/q/399689/101

--- C/ControlFlux/LoopExit.c
+++ C/ControlFlux/LoopExit.c
@@ -4,9 +4,8 @@ int main(void) {
     while (1) {
         float temp;
         puts("Graus Celsius:");
-        scanf("%f",&temp);
-        formula = temp * 1.8 + 32;
-        printf("\nCelsius:%.1f\n\nFahrenheit:%.1f\n\n", temp, temp * 1.8 + 32);
+        scanf("%f", &temp);
+        printf("\nCelsius: %.1f\nFahrenheit: %.1f\n", temp, temp * 1.8 + 32);
         puts("Digite 1 para sair ou 2 para uma nova conversao...\n");
         int sair;
         scanf("%d", &sair);
