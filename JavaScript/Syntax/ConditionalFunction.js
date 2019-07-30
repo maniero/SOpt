3c2f71c1955041a13e4ddf8972f5382f0b68ea1a
Antonio Maniero <maniero@klip.net>
2019-07-30 12:37:05 -0300

    https://pt.stackoverflow.com/q/400320/101

---
+++ JavaScript/Syntax/ConditionalFunction.js
@@ -0,0 +1,10 @@
+if (true) console.log("ok");
+
+var variavel = true;
+if (variavel) console.log("ok");
+
+var entrada = prompt("digita a");
+var variavel = entrada == "a";
+if (variavel) console.log("ok");
+
+//https://pt.stackoverflow.com/q/400320/101
