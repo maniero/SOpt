4c860250b695e7493a245c247137573bbbb781bf
Antonio Maniero <maniero@klip.net>
2019-07-31 10:16:44 -0300

    https://pt.stackoverflow.com/q/51770/101

---
+++ SQL/DML/DeleteAllTables.sql
@@ -0,0 +1,12 @@
+EXEC sp_MSForEachTable 'DISABLE TRIGGER ALL ON ?'
+GO
+EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL'
+GO
+EXEC sp_MSForEachTable 'DELETE FROM ?'
+GO
+EXEC sp_MSForEachTable 'ALTER TABLE ? CHECK CONSTRAINT ALL'
+GO
+EXEC sp_MSForEachTable 'ENABLE TRIGGER ALL ON ?'
+GO
+
+--https://pt.stackoverflow.com/q/51770/101
