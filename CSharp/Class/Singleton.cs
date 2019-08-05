cdac3e23b7184f5711822bc4869f99ca5024cdb3
Antonio Maniero <maniero@klip.net>
2019-08-05 10:56:00 -0300

    https://pt.stackoverflow.com/q/52250/101

---
+++ CSharp/Class/Singleton.cs
@@ -0,0 +1,20 @@
+public class Program {
+	public static void Main() {
+		var singleton = MyClass.Instance;
+	}
+}
+
+public class MyClass {
+   private static MyClass instance;
+   private MyClass() {}
+   public static MyClass Instance {
+      get {
+         if (instance == null) {
+            instance = new MyClass();
+         }
+         return instance;
+      }
+   }
+}
+
+//https://pt.stackoverflow.com/q/52250/101
