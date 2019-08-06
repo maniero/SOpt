d55962e8bba44f45c805b52df99e46e5ec761142
Antonio Maniero <maniero@klip.net>
2019-08-06 09:47:45 -0300

    https://pt.stackoverflow.com/q/52367/101

---
+++ CSharp/NameSpace/UsingStatic.cs
@@ -0,0 +1,8 @@
+using static System.Console;
+using static System.Math;
+			
+public class Program {
+	public static void Main() => WriteLine(Sin(12));	
+}
+
+//https://pt.stackoverflow.com/q/52367/101
