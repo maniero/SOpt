985c8f1ef4ae008aad5b202f2d3280a67d3b6755
Antonio Maniero <maniero@klip.net>
2019-08-12 10:48:32 -0300

    https://pt.stackoverflow.com/q/402090/101

---
+++ CPP/Pointer/Syntax.cpp
@@ -0,0 +1,11 @@
+void Metodo(Tipo objeto&) {
+    objeto.membro;
+}
+
+//Se fosse ponteiro acessaria assim:
+
+void Metodo(Tipo *objeto) {
+    objeto->membro;
+}
+
+//https://pt.stackoverflow.com/q/402090/101
