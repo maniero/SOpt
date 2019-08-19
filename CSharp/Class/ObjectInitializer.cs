7af1b730aab20bb839e556aea2a3d1f1e7ee5fa3
Antonio Maniero <maniero@klip.net>
2019-08-19 12:44:03 -0300

    https://pt.stackoverflow.com/q/54334/101

---
+++ CSharp/Class/ObjectInitializer.cs
@@ -0,0 +1,20 @@
+var pessoa = new PessoaJuridica {
+    NomeFantasia = "abc",
+    RazaoSocial = "abc",
+    Endereco = "rua abc",
+    Bairro = "abc",
+    Cidade = "abc",
+    Estado = "sp",
+    CEP = "12345678",
+    Telefone1 = "12345678",
+    Telefone2 = "12345678",
+    Fax = "12345678",
+    CNPJ = "11111111000111",
+    InscricaoEstadual = "11111111111",
+    InscricaoMunicipal = "1111111",
+    DataConstituicao = new DateTime(2000, 4, 15),
+    DataCadastro = new DateTime(DateTime.Today),
+    Observacoes = "abc"
+}
+
+//https://pt.stackoverflow.com/q/54334/101
