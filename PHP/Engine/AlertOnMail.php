bc29f97095f2a25bdfdd2eec99db3bfc243f8d57
Antonio Maniero <maniero@klip.net>
2019-08-19 12:41:41 -0300

    https://pt.stackoverflow.com/q/54345/101

---
+++ PHP/Engine/AlertOnMail.php
@@ -0,0 +1,5 @@
+mail($email,$assunto,$mens,$headers);
+echo "<script type='javascript'>alert('Email enviado com Sucesso!');";
+echo "javascript:window.location='index.php';</script>";
+
+//https://pt.stackoverflow.com/q/54345/101
