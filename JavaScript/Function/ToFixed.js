c183c92d83c52f1d0c52a6224de825c055db57ae
Antonio Maniero <maniero@klip.net>
2019-08-22 08:27:46 -0300

    https://pt.stackoverflow.com/q/404735/101

---
+++ JavaScript/Function/ToFixed.js
@@ -0,0 +1,6 @@
+function conversao(fahreinheit) {
+    return (5/9) * (fahreinheit-32);
+}
+console.log(conversao(parseFloat(prompt('Entre com um grau em Fahreinheit: '))).toFixed(2));
+
+//https://pt.stackoverflow.com/q/404735/101
