7b265cb912635e328aaa4044ab8b44f5bc6a1ca2
Antonio Maniero <maniero@klip.net>
2019-08-26 15:16:07 -0300

    https://pt.stackoverflow.com/q/405680/101

---
+++ Python/Collection/KeyOnDictionary.py
@@ -0,0 +1,9 @@
+notas_por_materias = { "Matematica" : 7,
+                       "Portugues" : 8,
+                       "Historia" : 10 }
+print (f"Sua nota em Matematica, foi de {notas_por_materias['Matematica']}")
+
+for key, value in notas_por_materias.items() :
+    print (f"Sua nota em {key}, foi de {value}")
+
+#https://pt.stackoverflow.com/q/405680/101
