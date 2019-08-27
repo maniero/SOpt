f993c762b78214e9c8a29b727eeabc28612d1848
Antonio Maniero <maniero@klip.net>
2019-08-27 08:09:07 -0300

    https://pt.stackoverflow.com/q/405745/101

---
+++ Python/Operator/OrCondition.py
@@ -0,0 +1,9 @@
+letra = input("Qual seu gênero:")
+if letra == "F" or letra == "f":
+    print("Feminino")
+elif letra == "M" or letra == "m":
+    print("Masculino")
+else:
+    ("sexo invalido")
+    
+#https://pt.stackoverflow.com/q/405745/101
