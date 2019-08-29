a686457b52109d597d3e367980c4b786ce651814
Antonio Maniero <maniero@klip.net>
2019-08-29 13:57:29 -0300

    https://pt.stackoverflow.com/q/56912/101

---
+++ Lua/Test.lua
@@ -0,0 +1,10 @@
+function test (a)
+    if a then print(type(a) .. " '" .. tostring(a) .. "'" .. " corresponde a verdadeiro")
+    else print(type(a) .. " '" .. tostring(a) .. "'" .. " corresponde a falso") end
+end
+
+temp = test("masuia")
+print(tmp) -- só para você ver o resultado intermediário.
+test(tmp)
+
+--https://pt.stackoverflow.com/q/56912/101
