51c0ac5ff7e3d06a5d3caaa68a1301faf59e07ae
Antonio Maniero <maniero@klip.net>
2019-08-29 13:54:26 -0300

    https://pt.stackoverflow.com/q/56798/101

---
+++ CSharp/Enviroment/CommandLIneVars.cs
@@ -0,0 +1,26 @@
+void Main(string[] args) {
+    foreach(var arg in args) {
+        switch(arg) {
+            case "-r":
+            //faz algo aqui
+            case "-v":
+            //faz algo aqui
+        }
+    }
+}
+
+//
+
+void Main() {
+    var args = Environment.GetCommandLineArgs();
+    foreach(var arg in args) {
+        switch(arg) {
+            case "-r":
+            //faz algo aqui
+            case "-v":
+            //faz algo aqui
+        }
+    }
+}
+
+//https://pt.stackoverflow.com/q/56798/101
