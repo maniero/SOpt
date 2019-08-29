5a2bc6c5bbc100023804946dc22cb68fb5dde109
Antonio Maniero <maniero@klip.net>
2019-08-29 14:09:28 -0300

    https://pt.stackoverflow.com/q/56919/101

---
+++ Java/Method/Wildcard.java
@@ -0,0 +1,12 @@
+class Main {
+	public static void main (String[] args) {
+		String st = to("");
+		Integer it = to(0);
+	}
+	
+	public static <E> E to(E e) {
+    	return (E)e;
+	}
+}
+
+//https://pt.stackoverflow.com/q/56919/101
