dcb8552ea35801adc067902d56d2baf89caa6470
Antonio Maniero <maniero@klip.net>
2019-08-30 08:46:10 -0300

    https://pt.stackoverflow.com/q/56982/101

---
+++ Java/Collections/MyEntry.java
@@ -0,0 +1,30 @@
+import java.util.Map;
+
+final class MyEntry<K, V> implements Map.Entry<K, V> {
+    private final K key;
+    private V value;
+
+    public MyEntry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public V setValue(V value) {
+        V old = this.value;
+        this.value = value;
+        return old;
+    }
+}
+
+//https://pt.stackoverflow.com/q/56982/101
