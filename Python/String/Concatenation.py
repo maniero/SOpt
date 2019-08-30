3579fd904d63261c543e0a73de4eea0c4db2d299
Antonio Maniero <maniero@klip.net>
2019-08-30 10:19:37 -0300

    https://pt.stackoverflow.com/q/406643/101

---
+++ Python/String/Concatenation.py
@@ -0,0 +1,6 @@
+character_name = "Johnny"
+character_age = "64"
+print(character_name + " is cool.")
+print("And he is " + character_age + " years old.")
+
+#https://pt.stackoverflow.com/q/406643/101
