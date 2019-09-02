694a493a966412f24f4ef4df62f1ba6e8944c32c
Antonio Maniero <maniero@klip.net>
2019-09-02 12:44:36 -0300

    https://pt.stackoverflow.com/q/57228/101

---
+++ JavaScript/DOM/GetPixelsSVG.js
@@ -0,0 +1,7 @@
+document.body.innerHTML += document.getElementById('rect').getBoundingClientRect().width;
+
+<svg width=100% height=100%>
+    <rect id=rect rx=0 ry=0 x=0 y=0 width=100% height=50px fill=HoneyDew />
+</svg>
+
+//https://pt.stackoverflow.com/q/57228/101
