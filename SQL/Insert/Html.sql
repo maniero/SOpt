7d8170c64b3469f7fcd132f32eee19e26f7cdba5
Antonio Maniero <maniero@klip.net>
2019-09-03 11:43:57 -0300

    https://pt.stackoverflow.com/q/57732/101

---
+++ SQL/Insert/Html.sql
@@ -0,0 +1,11 @@
+INSERT INTO tabela (nome) VALUES ('0 Anos\n3 Dias');
+SELECT nome FROM tabela
+
+INSERT INTO tabela (nome) VALUES ('0 Anos
+3 Dias');
+SELECT nome FROM tabela
+
+INSERT INTO tabela (nome) VALUES ('0 Anos<br>3 Dias');
+SELECT nome FROM tabela
+
+--https://pt.stackoverflow.com/q/57732/101
