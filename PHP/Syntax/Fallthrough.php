ecf70815910a3bbd2cf198c8ff50c0fa0c77bbdf
Antonio Maniero <maniero@klip.net>
2019-09-03 16:03:28 -0300

    https://pt.stackoverflow.com/q/57910/101

---
+++ PHP/Syntax/Fallthrough.php
@@ -0,0 +1,11 @@
+switch ($options) {
+case 1:
+case 2:
+    echo "Valor de opção 1 e 2";
+    break;
+case 3:
+    echo "Valor de opção 3";
+    break;
+}
+
+//https://pt.stackoverflow.com/q/57910/101
