1f88bbae42b7fa0e8806dff28dc8919cce105072
Antonio Maniero <maniero@klip.net>
2019-09-03 16:00:29 -0300

    https://pt.stackoverflow.com/q/57891/101

---
+++ PHP/Syntax/IfElseIf.php
@@ -0,0 +1,36 @@
+<?php
+if (true) 
+	echo "true";
+elseif (false)
+    echo "false";
+echo "\n";
+if (true) 
+	echo "true";
+else if (false)
+    echo "false";
+echo "\n";
+if (true):
+	echo "true";
+elseif (false):
+    echo "false";
+endif;
+echo "\n";
+if (true):
+	echo "true";
+//else if (false): //isto não compilaria
+//    echo "false";
+endif;
+echo "\n";
+if (true) {
+	echo "true";
+} elseif (false) {
+    echo "false";
+}
+echo "\n";
+if (true) {
+	echo "true";
+} else if (false) {
+    echo "false";
+}
+
+//https://pt.stackoverflow.com/q/57891/101
