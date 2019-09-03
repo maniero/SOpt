42e627b24c8c0021f89d7d864e8ae572f82a6e04
Antonio Maniero <maniero@klip.net>
2019-09-03 16:14:14 -0300

    https://pt.stackoverflow.com/q/57943/101

---
+++ C/Console/PrintfFormat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int i;
+	printf("\nDigite as horas : %d"); //pega qualquer coisa para exibir
+	printf("\nDigite as horas : %d", 12); //aqui imprime o 12
+	
+	printf("\nDigite as horas :"); //não tem dados variáveis para imprimir
+	scanf ("%d", &i); //pede o dado aqui
+	return 0;
+}
+
+//https://pt.stackoverflow.com/q/57943/101
