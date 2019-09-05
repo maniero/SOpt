c97377329bc1078b425165fa968a661f8ce4e7ad
Antonio Maniero <maniero@klip.net>
2019-09-05 12:09:10 -0300

    https://pt.stackoverflow.com/q/58192/101

---
+++ PHP/Syntax/SwitchIfArray.php
@@ -0,0 +1,28 @@
+switch ($var) {
+    case 0:
+        //faz algo
+        break;
+    case 1:
+        //faz algo
+        break;
+    case 2:
+        //faz algo
+        break;
+    case 3:
+        //faz algo
+        break;
+}
+
+if ($var == 0) {
+    //faz algo
+} elseif ($var == 1) {
+    //faz algo
+} elseif ($var == 2) {
+    //faz algo
+} elseif ($var == 3) {
+    //faz algo
+}
+
+['stack', 'overflow'][$x]
+
+//https://pt.stackoverflow.com/q/58192/101
