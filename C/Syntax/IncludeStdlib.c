3bcef4ba8dbe7c1e6d9f9fe214bd0acf5d765500
Antonio Maniero <maniero@klip.net>
2019-09-05 13:36:18 -0300

    https://pt.stackoverflow.com/q/58741/101

--- C/Syntax/IncludeStdlib.c
+++ C/Syntax/IncludeStdlib.c
@@ -9,10 +9,11 @@ int main(void) { // <========== o retorno aqui precisa ser um int
     if (str == NULL) printf("Não já espaço para alocar");
     else { 
         strcpy(str, Nome); // <======= acredito que queria usar esta função, não tinha o nome dela
-        printf("Nome=[%s] tem [%d] caracters",str, strlen(Nome));
-        for (int i = 0; i < strlen(Nome); i++) {
-            printf("\nCaracter[%d] = [%c]\n", i , *(str+i));
-            if (((*(str+i) == 'a')|| (*(str+i) == 'e')) || (*(str+i) == 'i') || (*(str+i )== 'o') || (*(str+i) == 'u')) printf("Vogal");
+        int tamanho = strlen(Nome);
+        printf("Nome=[%s] tem [%d] caracters", str, tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            printf("\nCaracter[%d] = [%c]\n", i , str[i]);
+            if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') printf("Vogal");
         }
         free(str);
     }
