c71842584c43f489cb678e0708b4f6b654218005
Antonio Maniero <maniero@klip.net>
2019-09-05 13:27:46 -0300

    https://pt.stackoverflow.com/q/58741/101

---
+++ C/Syntax/IncludeStdlib.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> // <=========== faltava este include
+#define Nome "nome"
+#define TAM_STRING 12
+
+int main(void) { // <========== o retorno aqui precisa ser um int
+    char * str = malloc(TAM_STRING); // <======= a declaração pode e deve ser mais simples
+    if (str == NULL) printf("Não já espaço para alocar");
+    else { 
+        strcpy(str, Nome); // <======= acredito que queria usar esta função, não tinha o nome dela
+        printf("Nome=[%s] tem [%d] caracters",str, strlen(Nome));
+        for (int i = 0; i < strlen(Nome); i++) {
+            printf("\nCaracter[%d] = [%c]\n", i , *(str+i));
+            if (((*(str+i) == 'a')|| (*(str+i) == 'e')) || (*(str+i) == 'i') || (*(str+i )== 'o') || (*(str+i) == 'u')) printf("Vogal");
+        }
+        free(str);
+    }
+}
+
+//https://pt.stackoverflow.com/q/58741/101
