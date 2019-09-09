42096020b0a38f0bd164348fd32de37b169528a6
Antonio Maniero <maniero@klip.net>
2019-09-09 12:40:06 -0300

    https://pt.stackoverflow.com/q/74144/101

---
+++ CSharp/Generics/InterfaceConstraaint.cs
@@ -0,0 +1,7 @@
+public interface IRepository<T> where T : IEnumerable
+
+new IRepository<List>();
+new IRepository<Dicionary>();
+new IRepository<String>();
+
+//https://pt.stackoverflow.com/q/74144/101
