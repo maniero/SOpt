c4717fd4d8563072640cec3a8afc64d63aca629f
Antonio Maniero <maniero@klip.net>
2019-09-10 09:57:32 -0300

    https://pt.stackoverflow.com/q/74506/101

---
+++ CSharp/String/BasicSubstring.cs
@@ -0,0 +1,11 @@
+using static System.Console;
+					
+public class Program {
+	public static void Main() {
+		var nome = "Josénildo da Silva Ramos do Carmo";
+		nome = nome.Substring(0, 20);
+		WriteLine(nome);
+	}
+}
+
+//https://pt.stackoverflow.com/q/74506/101
