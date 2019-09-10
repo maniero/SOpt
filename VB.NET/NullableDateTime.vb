7049128ee6129ccf8ffefd493874c24a05f4f865
Antonio Maniero <maniero@klip.net>
2019-09-10 09:55:35 -0300

    https://pt.stackoverflow.com/q/74399/101

---
+++ VB.NET/NullableDateTime.vb
@@ -0,0 +1,8 @@
+Public Function RetornaDataFormatada(ByVal strValor As String) As DateTime?
+    If strValor = "" Then
+        Return Nothing
+    End If
+    Return Nothing
+End Function
+
+'https://pt.stackoverflow.com/q/74399/101
