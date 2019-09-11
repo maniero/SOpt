b235228949ed049477d9d881d81bf638f15312fc
Antonio Maniero <maniero@klip.net>
2019-09-11 11:40:44 -0300

    https://pt.stackoverflow.com/q/75000/101

---
+++ Java/String/ReplaceIfExists.java
@@ -0,0 +1,9 @@
+class Main {
+	public static void main (String[] args)	{
+		String str = " Hello Word";
+		str = str.replace("Hello", "Olá");
+		System.out.println(str);
+	}
+}
+
+//https://pt.stackoverflow.com/q/75000/101
