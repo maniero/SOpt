f6a7809e6a7dcd0bae59b46f613cd4dc4db3fa08
Antonio Maniero <maniero@klip.net>
2019-09-12 13:44:00 -0300

    https://pt.stackoverflow.com/q/75983/101

---
+++ PHP/Syntax/IndentHTML.php
@@ -0,0 +1,23 @@
+<?php
+
+echo 
+"<div>\n".
+"    <table>\n".
+"        <tr>\n".
+"            <td>\n".
+"            </td>\n".
+"        </tr>\n".
+"    </table>\n".
+"</div>\n";
+
+echo 
+'<div>
+    <table>
+        <tr>
+            <td>
+            </td>
+        </tr>
+    </table>
+</div>';
+
+//https://pt.stackoverflow.com/q/75983/101
