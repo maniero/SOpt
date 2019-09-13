7aa2eec7b151a8f045d76927c8ff11c01b59f7b3
Antonio Maniero <maniero@klip.net>
2019-09-13 10:11:15 -0300

    https://pt.stackoverflow.com/q/76428/101

---
+++ Python/Math/Random.py
@@ -0,0 +1,8 @@
+from random import *
+random.seed() #inicia a semente dos número pseudo randômicos
+random.randrange(0, 9, 2) # pares entre 0 e 9
+random.choice('abcdefghij') # seleciona um dos elementos aleatoriamente
+items = [1, 2, 3, 4, 5, 6, 7]
+random.shuffle(items) # embaralha os itens aleatoriamente
+
+#https://pt.stackoverflow.com/q/76428/101
