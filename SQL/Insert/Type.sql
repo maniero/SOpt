570dd863a945b09eb8cf9e47ee6eab190895fdf0
Antonio Maniero <maniero@klip.net>
2019-09-13 10:02:20 -0300

    https://pt.stackoverflow.com/q/76362/101

---
+++ SQL/Insert/Type.sql
@@ -0,0 +1,7 @@
+INSERT INTO match VALUES(NULL, 5, 0, 'open')
+
+INSERT INTO match VALUES(NULL, $usuario1, $usuario2, 'open')
+
+INSERT INTO `match` VALUES(NULL, $usuario1, $usuario2, 'open')
+
+--https://pt.stackoverflow.com/q/76362/101
