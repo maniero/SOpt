0145ec9772818887c1a8d5ca12d29f6820119e08
Antonio Maniero <maniero@klip.net>
2019-09-16 14:58:45 -0300

    https://pt.stackoverflow.com/q/410715/101

---
+++ C/Algorithm/LoopSum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int contador = 0, soma = 0;
+    while (calculo < 4) {
+    	int numero;
+        printf("Digite o número: ");
+        scanf("%d", &numero);
+        contador++;
+        soma += numero;
+    }
+	printf("A soma entre os valores digitados é de: %d", soma);
+}
+
+//https://pt.stackoverflow.com/q/410715/101
