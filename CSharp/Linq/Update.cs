1a10d8fcc1f095a886bd5c2a6d88258ea0e24ac3
Antonio Maniero <maniero@klip.net>
2019-09-24 11:47:25 -0300

    https://pt.stackoverflow.com/q/80773/101

---
+++ CSharp/Linq/Update.cs
@@ -0,0 +1,11 @@
+foreach (var item in clientes) {
+    item.NomeFantasia.Replace("*", "");
+}
+
+foreach (var item in clientes) {
+    item.NomeFantasia.TrimStart('*');
+}
+
+var resultado = clientes.Select(item => { item.NomeFantasia.TrimStart('*'); return item; })
+
+//https://pt.stackoverflow.com/q/80773/101
