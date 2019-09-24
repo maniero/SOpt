6c27f6dd94e0aed45025a2921c4b7b19c7cbb151
Antonio Maniero <maniero@klip.net>
2019-09-24 09:01:10 -0300

    https://pt.stackoverflow.com/q/412012/101

---
+++ JavaScript/Syntax/If.js
@@ -0,0 +1,13 @@
+var url = window.location.href;
+console.log(url);
+if (url == 'tela03.html') {
+    window.location.href = 'tela04.html';
+} else if (url == 'tela05.html') {
+    window.location.href = 'tela06.html';
+} else if (url == 'tela07.html') {
+    window.location.href = 'tela08.html';
+} else if (url == 'tela09.html') {
+    window.location.href = 'tela10.html';
+}
+
+//https://pt.stackoverflow.com/q/412012/101
