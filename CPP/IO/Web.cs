4736e967b3d5a688942552b3da9bd5438dd33c1e
Antonio Maniero <maniero@klip.net>
2019-09-25 11:39:17 -0300

    https://pt.stackoverflow.com/q/81985/101

---
+++ CPP/IO/Web.cs
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Content-type: text/plain" << endl << endl;
+    cout << "Hello World!" << endl;
+}
+
+//https://pt.stackoverflow.com/q/81985/101
