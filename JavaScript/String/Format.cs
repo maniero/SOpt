c46780795bd439f1ad1fb3b25a1d07661de8d2f9
Antonio Maniero <maniero@klip.net>
2019-09-26 12:28:06 -0300

    https://pt.stackoverflow.com/q/82852/101

---
+++ JavaScript/String/Format.cs
@@ -0,0 +1,13 @@
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) { 
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+            ;
+        });
+    };
+}
+
+//https://pt.stackoverflow.com/q/82852/101
