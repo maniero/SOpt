ecb2936616dac7056529de26f0cbd27b9d99ed71
Antonio Maniero <maniero@klip.net>
2019-09-27 11:48:07 -0300

    https://pt.stackoverflow.com/q/83307/101

---
+++ JavaScript/DOM/HiddenAfterTime.js
@@ -0,0 +1,8 @@
+function esconde() {
+  document.getElementById("exemplo").style.visibility = "hidden";
+}
+setTimeout(esconde, 3000);
+
+<div id="exemplo" style="visibility: visible">Vou sumir em 3 segundos</div>
+
+//https://pt.stackoverflow.com/q/83307/101
