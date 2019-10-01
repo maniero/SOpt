0e138482bb211262b3f84ffb71003eedf7efa15c
Antonio Maniero <maniero@klip.net>
2019-10-01 13:27:14 -0300

    https://pt.stackoverflow.com/q/85554/101

---
+++ JavaScript/HTML_CSS/ActionButton.js
@@ -0,0 +1,17 @@
+<form action="paginas/login.php">
+    <p id="log">Usuário: </p> <input id="cmpG" type="text" name="usuario"/>
+    <p id="log">Senha: </p> <input id="cmpG" type="password" name="senha" size=8 maxlength=8/>
+    <p id="log">A senha deve conter 08 caracteres (alfanumerico)</p>
+    <!-- Barra de botões -->
+    <div id="barra">
+        <input id="bnt" type="submit" value="Login"/></a>
+        <input id="bnt" type="reset" value="Limpar"/>
+    </div>
+</form>
+<form action="paginas/cadastrar.php">
+    <BUTTON >Cadastrar</BUTTON>
+</form>
+
+<BUTTON onclick="window.location.href='paginas/cadastrar.php'">Cadastrar</BUTTON>
+
+<!--https://pt.stackoverflow.com/q/85554/101
