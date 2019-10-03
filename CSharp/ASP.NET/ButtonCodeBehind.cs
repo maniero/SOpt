c6475a6ea0611ee248e9a0981c980b133416a71e
Antonio Maniero <maniero@klip.net>
2019-10-03 10:55:19 -0300

    https://pt.stackoverflow.com/q/86478/101

---
+++ CSharp/ASP.NET/ButtonCodeBehind.cs
@@ -0,0 +1,7 @@
+<asp:Button ID="botao" OnClick="botao_Click" runat="server" />
+
+protected void botao_Click(object sender, EventArgs e) {
+   //faça o que quer aqui
+}
+
+//https://pt.stackoverflow.com/q/86478/101
