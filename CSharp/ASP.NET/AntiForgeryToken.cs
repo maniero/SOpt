d13c15b83b31b5e0111a612a46b37c1c221a36cd
Antonio Maniero <maniero@klip.net>
2019-10-03 07:53:33 -0300

    https://pt.stackoverflow.com/q/86063/101

---
+++ CSharp/ASP.NET/AntiForgeryToken.cs
@@ -0,0 +1,4 @@
+<input name="__RequestVerificationToken" type="hidden"
+    value="saTFWpkKN0BYazFtN6c4YbZAmsEwG0srqlUqqloi/fVgeV2ciIFVmelvzwRZpArs" />
+    
+//https://pt.stackoverflow.com/q/86063/101
