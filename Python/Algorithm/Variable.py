9e8c2c2443601f42602a8b14c539c693c5427796
Antonio Maniero <maniero@klip.net>
2019-10-04 08:00:15 -0300

    https://pt.stackoverflow.com/q/414121/101

---
+++ Python/Algorithm/Variable.py
@@ -0,0 +1,7 @@
+a = int(input('Digite um numero: '))
+b = int(input('Digite outro numero: '))
+c = a - b
+if c > 0:
+    print(f'O numero {c} é maior que Zero')
+    
+#https://pt.stackoverflow.com/q/414121/101
