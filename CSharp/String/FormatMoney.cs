aaae4e65169bb17e76cbf89d3cb7187bc886cb1a
Antonio Maniero <maniero@klip.net>
2019-10-07 07:54:44 -0300

    https://pt.stackoverflow.com/q/414468/101

---
+++ CSharp/String/FormatMoney.cs
@@ -0,0 +1,12 @@
+using static System.Console;
+
+namespace AtividadeRepeticao {
+    class Program2 {
+        static void Main() {
+            decimal valor = 1000.50M;
+            Write($"R$: {valor}");
+        }
+    }
+}
+
+//https://pt.stackoverflow.com/q/414468/101
