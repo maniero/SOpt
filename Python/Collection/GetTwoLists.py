d4ce32cfdb665ceadcd4bcfc5eea70e4d458010a
Antonio Maniero <maniero@klip.net>
2019-10-07 08:26:37 -0300

    https://pt.stackoverflow.com/q/414318/101

---
+++ Python/Collection/GetTwoLists.py
@@ -0,0 +1,7 @@
+nomes = ['CARLOS', 'JOAO', 'PEDRO']
+idades = ['30', '25', '22']
+rows = len(nomes)
+for i in range(rows):
+    print(nomes[i] + ":" + idades[i])
+
+#https://pt.stackoverflow.com/q/414318/101
