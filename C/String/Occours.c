05e32a7699060e9148477e77be48cf2b59a8699c
Antonio Maniero <maniero@klip.net>
2019-10-08 15:07:35 -0300

    https://pt.stackoverflow.com/q/211189/101

---
+++ C/String/Occours.c
@@ -0,0 +1,8 @@
+int ocorrencias(char palavra[], char letra) {
+    int contagem = 0;
+    int = 0;
+    while (palavra[i] != '\0') contagem += letra == palavra[i++];
+    return contagem;
+}
+
+//https://pt.stackoverflow.com/q/211189/101
