569d2a7c2a5a53488dd96272390a65471b87cdcc
Antonio Maniero <maniero@klip.net>
2019-10-08 14:56:08 -0300

    https://pt.stackoverflow.com/q/87980/101

---
+++ CPP/Syntax/Initialization.cpp
@@ -0,0 +1,13 @@
+Carro carro = new Carro;
+
+Carro carro;
+
+Carro carro();
+
+Carro carro("Astra", "Chevrolet");
+
+Carro *carro = new Carro;
+
+Carro *carro = new Carro();
+
+//https://pt.stackoverflow.com/q/87980/101
