de24bb2688b64cd124b35d53b4b7b77e3eaf0517
Antonio Maniero <maniero@klip.net>
2019-10-10 08:06:48 -0300

    https://pt.stackoverflow.com/q/88366/101

---
+++ CSharp/DateTime/GetTimeZone.cs
@@ -0,0 +1,13 @@
+using System;
+using static System.Console;
+					
+public class Program {
+	public static void Main() {
+		var minhaData = DateTime.UtcNow;
+		WriteLine($"Hora universal {minhaData.ToLocalTime()}");
+		var fuso = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+		var diferenca = new DateTimeOffset(minhaData, TimeSpan.Zero);
+		WriteLine($"Hora local {diferenca.ToOffset(fuso.GetUtcOffset(diferenca))}");
+		WriteLine($"Diferença {fuso.GetUtcOffset(diferenca)}");
+	}
+}
