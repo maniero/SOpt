59d02c0b120010a943a6c321fa20e799f5a29f3a
Antonio Maniero <maniero@klip.net>
2019-10-10 09:29:18 -0300

    https://pt.stackoverflow.com/q/89004/101

---
+++ CSharp/ADO.NET/Bit.cs
@@ -0,0 +1,4 @@
+Parameters.Add(New SqlParameter("@Ativo", SqlDbType.bit)).Value =
+                                             Convert.ToInt32(chbAtivo.Checked)
+                                             
+//https://pt.stackoverflow.com/q/89004/101
