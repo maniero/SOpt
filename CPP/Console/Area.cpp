e02fe957a1eb24ff4ca6203286e1ff7cbda478d7
Antonio Maniero <maniero@klip.net>
2019-10-10 13:35:52 -0300

    https://pt.stackoverflow.com/q/89239/101

---
+++ CPP/Console/Area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float lado;
+    cout << "digite o lado";
+    cin >> lado;
+    float area = lado * lado;
+    cout << area;
+}
+
+//https://pt.stackoverflow.com/q/89239/101
