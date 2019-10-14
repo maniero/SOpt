15dd2261ade503c96887defe39b0517f75ecf4d2
Antonio Maniero <maniero@klip.net>
2019-10-14 08:18:24 -0300

    https://pt.stackoverflow.com/q/415179/101

---
+++ Java/Class/InstanceMember.java
@@ -0,0 +1,6 @@
+var endereco = new Endereco();
+endereco.setId(1); // sei lá se isso deveria existir e se deveria ser livre assim
+endereco.setRua("rua");
+c1.setEndereco(endereco);
+
+//https://pt.stackoverflow.com/q/415179/101
