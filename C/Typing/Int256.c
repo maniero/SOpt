864b316d1f86de1e4a0220fe17326f7c73ee4184
Antonio Maniero <maniero@klip.net>
2019-10-15 08:17:57 -0300

    https://pt.stackoverflow.com/q/415812/101

---
+++ C/Typing/Int256.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main() {
+    int valor_1, valor_2;
+    printf("\nDigite um numero para o valor 1: ");
+    scanf("%d", &valor_1);
+    printf("Digite um numero para o valor 2: ");
+    scanf("%d", &valor_2);
+    printf("\nvalor 1: %d", valor_1);
+    printf("\nvalor 2: %d\n", valor_2);
+}
+
+//https://pt.stackoverflow.com/q/415812/101
