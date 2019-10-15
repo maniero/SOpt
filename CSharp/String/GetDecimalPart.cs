d15c7b58509df174e0d9b7229d195a459190f3f2
Antonio Maniero <maniero@klip.net>
2019-10-15 10:11:03 -0300

    https://pt.stackoverflow.com/q/90782/101

---
+++ CSharp/String/GetDecimalPart.cs
@@ -0,0 +1,7 @@
+using static System.Console;
+					
+public class Program {
+	public static void Main() => WriteLine((3.1415.ToString()).Split('.')[1].Length);
+}
+
+//https://pt.stackoverflow.com/q/90782/101
