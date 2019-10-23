f4bddef0f8e006984dceb0d81cd40d6d6d3985c3
Antonio Maniero <maniero@klip.net>
2019-10-23 12:59:58 -0300

    https://pt.stackoverflow.com/q/93815/101

---
+++ VB.NET/Then.vb
@@ -0,0 +1,21 @@
+If x > 0 Then y = 0
+
+Ou mais de um comando em uma linha:
+
+If x > 0 Then y = 0 : z = 0
+
+Bloco de código:
+
+If x > 0
+    y = 0
+    z = 0
+End If
+
+Também funciona igual:
+
+If x > 0 Then
+    y = 0
+    z = 0
+End If
+
+'https://pt.stackoverflow.com/q/93815/101
