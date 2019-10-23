16bbcb6bd1e943bbb76d62b0ea594a9002bf3dca
Antonio Maniero <maniero@klip.net>
2019-10-23 09:52:23 -0300

    https://pt.stackoverflow.com/q/93042/101

---
+++ C/Algorithm/ExitLoop.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int soma(void) {
+    int valor = 0, soma = 0;
+    printf("Foi escolhida a soma:\n\n");
+    do {
+        printf("Informe os valores desejados e 0 (zero) para concluir:");
+        scanf("%d", &valor);
+        soma += valor;
+    } while (valor != 0);
+    return soma;
+}
+
+int mult(void) {
+    int valor = 0, mult = 1;
+    printf("Foi escolhida a multiplicacao:\n\n");
+    do {
+        printf("Informe os valores desejados e 0 (zero) para concluir:");
+        scanf("%d", &valor);
+        if (valor == 0) break;
+        mult *= valor;
+    } while (valor != 0);
+    return mult;
+}
+
+int main() {
+    int op = 0, result;
+    do {
+	    printf("Informe a operacao desejada soma(1) ou produto (2):");
+	    scanf("%d", &op);
+	    if (op == 1) result = soma();
+	    else if (op == 2) result = mult();
+		if (op != 0) printf("O resultado foi: %d", result);
+    } while (op != 0);
+}
+
+//https://pt.stackoverflow.com/q/93042/101
