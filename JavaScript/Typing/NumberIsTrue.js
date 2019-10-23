f04287dfd4be2a57c17974ff97dcfacaac128748
Antonio Maniero <maniero@klip.net>
2019-10-23 10:01:04 -0300

    https://pt.stackoverflow.com/q/91723/101

--- JavaScript/Typing/NumberIsTrue.js
+++ JavaScript/Typing/NumberIsTrue.js
@@ -5,15 +5,11 @@ $(function() {
     }
 });
 
-Se não for usar esta variável local em outros lugares do código (no momento não está usando) e quiser simplificar:
-
 $(function() {
     if (_userdata["session_logged_in"]) {
         $('#rankPersonalizado').after('Você está logado.');
     }
 });
-
-Se não tiver controle sobre o valor desta variável "global" que indica se o usuário está logado, ainda pode transformar em booleano nas demais operações:
 
 $(function() {
     var logado = _userdata["session_logged_in"] == 1;
