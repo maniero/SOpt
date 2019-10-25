1a19b784f4ce9e578f235b9cfab4624895790e17
Antonio Maniero <maniero@klip.net>
2019-10-25 11:26:44 -0300

    https://pt.stackoverflow.com/q/96062/101

---
+++ CSharp/IO/RelativePath.cs
@@ -0,0 +1,6 @@
+Image img = System.Drawing.Image.FromFile(
+           System.Web.HttpContext.Current.Server.MapPath("~/Content/images/SemFoto.jpg"));
+
+System.Web.Hosting.HostingEnvironment.MapPath(path);
+
+//https://pt.stackoverflow.com/q/96062/101
