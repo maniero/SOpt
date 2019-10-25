5f8dd36107fc59f2c68b522c09d5142f3e0f1c26
Antonio Maniero <maniero@klip.net>
2019-10-25 09:23:44 -0300

    https://pt.stackoverflow.com/q/417897/101

---
+++ CPP/Syntax/Terminator.cpp
@@ -0,0 +1,21 @@
+int base_calc(int cb)
+    if( cb < 30 ) {
+    } else if( cb < 20 ) {
+        cb = 30 - 1;
+    } else {
+        cb = 30;
+    }
+    return cb;
+}
+
+int base_calc(int cb)
+    if( cb < 30 )
+        ;
+    else if( cb < 20 )
+        cb = 30 - 1;
+    else
+        cb = 30;
+    return cb;
+}
+
+//https://pt.stackoverflow.com/q/417897/101
