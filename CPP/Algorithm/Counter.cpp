23d3f92d4182a1b8235f64dfa6eefd6ce554a50c
Antonio Maniero <maniero@klip.net>
2019-10-25 14:57:29 -0300

    https://pt.stackoverflow.com/q/95827/101

---
+++ CPP/Algorithm/Counter.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int getnegativo(int a, int b){
+    if (a < 0)  b++;
+    return b;
+}
+
+int main() {
+    setlocale(LC_ALL, "portuguese");
+    int cont, cot = 0;
+    do {
+        cout << "digite um valor: ";
+        cin >> cont;
+
+      cot = getnegativo(cont, cot);
+
+    } while (cont != -1);
+    cout << "número de valores negativos digitados: " << cot;
+    cout << endl << endl;
+}
+
+//https://pt.stackoverflow.com/q/95827/101
