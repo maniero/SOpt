ae863a460a5d12697a0275335b86b00316c3730b
Antonio Maniero <maniero@klip.net>
2019-10-25 12:26:28 -0300

    https://pt.stackoverflow.com/q/96498/101

---
+++ CSharp/WinForms/Inheritance.cs
@@ -0,0 +1,14 @@
+class frmCadastroFuncionario: Views.formulariobase {
+//herdando os botoes e o evento 'onclick'
+    public override void Altera(string nome) {
+        //faça alguma coisa específica aqui
+    }
+}
+
+Class formulariobase {
+    public virtual void Altera(string nome) {
+        //faça alguma coisa
+    }
+}
+
+//https://pt.stackoverflow.com/q/96498/101
