7b1193eef416520fb8fcbcdf44861437ac5258ee
Antonio Maniero <maniero@klip.net>
2019-10-25 15:29:41 -0300

    https://pt.stackoverflow.com/q/96865/101

--- CPP/Algorithm/LoopExit.cpp
+++ CPP/Algorithm/LoopExit.cpp
@@ -1,1 +1,16 @@
-https://ideone.com/L1omFD
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int maior = 0, numero = 0;
+	while (numero >= 0) {
+	    cin >> numero;
+	    if (numero > maior) {
+	    	maior = numero;
+	        cout << "maior ate agora " << maior << endl;
+	    }
+	}
+}
+
+//https://pt.stackoverflow.com/q/96865/101
