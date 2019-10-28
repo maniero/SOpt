ab4f4a2361421f3f278fb21951c3610c035f1a33
Antonio Maniero <maniero@klip.net>
2019-10-28 11:27:19 -0300

    https://pt.stackoverflow.com/q/98057/101

---
+++ PHP/Algorithm/Calculator.php
@@ -0,0 +1,12 @@
+$operator = "+";
+$value1 = 10;
+switch ($operator) {
+    case "+":
+        $value2 = $value1 + 10;
+        break;
+    case "-":
+        $value2 = $value1 - 10;
+        break;
+}
+
+//https://pt.stackoverflow.com/q/98057/101
