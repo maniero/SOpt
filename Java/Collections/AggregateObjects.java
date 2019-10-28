df8d407230b16a4430409426f062ab482b9e65be
Antonio Maniero <maniero@klip.net>
2019-10-28 09:00:04 -0300

    https://pt.stackoverflow.com/q/417923/101

---
+++ Java/Collections/AggregateObjects.java
@@ -0,0 +1,5 @@
+Pessoa p1 = new Pessoa("Marta Pereira");
+p1.setNIF(3454543);
+p1.setContactos(new Telefone[] { new Telefone("Casa", 123456), new Telefone("telemovel", 123456) });
+
+//https://pt.stackoverflow.com/q/417923/101
