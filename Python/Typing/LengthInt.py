ea9457121bb64216708602ecc42362ce1ce0217d
Antonio Maniero <maniero@klip.net>
2019-10-28 09:49:46 -0300

    https://pt.stackoverflow.com/q/418413/101

---
+++ Python/Typing/LengthInt.py
@@ -0,0 +1,9 @@
+while True:
+    mes = input('Digite o mês inicial:')
+    if len(mes) == 2:
+        break;
+    print ('Valor inválido! O mês deve ter 2 dígitos.')
+mes1 = int(mes)
+print(mes1)
+
+#https://pt.stackoverflow.com/q/418413/101
