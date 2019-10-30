1c194b56743fdcff87483ab38716a0a535ce8a26
Antonio Maniero <maniero@klip.net>
2019-10-30 10:00:09 -0300

    https://pt.stackoverflow.com/q/100276/101

---
+++ PHP/OOP/Interface.php
@@ -0,0 +1,12 @@
+interface iArrayList {
+    public function add($item);
+    public function remove($item);
+    public function get($indice);
+    public function size();
+}
+
+class ArrayListDB implements iArrayList
+
+class ArrayListJson implements iArrayList
+
+//https://pt.stackoverflow.com/q/100276/101
