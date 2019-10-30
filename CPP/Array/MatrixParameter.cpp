163996d696806c6aa83ed3783ed6b6bbce08eeed
Antonio Maniero <maniero@klip.net>
2019-10-30 14:38:56 -0300

    https://pt.stackoverflow.com/q/99941/101

---
+++ CPP/Array/MatrixParameter.cpp
@@ -0,0 +1,13 @@
+
+void recebeValor(int size, int mat[][size]) {
+   mat[0][0] = 2;
+}
+
+int main() {
+	int lin = 3;
+	int col = 3;
+	int mat[lin][col];
+	recebeValor(col, mat);
+}
+
+//https://pt.stackoverflow.com/q/99941/101
