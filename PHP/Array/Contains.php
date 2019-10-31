eca1b38761b9d3f244213b6077128c7b461e240e
Antonio Maniero <maniero@klip.net>
2019-10-31 09:09:48 -0300

    https://pt.stackoverflow.com/q/100963/101

---
+++ PHP/Array/Contains.php
@@ -0,0 +1,6 @@
+<?php
+$fthi = array("1", "2", "3", "4", "5", "6", "8", "15", "25", "40");
+if (in_array("25", $fthi)) echo "25 está presente";
+if (in_array("35", $fthi)) echo "35 está presente";
+
+//https://pt.stackoverflow.com/q/100963/101
