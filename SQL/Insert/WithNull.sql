5435c0aa8c0fd02a9b5e4a4a6fa2d9e6c076008a
Antonio Maniero <maniero@klip.net>
2019-11-01 12:32:32 -0300

    https://pt.stackoverflow.com/q/102133/101

---
+++ SQL/Insert/WithNull.sql
@@ -0,0 +1,4 @@
+INSERT INTO Produtos (descricao, marca, preco, Tipo) VALUES ('Geladeira', 'BrasTemp', 700, 'Eletro')
+INSERT INTO Eletrodomesticos (voltagem, CodBarra) VALUES (220, '')
+
+--https://pt.stackoverflow.com/q/102133/101
