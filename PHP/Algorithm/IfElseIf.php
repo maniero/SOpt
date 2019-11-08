0c0c5f9d944e572128f6d0d030553484f01645b5
Antonio Maniero <maniero@klip.net>
2019-11-08 09:28:17 -0300

    https://pt.stackoverflow.com/q/104060/101

---
+++ PHP/Algorithm/IfElseIf.php
@@ -0,0 +1,9 @@
+if ($bike && !$car) {
+    echo "Olar, eu tenho uma bike";
+} elseif ($car && !$bike) {
+    echo "Olar, eu tenho um carro";
+} elseif ($car && $bike) {
+    echo "Olar, eu tenho os dois";
+}
+
+//https://pt.stackoverflow.com/q/104060/101
