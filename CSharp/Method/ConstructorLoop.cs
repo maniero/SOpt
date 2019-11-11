6c262bd83b1a446c09489e560d1b212ebc8eefaa
Antonio Maniero <maniero@klip.net>
2019-11-11 07:31:54 -0300

    https://pt.stackoverflow.com/q/420762/101

---
+++ CSharp/Method/ConstructorLoop.cs
@@ -0,0 +1,14 @@
+public class Program {
+	public static void Main() {
+		new Exemplo();
+	}
+}
+
+public class Exemplo {
+    Exemplo obj;
+    public Exemplo() {
+        obj = new Exemplo();
+    }
+}
+
+//https://pt.stackoverflow.com/q/420762/101
