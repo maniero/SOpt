1d9c8287b0df31d1639c0677414d7a513b607e60
Antonio Maniero <maniero@klip.net>
2019-11-11 13:05:46 -0300

    https://pt.stackoverflow.com/q/105019/101

---
+++ CSharp/ASP.NET/FormatNumber.cs
@@ -0,0 +1,9 @@
+Html.EditorFor(model => model.contatoEfeitvo)
+
+Eu acho este controle até melhor.
+
+Aí no model usa na propriedade do contatoEfeitvo:
+
+[DisplayFormat(DataFormatString = "{N0}")]
+
+//https://pt.stackoverflow.com/q/105019/101
