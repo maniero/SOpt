34d70b0d8b063d61832e0f9f7b1aa391a9b8d179
Antonio Maniero <maniero@klip.net>
2019-11-13 10:12:39 -0300

    https://pt.stackoverflow.com/q/105206/101

---
+++ CPP/Typing/Precison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double num = sqrt(2);
+    cout << fixed;
+    cout.precision(52);
+    cout << num << endl;
+}
+
+//https://pt.stackoverflow.com/q/105206/101
