08a8bd5d674b960109492e0bb71a1e42cb99b173
Antonio Maniero <maniero@klip.net>
2019-11-13 10:15:24 -0300

    https://pt.stackoverflow.com/q/106457/101

---
+++ CSharp/DateTime/Format.cs
@@ -0,0 +1,8 @@
+using static System.Console;
+using static System.Convert;
+					
+public class Program {
+	public static void Main() => WriteLine(ToDateTime("03/25/2015").ToString("dd/MM/yyyy"));
+}
+
+//https://pt.stackoverflow.com/q/106457/101
