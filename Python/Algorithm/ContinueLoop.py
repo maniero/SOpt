4ce29fef07026989474fb14e9231aaa16aab764f
Antonio Maniero <maniero@klip.net>
2019-11-13 18:07:02 -0300

    https://pt.stackoverflow.com/q/421327/101

---
+++ Python/Algorithm/ContinueLoop.py
@@ -0,0 +1,8 @@
+continuar = input("Deseja continuar? ")
+while continuar == 's':
+    print("Você está continuando")
+    continuar = input("Deseja continuar? ")
+else:
+    print("Você saiu")
+    
+#https://pt.stackoverflow.com/q/421327/101
