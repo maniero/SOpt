7b51b83dd9f79ab4fccdb404e723314051c3724c
Antonio Maniero <maniero@klip.net>
2019-11-13 18:08:59 -0300

    https://pt.stackoverflow.com/q/421327/101

---
+++ Python/Algorithm/ContinueLoop2.py
@@ -0,0 +1,7 @@
+while True:
+    continuar = input("Deseja continuar? ")
+    if continuar != 'S' and continuar != 's':
+        break
+print("Encerrou")
+
+#https://pt.stackoverflow.com/q/421327/101
