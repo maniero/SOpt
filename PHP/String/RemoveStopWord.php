48ed6e0417b4ad1e220af5a7fdc53e961b132c56
Antonio Maniero <maniero@klip.net>
2019-11-14 12:23:05 -0300

    https://pt.stackoverflow.com/q/105683/101

---
+++ PHP/String/RemoveStopWord.php
@@ -0,0 +1,5 @@
+<?php
+echo str_replace(array("De ", "Do ", "Dos ", "Da ", "Das "), array("de ", "do ", "dos ", "da ", "das "), ucwords(strtolower("PORTO DE GALINHAS")));
+echo "\n" . str_replace(array("De ", "Do ", "Dos ", "Da ", "Das "), array("de ", "do ", "dos ", "da ", "das "), ucwords(strtolower("joão da silva")));
+
+//https://pt.stackoverflow.com/q/105683/101
