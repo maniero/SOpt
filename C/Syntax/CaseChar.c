1bf5007eb895d05d763675bb05b353ffcbdee6f8
Antonio Maniero <maniero@klip.net>
2019-11-26 16:25:42 -0300

    https://pt.stackoverflow.com/q/109643/101

---
+++ C/Syntax/CaseChar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char op1;
+    scanf("%c", &op1);
+    switch (op1) {
+         case '+':
+             printf("ola");
+    }
+}
+
+//https://pt.stackoverflow.com/q/109643/101
