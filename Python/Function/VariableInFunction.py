a4395700a0213e1167160df63282c9a6ced0558a
Antonio Maniero <maniero@klip.net>
2019-11-26 16:54:54 -0300

    https://pt.stackoverflow.com/q/109893/101

---
+++ Python/Function/VariableInFunction.py
@@ -0,0 +1,10 @@
+def Luggage():
+    mala = []
+    take = input("O que você deseja levar na sua viagem?")
+    mala.append(take)
+    return mala
+
+x = Luggage()
+print(x)
+
+#https://pt.stackoverflow.com/q/109893/101
