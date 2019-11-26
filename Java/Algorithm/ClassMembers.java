f2259325fd5026a0dc83fabb3c1b9d89b20d77d4
Antonio Maniero <maniero@klip.net>
2019-11-26 07:15:12 -0300

    https://pt.stackoverflow.com/q/423428/101

---
+++ Java/Algorithm/ClassMembers.java
@@ -0,0 +1,14 @@
+static int numeroDisciplinas = 0;
+static Scanner entrada = new Scanner(System.in);
+static String[] disciplinas = new String[5];
+
+public class Main {
+    Aluno aluno = new Aluno(); //espero que isto seja usado adequadamente, quem sabe deva estar fora também
+    cadastrarDisciplina(numeroDisciplinas);
+}
+
+public static void cadastrarDisciplina() {
+    numeroDisciplinas + 1;
+}
+
+//https://pt.stackoverflow.com/q/423428/101
