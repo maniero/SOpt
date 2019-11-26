bef2892b644b9cfa36c64e4fe9b80ccc985ab792
Antonio Maniero <maniero@klip.net>
2019-11-26 16:12:56 -0300

    https://pt.stackoverflow.com/q/109625/101

---
+++ CSharp/Syntax/Scope.cs
@@ -0,0 +1,28 @@
+{
+    {
+        int x = 0;
+    }
+
+    {
+        int x = 0;
+    }
+}
+
+{
+    {
+        int x = 0;
+    }
+    int x = 0;
+}
+
+{
+    if (true) { //aqui iria uma condição real
+        int x = 0;
+    }
+
+    {
+        int x = 0;
+    }
+}
+
+//https://pt.stackoverflow.com/q/109625/101
