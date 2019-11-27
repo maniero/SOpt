651e55e98dff9105afb6c51837bee5c9f8572818
Antonio Maniero <maniero@klip.net>
2019-11-27 07:12:00 -0300

    https://pt.stackoverflow.com/q/423756/101

---
+++ JavaScript/Algorithm/StepTwo.js
@@ -0,0 +1,6 @@
+function passandoPelosPares() {
+    for (var i = 0; i <= 6; i+= 2) console.log('aqui eu tenho o valor de ' + i);
+}
+passandoPelosPares();
+
+//https://pt.stackoverflow.com/q/423756/101
