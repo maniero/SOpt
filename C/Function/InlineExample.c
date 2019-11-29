c8b6bacfb0f5e04b6ed95554b6902e852952baca
Antonio Maniero <maniero@klip.net>
2019-11-29 14:23:15 -0300

    https://pt.stackoverflow.com/q/110886/101

---
+++ C/Function/InlineExample.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("\nStack OverFlow\nFunção inline.\n\n");
+    printf("\nStack OverFlow\nFunção comum.\n\n");
+}
+
+//https://pt.stackoverflow.com/q/110886/101
