3efc70ec8b445ec7fdad5309117fe848fe645cf2
Antonio Maniero <maniero@klip.net>
2019-12-05 13:16:54 -0300

    https://pt.stackoverflow.com/q/114590/101

---
+++ VB.NET/IO/FormLoad.vb
@@ -0,0 +1,5 @@
+Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    turmas.add(New turma() With { .id_turma = 1 })
+End Sub
+
+'https://pt.stackoverflow.com/q/114590/101
