941832e480ed7a4ae84e492f9dcb6724e80acbb8
Antonio Maniero <maniero@klip.net>
2019-12-06 11:16:00 -0300

    https://pt.stackoverflow.com/q/111739/101

---
+++ Java/String/isDigit.java
@@ -0,0 +1,10 @@
+    class Main {
+    	public static void main (String[] args) {
+    		String x = "1234";
+    		System.out.println(Character.isDigit(x.charAt(0)));
+    		x = "a123";
+    		System.out.println(Character.isDigit(x.charAt(0)));
+    	}
+    }
+     
+    //https://pt.stackoverflow.com/q/111739/101
