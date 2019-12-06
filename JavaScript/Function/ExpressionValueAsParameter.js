77d238d5d0bffad5eb15c5dcbce3dc0171c10630
Antonio Maniero <maniero@klip.net>
2019-12-06 07:22:57 -0300

    https://pt.stackoverflow.com/q/425341/101

---
+++ JavaScript/Function/ExpressionValueAsParameter.js
@@ -0,0 +1,6 @@
+const soma = function(a, b) { return a + b; }
+let nun1 = 3;
+const soma2 = function(soma, nun1) { return soma + nun1 }
+console.log(soma2(soma(2, 4), nun1));
+
+//https://pt.stackoverflow.com/q/425341/101
