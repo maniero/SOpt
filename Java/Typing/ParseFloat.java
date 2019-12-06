1e32deefd9944b04f1b06f28439e94fc017d713e
Antonio Maniero <maniero@klip.net>
2019-12-06 10:33:47 -0300

    https://pt.stackoverflow.com/q/111908/101

---
+++ Java/Typing/ParseFloat.java
@@ -0,0 +1,12 @@
+class Main {
+	public static void main (String[] args) {
+	    float n1 = Float.parseFloat("7.5");
+        float n2 = Float.parseFloat("8.5");
+        float n3 = Float.parseFloat("3.5");
+        float n4 = Float.parseFloat("7.5");
+        float notaf = (n1+n2+n3+n4)/4;
+        System.out.println(notaf);
+	}
+}
+
+//https://pt.stackoverflow.com/q/111908/101
