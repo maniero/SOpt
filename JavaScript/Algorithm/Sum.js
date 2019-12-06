445abc137c2491b87b15b28f9bc8f73125223697
Antonio Maniero <maniero@klip.net>
2019-12-06 07:15:02 -0300

    https://pt.stackoverflow.com/q/425457/101

---
+++ JavaScript/Algorithm/Sum.js
@@ -0,0 +1,9 @@
+function somaDosPares(x) {
+    let soma = 0;
+    for (let i = 0; i <= x; i += 2) soma += i
+    return soma;
+}
+
+console.log(somaDosPares(6));
+
+//https://pt.stackoverflow.com/q/425457/101
