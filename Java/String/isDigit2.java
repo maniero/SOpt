e493009401c42501783c7a54e6e74fb97974e3ea
Antonio Maniero <maniero@klip.net>
2019-12-06 11:15:58 -0300

    https://pt.stackoverflow.com/q/111739/101

---
+++ Java/String/isDigit2.java
@@ -0,0 +1,10 @@
+    class Main {
+    	public static void main (String[] args) {
+    		String x = "1234";
+    		System.out.println(x.charAt(0) >= '0' && x.charAt(0) <= '9');
+    		x = "a123";
+    		System.out.println(x.charAt(0) >= '0' && x.charAt(0) <= '9');
+    	}
+    }
+     
+    //https://pt.stackoverflow.com/q/111739/101
