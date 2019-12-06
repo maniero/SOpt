c235afc77231493ca3e8793b4ceeaaf0bf028562
Antonio Maniero <maniero@klip.net>
2019-12-06 15:39:29 -0300

    https://pt.stackoverflow.com/q/115809/101

---
+++ Java/Math/BigInteger.java
@@ -0,0 +1,11 @@
+import java.math.*;
+
+class Main {
+	public static void main(String[] args) {
+	    BigInteger x = new BigInteger("12345678901234567890");
+	    BigInteger y = new BigInteger("9876543210");
+		System.out.println(x.multiply(y));
+    }
+}
+
+//https://pt.stackoverflow.com/q/115809/101
