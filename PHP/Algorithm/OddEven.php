02264301c3994e2f6904d88825619dbf01212257
Antonio Maniero <maniero@klip.net>
2019-12-06 12:27:36 -0300

    https://pt.stackoverflow.com/q/110925/101

---
+++ PHP/Algorithm/OddEven.php
@@ -0,0 +1,7 @@
+$par_ou_impar = array(2,3,4,56,5,42,98,100);
+ for ($i = 0; $i < count($par_ou_impar); $i++){
+    if ($par_ou_impar[$i] % 2 == 0) echo "O Número é par: $par_ou_impar[$i]<br />";
+    else echo "O Número é impar: $par_ou_impar[$i]<br />";
+ }
+
+//https://pt.stackoverflow.com/q/110925/101
