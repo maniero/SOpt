08c980cfbd717e3a9e63ea6b4477948d564cf706
Antonio Maniero <maniero@klip.net>
2019-12-09 08:05:25 -0300

    https://pt.stackoverflow.com/q/116370/101

---
+++ Java/Array/SizeUndeterminated.java
@@ -0,0 +1,8 @@
+ArrayList<Teste> t = new ArrayList<>(10);
+t.add(new Teste());
+t.get(0).nome = nome1;
+System.out.println(t.get(0).nome);
+t.add(new Teste());
+t.get(1).nome = nome1;
+
+//https://pt.stackoverflow.com/q/116370/101
