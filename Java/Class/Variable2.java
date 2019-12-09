2ded430bf66d9146bb335c01dde5bf9b7e1f907e
Antonio Maniero <maniero@klip.net>
2019-12-09 09:09:45 -0300

    https://pt.stackoverflow.com/q/117337/101

---
+++ Java/Class/Variable2.java
@@ -0,0 +1,8 @@
+public void run() {
+    Calculadora calc = new Calculadora();
+    calc.perguntarValores();
+    calc.calcular();
+    calc.exibirResultados();
+}
+
+//https://pt.stackoverflow.com/q/117337/101
