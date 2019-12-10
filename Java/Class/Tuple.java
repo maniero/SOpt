87f13a061d735942a55e4b23af283f67302349d7
Antonio Maniero <maniero@klip.net>
2019-12-10 14:14:08 -0300

    https://pt.stackoverflow.com/q/118237/101

---
+++ Java/Class/Tuple.java
@@ -0,0 +1,10 @@
+public class Tuple<X, Y> { 
+    public final X x; 
+    public final Y y; 
+    public Tuple(X x, Y y) { 
+        this.x = x; 
+        this.y = y; 
+    } 
+}
+
+//https://pt.stackoverflow.com/q/118237/101
