7a43cb017f92c9f6c3ba45f355687cd73cb410ff
Antonio Maniero <maniero@klip.net>
2019-12-10 14:27:47 -0300

    https://pt.stackoverflow.com/q/118429/101

---
+++ CPP/Vector/Initialization.cpp
@@ -0,0 +1,14 @@
+std::fill_n(array, 100, 0);
+
+int array[100] = {0};
+
+int array[100] = {};
+
+int array[100] = { [0 ... 99] = 0 };
+
+int array[100];
+memset(array, 0, sizeof(array));
+
+std::vector<int> vector1(100, 0);
+
+//https://pt.stackoverflow.com/q/118429/101
