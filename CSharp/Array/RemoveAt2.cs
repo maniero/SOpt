b0d6ef03772ba15ac968649f6d9d826fe551d8bf
Antonio Maniero <maniero@klip.net>
2019-12-10 14:05:53 -0300

    https://pt.stackoverflow.com/q/117570/101

---
+++ CSharp/Array/RemoveAt2.cs
@@ -0,0 +1,8 @@
+public static T[] RemoveAt<T>(this T[] source, int index) {
+    T[] dest = new T[source.Length - 1];
+    if(index > 0) Array.Copy(source, 0, dest, 0, index);
+    if(index < source.Length - 1) Array.Copy(source, index + 1, dest, index, source.Length - index - 1);
+    return dest;
+}
+
+//https://pt.stackoverflow.com/q/117570/101
