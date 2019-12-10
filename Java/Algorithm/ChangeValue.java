119e613cb9f7037d72a7b3f1d0e9ee040a5036d6
Antonio Maniero <maniero@klip.net>
2019-12-10 14:08:35 -0300

    https://pt.stackoverflow.com/q/117836/101

---
+++ Java/Algorithm/ChangeValue.java
@@ -0,0 +1,8 @@
+Jogador Player2;
+if (menu == 1) { Player2 = new Ryu(); }
+else if (menu == 2) { Player2 = new Blanka(); }
+else if (menu == 3) { Player2 = new Zangief(); }
+else if (menu == 4) { Player2 = new ChunLi(); }
+else if (menu == 5) { Player2 = new Ken(); }
+
+//https://pt.stackoverflow.com/q/117836/101
