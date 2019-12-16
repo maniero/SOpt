c302c04f5f45ca859651f4bf6a402277759548ec
Antonio Maniero <maniero@klip.net>
2019-12-16 14:36:48 -0300

    https://pt.stackoverflow.com/q/120248/101

---
+++ Java/Typing/NullPointerException.java
@@ -0,0 +1,6 @@
+public class Principal extends AppCompatActivity {
+    private Button btn;
+    private EditText texto;
+    BancodeDados DB = new BancodeDados(); // <===================== aqui
+    
+//https://pt.stackoverflow.com/q/120248/101
