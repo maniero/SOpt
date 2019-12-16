2db746e1902926fc471da66be4dd2afa614c9b5e
Antonio Maniero <maniero@klip.net>
2019-12-16 16:24:20 -0300

    https://pt.stackoverflow.com/q/120231/101

---
+++ CPP/Vector/AddObject.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Nome {
+	protected:
+    	string nome;
+    public:
+        Nome(string _nome) {
+			nome = _nome;
+		}
+        virtual void exibirNome() = 0;
+};
+class SobreNome : public Nome {
+    string sobre_nome;
+	public:
+	    SobreNome(string nome, string _sobre_nome) : Nome(nome) {
+		    sobre_nome = _sobre_nome;
+		}
+	    void exibirNome() {
+			cout << nome << " " << sobre_nome;
+		}
+};
+
+int main() {
+    vector<Nome*> *nome = new vector<Nome*>;
+    Nome *n = new SobreNome("João", "Alves");
+    nome->push_back(n);
+    nome->at(0)->exibirNome();
+}
+
+//https://pt.stackoverflow.com/q/120231/101
