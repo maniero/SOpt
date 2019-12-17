167d0159984307d6fb2e34a19f384113cd04f8a8
Antonio Maniero <maniero@klip.net>
2019-12-17 09:52:58 -0300

    https://pt.stackoverflow.com/q/121101/101

---
+++ C/Algorithm/Variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int x, y;
+	printf("Entre com 2 números: ");
+	scanf("%i %i", &x, &y);
+	int z = x + y;
+	printf("\n%i", z);
+}
+
+//https://pt.stackoverflow.com/q/121101/101
