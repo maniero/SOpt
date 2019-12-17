cfe418ef7f3a5bcb55678fe048ae7e7b616c1f54
Antonio Maniero <maniero@klip.net>
2019-12-17 10:16:17 -0300

    https://pt.stackoverflow.com/q/121135/101

---
+++ JavaScript/Browser/DatePicker.js
@@ -0,0 +1,12 @@
+<script>
+    $(document).ready(function() {
+        $('#dataSolicitacao').datepicker({
+            format: "DD/MM/yyyy",
+            language: "pt-BR",
+            minViewMode: 0,
+            orientation: auto
+        });
+    });
+</script>
+
+//https://pt.stackoverflow.com/q/121135/101
