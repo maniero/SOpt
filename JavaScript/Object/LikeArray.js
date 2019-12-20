ca2c42bd076707b9f08b40f821e43717f12d9896
Antonio Maniero <maniero@klip.net>
2019-12-20 13:29:59 -0300

    https://pt.stackoverflow.com/q/122341/101

---
+++ JavaScript/Object/LikeArray.js
@@ -0,0 +1,8 @@
+var meuObjeto = {
+    a: 1,
+    "b": 2
+};
+for (var chave in meuObjeto) console.log("key " + chave + "; value " + meuObjeto[chave]);
+console.log("tipo: " + typeof(meuObjeto));
+
+//https://pt.stackoverflow.com/q/122341/101
