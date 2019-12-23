06b51893e7169d8ed1d23b9c7fbeef8619f12297
Antonio Maniero <maniero@klip.net>
2019-12-23 12:15:48 -0300

    https://pt.stackoverflow.com/q/122832/101

---
+++ JavaScript/Array/In.js
@@ -0,0 +1,7 @@
+if ([1, 2, 5].indexOf(tipoDemissao) > -1) valorDemissao = 525.20;
+
+if ($.inArray(tipoDemissao, [1, 2, 5])) valorDemissao = 525.20;
+
+if ([1, 2, 5].includes(tipoDemissao)) valorDemissao = 525.20;
+
+//https://pt.stackoverflow.com/q/122832/101
