1304e6a6f19ddf2f2453ea97c9bfafba0e999645
Antonio Maniero <maniero@klip.net>
2019-12-23 12:18:06 -0300

    https://pt.stackoverflow.com/q/122908/101

---
+++ CSharp/Enviroment/StartProcess.cs
@@ -0,0 +1,5 @@
+var startInfo = new ProcessStartInfo("IntegradorTEF-IP.exe");
+startInfo.WorkingDirectory = @"C:\Program Files (x86)\IntegradorTEF-IP";
+Process.Start(startInfo);
+
+//https://pt.stackoverflow.com/q/122908/101
