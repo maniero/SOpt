ebc49df8b115b22e934d6d39b9bc29b0b8982b46
Antonio Maniero <maniero@klip.net>
2020-01-06 10:02:54 -0300

    https://pt.stackoverflow.com/q/125863/101

---
+++ Python/Syntax/GlobalVariable.py
@@ -0,0 +1,12 @@
+donos = []
+def calcula_media():
+    lista_dicionario = [1,2,3]
+    donos.extend(lista_dicionario)
+
+def funcao2():
+    print(donos)
+
+calcula_media()
+funcao2()
+
+#https://pt.stackoverflow.com/q/125863/101
