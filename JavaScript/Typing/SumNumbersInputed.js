b56907adc8d61cb4fdab01219277e9d30e81f1f4
Antonio Maniero <maniero@klip.net>
2020-01-06 09:39:01 -0300

    https://pt.stackoverflow.com/q/126163/101

---
+++ JavaScript/Typing/SumNumbersInputed.js
@@ -0,0 +1,11 @@
+num1 = parseInt(prompt("Digite um número ",""));
+num2 = parseInt(prompt("Digite outro número para somar ",""));
+
+function soma(){
+    var res = num1 + num2;
+    alert("Reultado da Operação "+ res);
+}
+
+soma();
+
+//https://pt.stackoverflow.com/q/126163/101
