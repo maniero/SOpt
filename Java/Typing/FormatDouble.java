add87a75ae5b2f6d5a9682232779f9070ecdf9d8
Antonio Maniero <maniero@klip.net>
2020-01-08 13:43:58 -0300

    https://pt.stackoverflow.com/q/126681/101

---
+++ Java/Typing/FormatDouble.java
@@ -0,0 +1,10 @@
+class Ideone {
+	public static void main (String[] args) {
+		String teste = "101010101";
+		double number = Double.parseDouble(teste);
+		System.out.println("The number is: " + number);
+		System.out.printf("The number is: %f", number);
+	}
+}
+
+//https://pt.stackoverflow.com/q/126681/101
