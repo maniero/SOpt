2777d480efb945b7dc40b41b43bc7b2f6b0fe670
Antonio Maniero <maniero@klip.net>
2020-01-08 13:40:06 -0300

    https://pt.stackoverflow.com/q/126793/101

---
+++ C/Struct/Date.c
@@ -0,0 +1,12 @@
+typedef struct {
+    int ano;
+    short mes;
+    short dia;
+} Data
+struct pessoa {
+    char nome;
+    double cpf;
+    Data data_nasc;
+} x,*ptr;
+
+//https://pt.stackoverflow.com/q/126793/101
