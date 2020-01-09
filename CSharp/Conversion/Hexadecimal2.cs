468ba02830aae9d94b346644e032d53d77c610e3
Antonio Maniero <maniero@klip.net>
2020-01-09 08:22:04 -0300

    https://pt.stackoverflow.com/q/127961/101

---
+++ CSharp/Conversion/Hexadecimal2.cs
@@ -0,0 +1,5 @@
+public class Program {
+	public static void Main() => System.Console.WriteLine(System.Convert.ToString(System.Convert.ToInt32("4F56A", 16), 2));
+}
+
+//https://pt.stackoverflow.com/q/127961/101
