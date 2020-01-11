29f67179747391206a88ab7b90032a97ca94c3ea
Antonio Maniero <maniero@klip.net>
2020-01-11 11:58:16 -0300

    https://pt.stackoverflow.com/q/430383/101

---
+++ Java/Array/LinearSearch.java
@@ -0,0 +1,8 @@
+class Main {
+	public static void main (String[] args) {
+        int vet[] = { 3, 7, 6, 1, 9, 4, 5 };
+        for (int i = 0; i < vet.length; i++) if (vet[i] == 3) System.out.println(i);
+    }
+}
+
+//https://pt.stackoverflow.com/q/430383/101
