99c71304a577488d028cd66c43d60d733ec2335d
Antonio Maniero <maniero@klip.net>
2020-01-13 09:56:02 -0300

    https://pt.stackoverflow.com/q/430322/101

---
+++ JavaScript/Object/AssociativeArray.js
@@ -0,0 +1,11 @@
+var pessoa = {
+    nome: "João",
+    animais: {
+        cachorro: "Rex",
+        gato: "Pipoca",
+    }
+}
+
+console.log(pessoa["animais"]["gato"]);
+
+//https://pt.stackoverflow.com/q/430322/101
