e54189cc28e991a466377bb4367d768c9548854b
Antonio Maniero <maniero@klip.net>
2020-01-14 08:57:16 -0300

    https://pt.stackoverflow.com/q/128614/101

---
+++ C/String/Comparing.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *substring(char *str, char *buf, int start, int end) {
+    for (int i = start; i < end; i++) buf[i] = str[i];
+    return buf;
+}
+
+int main() {
+	char *texto = malloc(6);
+    if (strncmp(substring("Hello World", texto, 0, 5), "Hello", 5) == 0) printf("yep");
+    free(texto);
+}
+
+//https://pt.stackoverflow.com/q/128614/101
