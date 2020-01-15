434187ba55886988b553c160482e5537b70cfa63
Antonio Maniero <maniero@klip.net>
2020-01-15 14:38:49 -0300

    https://pt.stackoverflow.com/q/130707/101

---
+++ Java/Array/Object.java
@@ -0,0 +1,12 @@
+class Main {
+	public static void main (String[] args)	{
+		Object[] array = new Object[5];
+		array[0] = 0;
+		array[1] = "texto";
+		array[2] = 12.34;
+		array[3] = 98.76f;
+		array[4] = 1;
+	}
+}
+
+//https://pt.stackoverflow.com/q/130707/101
