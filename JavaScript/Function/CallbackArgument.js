1615325ef7844da86dac1ab945f7c3c4d71efbfa
Antonio Maniero <maniero@klip.net>
2020-01-15 08:24:34 -0300

    https://pt.stackoverflow.com/q/430811/101

---
+++ JavaScript/Function/CallbackArgument.js
@@ -0,0 +1,33 @@
+unction rand(min = 1, max = 2){
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function f1(callback){
+    setTimeout(function(){
+        console.log('f1')
+        if(callback)
+            callback()
+    }, rand())
+}
+
+function f2(callback){
+    setTimeout(function(){
+        console.log('f2')
+        if(callback)
+            callback()
+    , rand()})
+}
+
+function f3(callback){
+    setTimeout(function(){
+        console.log('f3')
+        if(callback)
+            callback()
+    }, rand())
+}
+
+function imprime() {
+    console.log('Terminei.')
+}
+
+f1(f2(f3(imprime)));
