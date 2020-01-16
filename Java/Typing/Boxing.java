cadaffa5c279126991b5b2bad37d3091792f8465
Antonio Maniero <maniero@klip.net>
2020-01-16 15:40:11 -0300

    https://pt.stackoverflow.com/q/131136/101

---
+++ Java/Typing/Boxing.java
@@ -0,0 +1,11 @@
+class Main {
+	public static void main (String[] args)	{
+		double d1 = 10;
+		Double d2 = (double)10;
+		Double d3 = 10.0;
+		Double d4 = 10d;
+		Double d5 = new Double(10);
+	}
+}
+
+//https://pt.stackoverflow.com/q/131136/101
