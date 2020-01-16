663e2b24ea88842677b46ff1d9b95208f34e9acb
Antonio Maniero <maniero@klip.net>
2020-01-16 16:11:37 -0300

    https://pt.stackoverflow.com/q/131079/101

---
+++ Java/Algorithm/ShuffleLetters.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+ 
+class Ideone {
+	public static void main (String[] args) {
+		char mat[][] = new char[5][5];
+		List<Integer> random = randomNumbers(65, 90, 25);
+		int posicao  = 0;
+        for (int ctlin = 0; ctlin < 5; ctlin++)
+            for (int ctcol = 0; ctcol < 5; ctcol++) mat[ctlin][ctcol] = (char)(int)random.get(posicao++);
+        for (int ctlin = 0; ctlin < 5; ctlin++) {
+            for (int ctcol = 0; ctcol < 5; ctcol++) System.out.print(mat[ctlin][ctcol] + " ");
+            System.out.println();
+        }       
+	}
+    public static List<Integer> randomNumbers(int start, int end, int count) {
+		List<Integer> lista = new ArrayList<>(end - start + 1);
+		for (int i = start; i <= end; i++) lista.add(i);
+		}
+		Collections.shuffle(lista);
+		lista = lista.subList(0, count);
+		return lista;
+    }
+}
+
+//https://pt.stackoverflow.com/q/131079/101
