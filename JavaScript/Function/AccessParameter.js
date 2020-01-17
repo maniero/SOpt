995bd024df869f18e31414236e10fce336b2b170
Antonio Maniero <maniero@klip.net>
2020-01-17 13:26:06 -0300

    https://pt.stackoverflow.com/q/132156/101

---
+++ JavaScript/Function/AccessParameter.js
@@ -0,0 +1,12 @@
+function criarDataset(field, constraint, sorFields) {
+    totalTarefasAtrasadas = buscarTarefasAtrasadas();
+    function buscarTarefasAtrasadas() {
+        usuario = constraint;
+        console.log(usuario);
+        return 6;
+    }
+}
+
+criarDataset("a", "b", "c");
+
+//https://pt.stackoverflow.com/q/132156/101
