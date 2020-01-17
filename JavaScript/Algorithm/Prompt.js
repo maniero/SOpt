ce84a3bebceafa2d9b8372b1dca0e291eeece5a7
Antonio Maniero <maniero@klip.net>
2020-01-17 13:24:08 -0300

    https://pt.stackoverflow.com/q/132058/101

---
+++ JavaScript/Algorithm/Prompt.js
@@ -0,0 +1,4 @@
+var numeroPensado = Math.round(Math.random() * 10);
+document.write((prompt("Qual e o numero? ") ==  numeroPensado ? "Acertou! " : "Erro! ") + numeroPensado + " era o numero");
+
+//https://pt.stackoverflow.com/q/132058/101
