7cb899539eb1ae53184150b380382729166a5ff1
Antonio Maniero <maniero@klip.net>
2020-01-20 12:46:47 -0300

    https://pt.stackoverflow.com/q/134169/101

---
+++ CSharp/Collection/Dictionary2.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+					
+public class Program {
+	public static void Main() {
+		var ht = new Dictionary<string, string> { ["index"] = "0", ["tipo"] = "1" };
+		string tipo = ht["tipo"];
+	}
+}
+
+//https://pt.stackoverflow.com/q/134169/101
