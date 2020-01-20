a6e068d1ec739c08cf176b81ccce30cda258f42c
Antonio Maniero <maniero@klip.net>
2020-01-20 08:28:58 -0300

    https://pt.stackoverflow.com/q/431508/101

---
+++ Python/String/FStringVariable.py
@@ -0,0 +1,9 @@
+def titulo(msg):
+  tam = len(msg) + 4
+  print('=' * tam)
+  print(f'{msg:^{tam}}')
+  print('=' * tam)
+
+titulo('TRATAMENTO DE ERROS E EXCESSÕES')
+
+//https://pt.stackoverflow.com/q/431508/101
