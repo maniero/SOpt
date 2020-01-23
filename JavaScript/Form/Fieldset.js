6a3384e54286ad2a6056fce0af4d94a60c66703c
Antonio Maniero <maniero@klip.net>
2020-01-23 07:52:33 -0300

    https://pt.stackoverflow.com/q/431606/101

---
+++ JavaScript/Form/Fieldset.js
@@ -0,0 +1,15 @@
+fieldset {
+  background-color: #D0D0D0;
+  max-width: 300px;
+  padding: 12px;	
+}
+<form>
+   <fieldset>
+      <legend>Endereço</legend>
+      Tipo Logradouro: <input type = "text"><br/>
+      Logradouro:<input type = "text"><br/>
+      Número:<input type = "text">
+   </fieldset>
+</form>
+
+<!-- https://pt.stackoverflow.com/q/431606/101
