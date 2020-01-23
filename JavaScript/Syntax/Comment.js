c6e930bb468be6afa1b40153b5453316112fd5a2
Antonio Maniero <maniero@klip.net>
2020-01-23 08:29:31 -0300

    https://pt.stackoverflow.com/q/136257/101

---
+++ JavaScript/Syntax/Comment.js
@@ -0,0 +1,13 @@
+Tem um texto aqui <!-- the middle of --> com comentário no meio.
+
+<!--
+Pode ter
+várias linhas de
+comentário
+-->
+
+<div class="exemplo">
+Pode ter uma construção normal e simular o *inline* desde que abra e feche.<br>
+</div> <!-- /.exemplo -->
+
+<!-- https://pt.stackoverflow.com/q/136257/101
