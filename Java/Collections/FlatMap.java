cea62b66bfcb02e71c25978519432fff555287bd
Antonio Maniero <maniero@klip.net>
2020-01-24 06:48:40 -0300

    https://pt.stackoverflow.com/q/432269/101

--- Java/Collections/FlatMap.java
+++ Java/Collections/FlatMap.java
@@ -1,1 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+class Main {
+	public static void main(String[] args) {
+		List<List<Integer>> listOfListofInts = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), Arrays.asList(7, 8, 9));
+		System.out.println(listOfListofInts.stream().flatMap(List::stream).collect(Collectors.toList()));
+		System.out.println(listOfListofInts.stream().map(List::stream).collect(Collectors.toList()));
+	}
+}
+
+//https://pt.stackoverflow.com/q/432269/101
