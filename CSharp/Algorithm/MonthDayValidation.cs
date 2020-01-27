07b704eb526c01242a4415d16b26951f722aa92f
Antonio Maniero <maniero@klip.net>
2020-01-27 11:07:21 -0300

    https://pt.stackoverflow.com/q/432738/101

---
+++ CSharp/Algorithm/MonthDayValidation.cs
@@ -0,0 +1,18 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		while (true) {
+			WriteLine("Em que dia você nasceu?");
+			if (int.TryParse(ReadLine(), out var dia) && dia < 32) break;
+			WriteLine("Digite dia válido");
+		}
+		while (true) {
+			WriteLine("Em que mês você nasceu?");
+			if (int.TryParse(ReadLine(), out var dia) && dia < 32) break;
+			WriteLine("Digite mês válido");
+		}
+	}
+}
+
+//https://pt.stackoverflow.com/q/432738/101
