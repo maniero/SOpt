1fb990e8de17d2afbd81d11d21fe39c2a5bd929d
Antonio Maniero <maniero@klip.net>
2020-01-27 10:08:21 -0300

    https://pt.stackoverflow.com/q/432376/101

---
+++ JavaScript/Algorithm/ConcatOptions2.js
@@ -0,0 +1,6 @@
+let number = 18;
+console.log(parseInt((number.toString() + '000000000').substring(0, 8)));
+number = 678;
+console.log(parseInt((number.toString() + '000000000').substring(0, 8)));
+
+//https://pt.stackoverflow.com/q/432376/101
