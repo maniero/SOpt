a3d0fe360ffac20ab355bbf873d8e5fe28ab6b09
Antonio Maniero <maniero@klip.net>
2020-01-27 10:07:46 -0300

    https://pt.stackoverflow.com/q/432376/101

---
+++ JavaScript/Algorithm/ConcatOptions.js
@@ -0,0 +1,6 @@
+let numero = 18;
+console.log(numero * Math.pow(10, 8 - Math.log(numero) * Math.LOG10E + 1 | 0));
+numero = 678;
+console.log(numero * Math.pow(10, 8 - Math.log(numero) * Math.LOG10E + 1 | 0));
+
+//https://pt.stackoverflow.com/q/432376/101
