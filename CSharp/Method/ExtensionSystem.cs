c071b10ecac6cb6954ac0737f5ae3695adca029e
Antonio Maniero <maniero@klip.net>
2020-01-28 13:20:29 -0300

    https://pt.stackoverflow.com/q/136913/101

---
+++ CSharp/Method/ExtensionSystem.cs
@@ -0,0 +1,7 @@
+namespace System {
+    public static class ObjectExt {
+        pubic static bool IsNull(this object source) => source == null;
+    }
+}
+
+//https://pt.stackoverflow.com/q/136913/101
