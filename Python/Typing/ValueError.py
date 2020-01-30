93176699c5ca4bd52fa1baa6bc1fc5343ce0dc09
Antonio Maniero <maniero@klip.net>
2020-01-30 09:19:09 -0300

    https://pt.stackoverflow.com/q/433050/101

---
+++ Python/Typing/ValueError.py
@@ -0,0 +1,7 @@
+try:
+    a = int(input("insira um número :"))
+    print ("seu número foi :" + str(a))
+except:
+	print ("isto não é um numero inteiro")
+
+#https://pt.stackoverflow.com/q/433050/101
