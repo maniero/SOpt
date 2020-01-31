fbfba153ee9d1ee9a4e05ae875be3dc544a69c4e
Antonio Maniero <maniero@klip.net>
2020-01-31 13:41:21 -0300

    https://pt.stackoverflow.com/q/433623/101

---
+++ Python/Algorithm/WhileUpper.py
@@ -0,0 +1,7 @@
+while True:
+    n = input('Texto errado, digite tudo em maiúscula: ')
+    if n.isupper():
+        print('Texto correto')
+        break
+
+#https://pt.stackoverflow.com/q/433623/101
