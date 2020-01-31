c2037866dbe4efd6e7b35ee70a35c624f699591f
Antonio Maniero <maniero@klip.net>
2020-01-31 07:34:44 -0300

    https://pt.stackoverflow.com/q/433462/101

---
+++ Python/Algorithm/ValidationRange.py
@@ -0,0 +1,8 @@
+try:
+   a = int(input(a))
+   if a > 0 and a < 7:
+       print ("o valor deve ser entre 1 e 6")
+except ValueError:
+    print ("Escolha uma opção válida")
+    
+#https://pt.stackoverflow.com/q/433462/101
