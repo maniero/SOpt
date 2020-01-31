912bb12b0a824cb934ba693b935400fea1111797
Antonio Maniero <maniero@klip.net>
2020-01-31 09:43:28 -0300

    https://pt.stackoverflow.com/q/137323/101

---
+++ Java/Syntax/Methods.java
@@ -0,0 +1,11 @@
+class Main {
+	public static void main (String[] args) {
+		String x = teste().toString();
+		System.out.println(x);
+	}
+	public static Integer teste() {
+		return 42;
+	}
+}
+
+//https://pt.stackoverflow.com/q/137323/101
