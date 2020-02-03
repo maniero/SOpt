80c37a06de6933989cf0450723db3fff93e75556
Antonio Maniero <maniero@klip.net>
2020-02-03 16:14:38 -0300

    https://pt.stackoverflow.com/q/137873/101

---
+++ Java/String/ConcatNull.java
@@ -0,0 +1,9 @@
+class Main {
+	public static void main (String[] args) {
+		String a = "a";
+		String b = null;
+		System.out.println((b == null ? "" : b) + a);
+	}
+}
+
+//https://pt.stackoverflow.com/q/137873/101
