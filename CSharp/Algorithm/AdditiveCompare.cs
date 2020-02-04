37796d1695098b07e5a62fc0fb076c1a43ea1cfa
Antonio Maniero <maniero@klip.net>
2020-02-04 12:29:56 -0300

    https://pt.stackoverflow.com/q/138128/101

---
+++ CSharp/Algorithm/AdditiveCompare.cs
@@ -0,0 +1,4 @@
+if ((txtNome.Text != "") && (maskedCPF.Text != "") && (maskedCEP.Text != "") &&
+    (txtNum.Text != "") && ((maskedCelular.Text != "") || (txtTelefone.Text != "")))
+    
+//https://pt.stackoverflow.com/q/138128/101
