efbe3628b2bceb29c97e801d21e30d2cd2e40fd7
Antonio Maniero <maniero@klip.net>
2020-02-05 13:17:54 -0300

    https://pt.stackoverflow.com/q/138778/101

---
+++ CSharp/Algorithm/NullVerification.cs
@@ -0,0 +1,4 @@
+var nos = pagina.DocumentNode.SelectNodes("//*[@id='react - root']/section/main/article/div/div[1]/div[1]/a");
+var quantidade = nos == null ? 0 : nos.Count;
+
+//https://pt.stackoverflow.com/q/138778/101
