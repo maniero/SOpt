c78c863af573300d5e40e8b4b8dfdcca2510f75f
Antonio Maniero <maniero@klip.net>
2020-02-10 11:32:49 -0300

    https://pt.stackoverflow.com/q/435043/101

---
+++ Python/Collection/ReverseList.py
@@ -0,0 +1,12 @@
+i = 1
+lista = []
+while True:
+    i = int(input('Digite um número: '))
+    if i < 1: break
+    lista.append(i)
+for i in lista[::-1]:
+    print(i)
+for i in reversed(lista):
+    print(i)
+
+#https://pt.stackoverflow.com/q/435043/101
