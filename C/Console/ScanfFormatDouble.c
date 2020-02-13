70dbc13300f02cb7ce9406281114b02420908006
Antonio Maniero <maniero@klip.net>
2020-02-13 07:22:01 -0300

    https://pt.stackoverflow.com/q/435521/101

---
+++ C/Console/ScanfFormatDouble.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    double a, b;
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    printf("MEDIA = %5f", (a + b) / 2);
+}
+
+//https://pt.stackoverflow.com/q/435521/101
