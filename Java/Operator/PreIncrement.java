522c5cef32fae70b505bfe411edaf891192afac7
Antonio Maniero <maniero@klip.net>
2020-02-17 07:08:46 -0300

    https://pt.stackoverflow.com/q/435867/101

---
+++ Java/Operator/PreIncrement.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer numero1 = 10;
+        Integer numero2 = numero1;
+        numero1++;
+        System.out.println("numero: " + numero1 + ", numero2: " + numero2);
+    }
+}
+
+//https://pt.stackoverflow.com/q/435867/101
