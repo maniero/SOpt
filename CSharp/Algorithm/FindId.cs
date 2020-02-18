945102dd4009334f2a7c0ecc762cbba6dc07acc4
Antonio Maniero <maniero@klip.net>
2020-02-18 14:40:20 -0300

    https://pt.stackoverflow.com/q/148954/101

---
+++ CSharp/Algorithm/FindId.cs
@@ -0,0 +1,10 @@
+var codCli = Convert.ToInt32(Console.ReadLine()); //isso pode dar erro fácil
+if (clientes.Contains(codCli)) {
+    //aqui pede o resto dos dados
+    clientes[qtdCliente].cod_cli = codCli;
+    .
+    .
+    .
+}
+
+//https://pt.stackoverflow.com/q/148954/101
