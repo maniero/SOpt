4b82e8631a7959a2d3017f44cf597a84edb4f791
Antonio Maniero <maniero@klip.net>
2020-02-20 06:21:23 -0300

    https://pt.stackoverflow.com/q/436579/101

---
+++ CPP/String/MultiLine.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "isto é um teste \
+de texto multi linha \
+que pode ser usado em qauqluer contexto de string";
+}
+
+//https://pt.stackoverflow.com/q/436579/101
