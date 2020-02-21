84cd91ec274e4dcdaaa1cbcebe30401572d7e81c
Antonio Maniero <maniero@klip.net>
2020-02-21 06:32:42 -0300

    https://pt.stackoverflow.com/q/151159/101

---
+++ CSharp/String/Xml.cs
@@ -0,0 +1,10 @@
+<x:c r="C6" s="1" vm="15" t="str">
+   <x:f>CUBEVALUE("xlextdat9 Adventure Works",C$5,$A6)</x:f>
+   <x:v>2838512.355</x:v>
+</x:c>
+
+<x:c r="B2" t="inlineStr">
+   <is><t>test string</t></is>
+</c>
+
+//https://pt.stackoverflow.com/q/151159/101
