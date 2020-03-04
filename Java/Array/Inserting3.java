418e18e1923b3cf5ad5098c3bd9e59ef032e6bd9
Antonio Maniero <maniero@klip.net>
2020-03-04 08:58:46 -0300

    Create Inserting3.java

---
+++ Java/Array/Inserting3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Media {
+    public static void main(String[] args) {
+        float acumulador = 0;
+        int contador = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Insira um valor: ");
+            String dado = scanner.nextLine();
+            if (dado.equals("N")) break;
+            try {
+            	float valor = Float.parseFloat(dado);
+                contador++;
+                acumulador += valor;
+            } catch (NumberFormatException ex) {
+                System.out.println("Valor inválido digite um válido");
+            }
+         }
+        System.out.println("Média: "+ acumulador / contador);
+    }
+}
+
+//https://pt.stackoverflow.com/q/153379/101
