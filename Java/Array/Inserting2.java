10b779463545d79cdf98d2bce0f89ac6d53295aa
Antonio Maniero <maniero@klip.net>
2020-03-04 08:58:34 -0300

    Create Inserting2.java

---
+++ Java/Array/Inserting2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Media {
+    public static void main(String[] args) {
+        float acumulador = 0;
+        int contador = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Insira um valor: ");
+            float valor = scanner.nextFloat();
+            if (false) { //vai colocar a valisação aqui
+                System.out.println("Valor inválido digite um válido");
+                continue;
+            }
+            contador++;
+            acumulador += valor;
+            System.out.println("Deseja inserir outro valor S/N? ");
+            if (!scanner.next().equals("S")) break;
+        }
+        System.out.println("Média: "+ acumulador / contador);
+    }
+}
+
+//https://pt.stackoverflow.com/q/153379/101
