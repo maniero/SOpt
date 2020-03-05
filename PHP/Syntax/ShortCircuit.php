58a0cc9bc5f30c067fee8a78de1adfac2038a8da
Antonio Maniero <maniero@klip.net>
2020-03-05 14:20:15 -0300

    https://pt.stackoverflow.com/q/154056/101

---
+++ PHP/Syntax/ShortCircuit.php
@@ -0,0 +1,14 @@
+if ( $a() && $b() && $c() )
+
+if ( $a() )
+    if ( $b() )
+        if ( $c() )
+            fazAlgo;
+
+if( $a() || $b() || $c() )
+
+if ( $a() ) fazAlgo;
+else if ( $b() ) fazAlgo; //faz algo igual
+else if ( $c() ) fazAlgo; //tem que ser igual
+
+//https://pt.stackoverflow.com/q/154056/101
