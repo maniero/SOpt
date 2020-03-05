308b3ed46088c4a9482151e759dd1f431fe660ad
Antonio Maniero <maniero@klip.net>
2020-03-05 14:22:16 -0300

    https://pt.stackoverflow.com/q/154070/101

---
+++ C/Syntax/ExempleInclusde.c
@@ -0,0 +1,10 @@
+#include <seuOutroArquivo.h>
+
+int algumaRegraDeNegocio(int);
+
+int algumaRegraDeNegocio(int valor) {
+    //faz alguma coisa aqui
+    return 1;
+}
+
+//https://pt.stackoverflow.com/q/154070/101
