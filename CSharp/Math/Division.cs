fd4ef229e566898765e8151b44248f022aa0077c
Antonio Maniero <maniero@klip.net>
2020-03-09 14:22:48 -0300

    https://pt.stackoverflow.com/q/154861/101

---
+++ CSharp/Math/Division.cs
@@ -0,0 +1,8 @@
+using static System.Math;
+using static System.Console;
+
+public class Program {
+	public static void Main() => WriteLine(Pow(27, 1.0/3.0));
+}
+
+//https://pt.stackoverflow.com/q/154861/101
