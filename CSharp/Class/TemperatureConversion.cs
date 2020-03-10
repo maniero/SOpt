f2a1c10f652840aeb57b69b061359f5906755a5c
Antonio Maniero <maniero@klip.net>
2020-03-10 10:02:48 -0300

    https://pt.stackoverflow.com/q/155821/101

---
+++ CSharp/Class/TemperatureConversion.cs
@@ -0,0 +1,14 @@
+public static class TemperatureConverter {
+    public double CelsiusToFahrenheit(double valor)  { //calcula aqui }
+    public double CelsiusToKelvin(double valor) { //calcula aqui }
+    public double FahrenheitToCelsius(double valor) { //calcula aqui }
+    public double FahrenheitToKelvin(double valor) { //calcula aqui }
+    public double KelvinToFahrenheit(double valor) { //calcula aqui }
+    public double KelvinToCelsius(double valor) { //calcula aqui }
+}
+
+public static void Main(string[] args) {
+    double valor = Temperature.ConverCelsiusToKelvin(100);
+}
+
+//https://pt.stackoverflow.com/q/155821/101
