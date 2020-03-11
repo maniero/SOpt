6e3f484763bbdc67318ddcdd879a686a8411a113
Antonio Maniero <maniero@klip.net>
2020-03-11 12:34:51 -0300

    https://pt.stackoverflow.com/q/156913/101

---
+++ PHP/OOP/Trait.php
@@ -0,0 +1,40 @@
+<?php
+trait A {
+    public function smallTalk() {
+        echo 'a';
+    }
+    public function bigTalk() {
+        echo 'A';
+    }
+}
+
+trait B {
+    public function smallTalk() {
+        echo 'b';
+    }
+    public function bigTalk() {
+        echo 'B';
+    }
+}
+
+class Talker {
+    use A, B {
+        B::smallTalk insteadof A;
+        A::bigTalk insteadof B;
+    }
+}
+
+class Aliased_Talker {
+    use A, B {
+        B::smallTalk insteadof A;
+        A::bigTalk insteadof B;
+        B::bigTalk as talk;
+    }
+}
+
+$obj = new Aliased_Talker();
+echo $obj->smallTalk();
+echo $obj->bigTalk();
+echo $obj->talk();
+
+//https://pt.stackoverflow.com/q/156913/101
