1a9d02607597c88673835ed8b267f25ee8290db0
Antonio Maniero <maniero@klip.net>
2020-03-12 09:25:45 -0300

    https://pt.stackoverflow.com/q/157054/101

---
+++ PHP/Array/List.php
@@ -0,0 +1,14 @@
+<?php
+$lista = [];
+$lista[] = "Stack";
+$lista[] = "Over";
+$lista[] = "flow";
+print_r($lista);
+
+$lista = array();
+array_push($lista, "Stack");
+array_push($lista, "Over");
+array_push($lista, "flow");
+print_r($lista);
+
+//https://pt.stackoverflow.com/q/157054/101
