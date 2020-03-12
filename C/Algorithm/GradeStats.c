a9eddacf54051e7ae3375ba890626b3fe5336f33
Antonio Maniero <maniero@klip.net>
2020-03-12 09:29:45 -0300

    https://pt.stackoverflow.com/q/157124/101

---
+++ C/Algorithm/GradeStats.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <limits.h>
+#define NUM 5
+
+int main(){
+	float notas[NUM];
+	float maior = 0, menor = INT_MAX;
+	for (int i = 0; i < NUM; i++) {
+		scanf("%f", &notas[i]);
+		maior = notas[i] > maior ? notas[i] : maior;
+		menor = notas[i] < menor ? notas[i] : menor;
+		media += notas[i];
+	}
+	float media /= NUM;
+	printf("%.1f %.1f %.1f\n\n", maior, menor, media);
+	for (int i = 0; i < NUM; i++) printf("%.1f ", notas[i]);
+}
+
+//https://pt.stackoverflow.com/q/157124/101
