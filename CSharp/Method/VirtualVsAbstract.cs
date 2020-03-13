981ddadf7c8e25654aa1a982340637d4113b495e
Antonio Maniero <maniero@klip.net>
2020-03-13 09:38:23 -0300

    https://pt.stackoverflow.com/q/158579/101

---
+++ CSharp/Method/VirtualVsAbstract.cs
@@ -0,0 +1,18 @@
+public abstract class Base {
+    public abstract void MetodoAbstrato(string nome); //não há implmentação
+
+    public virtual void MetodoVirtual(int x) {
+        //faz algo aqui
+    }
+}
+
+public class Derivada : Base {
+    public override void MetodoAbstrato(string nome) {
+        //faz algo aqui
+    }
+    public override void MetodoVirtual(int x) {
+        //faz algo aqui
+    }
+}
+
+//https://pt.stackoverflow.com/q/158579/101
