63b5f5a57ccb5a753a66089c5b1d3c3d9bc76ca0
Antonio Maniero <maniero@klip.net>
2020-03-13 09:02:19 -0300

    https://pt.stackoverflow.com/q/439700/101

---
+++ PHP/Algorithm/RangeForPercent.php
@@ -0,0 +1,7 @@
+<?php
+$valor = 12000;
+$percentuais = Array(0.41, 0.42, 0.43, 0.44, 0.5, 0.6);
+$percentual = $valor > 80000 ? 5 : $percentuais[$valor / 5000];
+echo $percentual;
+
+//https://pt.stackoverflow.com/q/439700/101
