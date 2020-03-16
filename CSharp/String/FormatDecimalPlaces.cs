450f11746bb317c1141488fb77eb38c06319b61b
Antonio Maniero <maniero@klip.net>
2020-03-16 09:20:21 -0300

    https://pt.stackoverflow.com/q/439929/101

---
+++ CSharp/String/FormatDecimalPlaces.cs
@@ -0,0 +1,14 @@
+using static System.Console;
+using static System.Math;
+
+public class Program {
+	public static void Main() {
+		var media = 7.743;
+		var arredondado = Round(media, 2);
+		var texto = $"{media:0.##}";
+		WriteLine(arredondado);
+		WriteLine(media);
+	}
+}
+
+//https://pt.stackoverflow.com/q/439929/101
