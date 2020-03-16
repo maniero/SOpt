38c462229765398853b87622d60b4c1f713f34b9
Antonio Maniero <maniero@klip.net>
2020-03-16 10:33:03 -0300

    https://pt.stackoverflow.com/q/159069/101

---
+++ JavaScript/Array/Sort.js
@@ -0,0 +1,6 @@
+var idade = [];
+for (var i = 0; i < 3; i++) idade[i] = prompt("Informe a primeira idade: ");
+idade.sort();
+for (var i = 0; i < 3; i++) console.log(idade[i]);
+
+//https://pt.stackoverflow.com/q/159069/101
