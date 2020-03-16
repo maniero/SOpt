ea42f0503c059bd0c24f6ddecbabf6dd42974016
Antonio Maniero <maniero@klip.net>
2020-03-16 10:13:52 -0300

    https://pt.stackoverflow.com/q/158794/101

---
+++ JavaScript/Array/IndexOf.js
@@ -0,0 +1,10 @@
+var prazo = 0;
+var x = 8;
+if ([1, 8, 10, 12, 13].indexOf(x) > -1) prazo = 20;
+console.log(prazo);
+var prazo = 0;
+var x = 7;
+if ([1, 8, 10, 12, 13].indexOf(x) > -1) prazo = 20;
+console.log(prazo);
+
+//https://pt.stackoverflow.com/q/158794/101
