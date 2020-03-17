7fb5a2213f32466901b065a91c742477feb2a770
Antonio Maniero <maniero@klip.net>
2020-03-17 16:08:35 -0300

    https://pt.stackoverflow.com/q/440341/101

---
+++ Python/Syntax/Condition.py
@@ -0,0 +1,8 @@
+mensagem = input('''Clique "C" ''')
+print (mensagem)
+if mensagem == "C":
+    print ("a casa caiu")
+else:
+    print ("a casa não caiu")
+    
+#https://pt.stackoverflow.com/q/440341/101
