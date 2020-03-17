3468485fab980d53b402d1c0b952f4f0cb682abf
Antonio Maniero <maniero@klip.net>
2020-03-17 08:58:24 -0300

    https://pt.stackoverflow.com/q/159835/101

---
+++ CSharp/String/JustOneChar.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+        string frase = "Diego lima de Aquino";
+        for (int i = frase.Length - 1; i >= 0; i--) WriteLine(frase[i]);
+	}
+}
+
+//https://pt.stackoverflow.com/q/159835/101
