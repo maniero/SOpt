1e1c704fc3072042074432d599c04b4e691cd9d7
Antonio Maniero <maniero@klip.net>
2020-03-17 10:28:41 -0300

    https://pt.stackoverflow.com/q/160654/101

---
+++ Java/Math/RandomRange4.java
@@ -0,0 +1,7 @@
+class Main {
+	public static void main (String[] args) {
+		for (int i = 0; i < 1000; i++) System.out.println((int)(1000 + Math.random() * (9001)));
+	}
+}
+
+https://pt.stackoverflow.com/q/160654/101
