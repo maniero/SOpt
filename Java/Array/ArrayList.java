703249a840d956096f12d8d16b2c343d454d5a26
Antonio Maniero <maniero@klip.net>
2020-03-17 09:03:10 -0300

    https://pt.stackoverflow.com/q/159865/101

---
+++ Java/Array/ArrayList.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+	    ArrayList<Estudante> lista = new ArrayList<Estudante>();
+	    List<Estudante> lista1 = new ArrayList<Estudante>();
+	    ArrayList<Estudante> lista2 = new ArrayList<>();
+	}
+}
+
+class Estudante {
+	private int numero;
+	private String nome;
+	public Estudante(int numero, String nome) {
+	    this.numero = numero;
+	    this.nome = nome;
+	}
+}
+
+//https://pt.stackoverflow.com/q/159865/101
