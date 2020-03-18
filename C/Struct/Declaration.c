2f02bc5d555022bb8fcd34011b46f2dbe2af0f6c
Antonio Maniero <maniero@klip.net>
2020-03-18 08:47:20 -0300

    https://pt.stackoverflow.com/q/162173/101

---
+++ C/Struct/Declaration.c
@@ -0,0 +1,20 @@
+struct exem {
+    tipoDeDado variavel;
+};
+
+
+typedef struct exem {
+    tipoDeDado variavel;
+} exem;
+
+
+typedef struct exem {
+    tipoDeDado variavel;
+} Exem;
+
+
+typedef struct {
+    tipoDeDado variavel;
+} Exem;
+
+//https://pt.stackoverflow.com/q/162173/101
