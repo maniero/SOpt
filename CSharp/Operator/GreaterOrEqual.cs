1b2b32631b11a27ac5ad69ef57285bdd35bff574
Antonio Maniero <maniero@klip.net>
2020-03-18 10:14:23 -0300

    https://pt.stackoverflow.com/q/162647/101

---
+++ CSharp/Operator/GreaterOrEqual.cs
@@ -0,0 +1,8 @@
+public static void Main (string [] args) {
+    int x = 2;
+    if (x >= 2) WriteLine("Maior ou igual");
+    int y = 2;
+    if (y > 1) WriteLine("Maior");
+}
+
+//https://pt.stackoverflow.com/q/162647/101
