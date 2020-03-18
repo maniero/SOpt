6ed3d4c01c50b81247127ed4198505056511ca06
Antonio Maniero <maniero@klip.net>
2020-03-18 08:55:37 -0300

    https://pt.stackoverflow.com/q/162235/101

---
+++ C/Array/Itarate.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+	char *v[] = { "Brasil", "Alemanha", "Japão" };
+	for (int i = 0; i < 3; i++) printf("%s\n", v[i]);
+}
+
+//https://pt.stackoverflow.com/q/162235/101
