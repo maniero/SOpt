0885d33885e4b8491580c71f7bd86d75b0a15a3b
Antonio Maniero <maniero@klip.net>
2020-03-18 08:24:16 -0300

    https://pt.stackoverflow.com/q/440392/101

---
+++ JavaScript/Array/Includes.js
@@ -0,0 +1,8 @@
+function temHabilidade(skills) {
+    return skills.includes("JavaScript");
+}
+let skills = ["JavaScript", "ReactJS", "Flutter","React Native"];
+var resultado = temHabilidade(skills);
+console.log(resultado);
+
+//https://pt.stackoverflow.com/q/440392/101
