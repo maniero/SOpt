ff5e561e2029e197beebb67a2861e362a377350a
Antonio Maniero <maniero@klip.net>
2020-03-19 10:09:09 -0300

    https://pt.stackoverflow.com/q/164705/101

---
+++ C/Allocation/ToReturn.c
@@ -0,0 +1,7 @@
+int *verticesFromFace(int v1, int v2, int v3, int v4) {
+    int *vertices = malloc(4 * sizeof(int));
+    //inicaliza os elementos aqui
+    return vertices;
+}
+
+//https://pt.stackoverflow.com/q/164705/101
