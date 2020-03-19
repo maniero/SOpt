71a891d0729f7a17b5ec096b92a97572a60bb498
Antonio Maniero <maniero@klip.net>
2020-03-19 08:33:35 -0300

    https://pt.stackoverflow.com/q/16989/101

---
+++ JavaScript/String/CleanSpaces.js
@@ -0,0 +1,5 @@
+var str = "00000.00000  111111111.111111111111  33333333";
+while (str.indexOf('  ') != -1) str = str.replace('  ', ' ');
+console.log(str);
+
+//https://pt.stackoverflow.com/q/16989/101
