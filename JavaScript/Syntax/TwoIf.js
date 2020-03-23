e5dd6aa51ff663cb32c75b57dbadde7bef4b0ea9
Antonio Maniero <maniero@klip.net>
2020-03-23 09:02:23 -0300

    https://pt.stackoverflow.com/q/441060/101

---
+++ JavaScript/Syntax/TwoIf.js
@@ -0,0 +1,6 @@
+var nome = prompt("Digite seu nome");
+if (nome === null) alert("Você clicou em cancelar");
+else if (nome === "") alert("Seu nome não foi informado");
+else alert(nome);
+
+//https://pt.stackoverflow.com/q/441060/101
