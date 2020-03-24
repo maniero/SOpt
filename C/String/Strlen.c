82e82cd0ea844b1134b967e9451d6e506d72a63c
Antonio Maniero <maniero@klip.net>
2020-03-24 08:31:26 -0300

    https://pt.stackoverflow.com/q/167528/101

---
+++ C/String/Strlen.c
@@ -0,0 +1,24 @@
+size_t strlen (char *str) {
+    size_t len = 0;
+    while (*str != '\0') {
+        str++;
+        len++;
+    }
+    return len;
+}
+
+for (int i = 0; i < strlen(string); i++) {
+    if (string[i] == ' ') count++;
+}
+
+size_t size = strlen(string);
+for (int i = 0; i < size; i++) {
+    if (string[i] == ' ') count++;
+}
+
+while (*string != '\0')
+    if (*string == ' ') count++;
+    string++;
+}
+
+//https://pt.stackoverflow.com/q/167528/101
