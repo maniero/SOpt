ccf5d4e88bc410fa09832f59a4ab910d7ad2af95
Antonio Maniero <maniero@klip.net>
2020-03-25 13:08:07 -0300

    https://pt.stackoverflow.com/q/170324/101

---
+++ C/Syntax/EscopeVariable.c
@@ -0,0 +1,15 @@
+int number = 0;
+
+int main() {
+    printf(" The number is %d\n", number);
+    return (0);
+}
+
+
+int main() {
+    int number = 0;
+    printf(" The number is %d\n", number);
+    return (0);
+}
+
+//https://pt.stackoverflow.com/q/170324/101
