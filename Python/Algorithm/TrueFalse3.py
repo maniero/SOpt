5fa3691ebb0310a25e21be70715e0c927c278a75
Antonio Maniero <maniero@klip.net>
2020-03-27 08:01:39 -0300

    https://pt.stackoverflow.com/q/170784/101

---
+++ Python/Algorithm/TrueFalse3.py
@@ -0,0 +1,9 @@
+x = input("True ou False...? ").lower()
+if x == 'true':
+    print("Você escolheu True")
+elif x == 'false':
+    print("Você escolheu False")
+else:
+    print("Você não escolheu True ou False")
+    
+#https://pt.stackoverflow.com/q/170784/101
