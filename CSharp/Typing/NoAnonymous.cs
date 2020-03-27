5f0bc7b21c4f2cd08934acff2a81ee26a64fc346
Antonio Maniero <maniero@klip.net>
2020-03-27 08:59:44 -0300

    https://pt.stackoverflow.com/q/171125/101

---
+++ CSharp/Typing/NoAnonymous.cs
@@ -0,0 +1,11 @@
+public IEnumerable<Produto> listarProdutos(dynamic filter = null) {
+
+public IEnumerable<Produto> listarProdutos(Filter filter = null) {
+
+public class Filter  {
+    public int categoria = 1;
+    public ALgumTipoAqui cond2 = foo,
+    ...
+}
+
+//https://pt.stackoverflow.com/q/171125/101
