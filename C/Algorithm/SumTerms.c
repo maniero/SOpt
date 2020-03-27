bde050cd004a3aecbccfbb300daa7fda4fb33972
Antonio Maniero <maniero@klip.net>
2020-03-27 06:47:57 -0300

    https://pt.stackoverflow.com/q/441956/101

---
+++ C/Algorithm/SumTerms.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a1, q, n, soma;
+    scanf("%d %d %d", &a1, &q, &n);
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", a1);
+        a1 *= q;
+        soma += a1;
+    }
+    printf("%d\n", soma);
+}
+
+//https://pt.stackoverflow.com/q/441956/101
