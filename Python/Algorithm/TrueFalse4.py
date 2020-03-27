badd139c39c067b2528735d3225ee83f8ed95fcf
Antonio Maniero <maniero@klip.net>
2020-03-27 08:01:48 -0300

    https://pt.stackoverflow.com/q/170784/101

---
+++ Python/Algorithm/TrueFalse4.py
@@ -0,0 +1,7 @@
+x = bool(input("Digite alguma coisa ou deixe em branco...? "))
+if x: #não precisa usar True porque é isto que o if espera
+    print("Você digitou algo")
+else:
+    print("Você não digiyou algo")
+    
+#https://pt.stackoverflow.com/q/170784/101
