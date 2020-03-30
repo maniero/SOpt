219c5a455794156fcabf44231f9c6cc1ce5ed8a6
Antonio Maniero <maniero@klip.net>
2020-03-30 09:59:06 -0300

    https://pt.stackoverflow.com/q/171612/101

---
+++ CSharp/String/Format1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace T1 { 
+    public class Program {
+        public static void Main(string[] args) {
+            string x = Console.ReadLine();
+            Console.WriteLine("{0}", x);
+        }
+    }
+}
+
+//https://pt.stackoverflow.com/q/171612/101
