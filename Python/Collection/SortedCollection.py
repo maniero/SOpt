e754ce24b012aacd8684585d98aa6d3f6562812a
Antonio Maniero <maniero@klip.net>
2020-03-31 10:09:10 -0300

    https://pt.stackoverflow.com/q/172147/101

---
+++ Python/Collection/SortedCollection.py
@@ -0,0 +1,5 @@
+dic = {'759147': 54, '186398060': 8, '199846203': 42, '191725321': 10, '158947719': 4}
+for item in sorted(dic, key = dic.get):
+    print (dic[item])
+    
+#https://pt.stackoverflow.com/q/172147/101
