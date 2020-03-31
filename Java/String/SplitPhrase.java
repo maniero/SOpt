71e8d12b6997e6a31e78705b2e3119c109f5de8f
Antonio Maniero <maniero@klip.net>
2020-03-31 10:02:33 -0300

    https://pt.stackoverflow.com/q/172144/101

---
+++ Java/String/SplitPhrase.java
@@ -0,0 +1,7 @@
+class Teste30 {
+    public static void main(String[] args) {
+        for (String nome : "Isso funciona mesmo, ok?".split(" ")) System.out.println(nome);
+    }
+}
+
+//https://pt.stackoverflow.com/q/172144/101
