ac6ab317a600f6f66dfac8f04c0cf2472821f93d
Antonio Maniero <maniero@klip.net>
2020-03-31 17:02:26 -0300

    https://pt.stackoverflow.com/q/443761/101

---
+++ CPP/Vector/Strings.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    char *nomes[100] = { "gabriel" , "vanessa" };
+    string aNomes[] = { "gabriel" , "vanessa" };
+    vector<string> vNomes = { "gabriel" , "vanessa" };
+}
+
+//https://pt.stackoverflow.com/q/443761/101
