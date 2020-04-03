6b5ad1efb1d146bde5c8bb0097ac6d150342f349
Antonio Maniero <maniero@klip.net>
2020-04-03 09:47:00 -0300

    https://pt.stackoverflow.com/q/174153/101

---
+++ CSharp/Syntax/For.cs
@@ -0,0 +1,11 @@
+while (indField > 0 && indMask > 0)
+
+for (           //o comando
+    int i = 0;  //a inicialização da variável (geralmente, pode ser qualquer ação)
+    i < 10;     //a condição de término do laço (sempre tem que ser um resultado booleano
+    i++         //o passo a executar em cada interação, o incremento é muito comum
+) {             //fecha o comando e abre o bloco de execução
+
+for (int i = 0, j = 0; i < 10; i++, j++) { ... }
+
+//https://pt.stackoverflow.com/q/174153/101
