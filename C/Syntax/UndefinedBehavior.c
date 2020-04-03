3cf5c330b2c73660081f0a6998bcd412ed220d99
Antonio Maniero <maniero@klip.net>
2020-04-03 09:03:42 -0300

    https://pt.stackoverflow.com/q/173886/101

---
+++ C/Syntax/UndefinedBehavior.c
@@ -0,0 +1,14 @@
+void f(int n, int m);
+int f1() {
+    return 1;
+}
+int f2() {
+    return 2;
+}
+f(f1(), f2());
+
+int *o = malloc(0 * sizeof *o);
+
+i++ + ++i
+
+//https://pt.stackoverflow.com/q/173886/101
