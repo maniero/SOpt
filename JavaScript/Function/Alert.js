a70f50f59f6c03bc531726e8e0fa58034105acb5
Antonio Maniero <maniero@klip.net>
2020-04-06 07:56:45 -0300

    https://pt.stackoverflow.com/q/444621/101

---
+++ JavaScript/Function/Alert.js
@@ -0,0 +1,7 @@
+function funcao() {
+    var quantidade = prompt("Digite valor do saque");
+    alert("O valor digitado é " + quantidade);
+}
+funcao();
+
+//https://pt.stackoverflow.com/q/444621/101
