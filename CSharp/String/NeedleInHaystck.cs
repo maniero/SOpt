b5d917e24d39b855984f528337c8a79b1b78543b
Antonio Maniero <maniero@klip.net>
2020-04-09 08:26:22 -0300

    https://pt.stackoverflow.com/q/445252/101

---
+++ CSharp/String/NeedleInHaystck.cs
@@ -0,0 +1,18 @@
+using static System.Console;
+using System.Linq;
+
+public class Program {
+	public static void Main() {
+		string[] warnings = {
+			"INQUIRY REQUEST",
+			"ITEM ALREADY EXISTS",
+			"ITEM NOT FOUND",
+			"END OF FILE",
+			};
+		var status = " 18:46:24:97    INQUIRY REQUEST                                                 ";
+		if (warnings.Any(warning => (status.Trim()).Contains(warning))) WriteLine("achou");
+		else WriteLine("não achou");
+	}
+}
+
+//https://pt.stackoverflow.com/q/445252/101
