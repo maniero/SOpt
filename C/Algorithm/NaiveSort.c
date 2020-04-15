c4e372a203b518869983810e3084df8aac5f1159
Antonio Maniero <maniero@klip.net>
2020-04-15 09:10:45 -0300

    http://pt.stackoverflow.com/q/178136/101

--- C/Algorithm/NaiveSort.c
+++ C/Algorithm/NaiveSort.c
@@ -7,9 +7,7 @@ void swap(int *e1, int *e2) {
 }
 
 void imprime(int array[], int size) {
-    for (int j = 0; j < size; j++) {
-         printf("%d, ",array[j]);
-     }
+    for (int j = 0; j < size; j++) printf("%d, ",array[j]);
      printf("\n");
 }
 	
@@ -18,14 +16,11 @@ int main(void) {
 	int size = 10;
 	for (int pass = 0; pass < size - 1; pass++) {
 	    for (int j = 0; j < size - 1; j++) {
-	        if (array[j] > array[j + 1]) {
-	            swap(&array[j], &array[j + 1]);
-	         }
-	         imprime(array, size);
+	        if (array[j] > array[j + 1]) swap(&array[j], &array[j + 1]);
+	        imprime(array, size);
 	     }
 	     printf("--- Nova iteração ---\n");
 	 }
 }
-
 
 //http://pt.stackoverflow.com/q/178136/101
