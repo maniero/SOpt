600985a4dfc1399b32c729a9e4b5945d2185fd3a
Antonio Maniero <maniero@klip.net>
2020-04-15 09:11:32 -0300

    https://pt.stackoverflow.com/q/446248/101

---
+++ C/Pointer/StrLen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char* lista[5] = {
+        "coffe",
+        "tea"
+        "soda"
+        "water",
+        "juice"
+    };
+    printf("%zd", strlen(lista[0]));
+}
+
+//https://pt.stackoverflow.com/q/446248/101
