0562f8d42895d3f09928f289dde8fc01f6c1e06a
Antonio Maniero <maniero@klip.net>
2020-04-16 10:16:22 -0300

    https://pt.stackoverflow.com/q/179468/101

---
+++ JavaScript/DOM/InnerHtml.js
@@ -0,0 +1,7 @@
+window.a.innerHTML = 'CONTENT';
+<div id="a"></div>
+
+top.innerHTML = 'CONTENT'; //top é uma propriedade de window
+<div id="top"></div>
+
+//https://pt.stackoverflow.com/q/179468/101
