4b98d237d1395ce5daea5cc0d9a191a219cef08b
Antonio Maniero <maniero@klip.net>
2020-04-17 10:47:51 -0300

    https://pt.stackoverflow.com/q/446743/101

---
+++ Python/Algorithm/ExitNumber.py
@@ -0,0 +1,12 @@
+print('Digite 0 para terminar o cadastro!')
+cont = 1
+lista = []
+while True:
+    n = int(input('Funcionário {}: '.format(cont)))
+    if n == 0:
+        break
+    lista.append(n)
+    cont += 1
+print(lista)
+
+#https://pt.stackoverflow.com/q/446743/101
