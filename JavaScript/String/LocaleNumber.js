c4bf49d7739ab2b83ef53f1c0727848e354c90eb
Antonio Maniero <maniero@klip.net>
2020-04-24 11:14:52 -0300

    https://pt.stackoverflow.com/q/182290/101

---
+++ JavaScript/String/LocaleNumber.js
@@ -0,0 +1,8 @@
+function separarDeTresEmTres(numero) {
+    return numero.toLocaleString();
+}
+console.log(separarDeTresEmTres(1000));  
+console.log(separarDeTresEmTres(1000000));
+console.log(separarDeTresEmTres(10000000));
+
+//https://pt.stackoverflow.com/q/182290/101
