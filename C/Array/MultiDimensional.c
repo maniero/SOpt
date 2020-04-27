ecfd130ebf058cfabc1075c4b9c42f5fe41f1017
Antonio Maniero <maniero@klip.net>
2020-04-27 13:11:06 -0300

    https://pt.stackoverflow.com/q/183640/101

--- C/Array/MultiDimensional.c
+++ C/Array/MultiDimensional.c
@@ -4,11 +4,7 @@ int main () {
 	int DIM_X = 1000;
 	int DIM_Y = 1000;
 	int vectorMD[DIM_X][DIM_Y];
-	for (int x = 0; x < DIM_X; x++) {
-	    for (int y = 0; y < DIM_Y; y++) {
-	        vectorMD[x][y] = x;
-	    }
-	}
+	for (int x = 0; x < DIM_X; x++) for (int y = 0; y < DIM_Y; y++) vectorMD[x][y] = x;
 }
 
-//http://pt.stackoverflow.com/q/183640/101
+//https://pt.stackoverflow.com/q/183640/101
