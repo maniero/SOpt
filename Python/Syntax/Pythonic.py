38da12dd0680e7b91bb98acddc42754016a7a526
Antonio Maniero <maniero@klip.net>
2020-04-28 11:49:37 -0300

    https://pt.stackoverflow.com/q/184238/101

---
+++ Python/Syntax/Pythonic.py
@@ -0,0 +1,9 @@
+mylist = [1, 2, 3, 4]
+newlist = []
+for i in mylist:
+    newlist.append(i * 2)
+
+mylist = [1, 2, 3, 4]
+newlist = [(i * 2) for i in mylist]
+
+#https://pt.stackoverflow.com/q/184238/101
