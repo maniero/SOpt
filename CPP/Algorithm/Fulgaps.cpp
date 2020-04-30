67dbacb00a3e01630241a5693c7b7fec7ebaa236
Antonio Maniero <maniero@klip.net>
2020-04-30 14:57:50 -0300

    https://pt.stackoverflow.com/q/449212/101

---
+++ CPP/Algorithm/Fulgaps.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    for (int i = 1; i < 1000; i++) {
+        int FirstLastNumber;
+        if (i < 100) FirstLastNumber = i % 10;
+        else FirstLastNumber = ((i / 100) * 10) + i % 10;
+        if (FirstLastNumber == 0 || i % FirstLastNumber == 0) std::cout << i << std::endl;
+    }
+}
+
+//https://pt.stackoverflow.com/q/449212/101
