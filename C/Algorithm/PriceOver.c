07745b80973a99657c4c162ebbc217a65f8f2018
Antonio Maniero <maniero@klip.net>
2020-05-01 12:51:36 -0300

    https://pt.stackoverflow.com/q/449345/101

--- C/Algorithm/PriceOver.c
+++ C/Algorithm/PriceOver.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 int main(){   
-    float peso, excesso; 
+    float peso; 
     printf("Digite quantos quilos você coletou:\n");
     scanf("%f", &peso);
     printf("=================================================\n");
-    printf("Digite quantos quilos de excesso foram coletados:\n");
-    scanf("%f", &excesso);
-    printf("=================================================\n");
-    float total = peso + excesso;
-    printf("Você coletou: %f Kg.\n", total);
-    if (total >= 50) printf("A multa a ser paga, será de: R$%f\n", total * 4);
+    printf("Você coletou: %f Kg.\n", peso);
+    if (peso > 50) printf("A multa a ser paga, será de: R$%f\n", (peso - 50) * 4.0);
     else printf("Não será necessário pagar multa!\n");
 }
 
