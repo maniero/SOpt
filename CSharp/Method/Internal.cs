fb926831df3385864644e7b194e72f38996ec0fd
Antonio Maniero <maniero@klip.net>
2020-05-04 12:40:44 -0300

    http://pt.stackoverflow.com/q/186062/101

--- CSharp/Method/Internal.cs
+++ CSharp/Method/Internal.cs
@@ -3,7 +3,6 @@ public class Tricycle {
     internal void Pedal() { }
     //pode ser acessado por uma classe qualquer que derive desta, ou tipos desta DLL.
     protected internal int Wheels => { get; } => 3
-    }
 }
 
 //http://pt.stackoverflow.com/q/186062/101
