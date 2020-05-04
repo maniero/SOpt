b94e621e611f986d6207b5c215262cef31d8681d
Antonio Maniero <maniero@klip.net>
2020-05-04 11:17:38 -0300

    https://pt.stackoverflow.com/q/449742/101

---
+++ Python/Algorithm/FunctionExit.py
@@ -0,0 +1,16 @@
+def pedeChute():
+    while True:
+        try:
+            chute = int(input("chute: "))
+        except:
+       	    pass
+
+def jogo():
+    while True:
+        #algo aqui
+        chute = pedeChute() #sempre será válido
+        if chute == 0:
+        	break
+        #algo aqui
+        
+#https://pt.stackoverflow.com/q/449742/101
