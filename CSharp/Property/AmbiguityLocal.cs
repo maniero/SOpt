bc07c2d5a40b8b6e8b9717b0726f09e4be9c7f68
Antonio Maniero <maniero@klip.net>
2020-05-07 10:06:51 -0300

    https://pt.stackoverflow.com/q/189460/101

--- CSharp/Property/AmbiguityLocal.cs
+++ CSharp/Property/AmbiguityLocal.cs
@@ -1,128 +1,96 @@
-using System;
+using static System.Console;
+using System.Threading.Tasks;
 					
-public class Program
-{
+public class Program {
     public static int Pontu1 { get; set; }
 
-    public static int pont(int pontu1)
-    {
+    public static int IncrementaPontos(int pontu1) {
         Pontu1++;
         return pontu1;
     }
 
-    public static void rcerta()
-    {
-        Console.WriteLine("Resposta certa");
-        pont(Pontu1);
-        System.Threading.Thread.Sleep(2000);
+    public static void RespostaCerta() {
+        WriteLine("Resposta certa");
+        IncrementaPontos(Pontu1);
+        Task.Delay(2000);
 //        Console.Clear();
     }
 
-    public static void rerrada()
-    {
-        Console.WriteLine("Resposta errada");
-        Console.Clear();
+    public static void RespostaErrada() {
+        WriteLine("Resposta errada");
+        Clear();
     }
 
-    public static void resetar()
-    {
-        string r;
-        Console.WriteLine("Sua pontuação atual é: " + Pontu1);
-        Console.WriteLine("Deseja reiniciar sua pontuação?      S  / N");
-        r = Console.ReadLine();
-        if (r == "S")
-        {
+    public static void Resetar() {
+        WriteLine("Sua pontuação atual é: " + Pontu1);
+        WriteLine("Deseja reiniciar sua pontuação?      S  / N");
+        string r = ReadLine();
+        if (r == "S")  {
             Pontu1 = 0;
-            Console.WriteLine("Pontuação reiniciada.");
-            Console.WriteLine("Sua pontuação atual é: " + Pontu1);
-            Console.WriteLine("Pressione ENTER para voltar ao menu...");
-        }
-        else
-        {
-            Console.WriteLine("Pontuação mantida.");
-            Console.WriteLine("Sua pontuação atual é: " + Pontu1);
-            Console.WriteLine("Pressione ENTER para voltar ao menu...");
-        }
+            WriteLine("Pontuação reiniciada.");
+		} else WriteLine("Pontuação mantida.");
+        WriteLine("Sua pontuação atual é: " + Pontu1);
+        WriteLine("Pressione ENTER para voltar ao menu...");
     }
 
-    public static void QuizIncio()
-    {
-        int opcao;
-        Console.WriteLine("Escolha um assunto!");
-        Console.WriteLine("[ 1 ] teste");
-        Console.WriteLine("[ 2 ] teste2");
-        Console.WriteLine("-------------------------------------");
-        Console.Write("Digite uma opção: ");
-        opcao = Int32.Parse(Console.ReadLine());
-        switch (opcao)
-        {
+    public static void QuizIncio() {
+        WriteLine("Escolha um assunto!");
+        WriteLine("[ 1 ] teste");
+        WriteLine("[ 2 ] teste2");
+        WriteLine("-------------------------------------");
+        Write("Digite uma opção: ");
+        if (!int.TryParse(ReadLine(), out int opcao)) return;
+        switch (opcao) {
             case 1:
-                Class1.teste();
+                Class1.Teste();
                 break;
             case 2:
-                break;
-            default:
                 break;
         }
  //       Console.Clear();
     }
-    public static void Main(string[] args)
-    {
+	
+    public static void Main(string[] args) {
         int opcao;
         do
         {
-            Console.WriteLine("[ 1 ] Iniciar");
-            Console.WriteLine("[ 2 ] Ver Pontuação");
-            Console.WriteLine("[ 3 ] Resetar Pontuação");
-            Console.WriteLine("[ 0 ] Sair do Programa");
-            Console.WriteLine("-------------------------------------");
-            Console.Write("Digite uma opção: ");
-            opcao = Int32.Parse(Console.ReadLine());
-            switch (opcao)
-            {
+            WriteLine("[ 1 ] Iniciar");
+            WriteLine("[ 2 ] Ver Pontuação");
+            WriteLine("[ 3 ] Resetar Pontuação");
+            WriteLine("[ 0 ] Sair do Programa");
+            WriteLine("-------------------------------------");
+            Write("Digite uma opção: ");
+            if (!int.TryParse(ReadLine(), out opcao)) continue;
+            switch (opcao) {
                 case 1:
  //                   Console.Clear();
                     QuizIncio();
                     break;
                 case 2:
-                    Console.WriteLine("Sua pontuação é: " + Pontu1);
-                    Console.WriteLine("Pressione ENTER para continuar....");
+                    WriteLine("Sua pontuação é: " + Pontu1);
+                    WriteLine("Pressione ENTER para continuar....");
                     break;
                 case 3:
-                    Console.Clear();
-                    resetar();
+//                    Clear();
+                    Resetar();
                     break;
                 case 0:
                     break;
             }
 //            Console.Clear();
-        }
-        while (opcao != 0); 
-    }
-
-
-
-
-}
-
-class Class1 : Program
-{
-    public static void teste()
-    {
-        string r1, r2, r3, r4, r5;
-        Console.WriteLine("Quanto é 2 + 2?");
-        Console.WriteLine("a) 4");
-        Console.WriteLine("b) 3");
-        r1 = Console.ReadLine();
-        if (r1 == "a")
-        {
-            rcerta();
-        }
-        else
-        {
-            rerrada();
-        }
+        } while (opcao != 0); 
     }
 }
 
-//http://pt.stackoverflow.com/q/189460/101
+class Class1 : Program {
+    public static void Teste() {
+        WriteLine("Quanto é 2 + 2?");
+        WriteLine("a) 4");
+        WriteLine("b) 3");
+        string r1 = ReadLine();
+        if (r1 == "a") RespostaCerta();
+        else RespostaErrada();
+    }
+}
+
+//https://pt.stackoverflow.com/q/189460/101
