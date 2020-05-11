2e5c628f9ad7f50db59ddc1148f32fa6489a2a40
Antonio Maniero <maniero@klip.net>
2020-05-11 10:59:37 -0300

    https://pt.stackoverflow.com/q/191832/101

---
+++ CSharp/Class/Composition2.cs
@@ -0,0 +1,10 @@
+class Funcionario {
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public Funcao { get; set; } //aqui coloca a função do momento
+}
+class Funcao {
+    ...
+}
+
+//https://pt.stackoverflow.com/q/191832/101
