a4109872d9e30e26230a6a2186b5175698dd9ae2
Antonio Maniero <maniero@klip.net>
2020-05-13 11:44:35 -0300

    https://pt.stackoverflow.com/q/198632/101

--- CSharp/Enviroment/GetMemory.cs
+++ CSharp/Enviroment/GetMemory.cs
@@ -3,10 +3,9 @@ using System.Diagnostics;
 
 public class Program {
 	public static void Main() {
-		using (var proc = Process.GetCurrentProcess()) {
-			WriteLine(proc.PrivateMemorySize64);
-			WriteLine(proc.WorkingSet64);
-		}
+		using var proc = Process.GetCurrentProcess();
+		WriteLine(proc.PrivateMemorySize64);
+		WriteLine(proc.WorkingSet64);
 	}
 }
 
