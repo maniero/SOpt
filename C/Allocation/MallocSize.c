c957be63466f3a346cd01b910a12ea8edb3681fc
Antonio Maniero <maniero@klip.net>
2020-05-13 09:23:18 -0300

    https://pt.stackoverflow.com/q/198478/101

--- C/Allocation/MallocSize.c
+++ C/Allocation/MallocSize.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char const *argv[]) {
 	int n = 5;
-    int *p = malloc( n * sizeof(int));
-    printf("%d\n", n);
+    int *p = malloc(n * sizeof(int));
+    printf("%p = %d\n", (void *)p, n);
 }
 
 //https://pt.stackoverflow.com/q/198478/101
