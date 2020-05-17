2cd7ca24593c41ade4903b66ec7794e8e96505c4
Antonio Maniero <maniero@klip.net>
2020-05-16 21:56:01 -0300

    https://pt.stackoverflow.com/q/452177/101

--- C/Console/PrintChars.c
+++ C/Console/PrintChars.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Digite um caractere: \n");
+	char c;
+    printf("Digite um caractere:\n");
     char c1 = getchar();
-    printf("Digite um caractere: \n");
+    while ((c = getchar()) != '\n' && c != EOF) { }
+    printf("Digite um caractere:\n");
     char c2 = getchar();
-    printf("Digite um caractere: \n");
+    while ((c = getchar()) != '\n' && c != EOF) { }
+    printf("Digite um caractere:\n");
     char c3 = getchar();
+    while ((c = getchar()) != '\n' && c != EOF) { }
     printf("Primeiro caractere: %c\nSegundo caractere: %c\nTerceiro caractere: %c\n", c1, c2, c3);
 }
 
