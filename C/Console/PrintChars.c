b93ef538dcc7e7dca7d7b00e3d0562542e1e6397
Antonio Maniero <maniero@klip.net>
2020-05-16 21:31:41 -0300

    https://pt.stackoverflow.com/q/452177/101

---
+++ C/Console/PrintChars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Digite um caractere: \n");
+    char c1 = getchar();
+    printf("Digite um caractere: \n");
+    char c2 = getchar();
+    printf("Digite um caractere: \n");
+    char c3 = getchar();
+    printf("Primeiro caractere: %c\nSegundo caractere: %c\nTerceiro caractere: %c\n", c1, c2, c3);
+}
+
+//https://pt.stackoverflow.com/q/452177/101
