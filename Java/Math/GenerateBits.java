4bc4770b734ab88a0cafe4db7091ed05a170c0a5
Antonio Maniero <maniero@klip.net>
2020-05-18 15:46:01 -0300

    https://pt.stackoverflow.com/q/205295/101

--- Java/Math/GenerateBits.java
+++ Java/Math/GenerateBits.java
@@ -1,8 +1,6 @@
-class HelloWorld  {
+class Main {
 	public static void main (String[] args) {
-		for (int i = 0; i < 32; i++) {
-			System.out.println("" + i / 16 % 2 + i / 8 % 2 + i / 4 % 2 + i / 2 % 2 + i % 2);
-		}
+		for (int i = 0; i < 32; i++) System.out.println("" + i / 16 % 2 + i / 8 % 2 + i / 4 % 2 + i / 2 % 2 + i % 2);
 	}
 }
 
