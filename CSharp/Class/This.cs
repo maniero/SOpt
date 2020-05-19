6ad1cdb8c69b0d3e4e100e2986106fedfb797353
Antonio Maniero <maniero@klip.net>
2020-05-19 11:12:43 -0300

    https://pt.stackoverflow.com/q/206287/101

---
+++ CSharp/Class/This.cs
@@ -0,0 +1,7 @@
+public HelpController(HttpConfiguration config) => Configuration = config;
+
+public HelpController() => this.HelpController(GlobalConfiguration.Configuration);
+
+public HelpController(HttpConfiguration config = GlobalConfiguration.Configuration);
+
+//https://pt.stackoverflow.com/q/206287/101
