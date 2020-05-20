9405743a3963615a91962d5e00f276111d7f7c21
Antonio Maniero <maniero@klip.net>
2020-05-20 08:17:06 -0300

    https://pt.stackoverflow.com/q/207800/101

--- CSharp/Tuple/ReturnTwoValues.cs
+++ CSharp/Tuple/ReturnTwoValues.cs
@@ -1,9 +1,7 @@
 public (bool, TipoDoRetornoDoMetodoDadosDermaClube) CarregaProdutosDermaClube(string consulta) {
     var buscaProdutosDermaClube = new BuscaProdutosDermaClubeDAL();
     retorno = this.buscaProdutosDermaClube.DadosDermaClube("");
-    if (string.IsNullOrEmpty(consulta)) {
-        return (false, null);
-    }
+    if (string.IsNullOrEmpty(consulta)) return (false, null);
     return (retorno[0].idproduto.Contains(consulta), retorno);
 }
 
