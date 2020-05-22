25ba3973cbaf34b98ba2e89043d100d336663dfc
Antonio Maniero <maniero@klip.net>
2020-05-22 14:10:12 -0300

    https://pt.stackoverflow.com/q/453291/101

---
+++ C/Algorithm/FileterLetters.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char consoante[10];
+    int total = 0;
+    char c;
+    for (int i = 0; i < 10; i++) {
+        printf("Digite a letra: ");
+        char letra;
+        scanf("%c", &letra);
+        while ((c = getchar()) != '\n' && c != EOF) { }
+        if (letra != 'a' && letra != 'e' && letra != 'i' && letra != 'o' && letra != 'u') consoante[total++] = letra;
+    }
+    for (int i = 0; i < total; i++) printf("\nA consoante foi: %c", consoante[i]);
+}
+
+//https://pt.stackoverflow.com/q/453291/101
