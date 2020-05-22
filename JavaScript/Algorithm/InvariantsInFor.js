767b69402009650e10c96779026535705b5f42fd
Antonio Maniero <maniero@klip.net>
2020-05-22 09:19:47 -0300

    https://pt.stackoverflow.com/q/453204/101

---
+++ JavaScript/Algorithm/InvariantsInFor.js
@@ -0,0 +1,6 @@
+console.log(`Repeating because one is less than ten !`);
+console.log(`Repeating because two is less than ten !`);
+console.log(`Repeating because three is less than ten !`);
+for (var i = 4; i <= 10; i++) console.log(`Repeating because ${i} is less than ten !`);
+
+//https://pt.stackoverflow.com/q/453204/101
