09f3cb20b86cf871567da767e740267868e55fc9
Antonio Maniero <maniero@klip.net>
2020-05-25 11:30:06 -0300

    https://pt.stackoverflow.com/q/453471/101

---
+++ JavaScript/Function/ExecutionFromArray.js
@@ -0,0 +1,10 @@
+var funcoes = {
+    sen: Math.sin,
+    cos: Math.cos,
+    e: Math.E,
+    ln: Math.log,  
+}
+var funcao = "sen";
+console.log(funcoes[funcao](30));
+
+//https://pt.stackoverflow.com/q/453471/101
