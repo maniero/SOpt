66fd37d69243f8d3f13beaaa73c486db9e4bbdac
Antonio Maniero <maniero@klip.net>
2020-05-26 07:47:23 -0300

    https://pt.stackoverflow.com/q/453800/101

---
+++ JavaScript/String/CountChars.js
@@ -0,0 +1,6 @@
+function tamanhoNomeCompleto(nome,sobrenome) {
+    return nome.length + sobrenome.length + 1;
+}
+console.log(tamanhoNomeCompleto('Juan', 'Perez'));
+
+//https://pt.stackoverflow.com/q/453800/101
