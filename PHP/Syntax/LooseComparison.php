a3ea644c96944f88f38a85c2dc0a89adf79926dc
Antonio Maniero <maniero@klip.net>
2020-05-27 11:46:55 -0300

    https://pt.stackoverflow.com/q/215652/101

---
+++ PHP/Syntax/LooseComparison.php
@@ -0,0 +1,12 @@
+<?php
+$x = "1";
+switch ($x) {
+    case 1:
+        echo "é 1";
+        break;
+    case 2:
+        echo "é 2";
+        break;
+}
+
+//https://pt.stackoverflow.com/q/215652/101
