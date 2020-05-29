81eec3cf90ddab2eec3fc854f96a2c0d68f2d234
Antonio Maniero <maniero@klip.net>
2020-05-29 14:18:02 -0300

    https://pt.stackoverflow.com/q/220469/101

--- CSharp/Array/InvertMatrix.cs
+++ CSharp/Array/InvertMatrix.cs
@@ -1,16 +1,13 @@
 using static System.Console;
+
 public class Program {
 	public static void Main() {
 		int[,] array = new int[10,10];
 		for (int l = 0; l < 10; l++) {
-			for (int c = 0; c < 10; c++) {
-			   array[l, c] = c;
-			}
+			for (int c = 0; c < 10; c++) array[l, c] = c;
 		}
 		for (int l = 0; l < 10; l++) {
-			for (int c = 0; c < 10; c++) {
-				Write($"{array[l, c]} ");
-			}
+			for (int c = 0; c < 10; c++) Write($"{array[l, c]} ");
 			WriteLine();
 		}
 		WriteLine();
@@ -22,9 +19,7 @@ public class Program {
 			}
 		}
 		for (int l = 0; l < 10; l++) {
-			for (int c = 0; c < 10; c++) {
-				Write($"{array[l, c]} ");
-			}
+			for (int c = 0; c < 10; c++) Write($"{array[l, c]} ");
 			WriteLine();
 		}
 	}
