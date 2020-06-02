891ea8545a2065d40b477ee5699083ffcb16c8b5
Antonio Maniero <maniero@klip.net>
2020-06-02 14:34:30 -0300

    https://pt.stackoverflow.com/q/224232/101

---
+++ JavaScript/Array/LiteralVsType.js
@@ -0,0 +1,7 @@
+var weatherPattern = new Array('Clouds', "raining", "clowdy");
+var weatherPattern = ['Clouds', "raining", "clowdy")];
+
+var weatherPattern = new Array(5);
+var weatherPattern = [5];
+
+//https://pt.stackoverflow.com/q/224232/101
