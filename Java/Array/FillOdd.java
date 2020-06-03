99624a7bea73958deaba71e876de6c6f810c4fdc
Antonio Maniero <maniero@klip.net>
2020-06-03 10:33:31 -0300

    https://pt.stackoverflow.com/q/225246/101

--- Java/Array/FillOdd.java
+++ Java/Array/FillOdd.java
@@ -1,17 +1,13 @@
-class HelloWord {
+class Main {
 	public static void main (String[] args)	{
-		for (int i : oddNumbers(2, 5)) {
-			System.out.println(i);
-		}
+		for (int i : oddNumbers(2, 5)) System.out.println(i);
 	}
 	
 	public static int[] oddNumbers(int l, int r) {
 		l += 1 - l % 2;
 		r -= 1 - r % 2;
 	    int odd[] = new int[(r - l) / 2 + 1];
-	    for (int i = 0; i < odd.length; i++) {
-	        odd[i] = l + (i * 2);
-	    }
+	    for (int i = 0; i < odd.length; i++) odd[i] = l + (i * 2);
 	    return odd;
 	}
 }
