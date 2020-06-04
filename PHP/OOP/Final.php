337d43abea9a62b31aa636f2733d6b3644e7f3d1
Antonio Maniero <maniero@klip.net>
2020-06-04 09:10:28 -0300

    https://pt.stackoverflow.com/q/227213/101

---
+++ PHP/OOP/Final.php
@@ -0,0 +1,32 @@
+class BaseClass {
+   public function test() {
+       echo "BaseClass::test() called\n";
+   }
+
+   final public function moreTesting() {
+       echo "BaseClass::moreTesting() called\n";
+   }
+}
+
+class ChildClass extends BaseClass {
+   public function moreTesting() {
+       echo "ChildClass::moreTesting() called\n";
+   }
+}
+
+
+final class BaseClass {
+   public function test() {
+       echo "BaseClass::test() called\n";
+   }
+
+   // Here it doesn't matter if you specify the function as final or not
+   final public function moreTesting() {
+       echo "BaseClass::moreTesting() called\n";
+   }
+}
+
+class ChildClass extends BaseClass {
+}
+
+//https://pt.stackoverflow.com/q/227213/101
