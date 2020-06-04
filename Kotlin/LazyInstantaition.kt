06ceb17b4a12f91325282936e60853797fb6f36e
Antonio Maniero <maniero@klip.net>
2020-06-04 15:31:28 -0300

    https://pt.stackoverflow.com/q/228512/101

---
+++ Kotlin/LazyInstantaition.kt
@@ -0,0 +1,11 @@
+val lazyValue: String by lazy {
+    println("computed!")
+    "Hello"
+}
+
+fun main(args: Array<String>) {
+    println(lazyValue)
+    println(lazyValue)
+}
+
+//https://pt.stackoverflow.com/q/228512/101
