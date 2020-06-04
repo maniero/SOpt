8187cfdeabbe29e0ad7fa40ebf6f1a9f396b19b2
Antonio Maniero <maniero@klip.net>
2020-06-04 12:34:43 -0300

    https://pt.stackoverflow.com/q/228028/101

---
+++ Kotlin/Get.kt
@@ -0,0 +1,9 @@
+var content: String
+    get() = this.content.toUpperCase();
+    set(value) {
+        if (value != "") {
+            field = value
+        }
+    }
+    
+//https://pt.stackoverflow.com/q/228028/101
