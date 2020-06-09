55018b9c6fe55209d430ccb9c24f18c3ce6fb95c
Antonio Maniero <maniero@klip.net>
2020-06-09 10:49:42 -0300

    https://pt.stackoverflow.com/q/230364/101

---
+++ C/Struct/Typedef.c
@@ -0,0 +1,13 @@
+typedef int Inteiro
+
+struct exemplo {
+    int x;
+}
+
+typedef struct exemplo Exemplo;
+
+typedef struct exemplo {
+    int x;
+} Exemplo;
+
+//https://pt.stackoverflow.com/q/230364/101
