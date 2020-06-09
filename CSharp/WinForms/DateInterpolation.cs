da9051bc56bc8cc8ffccaa827b30173867c053dd
Antonio Maniero <maniero@klip.net>
2020-06-09 16:19:44 -0300

    https://pt.stackoverflow.com/q/233426/101

---
+++ CSharp/WinForms/DateInterpolation.cs
@@ -0,0 +1,7 @@
+var frase = $"A data de hoje é: {DateTime.Now.Date}";
+
+seuLabel.Text = $"A data de hoje é: <b>{DateTime.Now.Date}</b>";
+
+seuLabel.Font = new Font("nome da fonte", 11, FontStyle.Bold);
+
+//https://pt.stackoverflow.com/q/233426/101
