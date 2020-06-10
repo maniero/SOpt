f2c5e3a4b6b24f24fc2a9a7d9b416eb2735b1a27
Antonio Maniero <maniero@klip.net>
2020-06-10 14:12:42 -0300

    https://pt.stackoverflow.com/q/234063/101

---
+++ CSharp/Class/SuperSubClass.cs
@@ -0,0 +1,6 @@
+public class SuperClasse {}
+public class Subclasse : SuperClasse {}
+
+public class NovaSubClasse : SubClasse {} //aqui SubClasse não deixa de ser uma superclasse.
+
+//https://pt.stackoverflow.com/q/234063/101
