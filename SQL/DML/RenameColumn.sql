5aa397a3791008f60a8b4ce5714fd75545391e4f
Antonio Maniero <maniero@klip.net>
2020-06-12 14:37:24 -0300

    https://pt.stackoverflow.com/q/235719/101

---
+++ SQL/DML/RenameColumn.sql
@@ -0,0 +1,5 @@
+ALTER TABLE nomes_clientes 
+    CHANGE primeiro_nome nome VARCHAR(255) NOT NULL;
+    CHANGE segundo_nome sobrenome VARCHAR(255) NOT NULL;
+    
+//https://pt.stackoverflow.com/q/235719/101
