a586eeca15907e80612bf0e0fe2d9712c03e0bd9
Antonio Maniero <maniero@klip.net>
2020-06-12 16:18:29 -0300

    https://pt.stackoverflow.com/q/235989/101

--- C/Array/IndexError.c
+++ C/Array/IndexError.c
@@ -13,11 +13,7 @@ void retorna_matriz2D(int mat[][DIM]) {
 int main(){
     int mat2D[DIM][DIM];
     retorna_matriz2D(mat2D);
-    for (int x = 0; x < DIM; x++) {
-        for (int y = 0; y < DIM; y++) {
-            printf("%d\n", mat2D[x][y]);
-        }
-    }
+    for (int x = 0; x < DIM; x++) for (int y = 0; y < DIM; y++) printf("%d\n", mat2D[x][y]);
 }
 
 //https://pt.stackoverflow.com/q/235989/101
