3d7aafe64fa99d8400606b4a88fe5b4aa81583cb
Antonio Maniero <maniero@klip.net>
2020-06-12 14:30:24 -0300

    https://pt.stackoverflow.com/q/235683/101

---
+++ JavaScript/Array/Gaps.js
@@ -0,0 +1,10 @@
+for (var i = 0; i < vetor.length; i++) {
+    for (var j = 0; j < vetor.length; j++) {
+        if (vetor[i] == vetor[j]) {
+            i++;
+            break;
+        }
+    }
+}
+
+//https://pt.stackoverflow.com/q/235683/101
