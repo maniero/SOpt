983761d5eb6eb4fe9777e572db79189f92d35a2c
Antonio Maniero <maniero@klip.net>
2020-06-15 10:27:34 -0300

    https://pt.stackoverflow.com/q/457223/101

---
+++ Java/Algorithm/Smallest.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+       System.out.println(smallest(1, 3));
+    }
+    public static int smallest(int n1, int n2) {
+        if (n1 < n2) return n1;
+        else return n2;
+    }
+}
+
+//https://pt.stackoverflow.com/q/457223/101
