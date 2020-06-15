19e7331574c558a9e3197aeadb9c141aa227eb9c
Antonio Maniero <maniero@klip.net>
2020-06-15 14:27:26 -0300

    https://pt.stackoverflow.com/q/236570/101

---
+++ Java/Exception/ValidationNoException.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+    String nome = null; //obviamente que é só para ilustrar, ninguém faria isto, certo?
+    System.out.println(nome != null && nome.length() > 10 ? "Nome maior que 10 caracteres" : "Nome menor que 10 caracteres");
+}
+
+//https://pt.stackoverflow.com/q/236570/101
