ad6d6fe70f66aeb3bcd6f2c85da8dc4e06be419e
Antonio Maniero <maniero@klip.net>
2020-06-15 14:20:42 -0300

    https://pt.stackoverflow.com/q/236418/101

---
+++ JavaScript/Algorithm/Validation.js
@@ -0,0 +1,9 @@
+function validaCampo() {    
+   if (dias.length == 0) {
+        alert("O Campo Dias disponíveis é obrigatório!");
+        return false;
+   }
+   return true;
+}
+
+//https://pt.stackoverflow.com/q/236418/101
