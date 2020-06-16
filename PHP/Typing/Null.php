c7342a211cef7727b16cfedf3f983c684587dea4
Antonio Maniero <maniero@klip.net>
2020-06-16 13:42:44 -0300

    https://pt.stackoverflow.com/q/238961/101

---
+++ PHP/Typing/Null.php
@@ -0,0 +1,19 @@
+$valor = null;
+
+$valor = "";
+
+if ($valor == null){}
+
+if ($valor == "") {}
+
+if ($valor === null) {}
+
+if (is_null($valor)) {}
+
+if (empty($valor)) {}
+
+$a = array();
+echo $a == null; //true
+echo $a === null;  //false
+
+//https://pt.stackoverflow.com/q/238961/101
