69d80f385d260c917e47caa84e8c053099254428
Antonio Maniero <maniero@klip.net>
2020-06-17 16:00:08 -0300

    https://pt.stackoverflow.com/q/240733/101

---
+++ Java/Collections/ListExplain.java
@@ -0,0 +1,16 @@
+List<String> lista = new ArrayList<>();
+
+lista.add("Aline");
+lista.add("João");
+lista.add("Jona");
+lista.add("Pedro");
+
+return lista;
+
+Teste1 t = new Teste1();
+
+dados = t.getList();
+
+System.out.println(dados);
+
+//https://pt.stackoverflow.com/q/240733/101
