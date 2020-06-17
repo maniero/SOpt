27677e928dfdb5f75c8a4b470644c4b085e4083b
Antonio Maniero <maniero@klip.net>
2020-06-17 12:17:50 -0300

    https://pt.stackoverflow.com/q/240410/101

---
+++ Java/Syntax/SwitchStyle.java
@@ -0,0 +1,43 @@
+switch (estado) {
+    case "AL":
+    case "BA":
+    case "CE":
+    case "MA":
+    case "PB":
+    case "PE":
+    case "PI":
+    case "RN":
+    case "SE":
+        //FAZ ALGO AQUI PARA TODOS OS CASES DO NORDESTE
+        break;
+    case "ES":
+    case "MG":
+    case "RJ":
+    case "SP":
+        //FAZ ALGO AQUI PARA TODOS OS CASES DO SUDESTE
+        break;
+    etc...
+}
+
+switch (estado) {
+case "AL":
+case "BA":
+case "CE":
+case "MA":
+case "PB":
+case "PE":
+case "PI":
+case "RN":
+case "SE":
+    //FAZ ALGO AQUI PARA TODOS OS CASES DO NORDESTE
+    break;
+case "ES":
+case "MG":
+case "RJ":
+case "SP":
+    //FAZ ALGO AQUI PARA TODOS OS CASES DO SUDESTE
+    break;
+etc...
+}
+
+//https://pt.stackoverflow.com/q/240410/101
