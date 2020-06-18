adbc14196a2fe3d4578511f77f4cb2ad2a7485f3
Antonio Maniero <maniero@klip.net>
2020-06-18 10:11:05 -0300

    https://pt.stackoverflow.com/q/240882/101

--- CSharp/Syntax/NestedFor.cs
+++ CSharp/Syntax/NestedFor.cs
@@ -3,9 +3,7 @@ using static System.Console;
 public class Program {
 	public static void Main() {
 		for (int n1 = 1; n1 < 11; n1++) {
-			for (int n2 = 1; n2 < 11; n2++){
-				WriteLine($"{n1:d2} x {n2:d2} = {n1 * n2:d2}");
-			}
+			for (int n2 = 1; n2 < 11; n2++) WriteLine($"{n1:d2} x {n2:d2} = {n1 * n2:d2}");
 			WriteLine();
 		}
 	}
