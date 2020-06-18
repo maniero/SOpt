2a8040771aa3334a0900dcdf415a5fa43bd61e00
Antonio Maniero <maniero@klip.net>
2020-06-18 09:14:05 -0300

    https://pt.stackoverflow.com/q/240948/101

---
+++ Python/Syntax/FoExplain.py
@@ -0,0 +1,8 @@
+for i in range(0, len(text), key)]
+    groups = groups + [text[i:i + key].ljust(key, "*")
+
+for (int i = 0; i < len(text); i+= key)
+
+text[i:i + key]
+
+#https://pt.stackoverflow.com/q/240948/101
