74aa25ce1bd90528c196a581c863263d2cabe3b0
Antonio Maniero <maniero@klip.net>
2020-06-19 13:04:43 -0300

    https://pt.stackoverflow.com/q/243891/101

--- CSharp/IO/GetParent.cs
+++ CSharp/IO/GetParent.cs
@@ -2,9 +2,7 @@ using static System.Console;
 using System.IO;
 
 public class Program {
-	public static void Main() {
-		WriteLine(Directory.GetParent("/Pasta1/Pasta2"));
-	}
+    public static void Main() => WriteLine(Directory.GetParent("/Pasta1/Pasta2"));
 }
 
 //https://pt.stackoverflow.com/q/243891/101
