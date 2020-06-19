f037929089f6fa7884cdedc9e74d9c4bef08541f
Antonio Maniero <maniero@klip.net>
2020-06-19 14:08:45 -0300

    https://pt.stackoverflow.com/q/244556/101

---
+++ PHP/OOP/Extends.php
@@ -0,0 +1,7 @@
+class CustomListTable extends WP_List_Table
+
+class CustomProductsListTable extends CustomListTable
+
+class CustomReviewsListTable extends CustomListTable
+
+//https://pt.stackoverflow.com/q/244556/101
