dc054d500d07e319d6b3e616d0a1dcb442de6492
Antonio Maniero <maniero@klip.net>
2020-06-22 11:45:54 -0300

    https://pt.stackoverflow.com/q/245318/101

---
+++ CSharp/ADO.NET/ItemDataTble.cs
@@ -0,0 +1,10 @@
+this.txt_solicitacao.Text              = row.Cells[0].Value.ToString();
+this.txt_cliente.Text                  = row.Cells[1].Value.ToString();
+this.txt_Contato.Text                  = row.Cells[2].Value.ToString();
+this.txt_solicitante.Text              = row.Cells[3].Value.ToString();
+this.txt_representante.Text            = row.Cells[4].Value.ToString();
+this.txt_atuacao.Text                  = row.Cells[5].Value.ToString();
+this.txt_separador.Text                = row.Cells[7].Value.ToString();
+this.txt_idcliente.Text                = row.Cells[9].Value.ToString();
+
+//https://pt.stackoverflow.com/q/245318/101
