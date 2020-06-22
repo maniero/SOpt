3a468e528a8fc6ea185e6ebdbcbcaf8d6ed8742a
Antonio Maniero <maniero@klip.net>
2020-06-22 12:02:29 -0300

    https://pt.stackoverflow.com/q/246460/101

---
+++ CSharp/String/Nullable.cs
@@ -0,0 +1,7 @@
+public class DownloadResponse{
+    public bool IsValid { get; set; }
+    public int DownloadTypeID { get; set; }
+    public string? LicenseKey { get; set; }
+}
+
+//https://pt.stackoverflow.com/q/246460/101
