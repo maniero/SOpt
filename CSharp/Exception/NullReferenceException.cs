73eaf36ec73038bf7fa2751441b57ff31dbcceda
Antonio Maniero <maniero@klip.net>
2020-06-23 15:21:35 -0300

    https://pt.stackoverflow.com/q/249368/101

---
+++ CSharp/Exception/NullReferenceException.cs
@@ -0,0 +1,7 @@
+public StringBuilder listNewsletter { get; set; } = new StringBuilder();
+
+public modelExemplo() {
+    listNewsletter = new StringBuilder();
+}
+
+//https://pt.stackoverflow.com/q/249368/101
