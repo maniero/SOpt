36a289fc308c424cee6c556c5ef41014600df339
Antonio Maniero <maniero@klip.net>
2020-06-24 10:43:00 -0300

    https://pt.stackoverflow.com/q/250144/101

---
+++ SQL/Select/WithSyntax.sql
@@ -0,0 +1,27 @@
+USE AdventureWorks2012;  
+GO  
+WITH DirectReports(Name, Title, EmployeeID, EmployeeLevel, Sort)  
+AS (SELECT CONVERT(varchar(255), e.FirstName + ' ' + e.LastName),  
+        e.Title,  
+        e.EmployeeID,  
+        1,  
+        CONVERT(varchar(255), e.FirstName + ' ' + e.LastName)  
+    FROM dbo.MyEmployees AS e  
+    WHERE e.ManagerID IS NULL  
+    UNION ALL  
+    SELECT CONVERT(varchar(255), REPLICATE ('|    ' , EmployeeLevel) +  
+        e.FirstName + ' ' + e.LastName),  
+        e.Title,  
+        e.EmployeeID,  
+        EmployeeLevel + 1,  
+        CONVERT (varchar(255), RTRIM(Sort) + '|    ' + FirstName + ' ' +   
+                 LastName)  
+    FROM dbo.MyEmployees AS e  
+    JOIN DirectReports AS d ON e.ManagerID = d.EmployeeID  
+    )  
+SELECT EmployeeID, Name, Title, EmployeeLevel  
+FROM DirectReports   
+ORDER BY Sort;  
+GO
+
+--https://pt.stackoverflow.com/q/250144/101
