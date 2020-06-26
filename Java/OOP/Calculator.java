e2a0f923def7da43c93f9284b90ea1b279cff581
Antonio Maniero <maniero@klip.net>
2020-06-26 16:48:49 -0300

    https://pt.stackoverflow.com/q/254244/101

--- Java/OOP/Calculator.java
+++ Java/OOP/Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Ideone {
+class Main {
 	public static void main (String[] args)	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Informe dois valores: ");
