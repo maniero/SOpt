8c281240b66407b1c32f9d751e9dfa6db6de161b
Antonio Maniero <maniero@klip.net>
2020-06-30 20:05:33 -0300

    https://pt.stackoverflow.com/q/460146/101

---
+++ C/Algorithm/ReadWords.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char **texto = NULL;
+    int i = 0;
+    char letra;
+    do {
+        texto = realloc(texto, (i + 1) * sizeof(char*));
+        texto[i] = NULL;
+        int j = 0;
+        do {
+            letra = getchar();
+            texto[i] = realloc(texto[i], j + 1);
+            texto[i][j] = letra;
+            if ((i + 1) % 3 == 0) printf("%c", letra);
+            j++;
+        } while (letra != ' ' && letra != '\n');
+        i++;
+    } while (letra != '\n');
+    free(texto);
+}
+
+//https://pt.stackoverflow.com/q/460146/101
