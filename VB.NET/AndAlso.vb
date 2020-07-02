12f1f689012bef1bf350b6eae9b2c2c3de69e78b
Antonio Maniero <maniero@klip.net>
2020-07-02 12:03:48 -0300

    https://pt.stackoverflow.com/q/258806/101

---
+++ VB.NET/AndAlso.vb
@@ -0,0 +1,7 @@
+If TextBox3.Text < "3" Then
+    TextBox4.Text = "8%"
+ElseIf TextBox3.Text >= "4" AndAlso TextBox3.Text <= "6" Then
+    TextBox4.Text = "9%"
+End If
+
+'https://pt.stackoverflow.com/q/258806/101
