9c3d6afec50177ca562f18b8427d9dc4d63e7e03
Antonio Maniero <maniero@klip.net>
2020-07-03 14:37:30 -0300

    https://pt.stackoverflow.com/q/260369/101

---
+++ Java/Array/FindIt.java
@@ -0,0 +1,8 @@
+for (Fruta fruta : listaFrutas) { //eu chamaria só de frutas
+    if (fruta.contains(pesquisa)) {
+        System.out.println(pesquisa + "está na lista: ");
+        break;
+    }
+}
+
+//https://pt.stackoverflow.com/q/260369/101
