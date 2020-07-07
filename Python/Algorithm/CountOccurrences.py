57bce3fa2192c449f44e93a3e6fb92e6e7b7104b
Antonio Maniero <maniero@klip.net>
2020-07-07 10:09:13 -0300

    https://pt.stackoverflow.com/q/461085/101

---
+++ Python/Algorithm/CountOccurrences.py
@@ -0,0 +1,13 @@
+def main():
+    ocorrencia = 0
+    numeroReal = int(input("Digite um número inteiro positivo:\n"))
+    numero = numeroReal
+    digito = int(input("Digite um dígito para que analisemos sua ocorrência no número anteriormente digitado:\n"))
+    while numero != 0:
+            if (numero % 10 == digito):
+                ocorrencia += 1
+            numero = numero // 10
+    print("O dígito ", digito, "ocorre", ocorrencia, "vezes no número", numeroReal)
+main()
+
+#https://pt.stackoverflow.com/q/461085/101
