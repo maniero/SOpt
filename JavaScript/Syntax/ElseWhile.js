4c20e5e4e4597a68f0f0fbd88b035e17a684fdae
Antonio Maniero <maniero@klip.net>
2020-07-08 10:10:42 -0300

    https://pt.stackoverflow.com/q/269193/101

---
+++ JavaScript/Syntax/ElseWhile.js
@@ -0,0 +1,23 @@
+var entrada = 1; //teria que ser uma valor desconhecido
+var entrou = false;
+while (entrada < 10) {
+    entrou = true;
+    //faz algo mais aqui, caso contrário não teria sentido
+    entrada++;
+}
+if (!entrou) {
+    console.log("deu problema");
+}
+console.log("vamos tentar de novo");
+var entrada = 11; //teria que ser uma valor desconhecido
+var entrou = false;
+while (entrada < 10) {
+    entrou = true;
+    //faz algo mais aqui, caso contrário não teria sentido
+    entrada++;
+}
+if (!entrou) {
+    console.log("deu problema");
+}
+
+//https://pt.stackoverflow.com/q/269193/101
