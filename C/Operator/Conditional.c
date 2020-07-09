43960800b2d8afc0f13544a61e6e595063c45c03
Antonio Maniero <maniero@klip.net>
2020-07-09 13:45:09 -0300

    https://pt.stackoverflow.com/q/274098/101

---
+++ C/Operator/Conditional.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+    printf(num1 < num2 ? "sim\n" : "nao\n");
+    int resposta = num1 < num2 ? 10 : -10;
+    printf("%i\n", resposta);
+}
+
+//https://pt.stackoverflow.com/q/274098/101
