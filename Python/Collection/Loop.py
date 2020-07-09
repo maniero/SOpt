14a499223c4d53922b8f4b73a0a21adc6699efc4
Antonio Maniero <maniero@klip.net>
2020-07-09 09:17:59 -0300

    https://pt.stackoverflow.com/q/461302/101

---
+++ Python/Collection/Loop.py
@@ -0,0 +1,7 @@
+quantidade = int(input("quantas notas você já tem = "))
+notas = []
+for c in range (0, quantidade):
+    notas.append(float(input("entre com sua nota = ")))
+print(notas)
+
+//https://pt.stackoverflow.com/q/461346/101
