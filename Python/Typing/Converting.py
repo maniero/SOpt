9dcaa265ad32b7100e866d4cb208cd5f3b3ff7d5
Antonio Maniero <maniero@klip.net>
2020-07-10 12:01:19 -0300

    https://pt.stackoverflow.com/q/276400/101

---
+++ Python/Typing/Converting.py
@@ -0,0 +1,7 @@
+from random import randint
+if int(input('Digite um número: ')) == randint( 1, 2):
+    print ('Você ganhou!')
+else:
+    print ('Você perdeu!')
+    
+#https://pt.stackoverflow.com/q/276400/101
