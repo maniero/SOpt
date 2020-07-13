7be751c3e588e1eafba26f1e6d7e00083947eab8
Antonio Maniero <maniero@klip.net>
2020-07-13 07:31:17 -0300

    https://pt.stackoverflow.com/q/278614/101

---
+++ CSharp/Array/Filter.cs
@@ -0,0 +1,5 @@
+for (var i = 0 ; i < 9 ; i++) if (calculo[i] != 0) WriteLine(calculo[i]);
+
+foreach (var item in calculo) if (item != 0) WriteLine(item);
+
+//https://pt.stackoverflow.com/q/278614/101
