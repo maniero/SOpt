110ff6e5e03d79b1b30f510aaee1f25f222e50c7
Antonio Maniero <maniero@klip.net>
2020-07-13 08:27:22 -0300

    https://pt.stackoverflow.com/q/279029/101

---
+++ Python/Syntax/Escope.py
@@ -0,0 +1,15 @@
+def valores(variavel):
+    if (variavel == 0):
+        variavel = 100
+    elif (variavel == 1):
+        variavel = 200
+    elif (variavel == 2):
+        variavel = 300
+    elif (variavel == 3):
+        variavel = 400
+    return variavel
+
+variavel = 0
+variavel = valores(variavel)
+
+#https://pt.stackoverflow.com/q/279029/101
