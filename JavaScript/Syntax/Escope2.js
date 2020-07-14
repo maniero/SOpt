1cad194a18f097ebedf27b69289cb1df1ad79ecd
Antonio Maniero <maniero@klip.net>
2020-07-14 11:32:20 -0300

    https://pt.stackoverflow.com/q/284804/101

---
+++ JavaScript/Syntax/Escope2.js
@@ -0,0 +1,13 @@
+function main() {
+    var V = prompt("Enrte com o valor da tensão");
+    var R = prompt("Entre com o  valor da resistência");
+    var corrente = calcula(V, R);
+    document.write("O valor da corrente é ", corrente, "A");
+}
+
+function calcula() {
+    return V / R;
+}
+main()
+
+//https://pt.stackoverflow.com/q/284804/101
