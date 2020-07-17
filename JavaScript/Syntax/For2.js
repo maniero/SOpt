c07420c81bf0964adfbf78d16558049478933d31
Antonio Maniero <maniero@klip.net>
2020-07-17 09:38:16 -0300

    https://pt.stackoverflow.com/q/292741/101

---
+++ JavaScript/Syntax/For2.js
@@ -0,0 +1,17 @@
+var r = [2, 5, 6, 18, 20, 10, 23, 12, 19, 10];
+var s = [1, 5, 7, 13, 18, 21, 10, 25, 32, 17, 3];
+var x = [0];
+var i;
+console.log("i = " + i);
+for (i = 0; i <= 9; i++) {
+    x[i] = r[i];
+    console.log("i = " + i);
+}
+console.log("i = " + i);
+for (i = 0; i <= 10; i++) {
+    x[i + 10] = s[i];
+    console.log("i = " + i);
+}
+console.log("i = " + i);
+
+//https://pt.stackoverflow.com/q/292741/101
