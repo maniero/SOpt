8eb1abfc0271c2e3c9de2404244c63f94f7433cc
Antonio Maniero <maniero@klip.net>
2020-07-20 09:50:41 -0300

    https://pt.stackoverflow.com/q/462802/101

---
+++ JavaScript/Syntax/Style.js
@@ -0,0 +1,9 @@
+function boolPortugueseElement() {
+    document.getElementById('element').innerHTML = true ? 'verdadeiro' : 'falso';
+}
+
+boolPortugueseElement();
+<div id='element'>
+</div>
+
+//https://pt.stackoverflow.com/q/462802/101
