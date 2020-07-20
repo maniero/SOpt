25aba10235222a6bcfe9f89f03233658cc542e58
Antonio Maniero <maniero@klip.net>
2020-07-20 09:02:06 -0300

    https://pt.stackoverflow.com/q/295029/101

---
+++ CPP/Pointer/Parameter.cpp
@@ -0,0 +1,12 @@
+void teste(int *t) {
+    *t = 50;
+}   
+
+int main() {
+    int x = 10;
+    cout << "Sem usar o metodo com ponteiro: " << x << endl;
+    teste(&x);
+    cout << "Usando o metodo com ponteiro: " << x << endl;   
+}
+
+//https://pt.stackoverflow.com/q/295029/101
