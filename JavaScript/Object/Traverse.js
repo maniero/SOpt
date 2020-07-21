38a34f8b976f5897cfb69000c0f22e8f0f7d39ed
Antonio Maniero <maniero@klip.net>
2020-07-21 09:20:30 -0300

    https://pt.stackoverflow.com/q/302810/101

---
+++ JavaScript/Object/Traverse.js
@@ -0,0 +1,10 @@
+var meusDados = {
+    nome: 'Bruno Coelho',
+    email: 'brunobcoelho.94@gmail.com',
+    idade: 23
+};
+for (var dados in meusDados) {
+    console.log(meusDados[dados]);
+}
+
+//https://pt.stackoverflow.com/q/302810/101
