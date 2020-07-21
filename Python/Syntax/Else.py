c6bb99977b16c10f33687813cd29b727b5298a5a
Antonio Maniero <maniero@klip.net>
2020-07-21 09:10:23 -0300

    https://pt.stackoverflow.com/q/302129/101

---
+++ Python/Syntax/Else.py
@@ -0,0 +1,13 @@
+if carro == 'Peugeot':
+    c = 50
+elif carro == 'C3':
+    c = 60
+elif carro == 'Cruze':
+    c = 70
+elif carro == 'CRV':
+    c = 75
+else:
+    print('O carro digitado não está cadastrado no nosso sistema. Verifique se está digitado corretamente ou comunique a empresa.')
+    return #ou pode usar sys.exit() se quiser acabar de vez
+    
+#https://pt.stackoverflow.com/q/302129/101
