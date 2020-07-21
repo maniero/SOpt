5a9dd35d296d13ef6997f479abba1fcf279dafc0
Antonio Maniero <maniero@klip.net>
2020-07-21 12:37:04 -0300

    https://pt.stackoverflow.com/q/305011/101

---
+++ Python/Algorithm/Prefixes.py
@@ -0,0 +1,8 @@
+prefixes = "JKLMNOPQ"
+for letter in prefixes:
+    if letter[0] == "Q" or letter[0] == "O":
+        print(letter + "uack")
+    else:
+        print(letter + "ack")
+        
+#https://pt.stackoverflow.com/q/305011/101
