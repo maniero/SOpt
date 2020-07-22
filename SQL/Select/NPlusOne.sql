4a772617e8b7279f0651a627006b401f95073d4b
Antonio Maniero <maniero@klip.net>
2020-07-22 12:55:27 -0300

    https://pt.stackoverflow.com/q/307264/101

---
+++ SQL/Select/NPlusOne.sql
@@ -0,0 +1,14 @@
+SELECT * FROM Nf
+SELECT * FROM NfItem
+
+SELECT * FROM Nf
+SELECT * FROM NfItem WHERE NfNumero == Nf.Numero
+SELECT * FROM NfItem WHERE NfNumero == Nf.Numero
+SELECT * FROM NfItem WHERE NfNumero == Nf.Numero
+SELECT * FROM NfItem WHERE NfNumero == Nf.Numero
+--
+--
+--
+--Tantos quantos forem a quantidade de notas fiscais existentes.
+
+--https://pt.stackoverflow.com/q/307264/101
