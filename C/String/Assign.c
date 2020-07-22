5156728ca833a82484745cca347548db1290f5e9
Antonio Maniero <maniero@klip.net>
2020-07-22 14:29:43 -0300

    https://pt.stackoverflow.com/q/307975/101

---
+++ C/String/Assign.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char mat[200][100];
+    strcpy(mat[0], "paulo");
+    printf("%s", mat[0]);
+}
+
+//https://pt.stackoverflow.com/q/307975/101
