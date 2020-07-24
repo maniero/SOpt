fa02118284ed56c9f173cc6940579693300358be
Antonio Maniero <maniero@klip.net>
2020-07-24 11:57:34 -0300

    https://pt.stackoverflow.com/q/313052/101

---
+++ Java/String/CharSequence.java
@@ -0,0 +1,5 @@
+principal.quanti.setText("Você possui " + cont + " produtos cadastrados.")
+
+principal.quanti.setText((CharSequence)("Você possui " + cont + " produtos cadastrados."))
+
+//https://pt.stackoverflow.com/q/313052/101
