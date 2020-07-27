012cde65fd536c063cc171aa12c1fbd0ca979951
Antonio Maniero <maniero@klip.net>
2020-07-27 08:26:24 -0300

    https://pt.stackoverflow.com/q/315684/101

---
+++ CSharp/Typing/HasData.cs
@@ -0,0 +1,13 @@
+if (usuario == null)
+
+if (usuario.Ativo)
+
+if (lista.Count() > 0)
+
+if (lista != null && lista.Count > 0)
+
+if (texto != "")
+
+if(!string.IsNullOrWhiteSpace(texto))
+
+//https://pt.stackoverflow.com/q/315684/101
