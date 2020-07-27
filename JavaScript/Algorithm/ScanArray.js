d9720afbe48a19fa21dafd9fa0654b85e580488b
Antonio Maniero <maniero@klip.net>
2020-07-27 08:49:21 -0300

    https://pt.stackoverflow.com/q/317596/101

---
+++ JavaScript/Algorithm/ScanArray.js
@@ -0,0 +1,10 @@
+for (var item of ['aegir', 'aud', 'balder']) alert(item);
+
+var deuses = ['aegir', 'aud', 'balder'];
+var i = 0;
+while (i < deuses.length) { //vai até o tamanho já conhecido
+   alert(deuses[i]); //pega o elemento indexado pela variável de controle do laço
+   i++; //incrementa a variável para o próximo passo
+}
+
+//https://pt.stackoverflow.com/q/317596/101
