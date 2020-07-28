c3cdbfc4ca8b941ecdce1049bb6a12658fae29cd
Antonio Maniero <maniero@klip.net>
2020-07-28 09:40:52 -0300

    https://pt.stackoverflow.com/q/320208/101

---
+++ JavaScript/Typing/Convert.js
@@ -0,0 +1,21 @@
+switch (parseInt(prompt("Digite o mes:"))) {
+case 1:
+    alert("janeiro");
+    break;
+case 2:
+    alert("Fevereiro");
+    break;
+case 3:
+    alert("março");
+    break;
+case 4:
+    alert("abril");
+    break;
+case 5:
+    alert("maio");
+    break;
+  default:
+    alert("Nenhuma das alternativas")
+}
+
+//https://pt.stackoverflow.com/q/320208/101
