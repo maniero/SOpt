57ae8861490a759c4f794b111a36726306e17bea
Antonio Maniero <maniero@klip.net>
2020-07-29 12:42:36 -0300

    https://pt.stackoverflow.com/q/321404/101

---
+++ CPP/Operator/Xor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << (true != false) << endl;
+    cout << (true != true) << endl;
+    cout << (false != true) << endl;
+    cout << (false != false) << endl;
+}
+
+//https://pt.stackoverflow.com/q/321404/101
