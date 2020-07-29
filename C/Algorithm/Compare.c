f02ef1959fbaca50dadf90b33a894c1da80b3139
Antonio Maniero <maniero@klip.net>
2020-07-29 12:07:50 -0300

    https://pt.stackoverflow.com/q/464605/101

---
+++ C/Algorithm/Compare.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int Comparacao(int x, int y)  {
+    return x < y ? -1 : x != y);
+}
+
+int main(){
+    int a, b;
+    puts("Digite um valor para a: ");
+    scanf("%d", &a);
+    puts("Digite um valor para b: ");
+    scanf("%d", &b);
+    printf("%d", Comparacao(a, b));
+}
+
+//https://pt.stackoverflow.com/q/464605/101
