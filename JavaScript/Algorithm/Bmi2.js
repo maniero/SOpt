7af605e1bc1aab6ad39cf8633f3eadfd6d5bcbe9
Antonio Maniero <maniero@klip.net>
2020-08-05 11:38:14 -0300

    https://pt.stackoverflow.com/q/327751/101

---
+++ JavaScript/Algorithm/Bmi2.js
@@ -0,0 +1,5 @@
+function calculaIMC(altura, peso) { return peso / (altura * altura); }
+
+console.log("O seu imc é: " + calculaIMC(prompt("Sua altura? "), prompt("Seu peso? ")));
+
+//https://pt.stackoverflow.com/q/327751/101
