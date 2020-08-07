7ab3dee62d73006ea5ffdf5b94ad1bb489a1746e
Antonio Maniero <maniero@klip.net>
2020-08-07 08:28:35 -0300

    https://pt.stackoverflow.com/q/466737/101

--- Python/Collection/FilterList2.py
+++ Python/Collection/FilterList2.py
@@ -1,10 +1,8 @@
-import math
+frase = "A B C D e f g h".split()
+novo = []
+for i in frase:
+    if i.isupper():
+        novo.append(i)
+print(novo)
 
-is_magic = []
-for n in range(8, 27):
-    valor = pow(n, 0.5)
-    if valor == 2 or valor == 3 or valor == 5 or valor == 7:
-        is_magic.append(n)
-print(is_magic)
-
-#https://pt.stackoverflow.com/q/466768/101
+#https://pt.stackoverflow.com/q/466737/101
