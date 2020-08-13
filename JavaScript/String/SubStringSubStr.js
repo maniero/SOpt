2b30020478672689160efa7cc6e1f07f62ae3e19
Antonio Maniero <maniero@klip.net>
2020-08-13 11:55:21 -0300

    https://pt.stackoverflow.com/q/340337/101

---
+++ JavaScript/String/SubStringSubStr.js
@@ -0,0 +1,7 @@
+console.log("012345".substr(1, 3));
+console.log("012345".substring(1, 3));
+
+console.log("012345".substr(0, 3));
+console.log("012345".substring(0, 3));
+
+//https://pt.stackoverflow.com/q/340337/101
