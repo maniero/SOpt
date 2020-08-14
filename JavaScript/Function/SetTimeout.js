f8e9b77271c12efe721374773fb8bcb340d6b994
Antonio Maniero <maniero@klip.net>
2020-08-14 09:16:02 -0300

    https://pt.stackoverflow.com/q/343669/101

---
+++ JavaScript/Function/SetTimeout.js
@@ -0,0 +1,11 @@
+function logicaRotacao() {
+    console.log("ok");
+}
+setTimeout(()=>this.logicaRotacao(), 5000);
+
+function logicaRotacao() {
+    console.log("ok");
+}
+setTimeout(logicaRotacao(), 5000);
+
+//https://pt.stackoverflow.com/q/343669/101
