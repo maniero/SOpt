d53e2d7f8be2064ae0dc5637d2559435ed17778a
Antonio Maniero <maniero@klip.net>
2020-08-17 09:56:57 -0300

    https://pt.stackoverflow.com/q/467812/101

---
+++ Python/Syntax/For2.py
@@ -0,0 +1,6 @@
+quant = int(input('Quantos andares você quer que tenha a meia pirâmide? '))
+print('')
+for n in range(quant, 0, -1):
+    print('* ' * n)
+
+#https://pt.stackoverflow.com/q/467812/101
