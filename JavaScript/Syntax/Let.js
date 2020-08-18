93953340e78821d804bbf0e284c8f2d37e9130a1
Antonio Maniero <maniero@klip.net>
2020-08-18 09:56:03 -0300

    https://pt.stackoverflow.com/q/346112/101

---
+++ JavaScript/Syntax/Let.js
@@ -0,0 +1,11 @@
+let x = 'João';
+var y = 'João';
+z = 'João';
+console.log(this.x);
+console.log(this.y);
+console.log(this.z);
+console.log(x);
+console.log(y);
+console.log(z);
+
+//https://pt.stackoverflow.com/q/346112/101
