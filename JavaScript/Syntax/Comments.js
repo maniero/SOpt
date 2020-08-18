30573350223a1cad0eaa53f7cb353636c98c5699
Antonio Maniero <maniero@klip.net>
2020-08-18 10:05:36 -0300

    https://pt.stackoverflow.com/q/346435/101

---
+++ JavaScript/Syntax/Comments.js
@@ -0,0 +1,5 @@
+console.log("teste");
+// $('#descricaoOpcional').val($('#descricaoOpcional').val() + "\n______________________________________________________\n" + campo.val());
+console.log("fim");
+
+//https://pt.stackoverflow.com/q/346435/101
