5eadac4602ea1c51f58ffdb1f11d4a5c160ff69b
Antonio Maniero <maniero@klip.net>
2020-08-21 13:36:18 -0300

    https://pt.stackoverflow.com/q/354904/101

---
+++ JavaScript/Algorithm/Add.js
@@ -0,0 +1,6 @@
+let filaDeEspera = [];
+filaDeEspera.push({nome : "João", telefone : "1122223333", senha : "001"});
+filaDeEspera.push({nome : "Maria", telefone : "4455556666", senha : "002"});
+console.log(filaDeEspera);
+
+//https://pt.stackoverflow.com/q/354904/101
