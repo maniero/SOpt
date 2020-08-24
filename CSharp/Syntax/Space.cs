15b28c6acde03a5d1826b12cf5b9e22e7cad96cd
Antonio Maniero <maniero@klip.net>
2020-08-24 10:47:02 -0300

    https://pt.stackoverflow.com/q/355958/101

---
+++ CSharp/Syntax/Space.cs
@@ -0,0 +1,10 @@
+SomaProdutos(lista)
+
+int SomaProdutos(Array lista) { ... }
+
+SomaProdutos (lista)
+
+if (x == 1)
+    for (var i = 0; i < y; i++) { ... }
+    
+//https://pt.stackoverflow.com/q/355958/101
