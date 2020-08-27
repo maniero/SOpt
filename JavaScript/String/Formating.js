0cb2127392e64431d1eb18ef9e36a76d9b3ca24c
Antonio Maniero <maniero@klip.net>
2020-08-27 13:05:23 -0300

    https://pt.stackoverflow.com/q/362560/101

---
+++ JavaScript/String/Formating.js
@@ -0,0 +1,17 @@
+console.log((2).toString());
+
+console.log(2.);
+console.log((2.).toString());
+console.log(2.5.toString());
+
+console.log(2. .toString());
+console.log(2 .toString());
+//console.log(2.toString());
+//console.log(2..toString());
+
+console.log(2.0.toString());
+
+console.log(2.5.toString());
+//console.log(2.toFixed());
+
+//https://pt.stackoverflow.com/q/362560/101
