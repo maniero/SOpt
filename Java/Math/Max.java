3f03f880511e05514770c6e103b9f86465d244ac
Antonio Maniero <maniero@klip.net>
2020-09-09 08:36:50 -0300

    https://pt.stackoverflow.com/q/34991/101

---
+++ Java/Math/Max.java
@@ -0,0 +1,8 @@
+public void determineMaior(int y1 , int y2, int y3, int y4, int y5, int y6, int y7, int y8, int y9, int y10){
+    int maiorResultado = maximum(y1, y2, y3, y4, y5, y6, y7, y8, y9, y10);
+    System.out.print("Maior Numero: " + maiorResultado);
+}
+
+lernumeros.determineMaior(y1, y2, y3, y4, y5, y6, y7, y8, y9, y10);
+
+//https://pt.stackoverflow.com/q/34991/101
