1a08ab491da0134d004e8ae4bc44effbfacf42ab
Antonio Maniero <maniero@klip.net>
2020-09-10 09:19:21 -0300

    https://pt.stackoverflow.com/q/40450/101

---
+++ VB.NET/IO/Printer.vb
@@ -0,0 +1,6 @@
+qry = "SELECT * FROM Win32_PrinterDriver"
+For Each driver In objWMIService.ExecQuery(qry)
+  If driver.Name = "..." Then driver.Delete_
+Next
+
+'https://pt.stackoverflow.com/q/40450/101
