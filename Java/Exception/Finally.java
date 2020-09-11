b1dfc34c3caa0cc41d393b3c10b93c46d9c68ee7
Antonio Maniero <maniero@klip.net>
2020-09-11 09:44:49 -0300

    https://pt.stackoverflow.com/q/42313/101

--- Java/Exception/Finally.java
+++ Java/Exception/Finally.java
@@ -1,14 +1,5 @@
-/* package whatever; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
+public class Main {
+	public static void main(String[] args) {
 		System.out.println(teste());
 	}
 	
