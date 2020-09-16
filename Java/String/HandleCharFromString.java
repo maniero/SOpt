3d43b2d76449e370a79bb7a345083f6db317162f
Antonio Maniero <maniero@klip.net>
2020-09-16 08:09:37 -0300

    https://pt.stackoverflow.com/q/45347/101

--- Java/String/HandleCharFromString.java
+++ Java/String/HandleCharFromString.java
@@ -1,11 +1,11 @@
-class Program {
-	public static void main (String[] args)	{
+class Main {
+	public static void main(String[] args)	{
 		solve("Teste");
 	}
 	public static void solve(String text) {
 	    int len = text.length();
 	    StringBuffer buffer = new StringBuffer(len);
-	    for(int i = 0; i < len; i++){
+	    for (int i = 0; i < len; i++){
 	        char letter = text.charAt(i);
 	        buffer.append(i % 2 == 0 ? Character.toUpperCase(letter) : Character.toLowerCase(letter));
 	    }
