1fc36316ab411fb3e58bc03a4267d6d369a27640
Antonio Maniero <maniero@klip.net>
2020-09-18 12:38:33 -0300

    https://pt.stackoverflow.com/q/52093/101

--- CSharp/ASP.NET/GetLocation.cs
+++ CSharp/ASP.NET/GetLocation.cs
@@ -1,8 +1,6 @@
-using (var response = req.GetResponse()) {
-    var location = response.Headers["Location"];
-    using (var srResponseReader = new StreamReader(response.GetResponseStream())) {
-        html = srResponseReader.ReadToEnd();
-    }
-}
+using var response = req.GetResponse()) ;
+var location = response.Headers["Location"];
+using var srResponseReader = new StreamReader(response.GetResponseStream()));
+html = srResponseReader.ReadToEnd();
 
 //https://pt.stackoverflow.com/q/52093/101
