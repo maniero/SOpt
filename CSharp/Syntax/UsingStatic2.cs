340f739f34dd8d96bae0b417f777eb96bf79cc16
Antonio Maniero <maniero@klip.net>
2020-09-25 13:33:39 -0300

    https://pt.stackoverflow.com/q/75343/101

---
+++ CSharp/Syntax/UsingStatic2.cs
@@ -0,0 +1,10 @@
+using System;
+using static System.Console; //C# 6
+
+void Main() {
+    int i = 10; //raramente se usa o tipo do .NET, prefere-se o alias da linguagem
+    //ou usa-se o var mesmo, neste caso
+    WriteLine(i); //C# 6
+}
+
+//https://pt.stackoverflow.com/q/75343/101
