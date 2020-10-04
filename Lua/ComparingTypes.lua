947ac5e4bc850f95b6a73e223b1aae5152e5c9bd
Antonio Maniero <maniero@klip.net>
2020-10-04 14:31:31 -0300

    https://pt.stackoverflow.com/q/475123/101

---
+++ Lua/ComparingTypes.lua
@@ -0,0 +1,12 @@
+x = io.read()
+if x ~= 0 then
+	print(x .. " diferente de 0")
+end
+if x == "1" then
+	print(x .. " igual 1")
+end
+if tonumber(x) == 1 then
+	print(x .. " igual 1")
+end
+
+--https://pt.stackoverflow.com/q/475123/101
