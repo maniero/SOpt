9138b527ca72ffac511d8eb7a0dc15345f20c9c2
Antonio Maniero <maniero@klip.net>
2020-10-20 09:26:48 -0300

    https://pt.stackoverflow.com/q/122519/101

---
+++ C/Console/Printf2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void) {
+    char nome[6];
+    printf("Ola! Insira seu nome, por fovor: \n");
+    scanf("%s", nome);
+    printf("Seja bem vindo, %s\n", nome);
+}
+
+//https://pt.stackoverflow.com/q/122519/101
