786f5a32d9217dabaee210b5844bff17986b0442
Antonio Maniero <maniero@klip.net>
2020-10-27 14:33:59 -0300

    https://pt.stackoverflow.com/q/131955/101

---
+++ CPP/Vector/PushBack2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class bunny {};
+
+int main() {
+    vector<bunny> bunnies;
+    bunnies.push_back(bunny());
+}
+
+//https://pt.stackoverflow.com/q/131955/101
