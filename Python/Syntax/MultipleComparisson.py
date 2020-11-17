c09c4d6e66d7eb263d7d22ee3eec37bd6df4b5b8
Antonio Maniero <maniero@klip.net>
2020-11-17 06:39:34 -0300

    https://pt.stackoverflow.com/q/482565/101

---
+++ Python/Syntax/MultipleComparisson.py
@@ -0,0 +1,14 @@
+a = 0
+if 1 <= a <= 3:
+	print("0")
+a = 1
+if 1 <= a <= 3:
+	print("1")
+a = 3
+if 1 <= a <= 3:
+	print("3")
+a = 4
+if 1 <= a <= 3:
+	print("4")
+	
+#https://pt.stackoverflow.com/q/482565/101
