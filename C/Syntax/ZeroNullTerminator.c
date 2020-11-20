2ce4700070179c4ff2f01034f3f56d42f99c0888
Antonio Maniero <maniero@klip.net>
2020-11-20 08:21:33 -0300

    http://pt.stackoverflow.com/a/177620/101

--- C/Syntax/ZeroNullTerminator.c
+++ C/Syntax/ZeroNullTerminator.c
@@ -11,6 +11,7 @@ int main(void) {
 	if (!s[5]) printf("s ok\n"); //verifica se o 6o. caractere é o terminador
 	if (!p) printf("p ok\n"); //verifica se o ponteiro não é nulo
 //	if (p == i) printf("p NULL ok\n"); //tipos incompatíveis sendo comparados
+        printf("%zu", sizeof('x'));
 }
 
 //http://pt.stackoverflow.com/a/177620/101
