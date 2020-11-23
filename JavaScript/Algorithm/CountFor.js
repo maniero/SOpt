a3896bacf727f46ebc46dba7e96de7570dec14e9
Antonio Maniero <maniero@klip.net>
2020-11-23 08:30:08 -0300

    https://pt.stackoverflow.com/q/483199/101

---
+++ JavaScript/Algorithm/CountFor.js
@@ -0,0 +1,5 @@
+let vagas = ['Audi', 'HB20', 'Lamborghini', 'Maserati'];
+console.log(`No estacionamento tem ${vagas.length} vagas`);
+for (let i = 0; i < vagas.length; i++) console.log(`na vaga ${i + 1} está o Automóvel ${vagas[i]}`);
+
+//https://pt.stackoverflow.com/q/483199/101
