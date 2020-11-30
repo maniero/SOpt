c7599b951cb9a048d02f315e1c3b5774d79f03b4
Antonio Maniero <maniero@klip.net>
2020-11-30 09:26:13 -0300

    https://pt.stackoverflow.com/q/484418/101

--- Java/Algorithm/LoopInvalidData.java
+++ Java/Algorithm/LoopInvalidData.java
@@ -1,1 +1,21 @@
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        int notaAluno [] = new int[5];
+        for (int i = 0; i < notaAluno.length; i++) {
+            var nota = sc.nextInt();;
+            if (nota < 0 || nota > 100) {
+                System.out.println("Nota inválida, digite novamente:");
+                i--;
+                continue;
+            }
+            notaAluno[i] = nota;
+        }
+        for (int i = 0; i < notaAluno.length; i++) System.out.println("Nota do aluno " + i + ":" + notaAluno[i]);
+    }
+}
+
+//https://pt.stackoverflow.com/q/484418/101
