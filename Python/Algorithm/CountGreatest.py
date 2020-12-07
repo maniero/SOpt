1b041c5b201f1fa2dc906ffd753470e6867d2a01
Antonio Maniero <maniero@klip.net>
2020-12-07 07:17:49 -0300

    https://pt.stackoverflow.com/q/485318/101

---
+++ Python/Algorithm/CountGreatest.py
@@ -0,0 +1,14 @@
+num_lidos = int(input('Digite a quantidade de números: '))
+maior = 0
+qnt = 0
+for num in range(1, num_lidos):
+    num = int(input('Digite o valor: '))
+    if num > maior:
+        maior = num
+        qnt = 1
+    elif num == maior:
+        qnt += 1
+print(f'Maior valor = {maior}')
+print(f'Quantidade de vezes que o maior número foi lido: {qnt}')
+
+#https://pt.stackoverflow.com/q/485318/101
