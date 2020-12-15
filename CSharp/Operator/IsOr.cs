6eb50d142632c20fe846562a0046feeea5305e7e
Antonio Maniero <maniero@klip.net>
2020-12-15 13:14:47 -0300

    https://pt.stackoverflow.com/q/248148/101

---
+++ CSharp/Operator/IsOr.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+
+public class Program {
+	public static void Main() {
+		string name = "Girl";
+        Write(name is "Girl" or "girl");
+	}
+}
+
+//https://pt.stackoverflow.com/q/248148/101
