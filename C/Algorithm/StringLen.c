f73d93bdca1666d5bc127df1403b5118e6062588
Antonio Maniero <maniero@klip.net>
2020-12-16 16:32:55 -0300

    https://pt.quora.com/unanswered/Por-que-com-o-seguinte-c%C3%B3digo-em-C-sempre-obtenho-que-o-tamanho-da-string-%C3%A9-3-Linha-1-char-texto-9-linha-2-for-i-0-i-10-i-linha-3-printf-ld-n-strlen-texto-Obs-Isso-s%C3%B3-acontece-quando-strlen-est%C3%A1

---
+++ C/Algorithm/StringLen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char texto[9];
+	for (int i = 0; i < 10; i++) {;
+	    printf("%ld\n", strlen(texto));
+	}
+	printf("%ld\n", strlen(texto));
+}
+
+//https://pt.quora.com/unanswered/Por-que-com-o-seguinte-c%C3%B3digo-em-C-sempre-obtenho-que-o-tamanho-da-string-%C3%A9-3-Linha-1-char-texto-9-linha-2-for-i-0-i-10-i-linha-3-printf-ld-n-strlen-texto-Obs-Isso-s%C3%B3-acontece-quando-strlen-est%C3%A1
