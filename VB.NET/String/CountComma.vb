76be25718c8ff471617a44a5d1a34be758887f0f
Antonio Maniero <maniero@klip.net>
2020-12-22 10:45:01 -0300

    https://pt.stackoverflow.com/q/271817/101

---
+++ VB.NET/String/CountComma.vb
@@ -0,0 +1,9 @@
+Imports System
+				
+Public Module Module1
+	Public Sub Main()
+		For i As Integer = 0 To 9 : Console.WriteLine(i & ", ") : Next
+	End Sub
+End Module
+
+'https://pt.stackoverflow.com/q/271817/101
