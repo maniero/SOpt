47abff568aac2225f3b3a6f6b13d11218f0b744b
Antonio Maniero <maniero@klip.net>
2021-03-16 10:48:24 -0300

    https://pt.stackoverflow.com/q/499107/101

---
+++ PHP/Array/Print.php
@@ -0,0 +1,11 @@
+<?php
+class Dtb {
+    public static $funcionarios = ['M','G','N','I'] ;
+    public static $atividades = 'Sistemas';
+    public static $temp = 12;
+}
+echo print_r(Dtb::$funcionarios);
+echo Dtb::$atividades;
+echo Dtb::$temp;
+
+//https://pt.stackoverflow.com/q/499107/101
