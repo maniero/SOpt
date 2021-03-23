19f3baa0641c63e22572eb36a575b01d143afb56
Antonio Maniero <maniero@klip.net>
2021-03-23 09:37:57 -0300

    https://pt.stackoverflow.com/q/500511/101

---
+++ C/String/FindString.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int vetorPalavra(char str[10]) {
+    for (int i = 0; i < 10; i++) if (str[i] == 'a' && str[i + 1] == 's' && str[i + 2] == 'a' ) return 1;
+    return 0;
+}
+int main() {
+    printf("%d", vetorPalavra("xdmasahkp"));
+}
+
+//https://pt.stackoverflow.com/q/500511/101
