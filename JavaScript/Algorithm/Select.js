5fb331d3abac64ce2591e139e9e12abfdae81be7
Antonio Maniero <maniero@klip.net>
2021-03-28 09:28:20 -0300

    https://pt.stackoverflow.com/q/489966/101

---
+++ JavaScript/Algorithm/Select.js
@@ -0,0 +1,10 @@
+var roll1 = 3;
+if (roll1 == 1) {
+    console.log(`DESASTRE`);
+} else if (roll1 == 20) {
+    console.log(`NORMAL`);
+} else {
+    console.log(`FALHOU`);
+}
+
+//https://pt.stackoverflow.com/q/489966/101
