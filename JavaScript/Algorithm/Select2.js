f7aea8f21fb4980df5885de61cca74a54722bada
Antonio Maniero <maniero@klip.net>
2021-03-28 09:29:25 -0300

    https://pt.stackoverflow.com/q/489966/101

---
+++ JavaScript/Algorithm/Select2.js
@@ -0,0 +1,12 @@
+var roll1 = 3;
+if (roll1 == 1) {
+    console.log(`DESASTRE`);
+} else if (roll1 == 20) {
+    console.log(`NORMAL`);
+} else if (roll1 > 1 && roll1 < 20) {
+    console.log(`FALHOU`);
+} else {
+    console.log(`Deu núemro inválido`);
+}
+
+//https://pt.stackoverflow.com/q/489966/101
