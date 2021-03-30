2ee52cd3ff3346cb9a1b7584f33c99782de9263f
Antonio Maniero <maniero@klip.net>
2021-03-30 10:52:11 -0300

    https://pt.quora.com/Em-Linguagem-C-como
    https://pt.quora.com/Em-Linguagem-C-como-%C3%A9-poss%C3%ADvel-usar-a-keyword-typedef-struct-identificador-antes-desta-ter-seus-membros-definidos-previamente-no-c%C3%B3digo-sendo-que-com-fun%C3%A7%C3%B5es-a-ordem-de-defini%C3%A7%C3%A3o/answer/Antonio-Maniero

---
+++ C/Struct/TypedefStruct.c
@@ -0,0 +1,5 @@
+typedef struct teste *apontaTeste;
+
+int main(void) {}
+
+//https://pt.quora.com/Em-Linguagem-C-como-%C3%A9-poss%C3%ADvel-usar-a-keyword-typedef-struct-identificador-antes-desta-ter-seus-membros-definidos-previamente-no-c%C3%B3digo-sendo-que-com-fun%C3%A7%C3%B5es-a-ordem-de-defini%C3%A7%C3%A3o/answer/Antonio-Maniero
