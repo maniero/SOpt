aa4de6748e5fde87f986f9da4529e0e23343329c
Antonio Maniero <maniero@klip.net>
2021-03-31 09:54:16 -0300

    https://pt.stackoverflow.com/q/488872/101

---
+++ JavaScript/Math/Round.js
@@ -0,0 +1,7 @@
+var altura = 170
+var peso = 70
+var idade = 30
+var tmb = 66.5 + (13.75 * peso) + (5.0 * altura) - (6.8 * idade)
+console.log(Math.round(tmb / 100) * 100)
+
+//https://pt.stackoverflow.com/q/488872/101
