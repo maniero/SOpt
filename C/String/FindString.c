c183d3c47a5ab415c46b3d19a39df07ff1643deb
Antonio Maniero <maniero@klip.net>
2021-04-06 10:13:45 -0300

    https://pt.stackoverflow.com/q/500511/101

--- C/String/FindString.c
+++ C/String/FindString.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int vetorPalavra(char str[10]) {
-    for (int i = 0; i < 8; i++) if (str[i] == 'a' && str[i + 1] == 's' && str[i + 2] == 'a' ) return 1;
+    for (int i = 0; i < 7; i++) if (str[i] == 'a' && str[i + 1] == 's' && str[i + 2] == 'a' ) return 1;
     return 0;
 }
 int main() {
