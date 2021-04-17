32be5035769f80a4475d3b68a452e1161fef23f2
Antonio Maniero <maniero@klip.net>
2021-04-17 10:31:28 -0300

    https://pt.stackoverflow.com/q/399347/101

---
+++ C/Struct/Initialization.c
@@ -0,0 +1,13 @@
+struct Ponto {
+    let x: Int
+    let y: Int
+    init(x: Int, y: Int) {
+      self.x = x
+      self.y = y
+    }
+}
+var ponto1: Ponto;
+ponto1 = Ponto(x: 1, y: -1);
+print(ponto1.x, ponto1.y);
+
+//https://pt.stackoverflow.com/q/399347/101
