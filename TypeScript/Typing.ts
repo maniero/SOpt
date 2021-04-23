477055333e229ca33f3357b794aee9a685096c0d
Antonio Maniero <maniero@klip.net>
2021-04-23 08:20:52 -0300

    https://pt.stackoverflow.com/q/419887/101

---
+++ TypeScript/Typing.ts
@@ -0,0 +1,5 @@
+let month = new Date().getMonth() + 1;
+let todayMonth = month.toString();
+if (todayMonth < 10) todayMonth = '0' + month.toString();
+                    
+//https://pt.stackoverflow.com/q/419887/101
