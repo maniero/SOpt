264e835a97f94e9466225e805c7dbb65a07717e7
Antonio Maniero <maniero@klip.net>
2021-04-23 08:17:35 -0300

    https://pt.stackoverflow.com/q/428226/101

---
+++ CSharp/Method/StaticExample.cs
@@ -0,0 +1,4 @@
+public static class DateUtils { //algumas pessoas criticam este tipo de coisa
+    public static string ToShortDatePtBr() => StringFormats.DatetTimeToShortDateStrPtBR();
+    
+//https://pt.stackoverflow.com/q/428226/101
