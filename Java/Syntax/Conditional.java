a9a85daaf900a6580e3c2b5f61b18ba4633c58d3
Antonio Maniero <maniero@klip.net>
2021-04-24 17:34:42 -0300

    https://pt.stackoverflow.com/q/452210/101

---
+++ Java/Syntax/Conditional.java
@@ -0,0 +1,5 @@
+if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) p1.getSnake().setDirection(new Right());
+
+else if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) p1.getSnake().setDirection(new Left());
+
+//https://pt.stackoverflow.com/q/452210/101
