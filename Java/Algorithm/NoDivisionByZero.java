2eb3283d3fe2a3c9a37302a2ad110b77e17ff18f
Antonio Maniero <maniero@klip.net>
2021-04-25 10:14:08 -0300

    https://pt.stackoverflow.com/q/460064/101

---
+++ Java/Algorithm/NoDivisionByZero.java
@@ -0,0 +1,4 @@
+if (num1 < num2 || num2 == 0) System.out.println("Impossivel realizar calculo!!");    
+else System.out.println("A divisao e: " + num1 / num2);
+
+//https://pt.stackoverflow.com/q/460064/101
