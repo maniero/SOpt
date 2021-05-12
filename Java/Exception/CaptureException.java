47abfbff81aecba8bfea77088542965036450402
Antonio Maniero <maniero@klip.net>
2021-05-12 10:42:13 -0300

    https://pt.stackoverflow.com/q/30124/101

---
+++ Java/Exception/CaptureException.java
@@ -0,0 +1,7 @@
+try {
+    Integer i = Integer.parseInt("3")
+} catch(Exception e){ //em vez de NumberFormatException
+    System.out.println("Número inválido");
+}
+
+//https://pt.stackoverflow.com/q/30124/101
