857ee5476c79d882f35d43d95705633141c62d54
Antonio Maniero <maniero@klip.net>
2021-05-13 14:58:12 -0300

    https://pt.stackoverflow.com/q/42836/101

---
+++ C/Array/MultiDimentionalExample.c
@@ -0,0 +1,16 @@
+int a[3][4] = {  
+    {0, 1, 2, 3},
+    {4, 5, 6, 7},
+    {8, 9, 10, 11}
+};
+
+int a[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+array1[array2[3][i]][2][string] //estou considerando 3 dimensões e que string é um inteiro
+//também estou considerando que array2 é de um tipo inteiro
+
+array1[array2][2][string] //estou considerando 3 dimensões e que string é um inteiro
+
+array1[array2[0][0]][2][string] //estou considerando 3 dimensões e que string é um inteiro
+
+//https://pt.stackoverflow.com/q/42836/101
