2c65204a3d45b8e1b408b5031753c25335ec8d7b
Antonio Maniero <maniero@klip.net>
2021-05-14 11:00:41 -0300

    https://pt.stackoverflow.com/q/509059/101

---
+++ C/Syntax/EmptyStatement.c
@@ -0,0 +1,12 @@
+for (int i = 0; i < 10; i++);
+for (int i = 0; i < 10; i++) ;
+for (int i = 0; i < 10; i++)
+    ;
+for (int i = 0; i < 10; i++) {}
+for (int i = 0; i < 10; i++) {
+}
+for (int i = 0; i < 10; i++) {
+    ;
+}
+
+//https://pt.stackoverflow.com/q/509059/101
