8d3e58b85dab5693e74887af5f6cf1ce71b41512
Antonio Maniero <maniero@klip.net>
2021-05-19 11:56:42 -0300

    https://pt.stackoverflow.com/q/54012/101

---
+++ CSharp/Method/StaticVsInstance.cs
@@ -0,0 +1,7 @@
+public static void metodo1(Conta conta, int valor) { ... }
+
+public void metodo2(int valor) { ... } //pertence à classe Conta, é de instância
+
+public static void metodo3(int valor) { ... }
+
+//https://pt.stackoverflow.com/q/54012/101
