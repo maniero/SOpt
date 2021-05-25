ad04c96b8d4b9be77d9a73bcaa6d60c36f97cc3a
Antonio Maniero <maniero@klip.net>
2021-05-25 11:03:58 -0300

    https://pt.stackoverflow.com/q/77210/101

---
+++ CSharp/Syntax/VariableName.cs
@@ -0,0 +1,10 @@
+var 2d = 1;
+WriteLine(2d); //está imprimindo "1" (o valor de 2d), ou imprimindo "2.0" que é um double)
+var 3 = 1;
+WriteLine(3); //está imprimindo "1" (o valor de 3), ou imprimindo "3" que é um int)
+
+var 5dias = 1;
+//um dia uma pessoa cria o sufixo dias, aí seria permitido:
+var x = 5dias; //antes saberia que está guardando 1 em x, agora pode estar guardando 5
+
+//https://pt.stackoverflow.com/q/77210/101
