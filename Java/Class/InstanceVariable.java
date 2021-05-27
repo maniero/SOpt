67c10c9a2d7f3876fcb214a91db54cac49075c24
Antonio Maniero <maniero@klip.net>
2021-05-27 14:52:05 -0300

    https://pt.stackoverflow.com/q/96710/101

---
+++ Java/Class/InstanceVariable.java
@@ -0,0 +1,6 @@
+public class Exemplo {
+    private String info1; //variável de instância que é um campo
+    private static String info2; //variável de classe que é um campo
+}
+
+//https://pt.stackoverflow.com/q/96710/101
