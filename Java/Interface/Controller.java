24a6b7c95f1baf6c56ddb5ebab23f0f99367df3c
Antonio Maniero <maniero@klip.net>
2021-06-02 14:09:34 -0300

    https://pt.stackoverflow.com/q/119616/101

---
+++ Java/Interface/Controller.java
@@ -0,0 +1,7 @@
+public E adicionar(E Element);
+
+public interface IControllerEspecial<E> extends IController<E> {
+    public E adicionar(E Element);
+}
+
+//https://pt.stackoverflow.com/q/119616/101
