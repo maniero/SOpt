b4b7e50e41fad9ab12b4cf5a7d0044ff05fe31af
Antonio Maniero <maniero@klip.net>
2021-06-04 11:04:55 -0300

    https://pt.stackoverflow.com/q/123492/101

---
+++ Java/Class/Model.java
@@ -0,0 +1,8 @@
+public class Cliente {
+    private String CNPJ;
+    private String CPF;
+    private String Comprador; //só exemplos
+    private DateTime UltimaVenda;
+}
+
+//https://pt.stackoverflow.com/q/123492/101
