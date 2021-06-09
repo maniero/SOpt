6006c8640ac035a775126e9013246de5bf3e5016
Antonio Maniero <maniero@klip.net>
2021-06-09 11:49:28 -0300

    https://pt.stackoverflow.com/q/513275/101

---
+++ CPP/String/Length.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+   string nome = "Teste";
+   cout << nome.length() << endl;
+}
+
+//https://pt.stackoverflow.com/q/513275/101
