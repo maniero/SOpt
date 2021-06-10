98453b770d20a5a82e0dccac4286112bc693b81b
Antonio Maniero <maniero@klip.net>
2021-06-10 12:24:57 -0300

    https://pt.stackoverflow.com/q/130298/101

---
+++ Java/OOP/Instance.java
@@ -0,0 +1,12 @@
+Aluno aluno = null;
+cadastrarAluno(aluno);
+
+Aluno aluno = new Aluno();
+cadastrarAluno(aluno);
+
+Aluno aluno = new Aluno(null, null, null, 0, 0);
+cadastrarAluno(aluno);
+
+public static Aluno cadastrarAluno()
+    
+//https://pt.stackoverflow.com/q/130298/101
