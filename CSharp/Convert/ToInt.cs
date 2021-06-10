7f0ca68e6e20b5ccd3c4ac4a0e55d2f85d1d69ee
Antonio Maniero <maniero@klip.net>
2021-06-10 10:38:24 -0300

    https://pt.stackoverflow.com/q/129681/101

---
+++ CSharp/Convert/ToInt.cs
@@ -0,0 +1,3 @@
+if (Convert.ToInt32(_TextValMin.Text) != Convert.ToInt32(_TextValPalite.Text) && Convert.ToInt32(_TextValMax.Text) != Convert.ToInt32(_TextValPalite.Text))
+
+//https://pt.stackoverflow.com/q/129681/101
