e6565dfee205db0eb113daa6ffcdbf4345a0d19b
Antonio Maniero <maniero@klip.net>
2021-06-10 09:54:55 -0300

    https://pt.stackoverflow.com/q/513673/101

---
+++ CPP/Algorithm/FroRangeIndex.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   string editor = "Luiz";
+   int i = 0;
+   for (auto l : editor) cout << "Letra: " << l << " Índice: " << i++ << endl;
+}
+
+//https://pt.stackoverflow.com/q/513673/101
