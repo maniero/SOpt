9212939dc7a174f76bf98d7fabc6c0af1250a8b9
Antonio Maniero <maniero@klip.net>
2021-06-14 15:45:19 -0300

    https://pt.stackoverflow.com/q/149348/101

---
+++ Java/Class/getterSetter.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+    private int idade;
+    public int getIdade() { return idade; }
+    public void setIdade(int value) { idade = value; }
+    private String nome;
+    public String getNome() { return nome; }
+    public void setNome(String value) { nome = value; }
+}
+
+//https://pt.stackoverflow.com/q/149348/101
