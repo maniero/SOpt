daafb7f3b91c77bf546429a9e73325c5887e5271
Antonio Maniero <maniero@klip.net>
2021-06-15 12:50:49 -0300

    https://pt.stackoverflow.com/q/141385/101

---
+++ Java/Array/Declaration.java
@@ -0,0 +1,5 @@
+Cliente[] clientes = new Cliente[50]; //para 50 clientes
+
+ArrayList<Cliente> clientes = new ArrayList<>(); //inicializando p/ evitar exceção
+
+//https://pt.stackoverflow.com/q/141385/101
