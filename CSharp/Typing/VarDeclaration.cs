241d070c54ca281956d1572cf7da7d0ddcda9ebb
Antonio Maniero <maniero@klip.net>
2021-06-15 12:47:57 -0300

    https://pt.stackoverflow.com/q/138134/101

---
+++ CSharp/Typing/VarDeclaration.cs
@@ -0,0 +1,5 @@
+int x; //declarando sem inicializar. Será inicializado implicitamente com 0
+var y = 1; //usando inferência de tipo. Só funciona em variáveis locais
+int z = 2; //definição (declaração+atribuição) explícita
+
+//https://pt.stackoverflow.com/q/138134/101
