cd2d9ec22e6363f2a24dacf5208c94aa3d2310b3
Antonio Maniero <maniero@klip.net>
2021-06-16 11:48:45 -0300

    https://pt.stackoverflow.com/q/152266/101

---
+++ Java/OOP/Inheritance.java
@@ -0,0 +1,6 @@
+public interface MeioTransporte { ... }
+public interface Veiculo extends MeioTransporte { ... } //subtipo
+public class Carro implements Veiculo { ... } //sutipo
+public class Peugeot extends Carro { ... } //subclasse e subtipo
+
+//https://pt.stackoverflow.com/q/152266/101
