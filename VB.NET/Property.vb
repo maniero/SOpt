87b9c29ce55f682ec40512e57f6908780c776dad
Antonio Maniero <maniero@klip.net>
2021-06-16 11:54:46 -0300

    https://pt.stackoverflow.com/q/153511/101

---
+++ VB.NET/Property.vb
@@ -0,0 +1,9 @@
+Public Property Propriedade As String
+
+Public Property Propriedade As String = "Default"
+
+Public ReadOnly Property Propriedade As String = "Default"
+
+Public WriteOnly Property Propriedade As String
+                
+'https://pt.stackoverflow.com/q/153511/101
