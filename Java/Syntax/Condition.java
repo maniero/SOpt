5693c991e9046e270b51d493550e9ddd91525c9a
Antonio Maniero <maniero@klip.net>
2021-06-22 17:26:46 -0300

    https://pt.stackoverflow.com/q/165494/101

---
+++ Java/Syntax/Condition.java
@@ -0,0 +1,22 @@
+int x = 1;
+int y = x + 5;
+
+int y = 1 + 5
+
+if (metodo() == true)
+
+x = x; //sim, está só atribuindo o valor de x em x, todo mundo vê a redundância.
+
+public boolean condicao(int x) {
+    if (x == 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+public boolean condicao(int x) {
+    return x == 10; //retorna exatamente o que precisava
+}
+
+//https://pt.stackoverflow.com/q/165494/101
