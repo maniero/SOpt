a207721fbcacc5675547cdc3e4ce128a39eb3666
Antonio Maniero <maniero@klip.net>
2021-06-23 08:20:51 -0300

    https://pt.stackoverflow.com/q/164431/101

---
+++ CSharp/Property/SimpleSyntax.cs
@@ -0,0 +1,7 @@
+public string email;
+public string Email { get { return email; } set { email = value; } }
+é o mesmo que isto:
+
+public string Email { get; set; }
+
+//https://pt.stackoverflow.com/q/164431/101
