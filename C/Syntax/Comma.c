6732cef8418edadec0258bb87639a040d9ed465a
Antonio Maniero <maniero@klip.net>
2021-06-24 08:19:51 -0300

    https://pt.stackoverflow.com/q/175647/101

---
+++ C/Syntax/Comma.c
@@ -0,0 +1,12 @@
+int x = 1, 2, 3, 4; //o valor de x será 4, o resto seria apenas processado, mas não usado 
+
+int x = ((1, 2), 3), 4; //neste exemplo específico os números anteriores foram ignorados
+
+
+for (int x = 1, y = 1; x < 10 && y < 20; x++, y *= 2)
+
+if ((x = calcula(1, 2)), (y = x - 10), y > z)
+
+return 1, x, abs(x + 5), x * 2, x > 5;
+
+//https://pt.stackoverflow.com/q/175647/101
