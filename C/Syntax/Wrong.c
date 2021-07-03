984e5dd84168d013d53c061880820d199e7d8ee6
Antonio Maniero <maniero@klip.net>
2021-07-03 20:22:12 -0300

    https://pt.stackoverflow.com/q/178341/101

---
+++ C/Syntax/Wrong.c
@@ -0,0 +1,9 @@
+while scanf("%s", buf) != EOF // Falta parênteses aqui.
+
+for (p = bin[h]; p != NULL; p = p->next) //p não está declarado em lugar algum.
+
+return // Daqui pra frente nada será executado.
+
+p = malloc(sizeof(hashnode))
+    
+//https://pt.stackoverflow.com/q/178341/101
