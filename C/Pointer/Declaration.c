93288c91efe61b5691e4686091e4171700e74a88
Antonio Maniero <maniero@klip.net>
2021-07-06 06:54:30 -0300

    https://pt.stackoverflow.com/q/179463/101

---
+++ C/Pointer/Declaration.c
@@ -0,0 +1,16 @@
+int* variavel;
+
+int *variavel;
+
+int* var1, var2;
+
+int* var1, *var2;
+
+int *var1, *var2;
+
+int* var1;
+int* var2;
+
+^int var1, var2;
+
+//https://pt.stackoverflow.com/q/179463/101
