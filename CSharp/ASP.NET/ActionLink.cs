a42e9e4aa146fc2864057cc270bfeaf9544ee4d2
Antonio Maniero <maniero@klip.net>
2021-07-07 11:47:59 -0300

    https://pt.stackoverflow.com/q/193296/101

---
+++ CSharp/ASP.NET/ActionLink.cs
@@ -0,0 +1,9 @@
+@Html.ActionLink("texto", "action", "controller", new { id = "123" }, null)
+
+<a href="/controller/action/123">texto</a>
+
+Url.Action("action", "controller", new { id = "123" })
+
+/controller/action/123
+
+//https://pt.stackoverflow.com/q/193296/101
