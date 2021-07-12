ba3718070b52a4a5843a26ac6f9ef36e554ecceb
Antonio Maniero <maniero@klip.net>
2021-07-12 06:47:32 -0300

    https://pt.stackoverflow.com/q/207902/101

---
+++ Java/Method/Argument.java
@@ -0,0 +1,5 @@
+private fazerAlgo(AlgumaCoisa algumaCoisa) {
+    Utilidades.converter(formatador.formatarParaTal(algumaCoisa.a, algumaCoisa.b, algumaCoisa.c))
+}
+
+//https://pt.stackoverflow.com/q/207902/101
