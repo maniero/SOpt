0d6ad3ad9aef903162202ba82709e1425d93035a
Antonio Maniero <maniero@klip.net>
2021-07-16 05:35:27 -0300

    https://pt.stackoverflow.com/q/517683/101

---
+++ Python/Collection/UpdateDict.py
@@ -0,0 +1,9 @@
+cidade = {
+    'nome': 'São Paulo',
+    'estado': 'São Paulo'
+}
+cidade['país'] = 'Brasil'
+cidade.update({'cep': '01000'})
+print(cidade)
+
+#https://pt.stackoverflow.com/q/517683/101
