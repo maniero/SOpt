37ef64fd7ba1b44948b0847cff16814ccddeffe4
Antonio Maniero <maniero@klip.net>
2021-07-20 08:12:00 -0300

    https://pt.stackoverflow.com/q/227375/101

---
+++ Kotlin/StaticMethod.kt
@@ -0,0 +1,11 @@
+companion object {
+    fun getDragonGlass() : String = String.valueOf("All the dragon glasses.")
+}
+
+Classe.getDragonGlass()
+
+fun getDragonGlass() : String = String.valueOf("All the dragon glasses.")
+
+Pacote.getDragonGlass()
+
+//https://pt.stackoverflow.com/q/227375/101
