335180a65e0cc44b1bcbfbaaab4529b9e2aeb9c8
Antonio Maniero <maniero@klip.net>
2021-07-23 07:22:36 -0300

    https://pt.stackoverflow.com/q/254959/101

---
+++ JavaScript/DOM/Events.js
@@ -0,0 +1,20 @@
+.onclick = myFunction()
+
+.onclick = function() {myFunction()};
+
+function nome() {}
+
+nome = function() {}
+
+.onclick = myFunction
+
+nome = function() {};
+.onclick = nome;
+
+function nome() {}
+.onclick = nome;
+
+function nome() {}
+.onclick = function() {nome()};
+
+//https://pt.stackoverflow.com/q/254959/101
