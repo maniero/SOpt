a73149ef5a817c0defb000c35d6cd6804be29f0d
Antonio Maniero <maniero@klip.net>
2021-07-27 07:03:03 -0300

    https://pt.stackoverflow.com/q/263975/101

---
+++ CSharp/IO/FileNameChars.cs
@@ -0,0 +1,4 @@
+string path = @"C:\inetpub\site\videofolder\nome.mp4";
+string path2 = @"C:\Program Files (x86)\Wowza Media Systems\Wowza Streaming Engine 4.7.3\content\videostreaming\nome.mp4";
+
+//https://pt.stackoverflow.com/q/263975/101
