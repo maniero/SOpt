b338a0d8b41bcb45e0e68c5231b5c612ccfd18de
Antonio Maniero <maniero@klip.net>
2021-07-28 09:58:29 -0300

    https://pt.stackoverflow.com/q/269571/101

---
+++ CSharp/WinForms/Label.cs
@@ -0,0 +1,8 @@
+for (int i = 0; i < numletras; i++) letra[i].Text = palavra[i];
+
+for (int i = 0; i < numletras; i++) {
+    var letra[i] = new Label(); //só a base
+    //aqui teria outras coisas
+}
+
+//https://pt.stackoverflow.com/q/269571/101
