9464a05ce6cb47dc5723671be69ad718e3914bd6
Antonio Maniero <maniero@klip.net>
2021-07-28 09:56:07 -0300

    https://pt.stackoverflow.com/q/266811/101

---
+++ JavaScript/Operators/Comparison.js
@@ -0,0 +1,14 @@
+if (!condicao1 || !condicao2 || !condicao3) {
+    //faz algo geral aqui
+    if (!condicao1) {
+        //Faça o que precisa aqui
+    }
+    if (!condicao2) {
+        //Faça o que precisa aqui
+    }
+    if (!condicao3) {
+        //Faça o que precisa aqui
+    }
+}
+
+//https://pt.stackoverflow.com/q/266811/101
