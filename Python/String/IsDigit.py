91359b242589709c43569d603d82a7f05f87003f
Antonio Maniero <maniero@klip.net>
2021-07-29 07:46:35 -0300

    https://pt.stackoverflow.com/q/272912/101

---
+++ Python/String/IsDigit.py
@@ -0,0 +1,10 @@
+try:
+    resultado = Decimal(numero)
+except ValueError:
+    print('Não foi possível converter')
+
+print("{:f}".format(Decimal(".00000001"))
+
+print(format(Decimal(".00000000001"), "f"))
+      
+#https://pt.stackoverflow.com/q/272912/101
