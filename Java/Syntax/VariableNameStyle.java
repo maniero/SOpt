4c05e78eb63867eb37ed5c0f48ce3b8e73e80f58
Antonio Maniero <maniero@klip.net>
2021-08-04 12:45:59 -0300

    https://pt.stackoverflow.com/q/299597/101

---
+++ Java/Syntax/VariableNameStyle.java
@@ -0,0 +1,10 @@
+isDatasExistemTambemIguais //Fica longo, mas aceitável
+isDatasExistemTbIguais  //Menos legível, mas nada crítico
+isDatasExistemE_Iguais //Foge do padrão normalmente usado, mas resolve
+isDatasExistemAndIguais //Esquisito, mas válido, há precedente
+isDatasIguaisSeExistirem  //Questão se interpretação
+isDatasExistentesMasIguais //Mais para ter opções
+
+TemDatasIguais
+
+//https://pt.stackoverflow.com/q/299597/101
