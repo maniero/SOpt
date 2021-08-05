772a1f675711a8b20a505601cb9337f89ad4eb4f
Antonio Maniero <maniero@klip.net>
2021-08-05 11:52:41 -0300

    https://pt.stackoverflow.com/q/306914/101

---
+++ CSharp/Property/Lowered.cs
@@ -0,0 +1,7 @@
+public class C {
+    public string estante { get; set; }
+    public string armazem { get; set; }
+    public string filial { get; set; }
+}
+
+//https://pt.stackoverflow.com/q/306914/101
