43046b5deeec6cadcc0a455dc81f9f6a90ecd3b8
Antonio Maniero <maniero@klip.net>
2021-08-16 08:59:36 -0300

    https://pt.stackoverflow.com/q/335504/101

---
+++ JavaScript/Object/NodeList.js
@@ -0,0 +1,8 @@
+var list = document.querySelectorAll('input[type=checkbox]');
+for (var checkbox of list) {
+    checkbox.checked = true;
+}
+
+document.querySelectorAll("p")[1] //pega o segundo elemento da lista de nós de p
+
+//https://pt.stackoverflow.com/q/335504/101
