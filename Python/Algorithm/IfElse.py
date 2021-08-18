303d5df385777ee07757f35a097f41d698163885
Antonio Maniero <maniero@klip.net>
2021-08-18 08:44:04 -0300

    https://pt.stackoverflow.com/q/355551/101

---
+++ Python/Algorithm/IfElse.py
@@ -0,0 +1,19 @@
+def entrada(lista):
+    while True:
+        ad = (int(input('Digite um valor: ')))
+        if ad not in lista:
+            lista.append(ad)
+            print('Adicionado com sucesso!')
+        else:
+            print('Valor duplicado. Adição negada.')
+        while True:
+            ask = str(input('Deseja continuar?[S/N] ')).strip().upper()[0]
+            if ask == 'S':
+                break
+            elif ask == 'N':
+                return
+lista = []
+entrada(lista)
+print(lista)
+
+#https://pt.stackoverflow.com/q/355551/101
