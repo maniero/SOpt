6e2dd9088d571f586849ff5a0696d3389c1e37d3
Antonio Maniero <maniero@klip.net>
2021-08-24 09:59:08 -0300

    https://pt.stackoverflow.com/q/391004/101

---
+++ Java/Array/InheritanceElement.java
@@ -0,0 +1,7 @@
+System.out.println("\nCPF:" + ((PessoaFisica)c).getCpf());
+
+System.out.println("\nCPF:" + ((PessoaJuridica)c).getCnpj());
+
+if (c instanceof PessoaFisica) {
+    
+//https://pt.stackoverflow.com/q/391004/101
