43341c956e8d3392975c83ff9e8480290566499a
Antonio Maniero <maniero@klip.net>
2021-08-24 09:49:04 -0300

    https://pt.stackoverflow.com/q/387468/101

---
+++ JavaScript/Syntax/IfElse.js
@@ -0,0 +1,13 @@
+if (status == true)
+
+return x + 0;
+
+if (status == true && valor > 10) {
+    return true;
+} else {
+    return false;
+}
+
+return status && valor > 10;
+
+//https://pt.stackoverflow.com/q/387468/101
