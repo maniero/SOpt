51ab35a32c3baee89fa1d5e6d68406f037fb5102
Antonio Maniero <maniero@klip.net>
2021-08-25 10:02:00 -0300

    https://pt.stackoverflow.com/q/392837/101

---
+++ C/Syntax/VariableAssign.c
@@ -0,0 +1,12 @@
+char c = 'x';
+
+char c;
+scanf("%c", c);
+
+char texto[] = "teste";
+
+FILE *fp = NULL;
+//mais código aqui
+fp = fopen("Emails.txt", "rt");
+
+//https://pt.stackoverflow.com/q/392837/101
