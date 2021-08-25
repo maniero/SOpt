f2c84ed31985ab1e8d4b3784af8aa15f922c8bc0
Antonio Maniero <maniero@klip.net>
2021-08-25 10:16:36 -0300

    https://pt.stackoverflow.com/q/399573/101

---
+++ CSharp/Collection/AsReadOnly.cs
@@ -0,0 +1,4 @@
+var list = new List<string> { "Maria", "Lucas", "Renata", "Yara", "Josias", "Morten Granau""Morten Granau", "Phaxe" };
+Metodo(list.AsReadOnly());
+
+//https://pt.stackoverflow.com/q/399573/101
