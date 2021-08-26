23ac27446249582cc51f3ab4c9ecf3eedd715016
Antonio Maniero <maniero@klip.net>
2021-08-26 09:05:04 -0300

    https://pt.stackoverflow.com/q/400419/101

---
+++ JavaScript/DOM/ChangeDiv.js
@@ -0,0 +1,7 @@
+function mudarDiv() {
+    var texto = document.getElementById('texto');
+    texto.innerHTML = 'teste';
+    texto.style.background = 'red';
+}
+
+//https://pt.stackoverflow.com/q/400419/101
