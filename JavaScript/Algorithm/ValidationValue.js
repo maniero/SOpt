647e551b635541a7c77e48ce81cb891dcb152163
Antonio Maniero <maniero@klip.net>
2021-08-27 09:35:08 -0300

    https://pt.stackoverflow.com/q/411633/101

---
+++ JavaScript/Algorithm/ValidationValue.js
@@ -0,0 +1,3 @@
+if (scoreboard_visiting_team > scoreboard_local_team - 4 && scoreboard_visiting_team <scoreboard_local_team + 4 && scoreboard_visiting_team != scoreboard_local_team)
+
+//https://pt.stackoverflow.com/q/411633/101
