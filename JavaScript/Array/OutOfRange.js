94ae8bd39c7f011c1945221cb63daf105819cfb2
Antonio Maniero <maniero@klip.net>
2021-08-28 12:11:40 -0300

    https://pt.stackoverflow.com/q/411417/101

---
+++ JavaScript/Array/OutOfRange.js
@@ -0,0 +1,17 @@
+letr = [2, 5, 6, 18, 20, 10, 23, 12, 19, 10];
+let s = [1, 5, 7, 13, 18, 21, 10, 25, 32, 17, 3];
+let x = [0];
+let i;
+console.log("i = " + i);
+for (i = 0; i <= 9; i++) {
+    x[i] = r[i];
+    console.log("i = " + i);
+}
+console.log("i = " + i);
+for (i = 0; i <= 10; i++) {
+    x[i + 10] = s[i];
+    console.log("i = " + i);
+}
+console.log("i = " + i);
+
+//https://pt.stackoverflow.com/q/411417/101
