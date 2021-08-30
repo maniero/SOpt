32a54fe3b7f3fda33042fa36d23decb7595f7cc5
Antonio Maniero <maniero@klip.net>
2021-08-30 11:25:43 -0300

    https://pt.stackoverflow.com/q/417246/101

---
+++ JavaScript/Syntax/ExceptionXError.js
@@ -0,0 +1,7 @@
+//...ações
+if (error) console.log('Falha temporária do servidor');
+
+//...ações
+if (error) console.log(new Error('Falha temporária do servidor'));
+
+//https://pt.stackoverflow.com/q/417246/101
