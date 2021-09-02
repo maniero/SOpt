c3ce05526ab5c59152f8cdd2d62e5618977c8280
Antonio Maniero <maniero@klip.net>
2021-09-02 09:25:13 -0300

    https://pt.stackoverflow.com/q/432976/101

---
+++ Python/Syntax/IfBlock.py
@@ -0,0 +1,24 @@
+if (n > 2) ...
+
+if (n > 2)
+    ....
+
+if (n > 2)
+    printf("%d", n);
+    n++
+
+if n > 2: print(n)
+print()
+Ou assim:
+
+if n > 2:
+    print(n)
+n += 1 #não é condicional
+print()
+
+if n > 2:
+    print()
+    n += 1 #condicional
+print()
+
+//https://pt.stackoverflow.com/q/432976/101
