af4ee57f8d9b6246af98ac9889ba2bc587311f1d
Antonio Maniero <maniero@klip.net>
2021-09-02 10:34:35 -0300

    https://pt.quora.com/Como-voc%C3%AA-resolveria-o-teste-de-programa%C3%A7%C3%A3o-FizzBuzz/answer/Antonio-Maniero

---
+++ CSharp/Algorithm/FizzBuzz.cs
@@ -0,0 +1,3 @@
+for (var i = 1; i < 100; i++) System.Console.WriteLine(i % 3  == 0 || i % 5  == 0 ? $"{(i % 3 == 0 ? "Fizz" : "")}{(i % 5 == 0 ? "Buzz" : "")}" : i);
+
+//https://pt.quora.com/Como-voc%C3%AA-resolveria-o-teste-de-programa%C3%A7%C3%A3o-FizzBuzz/answer/Antonio-Maniero
