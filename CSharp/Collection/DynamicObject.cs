e60fc0e6889a5b7a79b39a6365731bda434db154
Antonio Maniero <maniero@klip.net>
2021-09-03 09:16:49 -0300

    https://pt.stackoverflow.com/q/438680/101

---
+++ CSharp/Collection/DynamicObject.cs
@@ -0,0 +1,7 @@
+private Dictionary<string, Action>> voidFunctions = new Dictionary<string, Action>();
+
+private Dictionary<string, dynamic> functions = new Dictionary<string, dynamic>();
+
+private Dictionary<string, object>> functions = new Dictionary<string, object>();
+
+//https://pt.stackoverflow.com/q/438680/101
