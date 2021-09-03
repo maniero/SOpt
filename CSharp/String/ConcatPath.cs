e413bebedab67d005cb7f5dbb5fe0589ff7d80dc
Antonio Maniero <maniero@klip.net>
2021-09-03 09:24:11 -0300

    https://pt.stackoverflow.com/q/445467/101

---
+++ CSharp/String/ConcatPath.cs
@@ -0,0 +1,7 @@
+static void PrepararTerreno() => Directory.CreateDirectory($"C:/Users/{Environment.UserName}/Documents/Cadastro");
+
+static void PrepararTerreno() => Directory.CreateDirectory($@"C:\Users\{Environment.UserName}\Documents\Cadastro");
+
+static void PrepararTerreno() => Directory.CreateDirectory($"C:\\Users\\{Environment.UserName}\\Documents\\Cadastro");
+
+//https://pt.stackoverflow.com/q/445467/101
