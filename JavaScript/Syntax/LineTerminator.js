39c6cf66de213d719f0644eb966da460c169c6d3
Antonio Maniero <maniero@klip.net>
2021-09-08 09:11:05 -0300

    https://pt.stackoverflow.com/q/3341/101

---
+++ JavaScript/Syntax/LineTerminator.js
@@ -0,0 +1,28 @@
+if (x == 1)
+    console.log("ok");
+
+if (x == 1) console.log("ok");
+
+if (x == 1) {
+    console.log("ok");
+}
+
+const hey = 'ola'
+const you = 'ola'
+const heyYou = hey + ' ' + you
+['o', 'l', 'a'].forEach((letter) => console.log(letter))
+
+const hey = 'ola';
+const you = 'ola';
+const heyYou = hey + ' ' + you;
+['o', 'l', 'a'].forEach((letter) => console.log(letter));
+
+const x = 1
+const y = 1
+const z = x + y
+(x + y).toString()
+
+var x = 1 + 1
+-1 + 1 === 0 ? console.log("sim") : console.log("não")
+
+//https://pt.stackoverflow.com/q/3341/101
