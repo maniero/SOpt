b0154b2e634bea905d73c91ae1a6091f46dec277
Antonio Maniero <maniero@klip.net>
2021-09-08 09:31:37 -0300

    https://pt.stackoverflow.com/q/448777/101

---
+++ CSharp/Property/Instancing.cs
@@ -0,0 +1,7 @@
+public Especialidades especialidade { get; private set; }
+
+profissional.especialidade = new Especialidades();
+
+public Especialidades especialidade { get; private set; } = new Especialidades();
+
+//https://pt.stackoverflow.com/q/448777/101
