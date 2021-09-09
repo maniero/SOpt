7f56edbaf30a65fcc12a986e1edb8163044a46c7
Antonio Maniero <maniero@klip.net>
2021-09-09 11:57:11 -0300

    https://pt.stackoverflow.com/q/487615/101

---
+++ CPP/Pointer/ReferenceSyntax.cpp
@@ -0,0 +1,9 @@
+const pointer<MyClass> my_class = object.getMyClass();
+
+const reference<MyClass> my_class = object.getMyClass();
+
+const MyClass& my_class = object.getMyClass();
+
+const MyClass &my_class = object.getMyClass();
+
+//https://pt.stackoverflow.com/q/487615/101
