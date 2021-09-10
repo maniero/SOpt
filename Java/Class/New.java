9a00475707d2e8801c1e3a14af3d8ada57669da0
Antonio Maniero <maniero@klip.net>
2021-09-10 09:28:36 -0300

    https://pt.stackoverflow.com/q/486213/101

---
+++ Java/Class/New.java
@@ -0,0 +1,13 @@
+public class Main {
+    public Main() { System.out.println(""); }
+
+    public static void main(String[] args) { new Main(); }
+}
+
+public class Main {
+    public static void Main() { System.out.println(""); }
+
+    public static void main(String[] args) { Main(); }
+}
+
+//https://pt.stackoverflow.com/q/486213/101
