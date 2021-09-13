86a8492dbd3cff09469f1c5964c54ea2a01921fb
Antonio Maniero <maniero@klip.net>
2021-09-13 09:23:59 -0300

    https://pt.stackoverflow.com/q/480483/101

---
+++ CSharp/Typing/Default.cs
@@ -0,0 +1,16 @@
+object obj = null;
+
+int number = 0;
+
+var obj = default(T);
+
+T obj = default;
+int number = default;
+
+string str = null;
+
+string? str = default;
+
+string str = "";
+
+//https://pt.stackoverflow.com/q/480483/101
