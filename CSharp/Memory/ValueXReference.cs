fb2702856f7b3e0dcb7983cbcdaa794ac768bebe
Antonio Maniero <maniero@klip.net>
2021-09-13 09:06:38 -0300

    https://pt.stackoverflow.com/q/14490/101

---
+++ CSharp/Memory/ValueXReference.cs
@@ -0,0 +1,17 @@
+struct Textos {
+    private string Texto1;
+    private string Texto2;
+    ... aqui vão os construtores e métodos/propriedades de acesso e alteração dos membros, garantindo a imutabilidade ...
+}
+
+public class Carro {
+    public string Nome;
+    public int Status;
+    public bool EhNovo;
+    public DataTime DataDaVenda;
+    public Decimal ValorDaVenda;
+}
+
+ var notas = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+ 
+ //https://pt.stackoverflow.com/q/14490/101
