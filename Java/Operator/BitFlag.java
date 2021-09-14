1268523acfecac5773bfad17d08120128921d8da
Antonio Maniero <maniero@klip.net>
2021-09-14 09:00:13 -0300

    https://pt.stackoverflow.com/q/32421/101

---
+++ Java/Operator/BitFlag.java
@@ -0,0 +1,6 @@
+FontFactory.getFont(FontFactory.defaultEncoding, 10, bold + underline)
+
+int config = com.lowagie.text.Font.BOLD | com.lowagie.text.Font.UNDERLINE;
+FontFactory.getFont(FontFactory.defaultEncoding, 10, config);
+
+//https://pt.stackoverflow.com/q/32421/101
