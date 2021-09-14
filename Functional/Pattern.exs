474d2c047080070154c81372a1c68870c8556281
Antonio Maniero <maniero@klip.net>
2021-09-14 09:17:27 -0300

    https://pt.stackoverflow.com/q/448123/101

---
+++ Functional/Pattern.exs
@@ -0,0 +1,7 @@
+function collide(me::Circle, other::Rectangle)
+function collide(me::Polygon, other::Circle)
+function collide(me::Polygon, other::Rectangle)
+
+collide(objeto, outro)
+
+#https://pt.stackoverflow.com/q/448123/101
