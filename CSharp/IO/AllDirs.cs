bc4fb87941a44ba486e793ea6773987bcfd45e6c
Antonio Maniero <maniero@klip.net>
2021-09-14 08:55:14 -0300

    https://pt.stackoverflow.com/q/31642/101

---
+++ CSharp/IO/AllDirs.cs
@@ -0,0 +1,5 @@
+Directory.EnumerateFiles(fbd.SelectedPath, "*.*", SearchOption.AllDirectories))
+
+Directory.GetFiles(fbd.SelectedPath, "*.*", SearchOption.AllDirectories))
+
+//https://pt.stackoverflow.com/q/31642/101
