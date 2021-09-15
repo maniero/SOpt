7f39ba9947e7099d091b5c916a7b1e3df64d39bd
Antonio Maniero <maniero@klip.net>
2021-09-15 10:55:54 -0300

    https://pt.stackoverflow.com/q/40190/101

---
+++ CSharp/Other/NamingTest.cs
@@ -0,0 +1,5 @@
+[TestMethod]
+[Description("Given two tables And same data But different column order | When TableAssert | Then true")]
+public void ColumnOrderDoesntMatter()
+
+//https://pt.stackoverflow.com/q/40190/101
