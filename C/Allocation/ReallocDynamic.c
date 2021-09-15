f4a305c918fb2b2f0261c9ec8762e116731c9f3c
Antonio Maniero <maniero@klip.net>
2021-09-15 11:15:59 -0300

    https://pt.stackoverflow.com/q/40562/101

---
+++ C/Allocation/ReallocDynamic.c
@@ -0,0 +1,5 @@
+pLocatarios = realloc(pLocatarios, (sizeof(pLocatarios) / sizeof(pLocatarios[0]) + 1) * sizeof(pLocatarios[0]);
+
+pLocatarios = realloc(pLocatarios, (totalLocatarios + 1) * sizeof(Locatarios);
+                      
+//https://pt.stackoverflow.com/q/40562/101
