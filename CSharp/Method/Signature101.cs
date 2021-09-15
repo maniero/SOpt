bd97c46afe72d4ea8344baa77291a2144f131bc7
Antonio Maniero <maniero@klip.net>
2021-09-15 10:57:28 -0300

    https://pt.stackoverflow.com/q/39870/101

---
+++ CSharp/Method/Signature101.cs
@@ -0,0 +1,36 @@
+int FazAlgumaCoisa() { // faz alguma coisa aqui }
+
+int FazAlgumacoisa(int valor) { // faz alguma coisa aqui }
+
+int FazAlgumaCoisa() { FazAlgumaCoisa( 0 ); }
+
+int FazAlgumaCoisa(int valor1) { // faz alguma coisa aqui }
+
+int FazAlgumaCoisa(int valor2) { // faz alguma coisa aqui }
+
+int FazAlgumaCoisa() { // faz alguma coisa aqui }
+
+int FazAlgumaCoisa() { // faz alguma coisa aqui }
+
+class Exemplo1 {
+    int FazAlgumaCoisa() { // faz alguma coisa aqui }
+}
+class Exemplo2 {
+    int FazAlgumaCoisa() { // faz alguma coisa aqui }
+}
+
+class Exemplo1 {
+    int FazAlgumaCoisa(Exemplo1 this) { // faz alguma coisa aqui }
+}
+class Exemplo2 {
+    int FazAlgumaCoisa(Exemplo2 this) { // faz alguma coisa aqui }
+}
+
+class Exemplo1 {
+    int FazAlgumaCoisa(Exemplo1 this, int valor) { // faz alguma coisa aqui }
+}
+class Exemplo2 {
+    int FazAlgumaCoisa(Exemplo2 this, int valor) { // faz alguma coisa aqui }
+}
+
+//https://pt.stackoverflow.com/q/39870/101
