f1fcedbeca820533e15aba9b606f35b4a94ca451
Antonio Maniero <maniero@klip.net>
2021-09-16 09:45:46 -0300

    https://pt.stackoverflow.com/q/42597/101

---
+++ TypeScript/Validation.ts
@@ -0,0 +1,9 @@
+function facaAlgo(str : string) {
+  // ...
+}
+
+// em algum outro lugar:
+var i = 2; // tipo de i inferido como numérico
+facaAlgo(i); // erro de compilação!
+
+//https://pt.stackoverflow.com/q/42597/101
