a1165a4aa61c00fc302f0dfdae01eb2e06f184f8
Antonio Maniero <maniero@klip.net>
2021-09-16 09:43:01 -0300

    https://pt.stackoverflow.com/q/41166/101

---
+++ CSharp/Syntax/RawString.cs
@@ -0,0 +1,16 @@
+string a = "\\\\servidor01\\arquivos"
+
+string a = "hello, world";                // hello, world
+string b = @"hello, world";               // hello, world
+string c = "hello \t world";              // hello     world
+string d = @"hello \t world";             // hello \t world
+string e = "Joe said \"Hello\" to me";    // Joe said "Hello" to me
+string f = @"Joe said ""Hello"" to me";   // Joe said "Hello" to me
+string g = "\\\\server\\share\\file.txt"; // \\server\share\file.txt
+string h = @"\\server\share\file.txt";    // \\server\share\file.txt
+string i = "one\r\ntwo\r\nthree";
+string j = @"one
+two
+three";
+
+//https://pt.stackoverflow.com/q/41166/101
