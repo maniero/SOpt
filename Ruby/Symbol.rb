393a991fa645e3eda5924522abbd34cf04b411e3
Antonio Maniero <maniero@klip.net>
2021-09-17 00:30:44 -0300

    https://pt.stackoverflow.com/q/527833/101

---
+++ Ruby/Symbol.rb
@@ -0,0 +1,9 @@
+module Teste
+  def minha_funcao
+    puts 'função'
+  end
+end
+puts Teste.method_defined?(:minha_funcao)
+#puts Teste.method_defined?(minha_funcao)
+
+//https://pt.stackoverflow.com/q/527833/101
