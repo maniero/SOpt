2925c4126e6669fc3dab1d1379044f23c7941524
Antonio Maniero <maniero@klip.net>
2021-09-17 08:53:29 -0300

    //https://pt.stackoverflow.com/q/45386/101

---
+++ CSharp/Class/Internal.cs
@@ -0,0 +1,9 @@
+ENTDBCORPFornecedor ventCorpFornecedor = null;
+
+ENTDBCORPFornecedor ventCorpFornecedor;
+
+var ventCorpFornecedor = new ENTDBCORPFornecedor();
+
+[InternalsVisibleTo("OutroAssembly")]
+
+//https://pt.stackoverflow.com/q/45386/101
