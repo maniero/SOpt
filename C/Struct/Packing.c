3272abf5e3c981739a5bf70dc3408565841bc2c9
Antonio Maniero <maniero@klip.net>
2021-09-19 11:40:08 -0300

    https://pt.stackoverflow.com/q/50614/101

---
+++ C/Struct/Packing.c
@@ -0,0 +1,14 @@
+struct {
+    int a;
+    char b;
+    char c;
+    char d;
+    char e;
+    float f;
+}
+
+#pragma pack(1)
+
+__attribute__((packed))
+    
+//https://pt.stackoverflow.com/q/50614/101
