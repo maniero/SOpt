5359df6a3413ac4cb99669144adc6d3a3cc1bd5e
Antonio Maniero <maniero@klip.net>
2021-09-20 11:44:24 -0300

    https://pt.stackoverflow.com/q/56285/101

---
+++ CSharp/Conversion/ParseCulture.cs
@@ -0,0 +1,4 @@
+Decimal.TryParse(txtVencimento.Text, NumberStyles.Any,
+        System.Globalization.CultureInfo.InvariantCulture, out valorDecimal)
+        
+//https://pt.stackoverflow.com/q/56285/101
