87c2aa4dd9254d4fece06bad5d62cfc31c4d4210
Antonio Maniero <maniero@klip.net>
2021-09-21 09:42:29 -0300

    https://pt.stackoverflow.com/q/73771/101

--- SQL/Select/IsExist.sql
+++ SQL/Select/IsExist.sql
@@ -1,3 +1,9 @@
 SELECT 0 FROM tabela WHERE usuario = 'exemplo';
 
+SELECT COUNT(0) FROM tabela WHERE usuario = 'exemplo';
+
+SELECT COUNT(*) FROM tabela WHERE usuario = 'exemplo';
+
+SELECT (SELECT COUNT(*) FROM tabela WHERE usuario = 'exemplo') > 0;
+
 --https://pt.stackoverflow.com/q/73771/101
