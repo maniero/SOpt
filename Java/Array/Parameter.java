215d0b12ee618aea28ee2533b928614cde7f3d10
Antonio Maniero <maniero@klip.net>
2021-09-27 00:20:09 -0300

    https://pt.stackoverflow.com/q/109667/101

---
+++ Java/Array/Parameter.java
@@ -0,0 +1,7 @@
+private static void pesquisaRegiao(Regiao[] regioes, String regiao) {
+
+public static void Cidade(Cidade cid, Regiao[] regioes) {
+
+Cidade(cid, regioes)
+    
+//https://pt.stackoverflow.com/q/109667/101
