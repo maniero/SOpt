09f3de1046f1419c74e0e8b873f2a2a778069c27
Antonio Maniero <maniero@klip.net>
2021-09-27 00:12:25 -0300

    https://pt.stackoverflow.com/q/106955/101

---
+++ CSharp/Syntax/Fluent.cs
@@ -0,0 +1,8 @@
+MailMessage.builder()
+    .from("eu@dominio.com")
+    .to("voce@dominio.com.br")
+    .subject("oi")
+    .message("Oi pra vc")
+    .send();
+    
+//https://pt.stackoverflow.com/q/106955/101
