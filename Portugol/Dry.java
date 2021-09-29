f5e8107851a85c185531643f34f2ba9cbb8f08a6
Antonio Maniero <maniero@klip.net>
2021-09-29 00:56:30 -0300

    https://pt.stackoverflow.com/q/120931/101

---
+++ Portugol/Dry.java
@@ -0,0 +1,34 @@
+valorDoProdutoComprado = valorDoProdutoComprado + 1
+
+valorDoProdutoComprado++
+
+meuObjeto.propriedade1 = 0
+meuObjeto.propriedade2 = ""
+meuObjeto.propriedade3 = true
+
+with meuObjeto
+    .propriedade1 = 0
+    .propriedade2 = ""
+    .propriedade3 = true
+
+if valor == 0
+    fazALgumaCoisa()
+fazOutraCoisa()
+
+if valor == 0 {
+    fazALgumaCoisa();
+}
+fazOutraCoisa();
+
+UmaClasse obj = new UmaClasse();
+
+var obj = new UmaClasse()
+
+if ok == true
+
+z = x + y
+print z
+
+print x + y
+
+//https://pt.stackoverflow.com/q/120931/101
