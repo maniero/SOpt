50a31dacf55e20a17747af7b01ae0c2236a985a3
Antonio Maniero <maniero@klip.net>
2021-09-29 00:58:35 -0300

    https://pt.stackoverflow.com/q/120529/101

---
+++ PHP/Operator/Logical.php
@@ -0,0 +1,5 @@
+$planohabilitargestao != "200" || $planohabilitargestao != "10000) && $usuario == ""
+
+$planohabilitargestao != "1000" && $planohabilitargestao != "200" && $planohabilitargestao != "10000 && $usuario == ""
+
+//https://pt.stackoverflow.com/q/120529/101
