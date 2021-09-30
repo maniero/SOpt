3d33f7d72c583bc2b8c858709675280831aaac47
Antonio Maniero <maniero@klip.net>
2021-09-30 08:23:03 -0300

    https://pt.stackoverflow.com/q/137647/101

---
+++ C/ControlFlux/Case.c
@@ -0,0 +1,7 @@
+case 2:{
+    varrer();
+    print("\n"); // <===================================== faltou um f no nome da função
+    printf(" medias calculadas com sucesso! \n");
+}
+
+//https://pt.stackoverflow.com/q/137647/101
