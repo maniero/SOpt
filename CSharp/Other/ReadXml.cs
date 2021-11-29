4b654a1f03efaa7a0b504583676004cae81dde48
Antonio Maniero <maniero@klip.net>
2021-11-29 10:05:20 -0300

    https://pt.stackoverflow.com/q/187181/101

--- CSharp/Other/ReadXml.cs
+++ CSharp/Other/ReadXml.cs
@@ -1,3 +1,13 @@
-theDataSet.ReadXml(theReader, XmlReadMode.InferSchema);
+using System;
+using Newtonsoft.Json.Linq;
+
+public class Program {
+    public static void Main() {
+        var text = "{\"SomeResponse\":{\"FIrstAttribute\":8,\"SecondAttribute\":\"On\",\"ThirdAttribute\":{\"Id\":2,\"FirstName\":\"Okkie\",\"Name\":\"Bokkie\",\"Street\":\"\",\"StreetNumber\":null,\"PostCode\":\"\",\"City\":\"\",\"Country\":\"}}}";
+        var token = JToken.Parse(text);
+        var json = JObject.Parse((string) token);
+        Console.WriteLine(json);
+    }
+}
 
 //https://pt.stackoverflow.com/q/187181/101
