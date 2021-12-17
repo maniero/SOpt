45e056acd4f2ad736f6010ae98ce7880f373c655
Antonio Maniero <maniero@klip.net>
2021-12-17 07:56:27 -0300

    https://pt.stackoverflow.com/q/45468/101

--- JavaScript/HTML_CSS/PlayVisibility.js
+++ JavaScript/HTML_CSS/PlayVisibility.js
@@ -2,4 +2,4 @@
 2. <span style="visibility: hidden;">texto escondido</span> teste.
 3. <span style="opacity: 0;">texto escondido</span> teste.
 
-<! https://pt.stackoverflow.com/q/44678/101
+<!-- https://pt.stackoverflow.com/q/44678/101
