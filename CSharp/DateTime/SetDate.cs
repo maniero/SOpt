f7f6002bf21c4fe0525528cd0db187ac5e84e1e0
Antonio Maniero <maniero@klip.net>
2022-02-16 08:10:00 -0300

    https://pt.stackoverflow.com/q/542999/101

---
+++ CSharp/DateTime/SetDate.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Appointment {
+    public string Content { get; set; }
+    private bool validDate = false;
+    public DateTime Date { get; }
+    public void SetDate(string date) {
+        if (DateTime.TryParse(date, out var dateValue)) {
+            Date = dateValue;
+            validDate = true;
+        }
+    }
+}
+
+//https://pt.stackoverflow.com/q/542999/101
