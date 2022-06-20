1dd2fc90c3514f2f32e7e640c53a8e6c2fc291da
Antonio Maniero <maniero@jumbo.com.br>
2022-06-20 04:13:39 -0300

    https://pt.stackoverflow.com/q/556383/101

---
+++ Java/String/CharUpperLower.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.*;
+
+class Program {
+    public static void main(String[] args) {
+        System.out.printf("Informe uma string:\n");
+        String s = (new Scanner(System.in)).nextLine();
+        String maius = "";
+        String minus = "";
+        for (char c : s.toCharArray()) {
+            if (c >= 'a' && c <= 'z') maius += Character.toUpperCase(c);
+            else maius += c;
+            if (c >= 'A' && c <= 'Z') minus += Character.toLowerCase(c);
+            else minus += c;
+        }
+        System.out.printf("\nResultado:\n%s\n%s\n", maius, minus);
+    }
+}
+
+//https://pt.stackoverflow.com/q/556383/101
