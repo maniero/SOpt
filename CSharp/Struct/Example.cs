40481dab803db8da60e22de7a31b828056892498
Antonio Maniero <maniero@jumbo.com.br>
2022-07-28 15:50:12 -0300

    https://pt.stackoverflow.com/q/560136/101

---
+++ CSharp/Struct/Example.cs
@@ -0,0 +1,12 @@
+struct Endereco {
+    public string TipoLogradouro;
+    public string Logradouro;
+    public string Numero;
+    public string Complemento;
+    public string Bairro;
+    public string Cidade;
+    public string Uf;
+    public string Cep;
+}
+
+//https://pt.stackoverflow.com/q/560136/101
