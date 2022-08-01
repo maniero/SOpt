e32394761998f8484893c3a5b6f0105b0ff1bcec
Antonio Maniero <maniero@jumbo.com.br>
2022-08-01 06:33:58 -0300

    https://pt.stackoverflow.com/q/560287/101

---
+++ JavaScript/Math/Random.js
@@ -0,0 +1,29 @@
+var num = Math.random();
+console.log(Math.floor(num));
+console.log(num);
+console.log(1 * num);
+console.log(2 * num);
+console.log(3 * num);
+console.log(4 * num);
+console.log(5 * num);
+console.log(Math.floor(1 * num));
+console.log(Math.floor(2 * num));
+console.log(Math.floor(3 * num));
+console.log(Math.floor(4 * num));
+console.log(Math.floor(5 * num));
+
+var num = 0.8346254; //tem que ser entre 0 e o maior valor antes de 1
+console.log(Math.floor(num));
+console.log(num);
+console.log(1 * num);
+console.log(2 * num);
+console.log(3 * num);
+console.log(4 * num);
+console.log(5 * num);
+console.log(Math.floor(1 * num));
+console.log(Math.floor(2 * num));
+console.log(Math.floor(3 * num));
+console.log(Math.floor(4 * num));
+console.log(Math.floor(5 * num));
+
+//https://pt.stackoverflow.com/q/560287/101
