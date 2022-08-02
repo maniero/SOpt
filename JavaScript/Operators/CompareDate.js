50958474efe9b4ad2bcafa49ddbe0357a676d710
Antonio Maniero <maniero@jumbo.com.br>
2022-08-02 07:05:15 -0300

    https://pt.stackoverflow.com/q/560584/101

---
+++ JavaScript/Operators/CompareDate.js
@@ -0,0 +1,10 @@
+var d1 = new Date(0);
+var d2 = new Date(0);
+console.log(d1 == d2);
+console.log(d1 === d2);
+console.log(d1 != d2);
+console.log(d1 !== d2);
+console.log(new Date(1) > new Date(0));
+console.log(d1.getTime() === d2.getTime());
+
+//https://pt.stackoverflow.com/q/560584/101
