256285e1f00a3f139de9b8682cc39f422b458be5
Antonio Maniero <maniero@jumbo.com.br>
2022-08-24 02:00:32 -0300

    https://pt.stackoverflow.com/q/423756/101

--- JavaScript/Algorithm/StepTwo.js
+++ JavaScript/Algorithm/StepTwo.js
@@ -1,5 +1,5 @@
 function passandoPelosPares() {
-    for (var i = 0; i <= 6; i+= 2) console.log('aqui eu tenho o valor de ' + i);
+    for (var i = 0; i <= 6; i += 2) console.log('aqui eu tenho o valor de ' + i);
 }
 passandoPelosPares();
 
