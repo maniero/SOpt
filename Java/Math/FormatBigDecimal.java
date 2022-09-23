a316ce3c01bff7679fb9f0655962aa8cb8a6d012
Antonio Maniero <maniero@jumbo.com.br>
2022-09-23 08:29:02 -0300

    https://pt.stackoverflow.com/q/30701/101

---
+++ Java/Math/FormatBigDecimal.java
@@ -0,0 +1,11 @@
+import java.math.*;
+
+class Main {
+	public static void main (String[] args)	{
+		BigDecimal valor = new BigDecimal("10.00");
+		valor.setScale(2);
+		System.out.println(valor);
+	}
+}
+
+//https://pt.stackoverflow.com/q/30701/101
