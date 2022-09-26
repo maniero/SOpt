2574cff29bf5109b99065325eac11627145d56a3
Antonio Maniero <maniero@jumbo.com.br>
2022-09-26 08:26:50 -0300

    https://pt.stackoverflow.com/q/128460/101

--- C/Algorithm/EvenOdd3.c
+++ C/Algorithm/EvenOdd3.c
@@ -3,9 +3,9 @@
 int main(void) {
     int num1, num2;
     printf("\nInforme os números: ");
-    scanf ("%i""%i", &num1, &num2);
-    printf ("\nO número %d é %s e é o número é o %s", num1, (num1 % 2 == 0 ? "par" : "impar"), (num1 > num2 ? "maior" : "menor"));
-    printf ("\nO número %d é %s e é o número é o %s", num2, (num2 % 2 == 0 ? "par" : "impar"), (num2 > num1 ? "maior" : "menor"));
+    scanf("%i""%i", &num1, &num2);
+    printf("\nO número %d é %s e é o número é o %s", num1, (num1 % 2 == 0 ? "par" : "impar"), (num1 > num2 ? "maior" : "menor"));
+    printf("\nO número %d é %s e é o número é o %s", num2, (num2 % 2 == 0 ? "par" : "impar"), (num2 > num1 ? "maior" : "menor"));
 }
 
 //https://pt.stackoverflow.com/q/128460/101
