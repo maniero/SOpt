bcb5801180e9cb9c30ecba008a79f5bbc0b84c01
Antonio Maniero <maniero@jumbo.com.br>
2022-09-26 08:25:30 -0300

    https://pt.stackoverflow.com/q/565371/101

--- JavaScript/Algorithm/SumSequenceFilter.js
+++ JavaScript/Algorithm/SumSequenceFilter.js
@@ -4,9 +4,10 @@ function sequence(limite, incremento) {
 }
 
 function solution(number) {
-    return sequence(number - 1, 3) + sequence(number - 1, 5);
+    return sequence(number - 1, 3) + sequence(number - 1, 5) -  sequence(number - 1, 15);
 }
 
 console.log(solution(10));
+console.log(solution(20));
 
 //https://pt.stackoverflow.com/q/565371/101
