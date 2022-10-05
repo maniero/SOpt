49a35562bde49e7e038d193132630bcf461d7f5c
Antonio Maniero <maniero@jumbo.com.br>
2022-10-05 10:58:28 -0300

    https://pt.stackoverflow.com/q/34060/101

--- C/Allocation/SimpleArray.c
+++ C/Allocation/SimpleArray.c
@@ -2,10 +2,11 @@
 
 int main(void) {
     int tam;
-    printf("\ndigite tam: ");
+    printf("Digite o tamanho: ");
     scanf("%d", &tam);
     int vet[tam];
-    printf("%d", vet[0]);
+    for (int i = 1; i <= tam; i++) vet[i] = i;
+    for (int i = 1; i <= tam; i++) printf("%d ", vet[i]);
 }
 
 //https://pt.stackoverflow.com/q/34060/101
