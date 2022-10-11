6509b5cfff0daf2c72b97318bbe7beed773705f2
Antonio Maniero <maniero@jumbo.com.br>
2022-10-11 13:18:25 -0300

    https://pt.stackoverflow.com/q/257905/101

---
+++ Python/Collection/MaxList.py
@@ -0,0 +1,9 @@
+lista = ['4', '07', '08', '2017', '364', '355673087875675']
+max = float('-inf')
+for item in lista:
+    item = int(item)
+    if item > max:
+        max = item
+print(max)
+
+#https://pt.stackoverflow.com/q/257905/101
