c98b2c5471c56c07da7c4ec2028e324a50d380b9
Antonio Maniero <maniero@jumbo.com.br>
2022-10-14 00:30:00 -0300

    https://pt.stackoverflow.com/q/122076/101

--- JavaScript/String/ReplaceFor.js
+++ JavaScript/String/ReplaceFor.js
@@ -1,6 +1,6 @@
 var texto = "Casa bonita";
 var novoTexto = "";
 for (var i = 0; i < texto.length; i++) novoTexto += (texto[i] === " " ? "\n" : texto[i]);
-console.log(novoTexto);
+alert(novoTexto);
 
 //https://pt.stackoverflow.com/q/122076/101
