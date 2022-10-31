3fbc2853e96cd66f7c565c8550a15d168c03e084
Antonio Maniero <maniero@jumbo.com.br>
2022-10-31 08:25:24 -0300

    https://pt.stackoverflow.com/q/568875/101

---
+++ CSharp/Algorithm/Quadrant2.cs
@@ -0,0 +1,14 @@
+using static System.Console;
+
+while (true) {
+    if (!int.TryParse(ReadLine(), out var x)) return;
+    if (x == 0) break;
+    if (!int.TryParse(ReadLine(), out var y)) return;
+    if (y == 0) break;
+    if (x > 0 && y > 0) WriteLine("primeiro");
+    else if (x < 0 && y > 0) WriteLine("segundo");
+    else if (x < 0 && y < 0) WriteLine("terceiro");
+    else WriteLine("quarto");
+}                   
+
+//https://pt.stackoverflow.com/q/568875/101
