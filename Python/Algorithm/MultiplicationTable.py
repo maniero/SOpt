970660d6db996b9c4e1132350b913ea1e8071023
Antonio Maniero <maniero@jumbo.com.br>
2022-11-09 00:02:00 -0300

    https://pt.quora.com/T%C3%B3pico-programa%C3%A7%C3%A3o-com-Python-fazer-um-programa-que-apresenta-no-monitor-a-tabuada-do-1-ao-10-com-o-m%C3%ADnimo-de-linhas-de-c%C3%B3digo-poss%C3%ADvel-Podem-ajudar-me/answer/Antonio-Maniero

---
+++ Python/Algorithm/MultiplicationTable.py
@@ -0,0 +1,5 @@
+for x in range(1, 11):
+	for y in range(1, 11):
+		print("%d x %d = %d" % (x, y, x * y))
+
+#https://pt.quora.com/T%C3%B3pico-programa%C3%A7%C3%A3o-com-Python-fazer-um-programa-que-apresenta-no-monitor-a-tabuada-do-1-ao-10-com-o-m%C3%ADnimo-de-linhas-de-c%C3%B3digo-poss%C3%ADvel-Podem-ajudar-me/answer/Antonio-Maniero
