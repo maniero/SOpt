890ace2509c2bae75ee1f4a45fb07e0b69946848
Antonio Maniero <maniero@jumbo.com.br>
2022-11-11 08:51:31 -0300

    https://pt.stackoverflow.com/q/75498/101

---
+++ CSharp/String/FormatCpf.cs
@@ -0,0 +1,11 @@
+using static System.Console;
+
+public class Test {
+	public static void Main()
+		WriteLine(FormatarCpf("12345678901"));
+		WriteLine(FormatarCpf("00123456789"));
+		string FormatarCpf(string cpf) => cpf.Substring(0, 3) + "." + cpf.Substring(3, 3) + "." + cpf.Substring(6, 3) + "-" + cpf.Substring(9, 2);
+	}
+}
+
+//https://pt.stackoverflow.com/q/75498/101
