1e9973c5ed2b4e6825b026b132a871a5d0d0c0c9
Antonio Maniero <maniero@jumbo.com.br>
2022-11-21 10:43:10 -0300

    https://pt.stackoverflow.com/q/127177/101

--- C/String/RemoveSpaces.c
+++ C/String/RemoveSpaces.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
 	char string[101];
+	int posicao = 0;
 	printf("Forneca uma string com espacos em branco: ");
-	fgets(string, sizeof(string), stdin);
-	int len = strlen(string);
+	fgets(string, sizeof(string), stdin);//mudei aqui para modernizar. ideone ñ aceita gets
+	int len = strlen(string); //só pra dar melhor performance
 	printf("%s\n", string);
-	for (int i = 0, posicao = 0; i < len; i++, posicao++) {
-	    if (string[posicao] == ' ') posicao++;
-        string[i] = string[posicao];
+	for (int i = 0; i < len; i++) {
+		if (string[i] == ' ') continue;
+		string[posicao] = string[i];
+		posicao++;
 	}
+	string[posicao] = '\0'; // coloca o null no fim pra finalizar a string
 	printf("String sem espaços em branco: %s\n", string);
 }
 
