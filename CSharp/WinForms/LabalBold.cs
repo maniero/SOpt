623b7a3c76d450a47c3e383937557cd3efac8bd8
Antonio Maniero <maniero@jumbo.com.br>
2022-12-02 07:20:11 -0300

    https://pt.stackoverflow.com/q/5536/101

--- CSharp/WinForms/LabalBold.cs
+++ CSharp/WinForms/LabalBold.cs
@@ -1,14 +1,14 @@
 class BoldLabel : Label {
     protected override void OnPaint(PaintEventArgs e) {
-        Point drawPoint = new Point(0, 0);
+        Point drawPoint = new(0, 0);
         string[] array = Text.Split(new char[] { '|' });
         if (array.Length == 2) {
             Font normalFont = this.Font;
-            Font boldFont = new Font(normalFont, FontStyle.Bold);
+            Font boldFont = new(normalFont, FontStyle.Bold);
             Size boldSize = TextRenderer.MeasureText(array[0], boldFont);
             Size normalSize = TextRenderer.MeasureText(array[1], normalFont);
-            Rectangle boldRect = new Rectangle(drawPoint, boldSize);
-            Rectangle normalRect = new Rectangle(boldRect.Right, boldRect.Top, normalSize.Width, normalSize.Height);
+            Rectangle boldRect = new(drawPoint, boldSize);
+            Rectangle normalRect = new(boldRect.Right, boldRect.Top, normalSize.Width, normalSize.Height);
             TextRenderer.DrawText(e.Graphics, array[0], boldFont, boldRect, ForeColor);
             TextRenderer.DrawText(e.Graphics, array[1], normalFont, normalRect, ForeColor);
         } else {
