ef559071a6bab8acebb07fdc6d7bb9264c1fa146
Antonio Maniero <maniero@jumbo.com.br>
2022-12-07 00:01:40 -0300

    https://pt.stackoverflow.com/q/63881/101

--- CSharp/Decimal/GetDecimalPart.cs
+++ CSharp/Decimal/GetDecimalPart.cs
@@ -5,13 +5,9 @@ WriteLine(GetDecimalPart(45.545M));
 WriteLine(GetDecimalPart(45.54M));
 WriteLine(GetDecimalPart(0.545M));
 WriteLine(GetDecimalPart(-45.545M));
+WriteLine(GetDecimalPart(45.5450M));
+WriteLine(GetDecimalPart(999988776655.443322116M));
 
-static int GetDecimalPart(decimal value) {
-	value = Abs(value) - Floor(Abs(value));
-	do {
-		value *= 10;
-	} while (value != Floor(value));
-	return (int)value;
-}
+static int GetDecimalPart(decimal value) => (int)((Abs(value) - Floor(Abs(value))) * (decimal)System.Math.Pow(10, value.Scale));
 
 //https://pt.stackoverflow.com/q/63881/101
