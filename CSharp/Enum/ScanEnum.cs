e6107d8a0047b04f09092675af8abab37552816f
Antonio Maniero <maniero@jumbo.com.br>
2022-12-08 01:04:16 -0300

    https://pt.stackoverflow.com/q/72986/101

--- CSharp/Enum/ScanEnum.cs
+++ CSharp/Enum/ScanEnum.cs
@@ -1,41 +1,12 @@
 using System;
-					
-public class Program {
-	public static void Main() {
-		var soma = 0;
-		foreach (var elemento in Enum.GetValues(typeof(TrianguloLetra))) {
-			soma += (int)elemento;
-		}
-		Console.WriteLine(soma);
-	}
-	public enum TrianguloLetra {
-            a = 1,
-            b = 2,
-            c = 3,
-            d = 4,
-            e = 5,
-            f = 6,
-            g = 7,
-            h = 8,
-            i = 9,
-            j = 10,
-            k = 11,
-            l = 12,
-            m = 13,
-            n = 14,
-            o = 15,
-            p = 16,
-            q = 17,
-            r = 18,
-            s = 19,
-            t = 20,
-            u = 21,
-            v = 22, 
-            w = 23,
-            x = 24,
-            y = 25,
-            z = 26
-    }
+using static System.Console;
+
+var soma = 0;
+foreach (var elemento in Enum.GetValues(typeof(TrianguloLetra))) soma += (int)elemento;
+WriteLine(soma);
+
+public enum TrianguloLetra {
+	a = 1, b, c, d,  e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
 }
 
-https://pt.stackoverflow.com/q/72986/101
+//https://pt.stackoverflow.com/q/72986/101
