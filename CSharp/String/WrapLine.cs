e04f94df52142701b9a0415516cd81a733f2e7fc
Antonio Maniero <maniero@jumbo.com.br>
2022-12-21 03:32:40 -0300

    https://pt.stackoverflow.com/q/228864/101

---
+++ CSharp/String/WrapLine.cs
@@ -0,0 +1,23 @@
+using static System.Console;
+using static System.Convert;
+using System.Text;
+					
+WriteLine(WrapString("0123456789012345678901234567890123456789", 10));
+WriteLine(WrapString("01234567890123456", 10));
+WriteLine(WrapString("012345", 10));
+
+static string WrapString(string text, int len) {
+	StringBuilder sb = new(text.Length + text.Length / len * 2);
+	var i = 0;
+	for (; i < text.Length / 10; i++) {
+		sb.Append(text[(i * 10)..(i * 10 + 10)]);
+		sb.Append("\r\n");
+	}
+	if (text.Length / 10 != ToSingle(text.Length) / 10) {
+		sb.Append(text[(i * 10)..]);
+		sb.Append("\r\n");
+	}
+	return sb.ToString();
+}
+
+//https://pt.stackoverflow.com/q/228864/101
