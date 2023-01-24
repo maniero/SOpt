46ce1dc30cd7e2ac1bf11d71d9d2894ff33c6628
Antonio Maniero <maniero@jumbo.com.br>
2023-01-24 00:17:56 -0300

    https://pt.stackoverflow.com/q/575585/101

---
+++ CSharp/Syntax/GlobalUsing.cs
@@ -0,0 +1,22 @@
+Write("Hello World");
+
+System
+System.Collections.Generic
+System.IO
+System.Linq
+System.Net.Http
+System.Threading
+System.Threading.Tasks
+Para web ainda tem:
+
+System.Net.Http.Json
+Microsoft.AspNetCore.Builder
+Microsoft.AspNetCore.Hosting
+Microsoft.AspNetCore.Http
+Microsoft.AspNetCore.Routing
+Microsoft.Extensions.Configuration
+Microsoft.Extensions.DependencyInjection
+Microsoft.Extensions.Hosting
+Microsoft.Extensions.Logging
+
+//https://pt.stackoverflow.com/q/575585/101
