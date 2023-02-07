6ea50e870243928463562023a1a2cdb61e43677a
Antonio Maniero <maniero@jumbo.com.br>
2023-02-07 10:28:59 -0300

    https://pt.stackoverflow.com/q/576610/101

---
+++ CSharp/Syntax/TargetType.cs
@@ -0,0 +1,5 @@
+List<Carro> list = new() { new(), new() };
+List<Carro> list = new List<Carro>() { new Carro(), new Carro() };
+var s = GetListCarro();
+
+//https://pt.stackoverflow.com/q/576610/101
