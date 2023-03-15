0695f93ec755fb389ae1822f59426901fe2cf2f1
Antonio Maniero <maniero@jumbo.com.br>
2023-03-15 06:21:37 -0300

    Rounding.java

---
+++ Java/Math/Rounding.java
@@ -0,0 +1,18 @@
+class Program {
+	public static void main (String[] args) {
+		System.out.println(Math.round(0.2f));
+		System.out.println(Math.round(0.5f));
+		System.out.println(Math.round(0.8f));
+		System.out.println(Math.round(1.2f));
+		System.out.println(Math.round(1.5f));
+		System.out.println(Math.round(1.8f));
+		System.out.println(Math.round(-0.2f));
+		System.out.println(Math.round(-0.5f));
+		System.out.println(Math.round(-0.8f));
+		System.out.println(Math.round(-1.2f));
+		System.out.println(Math.round(-1.5f));
+		System.out.println(Math.round(-1.8f));
+	}
+}
+
+//https://pt.stackoverflow.com/q/579752/101
