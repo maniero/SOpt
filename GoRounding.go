d1f3509b1df7b179492af024c35098bda3d03ee0
Antonio Maniero <maniero@jumbo.com.br>
2023-03-15 06:39:55 -0300

    https://pt.stackoverflow.com/q/579752/101

---
+++ GoRounding.go
@@ -0,0 +1,15 @@
+<?php
+echo round(0.2) . "\n";
+echo round(0.5) . "\n";
+echo round(0.8) . "\n";
+echo round(1.2) . "\n";
+echo round(1.5) . "\n";
+echo round(1.8) . "\n";
+echo round(-0.2) . "\n";
+echo round(-0.5) . "\n";
+echo round(-0.8) . "\n";
+echo round(-1.2) . "\n";
+echo round(-1.5) . "\n";
+echo round(-1.8) . "\n";
+
+//https://pt.stackoverflow.com/q/579752/101
