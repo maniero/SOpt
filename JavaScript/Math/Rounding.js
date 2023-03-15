3fc4958ce65f27390214acee088c62e1b19d929d
Antonio Maniero <maniero@jumbo.com.br>
2023-03-15 06:27:27 -0300

    https://pt.stackoverflow.com/q/579752/101

---
+++ JavaScript/Math/Rounding.js
@@ -0,0 +1,14 @@
+console.log(Math.round(0.2));
+console.log(Math.round(0.5));
+console.log(Math.round(0.8));
+console.log(Math.round(1.2));
+console.log(Math.round(1.5));
+console.log(Math.round(1.8));
+console.log(Math.round(-0.2));
+console.log(Math.round(-0.5));
+console.log(Math.round(-0.8));
+console.log(Math.round(-1.2));
+console.log(Math.round(-1.5));
+console.log(Math.round(-1.8));
+
+//https://pt.stackoverflow.com/q/579752/101
