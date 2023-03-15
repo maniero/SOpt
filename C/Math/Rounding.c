f312d65f7e82abbf85c5d7f96ecf0c19afe4360b
Antonio Maniero <maniero@jumbo.com.br>
2023-03-15 06:43:47 -0300

    https://pt.stackoverflow.com/q/579752/101

---
+++ C/Math/Rounding.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	printf("%.0f\n", round(0.2));
+	printf("%.0f\n", round(0.5));
+	printf("%.0f\n", round(0.8));
+	printf("%.0f\n", round(1.2));
+	printf("%.0f\n", round(1.5));
+	printf("%.0f\n", round(1.8));
+	printf("%.0f\n", round(-0.2));
+	printf("%.0f\n", round(-0.5));
+	printf("%.0f\n", round(-0.8));
+	printf("%.0f\n", round(-1.2));
+	printf("%.0f\n", round(-1.5));
+	printf("%.0f\n", round(-1.8));
+}
+
+//https://pt.stackoverflow.com/q/579752/101
