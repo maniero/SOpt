c2146b696b0f4e61c9a7551b8d7ab1c812e2e6b4
Antonio Maniero <maniero@jumbo.com.br>
2023-03-15 00:52:14 -0300

    https://pt.stackoverflow.com/q/579752/101

---
+++ CSharp/Math/Rounding.cs
@@ -0,0 +1,17 @@
+using static System.Console;
+using static System.Math;
+
+WriteLine(Round(0.2));
+WriteLine(Round(0.5));
+WriteLine(Round(0.8));
+WriteLine(Round(1.2));
+WriteLine(Round(1.5));
+WriteLine(Round(1.8));
+WriteLine(Round(-0.2));
+WriteLine(Round(-0.5));
+WriteLine(Round(-0.8));
+WriteLine(Round(-1.2));
+WriteLine(Round(-1.5));
+WriteLine(Round(-1.8));
+
+//https://pt.stackoverflow.com/q/579752/101
