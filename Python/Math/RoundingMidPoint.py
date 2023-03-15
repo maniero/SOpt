7b827167a3c31e91cf3184e0b446b77098c8aef2
Antonio Maniero <maniero@jumbo.com.br>
2023-03-15 06:08:58 -0300

    https://pt.stackoverflow.com/q/579752/101

---
+++ Python/Math/RoundingMidPoint.py
@@ -0,0 +1,14 @@
+print(round(0.2))
+print(round(0.5))
+print(round(0.8))
+print(round(1.2))
+print(round(1.5))
+print(round(1.8))
+print(round(-0.2))
+print(round(-0.5))
+print(round(-0.8))
+print(round(-1.2))
+print(round(-1.5))
+print(round(-1.8))
+
+#https://pt.stackoverflow.com/q/579752/101
