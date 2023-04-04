e691a3d20d6448acc942bfddb3c733e0c01c4fc0
Antonio Maniero <maniero@jumbo.com.br>
2023-04-04 00:25:32 -0300

    https://pt.stackoverflow.com/q/580898/101

---
+++ CSharp/DateTime/LastDayMonth.cs
@@ -0,0 +1,7 @@
+using static System.DateTime;
+using static System.Console;
+WriteLine(DaysInMonth(2023, 04));
+WriteLine(DaysInMonth(2020, 02));
+WriteLine(DaysInMonth(2023, 02));
+
+//https://pt.stackoverflow.com/q/580898/101
