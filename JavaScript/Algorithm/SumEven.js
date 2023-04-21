b13f6002b51a00746eb8f5d491050d3bd945e3e4
Antonio Maniero <maniero@jumbo.com.br>
2023-04-21 00:31:57 -0300

    https://pt.stackoverflow.com/q/581695/101

---
+++ JavaScript/Algorithm/SumEven.js
@@ -0,0 +1,6 @@
+const numbers = [2, 4, 5, 7, 10, 11, 12];
+let sum = 0;
+for (let i = 0; i <= numbers.length; i++) if (numbers[i] % 2 == 0) sum += numbers[i];
+console.log("Sum:", sum);
+
+//https://pt.stackoverflow.com/q/581695/101
