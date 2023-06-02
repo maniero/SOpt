359f9113611e113a015484770b1e1d6a335387a8
Antonio Maniero <maniero@jumbo.com.br>
2023-06-02 08:59:49 -0300

    https://www.tabnews.com.br/maniero/2d80a58b-bd6e-4c11-8dbd-54e72734b818

---
+++ JavaScript/String/PseudoiCrypt.js
@@ -0,0 +1,6 @@
+let texto = "GostoDeVoce";
+let cripto = "";
+for (let i = 0; i < texto.length; i++) cripto += String.fromCharCode(texto[i].charCodeAt() + 1);
+console.log(cripto);
+
+//https://www.tabnews.com.br/maniero/2d80a58b-bd6e-4c11-8dbd-54e72734b818
